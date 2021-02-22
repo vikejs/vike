b4a78b64b5a3a2f093b743f16cd25d90e8d4ac3b
Romuald Brillout <git@brillout.com>
2021-02-22 19:21:12 +0100

    support CJS

--- src/node.ts
+++ src/node.ts
@@ -2,3 +2,6 @@ import './user-files/infra.node'
 export { createRender } from './createRender.node'
 export { plugin as default } from './plugin.node'
 export { html } from './html.node'
+
+// Enable `const ssr = require('vite-plugin-ssr')`
+module.exports = Object.assign(exports.default, exports)
