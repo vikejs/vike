1f37ff64dd015e4688c531543cc4ab9d16de6bdf
Romuald Brillout <git@brillout.com>
2021-03-01 17:29:46 +0100

    workflow: impl release script

---
+++ scripts/_locations.ts
@@ -0,0 +1,3 @@
+export const DIR_BOILERPLATES = `${__dirname}/../create-vite-plugin-ssr/`
+export const DIR_EXAMPLES = `${__dirname}/../examples/`
+export const DIR_SRC = `${__dirname}/../src/`
---
+++ scripts/release.ts
@@ -0,0 +1,69 @@
+import { readdirSync, writeFileSync, lstatSync } from 'fs'
+import { join as pathJoin } from 'path'
+import * as assert from 'assert'
+import { DIR_BOILERPLATES, DIR_EXAMPLES, DIR_SRC } from './_locations'
+
+type PackageJson = {
+  version: string
+  dependencies: {
+    ['vite-plugin-ssr']: string
+  }
+}
+
+release()
+
+function release() {
+  const { versionCurrent, versionNew } = getVersion()
+  updateVersion(versionNew)
+  updateDependencies({ versionNew, versionCurrent })
+}
+
+function getVersion(): { versionNew: string; versionCurrent: string } {
+  const pkg = require(`${DIR_SRC}/package.json`) as PackageJson
+  const versionCurrent = pkg.version
+  assert(versionCurrent)
+  const versionNew = getVersionNew() || versionCurrent
+  return { versionNew, versionCurrent }
+}
+function updateVersion(versionNew: string) {
+  update([`${DIR_SRC}/package.json`], (pkg) => {
+    pkg.version = versionNew
+  })
+}
+function getVersionNew(): string | undefined {
+  const versionNew = process.argv[2]
+  return versionNew
+}
+
+function updateDependencies({ versionNew, versionCurrent }) {
+  const pkgPaths = [
+    ...retrievePkgPaths(DIR_BOILERPLATES),
+    ...retrievePkgPaths(DIR_EXAMPLES)
+  ]
+  update(pkgPaths, (pkg) => {
+    const version = pkg.dependencies['vite-plugin-ssr']
+    assert(version)
+    assert.strictEqual(version, `^${versionCurrent}`)
+    pkg.dependencies['vite-plugin-ssr'] = `^${versionNew}`
+  })
+}
+
+function retrievePkgPaths(rootDir: string): string[] {
+  const directories = readdirSync(rootDir)
+    .map((file) => `${rootDir}/${file}`)
+    .filter((filePath) => lstatSync(filePath).isDirectory())
+  const pkgPaths = []
+  for (const dir of directories) {
+    const pkgPath = require.resolve(`${dir}/package.json`)
+    pkgPaths.push(pkgPath)
+  }
+  return pkgPaths
+}
+
+function update(pkgPath: string[], updater: (pkg: PackageJson) => void) {
+  pkgPath.forEach((pkgPath) => {
+    const pkg = require(pkgPath) as PackageJson
+    updater(pkg)
+    writeFileSync(pkgPath, JSON.stringify(pkg, null, 2))
+  })
+}
