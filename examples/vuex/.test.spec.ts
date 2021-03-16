f62f43c00693d9181ae9acd49a00e30b6c41d83d
Romuald Brillout <git@brillout.com>
2021-03-16 16:24:27 +0100

    test: test vuex example

---
+++ examples/vuex/.test.spec.ts
@@ -0,0 +1,12 @@
+import { fetchHtml, run } from '../../tests/setup'
+
+run('npm run start')
+
+test('page content is rendered to HTML', async () => {
+  const html = await fetchHtml('/')
+  expect(html).toContain('<h1>To-do List</h1>')
+  expect(html).toContain('<li>Buy milk</li><li>Buy chocolate</li>')
+  expect(html).toContain(
+    'INITIAL_STATE:{todoList:[{id:0,text:"Buy milk"},{id:1,text:"Buy chocolate"}]}'
+  )
+})
