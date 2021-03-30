25c9695833c0c4aa8ccc00fe4ee92d170fbe5ac6
Romuald Brillout <git@brillout.com>
2021-03-30 15:51:12 +0200

    workflow: improve test:release script

--- scripts/install-examples.ts
+++ scripts/install-examples.ts
@@ -8,10 +8,10 @@ async function installExamples() {
   for (const cwd of dependers) {
     process.stdout.write(`Installing dependencies for: ${cwd}...`)
     try {
-      await runCommand('npm', ['install'], { cwd, silent: true })
+      await runCommand('npm', ['install', '--legacy-peer-deps'], { cwd, silent: true })
     } catch (err) {
       process.stdout.write(' Failed.\n')
-      await runCommand('npm', ['install'], { cwd }) // Not silent; print error
+      await runCommand('npm', ['install', '--legacy-peer-deps'], { cwd }) // Not silent; print error
     }
     process.stdout.write(' Done.\n')
   }
