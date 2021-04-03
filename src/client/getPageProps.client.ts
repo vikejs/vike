ec14616be0f400ed4a4e0432060f32311a815c8f
Romuald Brillout <git@brillout.com>
2021-04-03 11:03:53 +0200

    minor

--- src/client/getPageProps.client.ts
+++ src/client/getPageProps.client.ts
@@ -1,28 +1,33 @@
-import routingState from './routingState.client';
-import { assert, assertInfo, getFileUrl } from '../utils'
+import routingState from './routingState.client'
+import { assert, assertInfo, getFileUrl, hasProp } from '../utils'
 import { parse } from '@brillout/json-s'
-import { PageInfo } from './types';
+import { PageInfo } from './types'
 
 export { getPageProps }
 
-async function getPageProps(
-  url: string
-): Promise<Record<string, unknown>> {
-  if (!routingState.checkIfInitialUrl(url)) {
-    const response = await fetch(getFileUrl(url, '.pageProps.json'))
-    const responseText = await response.text()
-    const responseObject = parse(responseText) as
-      | { pageProps: Record<string, unknown> }
-      | { userError: true }
-    assertInfo(
-      !('userError' in responseObject),
-      `Couldn't get the \`pageProps\` for \`${url}\`: one of your hooks is throwing an error. Check out the server logs.`
-    )
-    const { pageProps } = responseObject
-    assert(pageProps.constructor === Object)
+async function getPageProps(url: string): Promise<Record<string, unknown>> {
+  if (routingState.checkIfInitialUrl(url)) {
+    const pageProps = window.__vite_plugin_ssr.pageProps
     return pageProps
   }
-  return window.__vite_plugin_ssr.pageProps
+  const pageProps = await fetchPageProps(url)
+  return pageProps
+}
+
+async function fetchPageProps(url: string): Promise<Record<string, unknown>> {
+  const response = await fetch(getFileUrl(url, '.pageProps.json'))
+  const responseText = await response.text()
+  const responseObject = parse(responseText) as
+    | { pageProps: Record<string, unknown> }
+    | { userError: true }
+  assertInfo(
+    !('userError' in responseObject),
+    `Couldn't get the \`pageProps\` for \`${url}\`: one of your hooks is throwing an error. Check out the server logs.`
+  )
+  assert(hasProp(responseObject, 'pageProps'))
+  const { pageProps } = responseObject
+  assert(pageProps.constructor === Object)
+  return pageProps
 }
 
 declare global {
@@ -30,3 +35,4 @@ declare global {
     __vite_plugin_ssr: PageInfo
   }
 }
+
