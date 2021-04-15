a2ef10ed7dffe5606c46a766d0543a73245399be
Romuald Brillout <git@brillout.com>
2021-04-15 08:34:19 +0200

    add vite-plugin-ssr version to assertion errors

--- src/utils/assert.ts
+++ src/utils/assert.ts
@@ -1,10 +1,11 @@
+import { version } from '../package.json'
 import { newError } from '@brillout/libassert'
 
 export { assert }
 export { assertUsage }
 export { assertWarning }
 
-const repoName = 'vite-plugin-ssr'
+const repoName = `vite-plugin-ssr@${version}`
 const libName = repoName
 const requestForContact = `Please open a new issue at https://github.com/brillout/${repoName}/issues/new and include this error stack.`
 const internalErrorPrefix = `[${libName}][Internal Error]`
