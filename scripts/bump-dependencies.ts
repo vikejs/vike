6f7a8f3b2ac213ee60cfbc2b742b114db46b4606
Romuald Brillout <git@brillout.com>
2021-05-14 13:42:37 +0200

    workflow: improve release script

--- scripts/bump-dependencies.ts
+++ scripts/bump-dependencies.ts
@@ -8,8 +8,8 @@ updateDependencies()
 async function updateDependencies() {
   for (const packageJson of await getAllPackageJson()) {
     const cwd = dirname(packageJson)
-    await run__follow(`${ncuBin} -u --dep dev,prod --reject vue`, { cwd })
-    await run__follow(`${ncuBin} -u --dep dev,prod --filter vue --target greatest`, { cwd })
+    await run__follow(`${ncuBin} -u --dep dev,prod --reject vue --reject @vue/server-renderer --reject @vue/compiler-sfc`, { cwd })
+    await run__follow(`${ncuBin} -u --dep dev,prod vue @vue/server-renderer @vue/compiler-sfc --target greatest`, { cwd })
   }
 }
 
