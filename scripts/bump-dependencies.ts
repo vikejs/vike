45fef5962cd8afb26eb78cda41fee9ac53128af3
Romuald Brillout <git@brillout.com>
2021-05-14 13:48:24 +0200

    remove vue from dep version bump

--- scripts/bump-dependencies.ts
+++ scripts/bump-dependencies.ts
@@ -9,7 +9,7 @@ async function updateDependencies() {
   for (const packageJson of await getAllPackageJson()) {
     const cwd = dirname(packageJson)
     await run__follow(`${ncuBin} -u --dep dev,prod --reject vue --reject @vue/server-renderer --reject @vue/compiler-sfc`, { cwd })
-    await run__follow(`${ncuBin} -u --dep dev,prod vue @vue/server-renderer @vue/compiler-sfc --target greatest`, { cwd })
+    // await run__follow(`${ncuBin} -u --dep dev,prod vue @vue/server-renderer @vue/compiler-sfc --target greatest`, { cwd })
   }
 }
 
