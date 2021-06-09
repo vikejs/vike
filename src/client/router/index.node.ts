cb3417de974de6697ab29a8a55347fd91f72feac
Romuald Brillout <git@brillout.com>
2021-06-09 19:51:47 +0200

    feat: enable Jest/Babel component unit testing (fix #91)

--- src/client/router/index.node.ts
+++ src/client/router/index.node.ts
@@ -1,10 +1,12 @@
-import { assert, assertUsage } from '../../utils'
+import { assertUsage } from '../../utils'
 
 export { navigate }
 
-assert(isNodejs())
-
 function navigate(): never {
+  assertUsage(
+    isNodejs(),
+    '[`navigate(url)`] Something is wrong with your environement (it loads the wrong `vite-plugin-ssr/client/router` entry). This may be happening if you use Jest or Babel. Open a new GitHub issue so we can discuss a solution.'
+  )
   assertUsage(
     false,
     '[`navigate(url)`] The `navigate(url)` function is only callable in the browser but you are calling it in Node.js.'
