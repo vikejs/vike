3e2e385c8226cd1e9d1456f473ad1f58ee4da52e
Romuald Brillout <git@brillout.com>
2021-08-10 17:45:52 +0200

    move RepoLink.tsx to libframe/docs

--- docs/_default.page.server.ts
+++ docs/_default.page.server.ts
@@ -2,4 +2,10 @@ export * from 'libframe-docs/_default.page.server'
 import logoUrl from './icons/vite-plugin-ssr.svg'
 import { setFrame } from 'libframe-docs/_default.page.server'
 import { headings } from './headings'
-setFrame({ headings, logoUrl })
+setFrame({
+  projectName: 'vite-plugin-ssr',
+  projectNameIsCodeSnippet: true,
+  repo: 'https://github.com/brillout/vite-plugin-ssr',
+  headings,
+  logoUrl
+})
