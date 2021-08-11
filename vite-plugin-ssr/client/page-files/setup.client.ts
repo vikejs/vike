46b91106a440ec4b61164243f28fbf2e9d074261
Romuald Brillout <git@brillout.com>
2021-08-11 19:19:11 +0200

    improve file hierarchy

--- vite-plugin-ssr/client/page-files/setup.client.ts
+++ vite-plugin-ssr/client/page-files/setup.client.ts
@@ -1,3 +1,3 @@
 import { setPageFiles } from '../../shared/getPageFiles.shared'
-import { pageFiles } from '../../page-files/pageFiles.client'
+import { pageFiles } from './pageFiles.client'
 setPageFiles(pageFiles)
