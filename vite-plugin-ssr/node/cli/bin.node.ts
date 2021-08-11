8d88ded6597fd3e1853ab565f2630b5898350c63
Romuald Brillout <git@brillout.com>
2021-08-11 19:06:49 +0200

    improve file hierarchy

--- vite-plugin-ssr/node/cli/bin.node.ts
+++ vite-plugin-ssr/node/cli/bin.node.ts
@@ -1,7 +1,7 @@
 import { cac } from 'cac'
 import { resolve } from 'path'
-import { prerender } from '../node/prerender'
-import { projectInfo } from '../utils'
+import { prerender } from '../prerender'
+import { projectInfo } from '../../utils'
 
 const cli = cac(projectInfo.name)
 
