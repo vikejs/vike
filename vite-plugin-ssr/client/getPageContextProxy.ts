65ac8288597068ed846723a3a1f7ca4328023971
Romuald Brillout <git@brillout.com>
2021-08-13 17:17:17 +0200

    fix: allow pageContext to be Vue reactive

--- vite-plugin-ssr/client/getPageContextProxy.ts
+++ vite-plugin-ssr/client/getPageContextProxy.ts
@@ -4,7 +4,7 @@ export { getPageContextProxy }
 
 const SKIP = [
   'then',
-  'toJSON', // Vue tries to access `toJSON`
+  'toJSON' // Vue tries to access `toJSON`
 ]
 const BUILT_IN = ['_pageId', 'Page', 'pageExports']
 
@@ -12,7 +12,9 @@ function getPageContextProxy<T extends Record<string, unknown>>(pageContext: T):
   return new Proxy(pageContext, { get })
 
   function get(_: never, prop: string) {
-    if( typeof prop === 'symbol' ) return pageContext[prop] // Vue tries to access some symbols
+    if (typeof prop === 'symbol') return pageContext[prop] // Vue tries to access some symbols
+    if (typeof prop !== 'string') return pageContext[prop]
+    if (prop.startsWith('__v_')) return pageContext[prop] // Vue internals upon `reactive(pageContext)`
     if (prop in pageContext || SKIP.includes(prop)) return pageContext[prop]
     assertUsage(
       false,
@@ -23,7 +25,7 @@ function getPageContextProxy<T extends Record<string, unknown>>(pageContext: T):
           .filter((prop) => !BUILT_IN.includes(prop))
           .map((prop) => `'${prop}'`)
           .join(', ')}]\`.)`,
-        'More infos at https://vite-plugin-ssr.com/passToClient',
+        'More infos at https://vite-plugin-ssr.com/passToClient'
       ].join(' ')
     )
   }
