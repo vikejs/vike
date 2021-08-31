ea046328c0abcc01339a238d2f3cdb597567c36a
Romuald Brillout <git@brillout.com>
2021-08-31 14:18:58 +0200

    add test suite for `/examples/i18n-prerender/`

---
+++ examples/i18n-prerender/.testPages.ts
@@ -0,0 +1,39 @@
+import { run, page, urlBase, fetchHtml, autoRetry } from '../../libframe/test/setup'
+
+export { testPages }
+
+function testPages(cmd: 'npm run dev' | 'npm run prod') {
+  run(cmd)
+
+  test('localized content is rendered to HTML', async () => {
+    const html = await fetchHtml('/de-DE')
+    expect(html).toContain('<h1>Wilkommen</h1>')
+  })
+
+  test('localized page is rendered to the DOM and interactive', async () => {
+    await page.goto(urlBase + '/fr-FR')
+    expect(await page.textContent('h1')).toBe('Bienvenue')
+
+    // Interactive button
+    expect(await page.textContent('button')).toBe('Compteur 0')
+    // `autoRetry` because browser-side code may not be loaded yet
+    await autoRetry(async () => {
+      await page.click('button')
+      expect(await page.textContent('button')).toContain('Compteur 1')
+    })
+
+    // Localized routing
+    expect(await page.$('a[href="/about"]')).toBeNull()
+    expect(await page.$('a[href="/fr-FR/about"]')).not.toBeNull()
+    await page.click('a[href="/fr-FR/about"]')
+    expect(await page.textContent('h1')).toBe('Bonjour')
+    expect(await page.textContent('body')).toContain('Une autre page')
+  })
+
+  test('default locale', async () => {
+    await page.goto(urlBase + '/')
+    expect(await page.textContent('h1')).toContain('Welcome')
+    expect(await page.$('a[href="/about"]')).not.toBeNull()
+    expect(await page.$('a[href="/fr-FR/about"]')).toBeNull()
+  })
+}
