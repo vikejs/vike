9e86a47ea21a7f694afce12a186a4088a29938ea
Romuald Brillout <git@brillout.com>
2021-09-03 16:30:00 +0200

    impl `/examples/html-fragments/`

---
+++ examples/html-fragments/index.page.server.ts
@@ -0,0 +1,23 @@
+import { html } from "vite-plugin-ssr";
+
+export { render };
+
+function render() {
+  // Note how `<b>` is (not) escaped
+  const htmlFragment = html`<b>I was defined by an HTML Fragment</b>`;
+  const htmlWithoutFragment = `<b>I was defined without an HTML Fragment</b>`;
+
+  // Some supported edge cases that are useful to implement conditional HTML Fragments
+  const emptyFragment1 = html``;
+  const emptyFragment2 = '';
+
+  return html`<!DOCTYPE html>
+    <html>
+      <body>
+        <h1>Hello</h1>
+        <div id='1'>${htmlFragment}</div>
+        <div id='2'>${htmlWithoutFragment}</div>
+        <div>Empty Fragments: 0${emptyFragment1}${emptyFragment2}1</div>
+      </body>
+    </html>`;
+}
---
+++ examples/html-fragments/readme.md
@@ -0,0 +1,10 @@
+Example of using [`html` fragments](https://vite-plugin-ssr.com/html-tag#html-fragments).
+
+To run it:
+
+```bash
+git clone git@github.com:brillout/vite-plugin-ssr
+cd vite-plugin-ssr/examples/html-fragments/
+npm install
+npm run start
+```
