06eb4db2f9cb40f6c79b85a0823b61aac5fcf90b
Romuald Brillout <git@brillout.com>
2021-09-06 17:17:44 +0200

    docs: move nav header code to user land

--- docs/renderer/_default.page.server.tsx
+++ docs/renderer/_default.page.server.tsx
@@ -1,14 +1,15 @@
 export * from 'libframe-docs/_default.page.server'
-import logoUrl from '../icons/vite-plugin-ssr.svg'
 import { setFrame } from 'libframe-docs/_default.page.server'
 import { headings } from '../headings'
 import { projectInfo } from '../utils'
+import React from 'react'
+import { NavHeader, NavHeaderMobile } from './NavHeader'
 
 setFrame({
   projectVersion: projectInfo.version,
   projectName: projectInfo.name,
-  projectNameIsCodeSnippet: true,
   repo: projectInfo.githubRepository,
-  headings,
-  logoUrl
+  navHeaderMobile: <NavHeaderMobile />,
+  navHeader: <NavHeader />,
+  headings
 })
