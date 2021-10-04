52135d159076f73797c6a8af889e5a94e00041d2
Romuald Brillout <git@brillout.com>
2021-10-04 23:26:27 +0200

    minor

--- examples/vue-full/renderer/app.ts
+++ examples/vue-full/renderer/app.ts
@@ -50,9 +50,6 @@ function createApp(pageContext: PageContext) {
 }
 
 // Same as `Object.assign()` but with type inference
-function objectAssign<Obj, ObjAddendum extends Record<string, unknown>>(
-  obj: Obj,
-  objAddendum: ObjAddendum
-): asserts obj is Obj & ObjAddendum {
+function objectAssign<Obj, ObjAddendum>(obj: Obj, objAddendum: ObjAddendum): asserts obj is Obj & ObjAddendum {
   Object.assign(obj, objAddendum)
 }
--- vite-plugin-ssr/shared/utils/objectAssign.ts
+++ vite-plugin-ssr/shared/utils/objectAssign.ts
@@ -1,5 +1,6 @@
 export { objectAssign }
 
+// Same as `Object.assign()` but with type inference
 function objectAssign<Obj extends Record<string, unknown>, ObjAddendum extends Record<string, unknown>>(
   obj: Obj,
   objAddendum: ObjAddendum
