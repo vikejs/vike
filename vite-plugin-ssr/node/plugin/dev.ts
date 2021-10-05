ed255d1a563ccd7f07b2387b7b15464390993860
Romuald Brillout <git@brillout.com>
2021-10-05 12:21:01 +0200

    fix: also add `@brillout/json-s` to `optimizeDeps`

--- vite-plugin-ssr/node/plugin/dev.ts
+++ vite-plugin-ssr/node/plugin/dev.ts
@@ -11,7 +11,7 @@ function dev(): Plugin {
       optimizeDeps: {
         entries: ['**/*.page.*([a-zA-Z0-9])', '**/*.page.client.*([a-zA-Z0-9])'],
         exclude: ['vite-plugin-ssr/client', 'vite-plugin-ssr/client/router'],
-        include: ['@brillout/libassert']
+        include: ['@brillout/libassert', '@brillout/json-s']
       }
     })
   }
