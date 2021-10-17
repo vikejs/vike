d3b59cfa18754c5d4810c3bda5c9eb1e0a0afc42
Romuald Brillout <git@brillout.com>
2021-10-17 13:39:10 +0200

    remove old deprecation warning

--- vite-plugin-ssr/node/index.ts
+++ vite-plugin-ssr/node/index.ts
@@ -7,16 +7,6 @@ export { injectAssets__public as _injectAssets } from './html/injectAssets'
 import { importBuild } from './importBuild'
 export const __private = { importBuild }
 
-// Plugin should be imported from `vite-plugin-ssr/plugin`
-import { assertUsage } from '../shared/utils'
-export default pluginWrongImport
-function pluginWrongImport(): never {
-  assertUsage(
-    false,
-    "`import ssr from 'vite-plugin-ssr';` is depecrated: use `import ssr from 'vite-plugin-ssr/plugin';` instead."
-  )
-}
-
 // Enable `const ssr = require('vite-plugin-ssr')`
 // This lives at the end of the file to ensure it happens after all assignments to `exports`
 module.exports = Object.assign(exports.default, exports)
