997942a1a9e04758a019c5795084d3d619738718
Romuald Brillout <git@brillout.com>
2021-10-17 13:44:00 +0200

    remove CJS default export

--- vite-plugin-ssr/node/index.ts
+++ vite-plugin-ssr/node/index.ts
@@ -8,7 +8,3 @@ export type { PageContextBuiltIn } from './types'
 
 import { importBuild } from './importBuild'
 export const __private = { importBuild }
-
-// Enable `const ssr = require('vite-plugin-ssr')`
-// This lives at the end of the file to ensure it happens after all assignments to `exports`
-module.exports = Object.assign(exports.default, exports)
