632c30bccdad6292c71e208d9e7a5a62d11c4f4e
Romuald Brillout <git@brillout.com>
2021-10-20 15:34:01 +0200

    [wip] make `pageContext` immutable

--- vite-plugin-ssr/shared/utils/objectAssign.ts
+++ vite-plugin-ssr/shared/utils/objectAssign.ts
@@ -1,3 +1,5 @@
+import { assertUsage, assertWarning } from './assert'
+
 export { objectAssign }
 
 // Same as `Object.assign()` but with type inference
@@ -5,5 +7,16 @@ function objectAssign<Obj extends Record<string, unknown>, ObjAddendum extends R
   obj: Obj,
   objAddendum: ObjAddendum
 ): asserts obj is Obj & ObjAddendum {
+  const keysAddendum = Object.keys(objAddendum)
+  const keysExisting = Object.keys(obj)
+  const keysOverriden = intersection(keysExisting, keysAddendum)
+  //assertWarning(keysOverriden.length===0, `Overriding keys: ${JSON.stringify(keysOverriden)}`)
+  assertUsage(keysOverriden.length === 0, `Overriding keys: ${JSON.stringify(keysOverriden)}`)
+  assertWarning // make TS happy
   Object.assign(obj, objAddendum)
+}
+
+function intersection(array1: string[], array2: string[]) {
+  const filteredArray = array1.filter((value) => array2.includes(value))
+  return filteredArray
 }
