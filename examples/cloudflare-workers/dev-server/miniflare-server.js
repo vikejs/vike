f6243484f608d1790abe8e5d8beca01bf0032dd4
Romuald Brillout <git@brillout.com>
2021-10-30 14:27:33 +0200

    Revert "Miniflare server running with miniflare dep"

--- examples/cloudflare-workers/dev-server/miniflare-server.js
+++
@@ -1,9 +0,0 @@
-const Miniflare = require("miniflare");
-
-const mf = new Miniflare.Miniflare({
-  wranglerConfigPath: "./wrangler.toml",
-  scriptPath: './worker/worker/script.js',
-});
-mf.createServer().listen(3000, () => {
-  console.log("Listening on :3000");
-});
