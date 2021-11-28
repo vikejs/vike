06ceb5a0cfd8fff189fe5a075a40f06536b06290
Patrick <37963339+patryk-smc@users.noreply.github.com>
2021-11-28 19:18:50 +0100

    Update Vercel example

--- examples/vercel/vercel/render.js
+++ examples/vercel/vercel/render.js
@@ -1,18 +1,25 @@
 import { createPageRenderer } from 'vite-plugin-ssr'
-// `importBuild.js` enables Vercel to bundle our serverless functions, see https://vite-plugin-ssr.com/vercel and https://vite-plugin-ssr.com/importBuild.js
 import '../dist/server/importBuild.js'
 
 const renderPage = createPageRenderer({ isProduction: true })
 
-export default async (req, res) => {
+export default async function handler(req, res) {
   const { url } = req
+
+  console.log('Request to url:', url)
+
   const pageContextInit = { url }
   const pageContext = await renderPage(pageContextInit)
   const { httpResponse } = pageContext
+
   if (!httpResponse) {
-    res.status(200).send('')
+    res.statusCode = 200
+    res.end()
   } else {
     const { body, statusCode, contentType } = httpResponse
-    res.status(statusCode).setHeader('content-type', contentType).send(body)
+
+    res.statusCode = statusCode
+    res.setHeader('content-type', contentType)
+    res.end(body)
   }
 }
