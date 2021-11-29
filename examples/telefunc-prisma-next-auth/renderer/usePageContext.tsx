a61fb44d992bbfc6a50135c9b3f8dff7ebe887c3
aslemammad <zorofight94@gmail.com>
2021-11-30 00:18:45 +0330

    minimal usePageContext

---
+++ examples/telefunc-prisma-next-auth/renderer/usePageContext.tsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import type { PageContext } from './types'
+
+export { PageContextProvider }
+export { usePageContext }
+
+const Context = React.createContext<PageContext>(undefined as any)
+
+function PageContextProvider({ pageContext, children }: { pageContext: PageContext; children: React.ReactNode }) {
+  return <Context.Provider value={pageContext}>{children}</Context.Provider>
+}
+
+function usePageContext() {
+  const pageContext = useContext(Context)
+  return pageContext
+}
