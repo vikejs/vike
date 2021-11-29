7c3f027b908669e390cbe4610283dbbdecc822a3
aslemammad <zorofight94@gmail.com>
2021-11-30 00:18:50 +0330

    types

---
+++ examples/telefunc-prisma-next-auth/renderer/types.ts
@@ -0,0 +1,14 @@
+// The `pageContext` that are available in both on the server-side and browser-side
+export type PageContext = {
+  Page: React.ReactNode
+  pageExports: {
+    documentProps?: {
+      title: string
+    }
+  }
+  urlPathname: string
+  pageProps: Record<string, unknown>
+  documentProps?: {
+    title: string
+  }
+}
