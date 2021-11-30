a2d24f706d8da4da8c8f3d61eff55d0a6fc46199
aslemammad <zorofight94@gmail.com>
2021-11-30 20:22:32 +0330

    inject fetch

--- examples/telefunc-prisma-next-auth/server/index.ts
+++ examples/telefunc-prisma-next-auth/server/index.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import { createPageRenderer } from 'vite-plugin-ssr'
 import { config } from 'dotenv'
 import * as vite from 'vite'
-import _NextAuth from 'next-auth'
 import NextAuth from './auth'
 import Providers from 'next-auth/providers'
 import { createTelefuncCaller, provideContext } from 'telefunc'
-import  $fetch from 'node-fetch'
+import $fetch from 'node-fetch'
 import { getSession } from 'next-auth/client'
+
+// @ts-ignore
+globalThis.fetch = $fetch
 
 // load .env file
 config()
@@ -17,14 +19,6 @@ const root = `${__dirname}/..`
 
 startServer()
 
-const nextAuth = NextAuth({
-  providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-  ],
-})
 async function startServer() {
   const app = express()
 
@@ -40,16 +34,23 @@ async function startServer() {
     app.use(viteDevServer.middlewares)
   }
 
-  app.use(nextAuth)
   const renderPage = createPageRenderer({ viteDevServer, isProduction, root })
   const callTelefunc = await createTelefuncCaller({ viteDevServer, isProduction, root })
 
   app.use(express.text())
 
-  app.all('/_telefunc', async (req, res, next) => {
-    // @ts-ignore
-    globalThis.fetch = $fetch 
+  app.use(
+    NextAuth({
+      providers: [
+        Providers.GitHub({
+          clientId: process.env.GITHUB_CLIENT_ID,
+          clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        }),
+      ],
+    }),
+  )
 
+  app.all('/_telefunc', async (req, res, next) => {
     const session = await getSession({ req })
 
     provideContext({
@@ -65,8 +66,11 @@ async function startServer() {
 
   app.get('*', async (req, res, next) => {
     const url = req.originalUrl
+    const session = await getSession({ req })
+
     const pageContextInit = {
       url,
+      session,
     }
     const pageContext = await renderPage(pageContextInit)
     const { httpResponse } = pageContext
@@ -76,23 +80,7 @@ async function startServer() {
     stream.pipe(res)
   })
 
-  const port = 3002
+  const port = 3000
   app.listen(port)
   console.log(`Server running at http://localhost:${port}`)
-}
-
-const _getSession = async ({ req, res, next }) => {
-  _NextAuth(
-    { ...req, method: 'GET', query: { nextauth: ['session'] } },
-    {
-      ...res,
-      json: () => {
-        console.log.bind(null, 'here')
-      },
-    },
-    () => {
-      debugger
-      console.trace('here 2')
-    },
-  )
 }
