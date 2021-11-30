f8ef090651fcc8763f934ed865b13883218c76cb
aslemammad <zorofight94@gmail.com>
2021-11-30 19:45:41 +0330

    add telefunc

--- examples/telefunc-prisma-next-auth/vite.config.ts
+++ examples/telefunc-prisma-next-auth/vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
+import telefunc from 'telefunc/vite'
 import { UserConfig } from 'vite'
 
 const config: UserConfig = {
-  plugins: [react(), ssr()],
+  plugins: [react(), ssr(), telefunc()],
   clearScreen: false,
   define: {
      // next-auth needs process.env to be defined in frontend
