724e2683eb67d04f308f9a1274e5706ef4f2a188
aslemammad <zorofight94@gmail.com>
2021-11-30 20:22:23 +0330

    SessionProvider

--- examples/telefunc-prisma-next-auth/renderer/PageShell.tsx
+++ examples/telefunc-prisma-next-auth/renderer/PageShell.tsx
@@ -1,19 +1,33 @@
 import React from 'react'
+import { Provider as SessionProvider } from 'next-auth/client'
 import { PageContextProvider } from './usePageContext'
 import type { PageContext } from './types'
+import { Link } from './Link'
+import { Session } from 'next-auth'
 
 export { PageShell }
 
-function PageShell({ pageContext, children }: { pageContext: PageContext; children: React.ReactNode }) {
+function PageShell({
+  pageContext,
+  children,
+}: {
+  pageContext: PageContext & { session: Session | null }
+  children: React.ReactNode
+}) {
   return (
     <React.StrictMode>
-      <a href="/">
-        <button>Home</button>
-      </a>
-      <a href="/auth">
-        <button>Auth</button>
-      </a>
-      <PageContextProvider pageContext={pageContext}>{children}</PageContextProvider>
+      <SessionProvider session={pageContext.session || undefined}>
+        <PageContextProvider pageContext={pageContext}>
+          <Link href="/">
+            <button>Home</button>
+          </Link>
+          <Link href="/auth">
+            <button>Auth</button>
+          </Link>
+
+          {children}
+        </PageContextProvider>
+      </SessionProvider>
     </React.StrictMode>
   )
 }
