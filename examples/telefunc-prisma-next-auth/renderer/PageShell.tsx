3c24a96c00aa4f185448c6f428c906928e40023d
aslemammad <zorofight94@gmail.com>
2021-11-30 19:45:27 +0330

    new links

--- examples/telefunc-prisma-next-auth/renderer/PageShell.tsx
+++ examples/telefunc-prisma-next-auth/renderer/PageShell.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { PageContextProvider } from './usePageContext'
-import './PageShell.css'
 import type { PageContext } from './types'
 
 export { PageShell }
@@ -8,7 +7,13 @@ export { PageShell }
 function PageShell({ pageContext, children }: { pageContext: PageContext; children: React.ReactNode }) {
   return (
     <React.StrictMode>
-        <PageContextProvider pageContext={pageContext}>{children}</PageContextProvider>
+      <a href="/">
+        <button>Home</button>
+      </a>
+      <a href="/auth">
+        <button>Auth</button>
+      </a>
+      <PageContextProvider pageContext={pageContext}>{children}</PageContextProvider>
     </React.StrictMode>
   )
 }
