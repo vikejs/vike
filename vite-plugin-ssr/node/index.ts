272807eb21bf8f93cf99c11c684c9e99fed215f8
Joël Charles <joel.charles91@gmail.com>
2021-11-30 17:19:46 +0100

    reexport types

--- vite-plugin-ssr/node/index.ts
+++ vite-plugin-ssr/node/index.ts
@@ -1,10 +1,19 @@
 import './page-files/setup'
+
 export { createPageRenderer } from './createPageRenderer'
 export { escapeInject, dangerouslySkipEscape } from './html/renderHtml'
 export { pipeWebStream, pipeNodeStream } from './html/stream'
 export { injectAssets__public as _injectAssets } from './html/injectAssets'
 
-export type { PageContextBuiltIn } from './types'
+export type {
+  PageContextBuiltIn,
+  OnBeforeRenderPageContextInternal,
+  VitePluginSsr,
+  withTypescript,
+  GetPageProps,
+  GetPage,
+} from './types'
 
 import { importBuild } from './importBuild'
+
 export const __private = { importBuild }
