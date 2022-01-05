fad11c0c1c80801f9164b1de4c71f949bcdc63bb
Joël Charles <joel.charles91@gmail.com>
2022-01-05 12:08:57 +0100

    trying new helper to validate onBeforeRender while infering PageProps

--- vite-plugin-ssr/node/types.ts
+++ vite-plugin-ssr/node/types.ts
@@ -32,3 +32,43 @@ export type GetPageProps<T extends (...args: any) => any> = GetPagePropsInternal
 export type GetPage<T extends (...args: any) => any> = Page<GetPageProps<T>>;
 
 export type GetPageContext<T extends {} = {}> = BuiltInPageContext & T;
+
+
+
+
+// --- TESTING
+
+// Defined by lib for default props + can be overriden by user
+export interface OnBeforeRenderPageContext {
+  something?: number
+  documentProps?: {
+    title?: string,
+  }
+}
+
+// Used to validate and return values from onBeforeRender
+// Allow props other than `pageContext` here ?
+function wrapResponse<P>(ctx: OnBeforeRenderPageContext & { pageProps: P }) {
+  return {
+    pageContext: {
+      ...ctx
+    },
+  }
+}
+
+const onBeforeRender = () => {
+
+  return wrapResponse({
+    pageProps: {
+      movies: ['']
+    },
+    // The page's <title>
+    documentProps: { title: '' },
+    something: 42,
+    // TS DOES complain !!
+    shouldNotExist: 1,
+  });
+}
+
+// PageProps here is good too !!
+type PageProps = GetPageProps<typeof onBeforeRender>
