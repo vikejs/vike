de4b82ee8c8d2985f784e0dfc6cad9bf90c6f02b
Romuald Brillout <git@brillout.com>
2022-02-04 11:14:31 +0100

    update libframe

--- docs/renderer/_default.page.server.tsx
+++ docs/renderer/_default.page.server.tsx
@@ -2,13 +2,13 @@ export * from 'libframe-docs/_default.page.server'
 import { setFrame } from 'libframe-docs/setFrame'
 import { headings, headingsWithoutLink } from '../headings'
 import { projectInfo } from '../utils'
-import logoUrl from '../icons/vite-plugin-ssr.svg'
+import faviconUrl from '../icons/vite-plugin-ssr.svg'
 import React from 'react'
 import { NavHeader, NavHeaderMobile } from './NavHeader'
 
 setFrame({
   projectInfo,
-  logoUrl,
+  faviconUrl,
   algolia: {
     appId: 'MUXG1ZE9F6',
     apiKey: '8d5986fca9ba9110bcbbfc51263de88b',
