3c04f7a7d152ba176b30f05a2275a1d922a270de
Sam Hulick <samh@reelcrafter.com>
2022-02-21 18:46:52 -0600

    Uncommented commandHooks & fixed copyFiles paths

--- examples/sst-vue/src/entry.ts
+++ examples/sst-vue/src/entry.ts
@@ -1,28 +1,28 @@
-import "../dist/server/importBuild";
-import { APIGatewayProxyHandlerV2 } from "aws-lambda";
-import { createPageRenderer } from "vite-plugin-ssr";
-import { inspect } from "util";
+import '../dist/server/importBuild'
+import { APIGatewayProxyHandlerV2 } from 'aws-lambda'
+import { createPageRenderer } from 'vite-plugin-ssr'
+import { inspect } from 'util'
 
-const renderPage = createPageRenderer({ isProduction: true });
+const renderPage = createPageRenderer({ isProduction: true })
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
-  let url = event.rawPath;
+  let url = event.rawPath
 
   if (event.rawQueryString) {
-    url += "?" + event.rawQueryString;
+    url += '?' + event.rawQueryString
   }
 
-  const pageContextInit = { url };
-  console.log("rendering...");
-  const pageContext = await renderPage(pageContextInit);
-  console.log("pageContext =", inspect(pageContext, { depth: null }));
-  const { httpResponse } = pageContext;
-  if (!httpResponse) return { statusCode: 200, body: "??" };
-  const { body, statusCode, contentType } = httpResponse;
+  const pageContextInit = { url }
+  console.log('rendering...', url)
+  const pageContext = await renderPage(pageContextInit)
+  console.log('pageContext =', inspect(pageContext, { depth: null }))
+  const { httpResponse } = pageContext
+  if (!httpResponse) return { statusCode: 200, body: 'No httpResponse' }
+  const { body, statusCode, contentType } = httpResponse
 
   return {
     statusCode,
-    headers: { "Content-Type": contentType },
+    headers: { 'Content-Type': contentType },
     body,
-  };
-};
+  }
+}
--- examples/sst-vue/stacks/MyStack.ts
+++ examples/sst-vue/stacks/MyStack.ts
@@ -1,43 +1,44 @@
-import * as sst from "@serverless-stack/resources";
+import * as sst from '@serverless-stack/resources'
 
 export default class MyStack extends sst.Stack {
   constructor(scope: sst.App, id: string, props?: sst.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // Create a HTTP API
-    const api = new sst.Api(this, "Api", {
+    const api = new sst.Api(this, 'Api', {
       routes: {
-        $default: new sst.Function(this, "EntryPointFunc", {
-          handler: "src/entry.handler",
+        $default: new sst.Function(this, 'EntryPointFunc', {
+          handler: 'src/entry.handler',
           bundle: {
             copyFiles: [
-              { from: "dist/server", to: "./src" },
-              { from: "dist/server", to: "./dist/server" },
+              { from: 'dist/server', to: './dist/server' },
+              { from: 'vue/pages', to: './vue/pages' },
+              { from: 'vue/renderer', to: './vue/renderer' },
             ],
-            // commandHooks: {
-            //   beforeInstall() {
-            //     return [];
-            //   },
-            //   beforeBundling(inputDir, outputDir) {
-            //     console.log("before:", inputDir, outputDir);
-            //     return [
-            //       `./node_modules/.bin/vite build --outDir ${outputDir} && ` +
-            //         `./node_modules/.bin/vite build --ssr --outDir ${outputDir}`,
-            //     ];
-            //   },
-            //   afterBundling(inputDir, outputDir) {
-            //     console.log("after:", inputDir, outputDir);
-            //     return ["echo after"];
-            //   },
-            // },
+            commandHooks: {
+              beforeInstall() {
+                return []
+              },
+              beforeBundling(inputDir, outputDir) {
+                console.log('before:', inputDir, outputDir)
+                return [
+                  `./node_modules/.bin/vite build --outDir ${outputDir} && ` +
+                    `./node_modules/.bin/vite build --ssr --outDir ${outputDir}`,
+                ]
+              },
+              afterBundling(inputDir, outputDir) {
+                console.log('after:', inputDir, outputDir)
+                return ['echo after']
+              },
+            },
           },
         }),
       },
-    });
+    })
 
     // Show the endpoint in the output
     this.addOutputs({
       ApiEndpoint: api.url,
-    });
+    })
   }
 }
