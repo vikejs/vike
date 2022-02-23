14dfe7ad91532ecf461417593da3037e58a35750
Romuald Brillout <git@brillout.com>
2022-02-23 09:09:25 +0100

    fix: help eslint resolve modules (fix #270)

---
+++ vite-plugin-ssr/client/router.js
@@ -0,0 +1,2 @@
+// eslint doesn't seem to support `package.json#exports`.
+export * from '../dist/esm/client/router'
---
+++ vite-plugin-ssr/plugin.js
@@ -0,0 +1,2 @@
+// eslint doesn't seem to support `package.json#exports`.
+module.exports = require('./dist/cjs/node/plugin')
