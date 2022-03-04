7211819954495d77c63f6734b7f40f87b026a7fa
Romuald Brillout <git@brillout.com>
2022-03-04 21:08:00 +0100

    refactor client-side entry [wip]

--- vite-plugin-ssr/utils/getPathDistance.ts
+++ vite-plugin-ssr/utils/getPathDistance.ts
@@ -1,12 +1,13 @@
 import { assert } from './assert'
+import { assertPosixPath } from './filesystemPathHandling'
 
 export { getPathDistance }
 
 function getPathDistance(pathA: string, pathB: string): number {
+  assertPosixPath(pathA)
+  assertPosixPath(pathB)
   assert(pathA.startsWith('/'))
   assert(pathB.startsWith('/'))
-  assert(!pathA.startsWith('\\'))
-  assert(!pathB.startsWith('\\'))
 
   let charIdx = 0
   for (; charIdx < pathA.length && charIdx < pathB.length; charIdx++) {
