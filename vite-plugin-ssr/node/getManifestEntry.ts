304ad611cd0caf8bc1b86b5239a010456b343a7c
Romuald Brillout <git@brillout.com>
2022-03-06 13:42:35 +0100

    refactor client-side entry [wip]

--- vite-plugin-ssr/node/getManifestEntry.ts
+++ vite-plugin-ssr/node/getManifestEntry.ts
@@ -16,40 +16,51 @@ function getManifestEntry(filePath: string, manifests: ViteManifest[], root: str
 function getManifestEntry(filePath: string, manifests: ViteManifest[], root: string, optional: true   ): ManifestEntryOptional
 // prettier-ignore
 function getManifestEntry(filePath: string, manifests: ViteManifest[], root: string, optional: boolean): ManifestEntryOptional {
+  assert(root.startsWith('/'))
   assert(filePath.startsWith('/'))
+  assertPosixPath(root)
+  assertPosixPath(filePath)
 
-  if( filePath.startsWith(root) ) {
-    filePath = filePath.slice(root.length)
-    assert(filePath.startsWith('/'))
-  }
-
-  filePath = filePath.slice(1)
-  const manifestKey1 = filePath
+  const manifestKey = getManifestKey(filePath, root)
   for (const manifest of manifests) {
-    let manifestEntry = manifest[manifestKey1]
+    let manifestEntry = manifest[manifestKey]
     if (manifestEntry) {
-      return { manifestEntry, manifest, manifestKey: manifestKey1 }
+      return { manifestEntry, manifest, manifestKey }
     }
   }
-  const manifestKey2 = resolveSymlink(filePath, root)
+
+  const filePath_resolvedSymlink = resolveSymlink(filePath, root)
+  const manifestKey_resolvedSymlink = getManifestKey(filePath_resolvedSymlink, root)
   for (const manifest of manifests) {
-    let manifestEntry = manifest[manifestKey2]
+    let manifestEntry = manifest[manifestKey_resolvedSymlink]
     if (manifestEntry) {
-      return { manifestEntry, manifest, manifestKey: manifestKey2 }
+      return { manifestEntry, manifest, manifestKey: manifestKey_resolvedSymlink }
     }
   }
-  assert(optional, { manifestKey1, manifestKey2, manifestLength: manifests.length })
-  return { manifestKey: manifestKey2, manifest: null, manifestEntry: null }
+
+  assert(optional, { manifestKey, filePath, manifestKey_resolvedSymlink, filePath_resolvedSymlink, manifestLength: manifests.length })
+  return { manifestKey: manifestKey_resolvedSymlink, manifest: null, manifestEntry: null }
+}
+
+function getManifestKey(filePath: string, root: string) {
+  let manifestKey = filePath
+  if (filePath.startsWith('/@fs/')) {
+    posix.relative(root, manifestKey.slice('/@fs'.length))
+  }
+  if (manifestKey.startsWith('/')) {
+    manifestKey = manifestKey.slice(1)
+  }
+  return manifestKey
 }
 
 function resolveSymlink(filePath: string, root: string) {
-  assertPosixPath(filePath)
-  assertPosixPath(root)
-  assert(!filePath.startsWith('/'))
-  const filePathAbsolute = [...root.split('/'), ...filePath.split('/')].join('/')
+  const filePathAbsolute = filePath.startsWith('/@fs/')
+    ? filePath.slice('/@fs'.length)
+    : `/${[...root.split('/'), ...filePath.split('/')].filter(Boolean).join('/')}`
   // Resolves symlinks
   const filePathResolved = require.resolve(filePathAbsolute)
-  const filePathRelative = posix.relative(root, filePathResolved)
+  let filePathRelative = posix.relative(root, filePathResolved)
   assert(!filePathRelative.startsWith('/'))
+  filePathRelative = '/' + filePathRelative
   return filePathRelative
 }
