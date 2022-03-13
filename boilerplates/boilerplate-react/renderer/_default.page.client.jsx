7c91ef421f947e16d70b51111015b40caf31021a
Romuald Brillout <git@brillout.com>
2022-03-13 17:18:08 +0100

    boilerplate-react: migrate to `vite-plugin-ssr@0.4.0`

--- boilerplates/boilerplate-react/renderer/_default.page.client.jsx
+++ boilerplates/boilerplate-react/renderer/_default.page.client.jsx
@@ -1,14 +1,12 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import { getPage } from 'vite-plugin-ssr/client'
 import { PageShell } from './PageShell'
 
-hydrate()
+export { render }
 
-async function hydrate() {
+async function render(pageContext) {
   // We do Server Routing, but we can also do Client Routing by using `useClientRouter()`
   // instead of `getPage()`, see https://vite-plugin-ssr.com/useClientRouter
-  const pageContext = await getPage()
   const { Page, pageProps } = pageContext
   ReactDOM.hydrate(
     <PageShell pageContext={pageContext}>
