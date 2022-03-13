e4031fe076d10cd672876727998bd278bc8f232e
Romuald Brillout <git@brillout.com>
2022-03-13 17:19:26 +0100

    boilerplate-vue: migrate to `vite-plugin-ssr@0.4.0`

--- boilerplates/boilerplate-vue/renderer/_default.page.client.js
+++ boilerplates/boilerplate-vue/renderer/_default.page.client.js
@@ -1,12 +1,10 @@
-import { getPage } from 'vite-plugin-ssr/client'
 import { createApp } from './app'
 
-hydrate()
+export { render }
 
-async function hydrate() {
+async function render(pageContext) {
   // We do Server Routing, but we can also do Client Routing by using `useClientRouter()`
   // instead of `getPage()`, see https://vite-plugin-ssr.com/useClientRouter
-  const pageContext = await getPage()
   const app = createApp(pageContext)
   app.mount('#app')
 }
