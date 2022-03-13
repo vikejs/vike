799f2965c3997d8b855528d7566bd53afd47b97f
Romuald Brillout <git@brillout.com>
2022-03-13 17:26:43 +0100

    revert react-full migration

--- examples/react-full/renderer/_default.page.client.tsx
+++ examples/react-full/renderer/_default.page.client.tsx
@@ -1,44 +1,44 @@
-export const clientRouting = true
-export { render }
-export { onHydrationEnd }
-export { onPageTransitionStart }
-export { onPageTransitionEnd }
-
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { useClientRouter } from 'vite-plugin-ssr/client/router'
 import { PageShell } from './PageShell'
 import { getPageTitle } from './getPageTitle'
 import type { PageContextBuiltInClient } from 'vite-plugin-ssr/client/router'
 import type { PageContext } from './types'
 
 let root: ReactDOM.Root
-async function render(pageContext: PageContextBuiltInClient & PageContext) {
-  const { Page, pageProps } = pageContext
-  const page = (
-    <PageShell pageContext={pageContext}>
-      <Page {...pageProps} />
-    </PageShell>
-  )
-  const container = document.getElementById('page-view')!
-  if (pageContext.isHydration) {
-    root = ReactDOM.hydrateRoot(container, page)
-  } else {
-    if (!root) {
-      root = ReactDOM.createRoot(container)
+const { hydrationPromise } = useClientRouter({
+  render(pageContext: PageContextBuiltInClient & PageContext) {
+    const { Page, pageProps } = pageContext
+    const page = (
+      <PageShell pageContext={pageContext}>
+        <Page {...pageProps} />
+      </PageShell>
+    )
+    const container = document.getElementById('page-view')!
+    if (pageContext.isHydration) {
+      root = ReactDOM.hydrateRoot(container, page)
+    } else {
+      if (!root) {
+        root = ReactDOM.createRoot(container)
+      }
+      root.render(page)
     }
-    root.render(page)
-  }
-  document.title = getPageTitle(pageContext)
-}
+    document.title = getPageTitle(pageContext)
+  },
+  onTransitionStart,
+  onTransitionEnd,
+})
 
-function onHydrationEnd() {
+hydrationPromise.then(() => {
   console.log('Hydration finished; page is now interactive.')
-}
-function onPageTransitionStart() {
+})
+
+function onTransitionStart() {
   console.log('Page transition start')
   document.querySelector('#page-content')!.classList.add('page-transition')
 }
-function onPageTransitionEnd() {
+function onTransitionEnd() {
   console.log('Page transition end')
   document.querySelector('#page-content')!.classList.remove('page-transition')
 }
