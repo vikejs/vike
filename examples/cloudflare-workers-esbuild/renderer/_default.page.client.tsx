e61385cf283eead0c4b6d0c74129063e46b5f57c
Daniel Lehr <daniellehr@gmx.de>
2022-03-16 13:58:13 +0100

    Add example for Cloudflare Workers with esbuild

---
+++ examples/cloudflare-workers-esbuild/renderer/_default.page.client.tsx
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import { getPage } from 'vite-plugin-ssr/client'
+import { PageLayout } from './PageLayout'
+
+hydrate()
+
+async function hydrate() {
+  const pageContext = await getPage<any>()
+  const { Page, pageProps } = pageContext
+
+  ReactDOM.hydrate(
+    <PageLayout>
+      <Page {...pageProps} />
+    </PageLayout>,
+    document.getElementById('page-view'),
+  )
+}
