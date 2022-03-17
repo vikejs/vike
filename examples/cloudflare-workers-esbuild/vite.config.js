5865992e04dd61b968717a49a028ad4606c9d06a
Romuald Brillout <git@brillout.com>
2022-03-17 09:33:36 +0100

    typo

--- examples/cloudflare-workers-esbuild/vite.config.js
+++ examples/cloudflare-workers-esbuild/vite.config.js
@@ -4,7 +4,7 @@ import ssr from 'vite-plugin-ssr/plugin'
 export default {
   plugins: [react(), ssr()],
   resolve: {
-    // Only neeeded for this example
+    // Only needed for this example
     preserveSymlinks: true,
   }
 }
