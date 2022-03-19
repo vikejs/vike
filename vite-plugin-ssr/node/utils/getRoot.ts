1c2826cb12a29a22e9065f50ba0fd7cbb875ca78
Romuald Brillout <git@brillout.com>
2022-03-19 10:11:15 +0100

    stop assuming `root` to be always defined

--- vite-plugin-ssr/node/utils/getRoot.ts
+++ vite-plugin-ssr/node/utils/getRoot.ts
@@ -1,12 +1,20 @@
 import { getSsrEnv } from '../ssrEnv'
 import { toPosixPath } from '../utils'
+import { hasProp } from '../../utils/hasProp'
 
 export { getRoot }
-//
+
 // TODO move normalized `root` to `pageContext`
-function getRoot(): string {
+function getRoot(): string | null {
   let { root } = getSsrEnv()
-  root = root || process.cwd()
+  if (root) {
+    root = toPosixPath(root)
+    return root
+  }
+  if (typeof process == 'undefined' || !hasProp(process, 'cwd')) {
+    return null
+  }
+  root = process.cwd()
   root = toPosixPath(root)
   return root
 }
