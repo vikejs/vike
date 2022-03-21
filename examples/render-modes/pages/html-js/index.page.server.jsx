0d0b5c26ccde2bb7832e0139fcb6cda56b3f6bbd
Romuald Brillout <git@brillout.com>
2022-03-21 17:40:59 +0100

    impl render modes example

---
+++ examples/render-modes/pages/html-js/index.page.server.jsx
@@ -0,0 +1,13 @@
+export { Page }
+
+function Page() {
+  return (
+    <>
+      <h1>HTML + JS</h1>
+      <p>This page has only few lines of browser-side JavaScript.</p>
+      <p>
+        <button>Loading...</button>
+      </p>
+    </>
+  )
+}
--- examples/render-modes/renderer/_default.page.server.jsx
+++ examples/render-modes/renderer/_default.page.server.jsx
@@ -1,26 +1,32 @@
-import ReactDOMServer from 'react-dom/server'
-import React from 'react'
-import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr'
-import { PageLayout } from './PageLayout'
-
 export { render }
 export { passToClient }
+
+import ReactDOMServer from 'react-dom/server'
+import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr'
+import { PageLayout } from './PageLayout'
 
 // See https://vite-plugin-ssr.com/data-fetching
 const passToClient = ['pageProps']
 
 function render(pageContext) {
-  const { Page, pageProps } = pageContext
-  const pageHtml = ReactDOMServer.renderToString(
-    <PageLayout>
-      <Page {...pageProps} />
-    </PageLayout>,
-  )
+  let pageHtml
+  if (!pageContext.Page) {
+    // SPA
+    pageHtml = ''
+  } else {
+    // SSR / HTML-only
+    const { Page, pageProps } = pageContext
+    pageHtml = ReactDOMServer.renderToString(
+      <PageLayout>
+        <Page {...pageProps} />
+      </PageLayout>,
+    )
+  }
 
   return escapeInject`<!DOCTYPE html>
     <html>
       <body>
-        <div id="page-view">${dangerouslySkipEscape(pageHtml)}</div>
+        <div id="react-container">${dangerouslySkipEscape(pageHtml)}</div>
       </body>
     </html>`
 }
