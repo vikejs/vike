eb8d5a27ba794b929038ff978067cec010fd3c80
Romuald Brillout <git@brillout.com>
2022-03-22 10:49:54 +0100

    improve render modes example

--- examples/render-modes/pages/html-js/index.page.server.jsx
+++ examples/render-modes/pages/html-js/index.page.server.jsx
@@ -4,7 +4,7 @@ function Page() {
   return (
     <>
       <h1>HTML + JS</h1>
-      <p>This page has only few lines of browser-side JavaScript.</p>
+      <p>This page is rendered to HTML and has only few lines of browser-side JavaScript.</p>
       <p>
         <button>Loading...</button>
       </p>
