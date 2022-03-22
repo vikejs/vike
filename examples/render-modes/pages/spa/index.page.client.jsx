dee52a37631f0e3b4274b6efdfff6a184103f964
Romuald Brillout <git@brillout.com>
2022-03-22 10:49:54 +0100

    improve render modes example

--- examples/render-modes/pages/spa/index.page.client.jsx
+++ examples/render-modes/pages/spa/index.page.client.jsx
@@ -5,10 +5,10 @@ import { Counter } from '../ssr/Counter'
 function Page() {
   return (
     <>
-      <h1>Welcome</h1>
+      <h1>SPA</h1>
       This page is:
       <ul>
-        <li>Rendered only to browser's DOM.</li>
+        <li>Rendered only to the browser's DOM. (Not rendered to HTML.)</li>
         <li>
           Interactive. <Counter />
         </li>
