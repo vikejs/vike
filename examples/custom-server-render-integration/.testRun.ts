24ea4c8b8753cf62d17fc03aacbb618224e4b04c
Romuald Brillout <git@brillout.com>
2022-03-22 14:15:33 +0100

    udpate test

--- examples/custom-server-render-integration/.testRun.ts
+++ examples/custom-server-render-integration/.testRun.ts
@@ -5,12 +5,10 @@ export { testRun }
 function testRun(npmScript: 'npm run dev' | 'npm run prod') {
   run(npmScript)
 
-  test(`page is rendered to HTML [${npmScript}]`, async () => {
+  test('page is rendered to HTML & interactive', async () => {
     const html = await fetchHtml('/')
     expect(html).toContain('I am rendered to HTML and interactive')
-  })
 
-  test(`page is interactive [${npmScript}]`, async () => {
     page.goto(`${urlBase}/`)
     expect(await page.textContent('body')).toContain('Count: 0')
     // `autoRetry` because browser-side code may not be loaded yet
