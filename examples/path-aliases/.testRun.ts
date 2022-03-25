57bfa1ab54c1169f86d5ff9318ac431dd2624d35
Romuald Brillout <git@brillout.com>
2022-03-25 19:50:16 +0100

    add test for extractStyles plugin support for aliased import paths

--- examples/path-aliases/.testRun.ts
+++ examples/path-aliases/.testRun.ts
@@ -5,7 +5,7 @@ export { testRun }
 function testRun(npmScript: 'npm run dev' | 'npm run prod' | 'npm run prod:static') {
   run(npmScript)
 
-  test(`Counter succesfully imported [${npmScript}]`, async () => {
+  test(`Counter works`, async () => {
     page.goto(`${urlBase}/`)
     expect(await page.textContent('body')).toContain('Counter 0')
     // `autoRetry` because browser-side code may not be loaded yet
@@ -13,5 +13,28 @@ function testRun(npmScript: 'npm run dev' | 'npm run prod' | 'npm run prod:stati
       await page.click('button')
       expect(await page.textContent('body')).toContain('Counter 1')
     })
+  })
+
+  test("CSS doesn't leak", async () => {
+    expect(await page.$eval('p', (e) => getComputedStyle(e).color)).toBe(`rgb(0, 0, 0)`)
+  })
+
+  test('PageLayout.css loaded', async () => {
+    expect(await page.$eval('a', (e) => getComputedStyle(e).color)).toBe(`rgb(0, 0, 255)`)
+  })
+
+  test(`About page`, async () => {
+    await page.click('a[href="/about"]')
+    expect(await page.textContent('body')).toContain('This liltle app uses path aliases.')
+  })
+
+  // This tests the `vite-plugin-ssr:extractStyles` plugin.
+  // (Retrieving the CSS from aliased import paths is not trivial.)
+  test('CSS loaded also for HTML-only pages', async () => {
+    page.goto(`${urlBase}/about`)
+    await autoRetry(async () => {
+      expect(await page.$eval('a', (e) => getComputedStyle(e).color)).toBe(`rgb(0, 0, 255)`)
+    })
+    expect(await page.$eval('p', (e) => getComputedStyle(e).color)).toBe(`rgb(255, 0, 255)`)
   })
 }
