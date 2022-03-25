fc86dea4890b82d6eac64f133f0ae9e55cf1ffb6
Romuald Brillout <git@brillout.com>
2022-03-25 11:50:37 +0100

    fix: improve wrong usage error message

--- vite-plugin-ssr/client/node.ts
+++ vite-plugin-ssr/client/node.ts
@@ -1,5 +1,5 @@
 import { assertUsage } from './utils'
 assertUsage(
   false,
-  'The module `vite-plugin-ssr` cannot be imported in the browser. Did you mean to import the module `vite-plugin-ssr/client` instead?',
+  'The module `vite-plugin-ssr` cannot be imported in the browser. Did you mean to import from `vite-plugin-ssr/client` or `vite-plugin-ssr/client/router` instead?',
 )
