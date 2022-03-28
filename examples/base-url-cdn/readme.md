4bdf27ac4a1046b15c7184f08408d72cf912e0ec
Romuald Brillout <git@brillout.com>
2022-03-28 15:15:59 +0200

    redesign config flow [wip]

--- examples/base-url-cdn/readme.md
+++ examples/base-url-cdn/readme.md
@@ -10,6 +10,5 @@ npm run dev
 ```
 
 Highlights:
- - Static Assets in the HTML returned by the `render()` hook: [renderer/_default.page.server.jsx](renderer/_default.page.server.jsx).
- - Setting the `baseAssets` config of [createPageRenderer](server/ssr.js).
- - Setting the `base` config of [vite.config.js](vite.config.js).
+ - Setting the `base` config: [vite.config.js](vite.config.js).
+ - HTML referencing assets deployed to a CDN: [renderer/_default.page.server.jsx](renderer/_default.page.server.jsx).
--- vite-plugin-ssr/node/plugin/resolveConfig.ts
+++ vite-plugin-ssr/node/plugin/resolveConfig.ts
@@ -3,10 +3,9 @@ import { resolvePageFiles } from './resolveConfig/resolvePageFiles'
 export { resolveConfig }
 export type { Config }
 
-type Config = { base?: string; baseAssets?: string; pageFiles?: { include: string[] } }
+type Config = { pageFiles?: { include: string[] } }
 
 function resolveConfig(config: Config | undefined) {
   const getGlobRoots = resolvePageFiles(config?.pageFiles)
-  const { baseAssets = null, base: baseUrl = '/' } = config ?? {}
-  return { getGlobRoots, baseAssets, baseUrl }
+  return { getGlobRoots }
 }
