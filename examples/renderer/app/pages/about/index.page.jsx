a194e7e52762394d5a521724d1f1a8e51f80c4f5
Romuald Brillout <git@brillout.com>
2022-04-02 14:07:21 +0200

    add renderer example

---
+++ examples/renderer/app/pages/about/index.page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './code.css'
+
+export { Page }
+
+function Page() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Demo using <code>vite-plugin-ssr</code>.
+      </p>
+    </>
+  )
+}
---
+++ examples/renderer/renderer/renderer/_default.page.server.tsx
@@ -0,0 +1,33 @@
+export { render }
+export const passToClient = ['pageProps', 'urlPathname']
+
+import ReactDOMServer from 'react-dom/server'
+import React from 'react'
+import { PageShell } from './PageShell'
+import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr'
+import { PageContext } from './types'
+
+async function render(pageContext: PageContext) {
+  const { Page, pageProps } = pageContext
+  const pageHtml = ReactDOMServer.renderToString(
+    <PageShell pageContext={pageContext}>
+      <Page {...pageProps} />
+    </PageShell>,
+  )
+
+  const documentHtml = escapeInject`<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <link rel="icon" href="${pageContext.exports.favicon}" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>
+        <div id="page-view">${dangerouslySkipEscape(pageHtml)}</div>
+      </body>
+    </html>`
+
+  return {
+    documentHtml,
+  }
+}
