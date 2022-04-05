0b613e34ee6c9dd005c966c763a55f1cac3d3bab
Romuald Brillout <git@brillout.com>
2022-04-05 13:58:08 +0200

    impl pageFilesDir option

--- vite-plugin-ssr/node/plugin/glob.ts
+++ vite-plugin-ssr/node/plugin/glob.ts
@@ -2,7 +2,7 @@ export { resolveGlobConfig }
 export { getGlobPath }
 
 import fs from 'fs'
-import { assertPosixPath, assertUsage, toPosixPath } from './utils'
+import { assertPosixPath, assertUsage, toPosixPath, assert } from './utils'
 import path from 'path'
 import symlinkDir from 'symlink-dir'
 import resolve from 'resolve'
@@ -24,43 +24,41 @@ function resolveGlobConfig(includePageFiles: string[]) {
 async function createIncludePath(pkgName: string, root: string): Promise<string> {
   assertUsage(
     isNpmName(pkgName),
-    `Wrong vite-plugin-ssr config \`pageFiles.include\`: it contains \`${pkgName}\` which is not a valid npm package name.`,
+    `Wrong vite-plugin-ssr config \`pageFiles.include\`: the string \`${pkgName}\` is not a valid npm package name.`,
   )
-  let pkgJsonPath: string
-  try {
-    // We cannot use Node.js's `require.resolve()`: https://stackoverflow.com/questions/10111163/in-node-js-how-can-i-get-the-path-of-a-module-i-have-loaded-via-require-that-is/63441056#63441056
-    pkgJsonPath = resolve.sync(`${pkgName}/package.json`, { preserveSymlinks: true, basedir: root })
-  } catch (err) {
-    assertUsage(false, `Cannot find \`${pkgName}\`. Did you install it?`)
-  }
-  pkgJsonPath = toPosixPath(pkgJsonPath)
-  // const pkgJson: { ['vite-plugin-ssr']?: { pageFilesDir?: string } } = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'))
-  const pkgRoot = path.posix.dirname(pkgJsonPath)
-  // let pageFilesDir = pkgJson['vite-plugin-ssr']?.pageFilesDir ?? ''
-  const pageFilesDir = ''
-  const pkgPathRelative = path.posix.relative(root, pkgRoot)
-  if (!pkgPathRelative.startsWith('.')) {
-    const includePath = path.posix.join(pkgPathRelative, pageFilesDir)
-    assertPosixPath(includePath)
+  const { pkgJson, pkgRoot } = resolvePackage(pkgName, { preserveSymlinks: true, root })
+  const pageFilesDir = pkgJson['vite-plugin-ssr']?.pageFilesDir ?? ''
+  const pkgRootResolved = resolvePackageRoot(pkgName, { preserveSymlinks: false, root })
+
+  const crawlRoot = path.posix.join(pkgRootResolved, pageFilesDir)
+  assertUsage(
+    !root.startsWith(crawlRoot),
+    `The page files include path ${crawlRoot} is a parent of the app's root ${root}. You need to use/change the \`pageFilesDir\` options. Contact the vite-plugin-ssr maintainer on GitHub / Discord for more information.`,
+  )
+
+  const pkgRootRelative = path.posix.relative(root, pkgRoot)
+  if (!pkgRootRelative.startsWith('..')) {
+    const includePath = path.posix.join(pkgRootRelative, pageFilesDir)
     return includePath
   }
+
   const includePath = path.posix.join('node_modules', pkgName, pageFilesDir)
   if (!fs.existsSync(includePath)) {
-    // const cwd = toPosixPath(process.cwd())
-    const cwd = root
-    const source = path.posix.relative(cwd, pkgRoot)
-    const target = path.posix.relative(cwd, `${root}/${includePath}`)
-    assertUsage(
-      !target.startsWith(source),
-      'Your file structure is not supported. Contact the vite-plugin-ssr maintainer on GitHub / Discord.',
-    )
+    const sourceAbsolute = crawlRoot
+    const targetAbsolute = `${root}/${includePath}`
+    assert(!root.startsWith(crawlRoot)) // See above
+    assert(!targetAbsolute.startsWith(sourceAbsolute)) // Ensure it's not a cyclic symlink
+    const source = path.posix.relative(root, sourceAbsolute)
+    const target = path.posix.relative(root, targetAbsolute)
     await symlinkDir(source, target)
   }
-  assertPosixPath(includePath)
   return includePath
 }
 
 function isNpmName(str: string) {
+  if (str.includes('.')) {
+    return false
+  }
   if (str.includes('\\')) {
     return false
   }
@@ -71,6 +69,30 @@ function isNpmName(str: string) {
     return true
   }
   return false
+}
+
+function resolvePackage(pkgName: string, options: ResolveOptions) {
+  const pkgJsonPath = resolvePackageJson(pkgName, options)
+  const pkgRoot = path.posix.dirname(pkgJsonPath)
+  const pkgJson: { ['vite-plugin-ssr']?: { pageFilesDir?: string } } = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'))
+  return { pkgJson, pkgRoot }
+}
+function resolvePackageRoot(pkgName: string, options: ResolveOptions) {
+  const pkgJsonPath = resolvePackageJson(pkgName, options)
+  const pkgRoot = path.posix.dirname(pkgJsonPath)
+  return pkgRoot
+}
+type ResolveOptions = { preserveSymlinks: boolean; root: string }
+function resolvePackageJson(pkgName: string, { preserveSymlinks, root }: ResolveOptions) {
+  let pkgJsonPath: string
+  try {
+    // We cannot use Node.js's `require.resolve()`: https://stackoverflow.com/questions/10111163/in-node-js-how-can-i-get-the-path-of-a-module-i-have-loaded-via-require-that-is/63441056#63441056
+    pkgJsonPath = resolve.sync(`${pkgName}/package.json`, { preserveSymlinks, basedir: root })
+  } catch (_err) {
+    assertUsage(false, `Cannot find \`${pkgName}\`. Did you install it?`)
+  }
+  pkgJsonPath = toPosixPath(pkgJsonPath)
+  return pkgJsonPath
 }
 
 function getGlobPath(
