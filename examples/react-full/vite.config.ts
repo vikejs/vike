80c60cbdbccdb2d518bdb0ec3121fd45386575c3
Romuald Brillout <git@brillout.com>
2022-04-09 15:10:07 +0200

    update examples

--- examples/react-full/vite.config.ts
+++ examples/react-full/vite.config.ts
@@ -4,8 +4,13 @@ import ssr from 'vite-plugin-ssr/plugin'
 import { UserConfig } from 'vite'
 
 const config: UserConfig = {
-  plugins: [react(), mdx(), ssr()],
-  clearScreen: false,
+  plugins: [
+    react(),
+    mdx(),
+    ssr({
+      prerender: true,
+    }),
+  ],
   resolve: {
     alias: {
       // Needed for MDX, see https://github.com/mdx-js/mdx/discussions/1794#discussioncomment-1581513
