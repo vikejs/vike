4ca3e39f75da0482f2050f3ab73e7f649d1752dd
Romuald Brillout <git@brillout.com>
2022-04-09 20:47:35 +0200

    fix: support `$ vite dev` command

---
+++ vite-plugin-ssr/node/plugin/utils/isViteCliCall.ts
@@ -0,0 +1,20 @@
+import { toPosixPath } from '../utils'
+
+export { isViteCliCall }
+
+function isViteCliCall({ command, ssr }: { command: 'build' | 'dev'; ssr?: true }) {
+  const { argv } = process
+  if (!argv.some((a) => toPosixPath(a).endsWith('/bin/vite.js'))) {
+    return false
+  }
+  if (ssr && !argv.includes('--ssr')) {
+    return false
+  }
+  if (!argv.includes(command)) {
+    if (command === 'dev') {
+      return argv.includes('serve')
+    }
+    return false
+  }
+  return true
+}
