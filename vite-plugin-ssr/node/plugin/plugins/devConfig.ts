edb76214a7d141c1e2f2b5b5b84dbc1e5d4355b2
Romuald Brillout <git@brillout.com>
2022-04-10 12:46:22 +0200

    fix: add support for `$ vite preview` command

--- vite-plugin-ssr/node/plugin/plugins/devConfig.ts
+++ vite-plugin-ssr/node/plugin/plugins/devConfig.ts
@@ -1,52 +1,36 @@
 export { devConfig }
 
-import type { Plugin, ViteDevServer } from 'vite'
-import { renderPage } from '../../renderPage'
-import { apply, isViteCliCall } from '../utils'
+import type { Plugin } from 'vite'
+import { apply, addSsrMiddleware } from '../utils'
 
-function devConfig(): Plugin {
-  return {
-    name: 'vite-plugin-ssr:devConfig',
-    apply: apply('dev'),
-    config: () => ({
-      ssr: { external: ['vite-plugin-ssr'] },
-      optimizeDeps: {
-        entries: ['**/*.page.*([a-zA-Z0-9])', '**/*.page.client.*([a-zA-Z0-9])'],
-        exclude: [
-          // We exclude the client code to support `import.meta.glob()`
-          'vite-plugin-ssr/client',
-          'vite-plugin-ssr/client/router',
-          // We cannot add these to `optimizeDeps.include` because of `pnpm`
-          '@brillout/libassert',
-          '@brillout/json-s',
-          '@brillout/json-s/parse',
-          '@brillout/json-s/stringify',
-        ],
-      },
-    }),
-    configureServer(server) {
-      if (isViteCliCall({ command: 'dev' })) {
-        return addSsrMiddleware(server.middlewares)
-      }
+function devConfig(): Plugin[] {
+  return [
+    {
+      name: 'vite-plugin-ssr:dev:config',
+      apply: apply('dev'),
+      config: () => ({
+        ssr: { external: ['vite-plugin-ssr'] },
+        optimizeDeps: {
+          entries: ['**/*.page.*([a-zA-Z0-9])', '**/*.page.client.*([a-zA-Z0-9])'],
+          exclude: [
+            // We exclude the client code to support `import.meta.glob()`
+            'vite-plugin-ssr/client',
+            'vite-plugin-ssr/client/router',
+            // We cannot add these to `optimizeDeps.include` because of `pnpm`
+            '@brillout/libassert',
+            '@brillout/json-s',
+            '@brillout/json-s/parse',
+            '@brillout/json-s/stringify',
+          ],
+        },
+      }),
     },
-  }
-}
-
-type ConnectServer = ViteDevServer['middlewares']
-
-function addSsrMiddleware(middlewares: ConnectServer) {
-  return () => {
-    middlewares.use(async (req, res, next) => {
-      if (res.headersSent) return next()
-      const url = req.originalUrl || req.url
-      if (!url) return next()
-      const pageContextInit = { url }
-      const pageContext = await renderPage(pageContextInit)
-      if (!pageContext.httpResponse) return next()
-      const { body, statusCode, contentType } = pageContext.httpResponse
-      res.setHeader('Content-Type', contentType)
-      res.statusCode = statusCode
-      res.end(body)
-    })
-  }
+    {
+      name: 'vite-plugin-ssr:dev:ssr-middleware',
+      apply: apply('dev', { middlewareMode: false }),
+      configureServer(server) {
+        return addSsrMiddleware(server.middlewares)
+      },
+    },
+  ]
 }
--- vite-plugin-ssr/node/plugin/utils/apply.ts
+++ vite-plugin-ssr/node/plugin/utils/apply.ts
@@ -1,14 +1,19 @@
 export { apply }
 
+import type { UserConfig } from 'vite'
 import { assert } from '../utils'
 
-function apply(when: 'dev' | 'preview') {
-  return (_config: unknown, { command, mode }: { command: string; mode: string }): boolean => {
+function apply(when: 'dev' | 'preview', { middlewareMode }: { middlewareMode?: false } = {}) {
+  return (config: UserConfig, { command, mode }: { command: string; mode: string }): boolean => {
     assert(command)
     assert(mode)
     if (when === 'dev') {
+      if (middlewareMode === false && config?.server?.middlewareMode) {
+        return false
+      }
       return command === 'serve' && mode === 'development'
     }
+    assert(middlewareMode === undefined)
     if (when === 'preview') {
       return command === 'serve' && mode === 'production'
     }
