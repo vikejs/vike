5a46ddd04d33cbae9b2a189b1c8f786a862b5f86
Romuald Brillout <git@brillout.com>
2022-04-11 13:22:18 +0200

    fix outDir logic for pre-rendering

--- vite-plugin-ssr/utils/getOutDirs.ts
+++ vite-plugin-ssr/utils/getOutDirs.ts
@@ -3,11 +3,17 @@ import { assertPosixPath } from './filesystemPathHandling'
 
 export { getOutDirs }
 
-function getOutDirs(outDir: string) {
-  assert((outDir && outDir.endsWith('/client')) || outDir.endsWith('/server'), { outDir })
+function getOutDirs(outDir: string, { prerenderConfig }: { prerenderConfig?: true } = {}) {
   assertPosixPath(outDir)
-  assert('/client'.length === '/server'.length)
-  const outDirRoot = outDir.slice(0, -1 * '/client'.length)
+  let outDirRoot: string
+  if (prerenderConfig) {
+    assert(!outDir.endsWith('/client') && !outDir.endsWith('/server'), { outDir })
+    outDirRoot = outDir
+  } else {
+    assert(outDir.endsWith('/client') || outDir.endsWith('/server'), { outDir })
+    assert('/client'.length === '/server'.length)
+    outDirRoot = outDir.slice(0, -1 * '/client'.length)
+  }
   const outDirClient = `${outDirRoot}/client`
   const outDirServer = `${outDirRoot}/server`
   return { outDirRoot, outDirClient, outDirServer }
