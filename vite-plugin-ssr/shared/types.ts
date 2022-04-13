95fb5516cdfbe86df628c7395dff817281b55cd8
Romuald Brillout <git@brillout.com>
2022-04-13 19:57:01 +0200

    fix: update pageContext.urlParsed type

--- vite-plugin-ssr/shared/types.ts
+++ vite-plugin-ssr/shared/types.ts
@@ -1,13 +1,10 @@
+import type { PageContextUrls } from './addComputedUrlProps'
+
 export type PageContextBuiltIn = {
   Page: any
-  pageExports: Record<string, unknown>
   routeParams: Record<string, string>
   exports: Record<string, unknown>
   url: string
-  urlPathname: string
-  urlParsed: {
-    pathname: string
-    search: null | Record<string, string>
-    hash: null | string
-  }
-}
+  /** @deprecated */
+  pageExports: Record<string, unknown>
+} & PageContextUrls
