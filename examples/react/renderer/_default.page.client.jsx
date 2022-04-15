9f274e3cc3d786454d480552c3c8abff3b1d3c86
Romuald Brillout <git@brillout.com>
2022-04-15 20:03:40 +0200

    streaming - progressive SSR rendering & hydration [wip]

--- examples/react/renderer/_default.page.client.jsx
+++ examples/react/renderer/_default.page.client.jsx
@@ -1,15 +1,15 @@
 export { render }
 
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import React from 'react'
 import { PageLayout } from './PageLayout'
 
 async function render(pageContext) {
   const { Page, pageProps } = pageContext
-  ReactDOM.hydrate(
+  const root = ReactDOM.createRoot(document.getElementById('page-view'))
+  root.render(
     <PageLayout>
       <Page {...pageProps} />
-    </PageLayout>,
-    document.getElementById('page-view'),
+    </PageLayout>
   )
 }
