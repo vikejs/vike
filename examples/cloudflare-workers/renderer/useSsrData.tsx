3426af3e14c61d2e7fac76371e5c9d7f177d069b
Romuald Brillout <git@brillout.com>
2022-04-16 12:36:02 +0200

    streaming - progressive SSR rendering & hydration [wip]

--- examples/cloudflare-workers/renderer/useSsrData.tsx
+++ examples/cloudflare-workers/renderer/useSsrData.tsx
@@ -3,15 +3,34 @@ import React, { useContext } from 'react'
 export { SsrDataProvider }
 export { useSsrData }
 
-const Context = React.createContext<Record<string, unknown>>(undefined as any)
+const Ctx = React.createContext<Record<string, unknown>>(undefined as any)
 
 function SsrDataProvider({ children }: { children: React.ReactNode }) {
-  const ssrData = {}
-  return <Context.Provider value={ssrData}>{children}</Context.Provider>
+  const data = {}
+  return <Ctx.Provider value={data}>{children}</Ctx.Provider>
 }
 
-function useSsrData() {
-  const pageContext = useContext(Context)
-  return pageContext
+function useSsrData<T = unknown>() {
+  const data = useContext(Ctx)
+  return {
+    /*
+    has(key: string): boolean {
+      return key in data
+    },
+    */
+    get(key: string): T {
+      return data[key] as T
+    },
+    set(key: string, value: T) {
+      if (key in data) {
+        throw new Error('Cannot mutate SSR data')
+      }
+      data[key] = value
+    },
+    isClientSide: isClientSide(),
+  }
 }
 
+function isClientSide() {
+  return typeof window !== 'undefined' && typeof window?.getComputedStyle === 'function'
+}
