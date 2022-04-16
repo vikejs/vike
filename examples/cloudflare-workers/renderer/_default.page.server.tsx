d8282d9209f58ff961da11c534fa49e7b7e07222
Romuald Brillout <git@brillout.com>
2022-04-16 22:05:35 +0200

    streaming - progressive SSR rendering & hydration [wip]

---
+++ examples/cloudflare-workers/renderer/_default.page.server.tsx
@@ -0,0 +1,132 @@
+import React from 'react'
+import { escapeInject, pipeNodeStream } from 'vite-plugin-ssr'
+// @ts-expect-error
+import { renderToPipeableStream } from 'react-dom/server'
+import { PageLayout } from './PageLayout'
+import { ssrDataBuffer, SsrDataProvider } from './useSsrData'
+import { ErrorBoundary } from 'react-error-boundary'
+import { Writable } from 'stream'
+
+export { render }
+export { passToClient }
+
+// See https://vite-plugin-ssr.com/data-fetching
+const passToClient = ['pageProps']
+
+async function render(pageContext: any) {
+  const { Page, pageProps } = pageContext
+  /*
+    <ErrorBoundary FallbackComponent={Error}>
+    </ErrorBoundary>,
+   * */
+  const stream: any = await renderToStream(
+      <PageLayout>
+        <Page {...pageProps} />
+      </PageLayout>
+  )
+
+  return escapeInject`<!DOCTYPE html>
+    <html>
+      <body>
+        <div id="page-view">${pipeNodeStream(stream)}</div>
+      </body>
+    </html>`
+}
+
+function renderToStream(element: React.ReactNode) {
+  let reject
+  let resolve
+  let resolved = false
+  const promise = new Promise((resolve_, reject_) => {
+    resolve = () => {
+      if (resolved) return
+      resolved = true
+      resolve_(pipe)
+    }
+    reject = (err) => {
+      if (resolved) return
+      resolved = true
+      reject_(err)
+    }
+  })
+
+  // https://github.com/omrilotan/isbot
+  // https://github.com/mahovich/isbot-fast
+  // https://stackoverflow.com/questions/34647657/how-to-detect-web-crawlers-for-seo-using-express/68869738#68869738
+  const isBot = false
+  const seoStrategy: string = 'conservative'
+  // const seoStrategy = 'speed'
+
+  const onError = (err) => {
+    reject(err)
+  }
+
+  element = <SsrDataProvider>{element}</SsrDataProvider>
+
+  let { pipe }: any = renderToPipeableStream(element, {
+    bootstrapScriptContent: 'window.BOOT ? BOOT() : (window.LOADED = true)',
+    //bootstrapScripts: ['bootstrap-chunk.js'],
+    onAllReady() {
+      resolve()
+    },
+    onShellReady() {
+      if (!isBot || seoStrategy === 'speed') {
+        resolve()
+      }
+    },
+    onShellError: onError,
+    onError,
+  })
+
+  const onBeforeWrite = () => {
+    /*
+    const scriptInjections = ssrDataBuffer.map(({key, val}) => (
+    ))
+    */
+    if (ssrDataBuffer.length === 0) {
+      return null
+    }
+    const htmlInjection = `<script class="react-streaming_ssr-data" type="application/json">${JSON.stringify(
+      ssrDataBuffer,
+    )}</script>`
+    ssrDataBuffer.length = 0
+    return htmlInjection
+  }
+  pipe = getPipeWrapper(pipe, onBeforeWrite)
+  return promise
+}
+
+function getPipeWrapper(pipe: (writable: Writable) => void, onBeforeWrite: () => null | string) {
+  const pipeWrapper = (writable: Writable) => {
+    //const { Writable } = await loadStreamNodeModule()
+    const writableProxy = new Writable({
+      async write(chunk, ...rest) {
+        const onBeforeChunk = onBeforeWrite()
+        onBeforeChunk && writable.write(onBeforeChunk)
+        //chunks.forEach((chunk) => writable.write(chunk))
+        writable.write(chunk, ...rest)
+      },
+      async final(callback) {
+        writable.end()
+        callback()
+      },
+    })
+    ;(writableProxy as any).flush = () => {
+      if (typeof (writable as any).flush === 'function') {
+        ;(writable as any).flush()
+      }
+    }
+    pipe(writableProxy)
+  }
+  return pipeWrapper
+}
+
+function Error({ error }) {
+  return (
+    <div>
+      <h1>Application Error</h1>
+      <pre style={{ whiteSpace: 'pre-wrap' }}>{error.stack}</pre>
+    </div>
+  )
+}
+
