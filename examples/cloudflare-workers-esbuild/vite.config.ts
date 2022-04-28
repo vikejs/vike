feb7ca52d46699d1f5500b545e47d0531bddb6db
Romuald Brillout <git@brillout.com>
2022-04-28 18:57:36 +0200

    update examples/cloudflare-workers-esbuild

--- examples/cloudflare-workers-esbuild/vite.config.ts
+++ examples/cloudflare-workers-esbuild/vite.config.ts
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
+import type { UserConfig } from 'vite'
 
 export default {
   plugins: [react(), ssr()],
   resolve: {
     // Only needed for this example
+    // TODO: check if still needed
     preserveSymlinks: true,
   },
-}
+} as UserConfig
