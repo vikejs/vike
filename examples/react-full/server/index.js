dfb83cfd7755d9102135706c81fc8f6f0692bf52
Romuald Brillout <git@brillout.com>
2022-04-28 18:57:07 +0200

    update examples/react-full

---
+++ examples/react-full/server/index.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import vite from 'vite'
+import { renderPage } from 'vite-plugin-ssr'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const isProduction = process.env.NODE_ENV === 'production'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const root = `${__dirname}/..`
+
+startServer()
+
+async function startServer() {
+  const app = express()
+
+  let viteDevServer
+  if (isProduction) {
+    app.use(express.static(`${root}/dist/client`))
+  } else {
+    viteDevServer = await vite.createServer({
+      root,
+      server: { middlewareMode: 'ssr' },
+    })
+    app.use(viteDevServer.middlewares)
+  }
+
+  app.get('*', async (req, res, next) => {
+    const url = req.originalUrl
+    const pageContextInit = {
+      url,
+    }
+    const pageContext = await renderPage(pageContextInit)
+    const { httpResponse } = pageContext
+    if (!httpResponse) return next()
+    const stream = await httpResponse.getNodeStream()
+    const { statusCode, contentType } = httpResponse
+    res.status(statusCode).type(contentType)
+    stream.pipe(res)
+  })
+
+  const port = 3000
+  app.listen(port)
+  console.log(`Server running at http://localhost:${port}`)
+}
