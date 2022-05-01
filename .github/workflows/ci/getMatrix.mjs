cf9d8b88295056862b593623b7f079704951b8df
Romuald Brillout <git@brillout.com>
2022-05-01 13:42:01 +0200

    impl SINGLE_TEST

--- .github/workflows/ci/getMatrix.mjs
+++ .github/workflows/ci/getMatrix.mjs
@@ -1,41 +1,73 @@
-import { execSync } from 'child_process'
+import { cmd } from './utils.mjs'
 
 // https://jestjs.io/docs/configuration#testregex-string--arraystring
 const testRE = /\/__tests__\/.*|(\.|\/)(test|spec)\.[jt]sx?$/
-
-function cmd(command, { cwd, result }) {
-  let stdout = execSync(command, { encoding: 'utf8', cwd, shell: true })
-  const words = stdout.split(/\s/).filter(Boolean)
-  if (result === 'single') {
-    return words[0]
-  }
-  if (result === 'many') {
-    return words
-  }
-}
 
 const root = cmd('git rev-parse --show-toplevel', { result: 'single' })
 const projectFiles = cmd(`git ls-files`, { cwd: root, result: 'many' })
 const testFiles = projectFiles.filter((file) => testRE.test(file))
 
-const matrix = [
+const setups = [
   {
-    name: 't1',
-    SINGLE_TEST: 'examples/vuex/.test.spec.ts',
     os: 'ubuntu-latest',
     node_version: '16',
   },
   {
-    name: 't2',
+    os: 'macos-latest',
+    node_version: '17',
+  },
+  {
+    os: 'windows-latest',
+    node_version: '14',
+  },
+]
+const setupFast = {
+  os: 'ubuntu-latest',
+  node_version: '18',
+}
+
+const matrix = [
+  {
+    setupName: 'TypeScript Types',
     SINGLE_TEST: 'TYPESCRIPT',
-    os: 'ubuntu-latest',
-    node_version: '18',
+    ...setupFast,
   },
 ]
 
+testFiles.forEach((testFile) => {
+  setups.forEach((setup) => {
+    const { os, node_version } = setup
+    const setupName = getSetupName(setup)
+    const name = testFile + ' ' + setupName
+    matrix.push({
+      name,
+      SINGLE_TEST: testFile,
+      os,
+      node_version,
+    })
+  })
+})
+
 /*
-console.log(testFiles)
-testFiles.forEach((testFile) => {})
+console.log(JSON.stringify(matrix, null, 2))
+console.log(matrix.length)
+/*/
+console.log(`{"include":${JSON.stringify(matrix)}}`)
 //*/
 
-console.log(`{"include":${JSON.stringify(matrix)}}`)
+function getSetupName(setup) {
+  const { os, node_version } = setup
+  let osName
+  if (os === 'ubuntu-latest') {
+    osName = 'Unix'
+  }
+  if (os === 'macos-latest') {
+    osName = 'Mac'
+  }
+  if (os === 'windows-latest') {
+    osName = 'Win'
+  }
+  const nodeName = 'Node ' + node_version
+  const setupName = `${osName} ${nodeName}`
+  return setupName
+}
