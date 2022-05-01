bc0b4a1e41b58aeb34cdf573bf9940a6a4e6a6db
Romuald Brillout <git@brillout.com>
2022-05-01 12:44:48 +0200

    SINGLE_TEST - wip

--- .github/workflows/ci/getMatrix.mjs
+++ .github/workflows/ci/getMatrix.mjs
@@ -6,23 +6,11 @@ const testRE = /\/__tests__\/.*|(\.|\/)(test|spec)\.[jt]sx?$/
 const projectFiles = (await quiet($`git ls-files $(git rev-parse --show-toplevel)`)).stdout.split(/\s/).filter(Boolean)
 const testFiles = projectFiles.filter(file => testRE.test(file))
 
-/*
-console.log(testFiles)
-// Jest default regex pattern: https://jestjs.io/docs/configuration#testregex-string--arraystring
-// (/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$
-
-/*
-let tests = (await quiet($`ls examples/`)).stdout.split(/\s/).filter(Boolean)
-
-const matrix = tests.map(test => {
-  return { project: test, config: 'bla2' }
-})
-*/
 
 const matrix = [
   {
     name: 't1',
-    SINGLE_TEST: './examples/vuex/.test.spec.ts',
+    SINGLE_TEST: 'examples/vuex/.test.spec.ts',
     os: 'ubuntu-latest',
     node_version: '16'
   },
@@ -33,5 +21,11 @@ const matrix = [
     node_version: '18'
   }
 ]
+
+/*
+console.log(testFiles)
+testFiles.forEach(testFile => {
+})
+*/
 
 console.log(`{"include":${JSON.stringify(matrix)}}`)
