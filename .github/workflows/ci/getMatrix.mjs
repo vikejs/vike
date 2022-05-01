2c2543bb5082c78dbd7a2e8e3692f3784bf8c739
Romuald Brillout <git@brillout.com>
2022-05-01 12:40:20 +0200

    SINGLE_TEST - wip

--- .github/workflows/ci/getMatrix.mjs
+++ .github/workflows/ci/getMatrix.mjs
@@ -1,11 +1,13 @@
 #!/usr/bin/env zx
 
+// https://jestjs.io/docs/configuration#testregex-string--arraystring
+const testRE = /\/__tests__\/.*|(\.|\/)(test|spec)\.[jt]sx?$/
+
+const projectFiles = (await quiet($`git ls-files $(git rev-parse --show-toplevel)`)).stdout.split(/\s/).filter(Boolean)
+const testFiles = projectFiles.filter(file => testRE.test(file))
+
 /*
-const projectFiles = (await quiet($`ls examples/`)).stdout.split(/\s/).filter(Boolean)
-console.log(projectFiles)
-
-const testFiles = projectFiles.filte
-
+console.log(testFiles)
 // Jest default regex pattern: https://jestjs.io/docs/configuration#testregex-string--arraystring
 // (/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$
 
@@ -19,11 +21,13 @@ const matrix = tests.map(test => {
 
 const matrix = [
   {
+    name: 't1',
     SINGLE_TEST: './examples/vuex/.test.spec.ts',
     os: 'ubuntu-latest',
     node_version: '16'
   },
   {
+    name: 't2',
     SINGLE_TEST: 'TYPESCRIPT',
     os: 'ubuntu-latest',
     node_version: '18'
