2b6cb3520084adbb9d7c405da0cf7938bc493646
Romuald Brillout <git@brillout.com>
2022-05-02 14:02:03 +0200

    remove vite preview CLI usage for examples/react

--- examples/react/vite.config.js
+++ examples/react/vite.config.js
@@ -3,15 +3,4 @@ import ssr from 'vite-plugin-ssr/plugin'
 
 export default {
   plugins: [react(), ssr()],
-  ...serverConfig(),
-}
-
-function serverConfig() {
-  const port = 3000
-  // Needed for vite-plugin-ssr's CI
-  const host = process.env.CI && process.platform === 'darwin'
-  return {
-    preview: { port, host },
-    server: { port, host },
-  }
 }
