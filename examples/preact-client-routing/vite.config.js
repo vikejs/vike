9bdf61bcdd3180550cfd923c5d0d3c0ae247b12f
Romuald Brillout <git@brillout.com>
2022-05-02 16:25:22 +0200

    remove vite pnpm override & bump lock file

--- examples/preact-client-routing/vite.config.js
+++ examples/preact-client-routing/vite.config.js
@@ -4,7 +4,7 @@ import ssr from 'vite-plugin-ssr/plugin'
 const config = {
   plugins: [preact(), ssr()],
   optimizeDeps: {
-    include: ['preact/devtools', 'preact/debug'],
+    include: ['preact/devtools', 'preact/debug', 'preact/jsx-dev-runtime'],
   },
 }
 
