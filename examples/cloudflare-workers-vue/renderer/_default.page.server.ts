b4cfa4c5132a15095ca1f87e58a3535d57ff11b6
Romuald Brillout <git@brillout.com>
2022-05-06 16:55:42 +0200

    update cf vue example

--- examples/cloudflare-workers-vue/renderer/_default.page.server.ts
+++ examples/cloudflare-workers-vue/renderer/_default.page.server.ts
@@ -1,7 +1,8 @@
 import { pipeToWebWritable, pipeToNodeWritable } from '@vue/server-renderer'
 
-import { escapeInject, pipeWebStream, pipeNodeStream } from 'vite-plugin-ssr'
+import { escapeInject, stampStreamPipe } from 'vite-plugin-ssr'
 import { createApp } from './app'
+import type { Writable } from 'stream'
 
 export { render }
 export { passToClient }
@@ -9,19 +10,20 @@ export { passToClient }
 // See https://vite-plugin-ssr.com/data-fetching
 const passToClient = ['pageProps']
 
-async function render(pageContext) {
+async function render(pageContext: any) {
   const app = createApp(pageContext)
 
   // While developing, we use Express.js instead of Cloudflare Workers.
   const pipe = isWorker()
-    ? pipeWebStream((writable) => {
+    ? (writable: WritableStream) => {
         pipeToWebWritable(app, {}, writable)
-      })
+      }
     : // We don't really need to use a stream for dev, but we do it for fun's sake :-).
       // For real apps, we should use `renderToString` instead of `pipeToNodeWritable` in dev.
-      pipeNodeStream((writable) => {
+      (writable: Writable) => {
         pipeToNodeWritable(app, {}, writable)
-      })
+      }
+  stampStreamPipe(pipe, { pipeType: isWorker() ? 'web' : 'node' })
 
   return escapeInject`<!DOCTYPE html>
     <html>
@@ -32,7 +34,8 @@ async function render(pageContext) {
 }
 function isWorker() {
   return (
-    // `IS_CLOUDFLARE_WORKER` is set by `webpack.config.js`
+    // `IS_CLOUDFLARE_WORKER` is set by `build-worker`
+    // @ts-ignore
     typeof IS_CLOUDFLARE_WORKER !== 'undefined' && IS_CLOUDFLARE_WORKER === true
   )
 }
