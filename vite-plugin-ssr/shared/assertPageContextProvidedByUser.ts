87f1a8a03071b4ff440f7d0c816bade05435f6f7
Romuald Brillout <git@brillout.com>
2022-05-09 10:02:00 +0200

    typo

--- vite-plugin-ssr/shared/assertPageContextProvidedByUser.ts
+++ vite-plugin-ssr/shared/assertPageContextProvidedByUser.ts
@@ -26,7 +26,11 @@ function assertPageContextProvidedByUser(
     !('_objectCreatedByVitePluginSsr' in pageContextProvidedByUser),
     `${errorMessagePrefix} should not be the whole \`pageContext\` object, see https://vite-plugin-ssr.com/pageContext-manipulation#do-not-return-entire-pagecontext`,
   )
-  // In principle, it's possible to use `onBeforeRoute()` to overwrite and define the whole routing.
-  // But, I don't know whether that's a thing we want to allow. Beyond deep integration with Vue Router or React Router, is there a use case for this?
-  assertWarning(!('_pageId' in pageContextProvidedByUser), "You are using `onBeforeRoute()` to overwrite the vite-plugin-ssr routing. This is experimental: make sure to contact a vite-plugin-ssr maintainer before using this.", { onlyOnce: true })
+  // In principle, it's possible to use `onBeforeRoute()` to override and define the whole routing.
+  // Is that a good idea to allow users to do this? Beyond deep integration with Vue Router or React Router, is there a use case for this?
+  assertWarning(
+    !('_pageId' in pageContextProvidedByUser),
+    'You are using `onBeforeRoute()` to override vite-plugin-ssr routing. This is experimental: make sure to contact a vite-plugin-ssr maintainer before using this.',
+    { onlyOnce: true },
+  )
 }
