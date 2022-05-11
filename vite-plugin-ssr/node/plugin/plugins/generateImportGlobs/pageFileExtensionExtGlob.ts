2b061f86efe5877a89c22d6a55a04c4b7cd967fe
Romuald Brillout <git@brillout.com>
2022-05-11 15:39:12 +0200

    add md and mdx to file extension whitelist

--- vite-plugin-ssr/node/plugin/plugins/generateImportGlobs/pageFileExtensionExtGlob.ts
+++ vite-plugin-ssr/node/plugin/plugins/generateImportGlobs/pageFileExtensionExtGlob.ts
@@ -1,3 +1,6 @@
 // Vite uses `fast-glob` which resolves globs with micromatch: https://github.com/micromatch/micromatch
 // Pattern is an Extglob: https://github.com/micromatch/micromatch#extglobs
-export const pageFileExtensionExtGlob = '(js|ts|jsx|tsx|cjs|cts|cjsx|ctsx|mjs|mts|mjsx|mtsx|vue|svelte|marko)'
+// We need this to be a whitelist because:
+//   - A pattern `*([a-zA-Z0-9]` doesn't work for ReScript (`.res` are ReScript source files; the ReScript compiler generates `.js` files alongside `.res` files.)
+//   - A post `import.meta.glob()` blacklist filtering doesn't work because Vite would still process the files (e.g. including them in the bundle).
+export const pageFileExtensionExtGlob = '(js|ts|jsx|tsx|cjs|cts|cjsx|ctsx|mjs|mts|mjsx|mtsx|vue|svelte|marko|md|mdx)'
