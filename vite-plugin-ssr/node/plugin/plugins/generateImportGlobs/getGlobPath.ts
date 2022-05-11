a040fe7e56ea34ab2aee2951d91e626abd870bcf
Romuald Brillout <git@brillout.com>
2022-05-11 15:24:57 +0200

    fix: add support for ReScript

--- vite-plugin-ssr/node/plugin/plugins/generateImportGlobs/getGlobPath.ts
+++ vite-plugin-ssr/node/plugin/plugins/generateImportGlobs/getGlobPath.ts
@@ -1,18 +1,16 @@
+export { getGlobPath }
+
 import { assertPosixPath, toPosixPath } from '../../utils'
 import path from 'path'
-
-export { getGlobPath }
+import { pageFileExtensionExtGlob } from './pageFileExtensionExtGlob'
 
 function getGlobPath(
   globRoot: string,
   fileSuffix: 'page' | 'page.client' | 'page.server' | 'page.route',
   root?: string,
 ): string {
-  // Vite uses `fast-glob` which resolves globs with micromatch: https://github.com/micromatch/micromatch
-  // Pattern `*([a-zA-Z0-9])` is an Extglob: https://github.com/micromatch/micromatch#extglobs
-  const fileExtention = '*([a-zA-Z0-9])'
   assertPosixPath(globRoot)
-  let globPath = [...globRoot.split('/'), '**', `*.${fileSuffix}.${fileExtention}`].filter(Boolean).join('/')
+  let globPath = [...globRoot.split('/'), '**', `*.${fileSuffix}.${pageFileExtensionExtGlob}`].filter(Boolean).join('/')
   if (root) {
     globPath = toPosixPath(path.posix.join(root, globPath))
   } else {
---
+++ vite-plugin-ssr/node/plugin/plugins/generateImportGlobs/pageFileExtensionExtGlob.ts
@@ -0,0 +1,3 @@
+// Vite uses `fast-glob` which resolves globs with micromatch: https://github.com/micromatch/micromatch
+// Pattern is an Extglob: https://github.com/micromatch/micromatch#extglobs
+export const pageFileExtensionExtGlob = '(js|ts|jsx|tsx|cjs|cts|cjsx|ctsx|mjs|mts|mjsx|mtsx|vue|svelte|marko)'
