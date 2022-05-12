0b993ca7fc6b8210a3d43d0567d7ab947d414c1b
Romuald Brillout <git@brillout.com>
2022-05-12 16:21:23 +0200

    support process.env.* static replacing

--- vite-plugin-ssr/utils/getEnv.ts
+++
@@ -1,8 +0,0 @@
-export { getEnv }
-
-function getEnv(envName: string): undefined | string {
-  if (typeof process === 'undefined' || typeof process.env === 'undefined') {
-    return undefined
-  }
-  return process.env[envName]
-}
