20b55589a9bdfdd868c2922d6de7b8aeae602104
Romuald Brillout <git@brillout.com>
2022-05-16 18:11:45 +0200

    update getFileUrl()

--- vite-plugin-ssr/utils/getFileUrl.ts
+++ vite-plugin-ssr/utils/getFileUrl.ts
@@ -1,10 +1,14 @@
+export { getFileUrl }
+export { handlePageContextRequestSuffix }
+
 import { parseUrl } from './parseUrl'
 import { assert } from './assert'
 import { slice } from './slice'
-const pageContextUrlSuffix = '.pageContext.json'
+const suffix = '.pageContext.json'
 
-export { getFileUrl }
-export { handlePageContextRequestSuffix }
+// - When doing a `.pageContext.json` HTTP request, the base URL should be preserved. (The server-side will handle the base URL.)
+// - While prerendering there is no base URL
+const baseUrl = '/'
 
 /**
  (`/`, `.html`) -> `/index.html`
@@ -21,12 +25,12 @@ function getFileUrl(
   doNotCreateExtraDirectory: boolean,
 ): string {
   assert(fileExtension !== '.pageContext.json' || doNotCreateExtraDirectory === true)
-  const { pathnameWithoutBaseUrl, searchString, hashString } = parseUrl(url, '/') // is Base URL missing?
+  const { pathnameWithBaseUrl, searchString, hashString } = parseUrl(url, baseUrl)
   if (url.startsWith('/')) {
-    assert(url === `${pathnameWithoutBaseUrl}${searchString || ''}${hashString || ''}`, { url })
+    assert(url === `${pathnameWithBaseUrl}${searchString || ''}${hashString || ''}`, { url })
   }
 
-  let pathnameModified = pathnameWithoutBaseUrl
+  let pathnameModified = pathnameWithBaseUrl
   if (doNotCreateExtraDirectory) {
     if (pathnameModified.endsWith('/')) {
       pathnameModified = slice(pathnameModified, 0, -1)
@@ -36,7 +40,7 @@ function getFileUrl(
       pathnameModified = '/index'
     }
   } else {
-    const trailingSlash = pathnameWithoutBaseUrl.endsWith('/') ? '' : '/'
+    const trailingSlash = pathnameWithBaseUrl.endsWith('/') ? '' : '/'
     pathnameModified = pathnameModified + `${trailingSlash}index`
   }
 
@@ -48,27 +52,26 @@ function handlePageContextRequestSuffix(url: string): {
   urlWithoutPageContextRequestSuffix: string
   isPageContextRequest: boolean
 } {
-  const pathname = parseUrl(url, '/').pathnameWithoutBaseUrl // is Base URL missing?
-  if (!pathname.endsWith(pageContextUrlSuffix)) {
+  if (!hasSuffix(url)) {
     return { urlWithoutPageContextRequestSuffix: url, isPageContextRequest: false }
   }
   return { urlWithoutPageContextRequestSuffix: removePageContextUrlSuffix(url), isPageContextRequest: true }
 }
 
+function hasSuffix(url: string) {
+  const { pathnameWithBaseUrl, pathnameWithoutBaseUrl } = parseUrl(url, baseUrl)
+  assert(pathnameWithBaseUrl.endsWith(suffix) === pathnameWithoutBaseUrl.endsWith(suffix), { url })
+  return pathnameWithBaseUrl.endsWith(suffix)
+}
+
 function removePageContextUrlSuffix(url: string): string {
-  const urlParsed = parseUrl(url, '/') // is Base URL missing?
+  const urlParsed = parseUrl(url, baseUrl)
   const { origin, searchString, hashString } = urlParsed
-  let pathname = urlParsed.pathnameWithoutBaseUrl
-  // TODO:
-  //  - Re-ensure the uncommented assert, and this time directly in `parseUrl()`
-  //    - (It's already there but post `decodeURI()`)
-  //  - Use `decodeURI` only for `urlParsed.search`, not for `urlParsed.searchString`
-  //  - Use `decodeURI` at `renderPage()`
-  //    - It seems like the browser calls `encodeURI()`, which we should decode ASAP, i.e. at the beginning of `renderPage()`
-  //       - E.g. `navigate('/foo?key=あ')`
-  // assert(url === `${origin || ''}${pathname}${searchString || ''}${hashString || ''}`, { url })
-  assert(pathname.endsWith(pageContextUrlSuffix), { url })
-  pathname = slice(pathname, 0, -1 * pageContextUrlSuffix.length)
+  // We cannot use `urlParsed.pathnameWithoutBaseUrl` because it would break the `urlParsed.pathnameWithBaseUrl` value of subsequent `parseUrl()` calls.
+  let pathname = urlParsed.pathnameWithBaseUrl
+  assert(pathname.endsWith(suffix))
+  assert(url === `${origin || ''}${pathname}${searchString || ''}${hashString || ''}`, { url })
+  pathname = slice(pathname, 0, -1 * suffix.length)
   if (pathname === '/index') pathname = '/'
   return `${origin || ''}${pathname}${searchString || ''}${hashString || ''}`
 }
