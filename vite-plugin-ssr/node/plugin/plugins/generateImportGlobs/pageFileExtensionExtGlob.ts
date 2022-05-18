2bac6089558ed77f9806aa17b776a65ceb3cb32a
Romuald Brillout <git@brillout.com>
2022-05-18 12:27:57 +0200

    minor

--- vite-plugin-ssr/node/plugin/plugins/generateImportGlobs/pageFileExtensionExtGlob.ts
+++ vite-plugin-ssr/node/plugin/plugins/generateImportGlobs/pageFileExtensionExtGlob.ts
@@ -1,4 +1,4 @@
-// Vite uses `fast-glob` which resolves globs with micromatch: https://github.com/micromatch/micromatch
+// Vite uses micromatch: https://github.com/micromatch/micromatch
 // Pattern is an Extglob: https://github.com/micromatch/micromatch#extglobs
 // We need this to be a whitelist because:
 //   - A pattern `*([a-zA-Z0-9]` doesn't work for ReScript (`.res` are ReScript source files; the ReScript compiler generates `.js` files alongside `.res` files.)
