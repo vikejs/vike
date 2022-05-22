976f313bf1550ba73c70d05b1645d9900c4b4e6b
Romuald Brillout <git@brillout.com>
2022-05-22 15:32:42 +0200

    override Vite's default port

--- vite-plugin-ssr/node/plugin/plugins/commonConfig.ts
+++ vite-plugin-ssr/node/plugin/plugins/commonConfig.ts
@@ -1,10 +1,31 @@
 export { commonConfig }
 
-import type { Plugin } from 'vite'
+import type { Plugin, ResolvedConfig } from 'vite'
 
 function commonConfig(): Plugin {
   return {
     name: 'vite-plugin-ssr:commonConfig',
     config: () => ({ spa: false }),
+    configResolved(config) {
+      setDefaultPort(config)
+      workaroundCI(config)
+    },
+  }
+}
+
+function setDefaultPort(config: ResolvedConfig) {
+  // @ts-ignore
+  config.server ??= {}
+  config.server.port ??= 3000
+  // @ts-ignore
+  config.preview ??= {}
+  config.preview.port ??= 3000
+}
+
+// Workaround GitHub Action running on MacOS failing to access the server
+function workaroundCI(config: ResolvedConfig) {
+  if (process.env.CI && process.platform === 'darwin') {
+    config.server.host ??= true
+    config.preview.host ??= true
   }
 }
