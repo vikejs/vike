4598bef4e29829b0f2bf57951e788959c437250d
Romuald Brillout <git@brillout.com>
2022-05-24 12:27:29 +0200

    typo

--- examples/react-full/pages/hello/index.page.route.ts
+++ examples/react-full/pages/hello/index.page.route.ts
@@ -1,6 +1,6 @@
 import type { PageContextBuiltIn } from 'vite-plugin-ssr'
 
-// Route Functions give us full flexibility
+// Route Functions give us full flexibility.
 // This is a route similar to `/hello/:name` but with details impossible to achieve with a route string.
 export default (pageContext: PageContextBuiltIn) => {
   const { url } = pageContext
