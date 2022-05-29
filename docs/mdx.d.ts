6e6641398570476f8b45fc43558aa3dc47d722da
Romuald Brillout <git@brillout.com>
2022-05-29 18:31:19 +0200

    bump vikepress

--- docs/mdx.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.mdx' {
-  import { FunctionComponent } from 'react'
-  const value: FunctionComponent
-  export default value
-  export const headings: { level: number; title: string; id: string; titleAddendum?: string }[]
-}
