d497c427d0aae4fc83520648e0d2d5f77d51a75d
Romuald Brillout <git@brillout.com>
2022-06-02 14:04:14 +0200

    docs: fix link

--- docs/headings.tsx
+++ docs/headings.tsx
@@ -498,7 +498,7 @@ const headings: HeadingDefinition[] = [
     level: 2,
     title: 'Cloudflare Workers',
     url: '/cloudflare-workers',
-    sectionTitles: ['Cloudflare Pages & wrangler 2'],
+    sectionTitles: ['Cloudflare Pages'],
   },
   {
     level: 2,
