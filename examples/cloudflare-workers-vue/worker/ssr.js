4bc78059332a3c3b8e68544aeb0b7cf73b0667ec
Romuald Brillout <git@brillout.com>
2022-06-07 15:40:04 +0200

    remove unused importBuild

--- examples/cloudflare-workers-vue/worker/ssr.js
+++ examples/cloudflare-workers-vue/worker/ssr.js
@@ -1,6 +1,4 @@
 import { createPageRenderer } from 'vite-plugin-ssr'
-// `importBuild.js` enables us to bundle our worker code into a single file, see https://vite-plugin-ssr.com/cloudflare-workers and https://vite-plugin-ssr.com/importBuild.js
-import '../dist/server/importBuild.js'
 
 export { handleSsr }
 
