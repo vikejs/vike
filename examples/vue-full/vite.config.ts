12b7e26a7f21c908859894e7576a4e711032ad83
Romuald Brillout <git@brillout.com>
2022-06-07 16:14:04 +0200

    add cross-fetch to optimizeDeps

--- examples/vue-full/vite.config.ts
+++ examples/vue-full/vite.config.ts
@@ -12,6 +12,7 @@ const config: UserConfig = {
     ssr(),
   ],
   clearScreen: false,
+  optimizeDeps: { include: ['cross-fetch'] },
   // Neeed if using an ESM-only library. This is not the case of this example and it's, in general, a rare case. But such situation will increasingly occur as ESM-only libraries emerge.
   build: {
     rollupOptions: {
