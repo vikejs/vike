5439510e16fdb181fbcb5584c328f6b48da95c1e
Romuald Brillout <git@brillout.com>
2022-06-08 09:46:50 +0200

    fix preact dev test

--- examples/preact-client-routing/vite.config.js
+++ examples/preact-client-routing/vite.config.js
@@ -4,7 +4,7 @@ import ssr from 'vite-plugin-ssr/plugin'
 const config = {
   plugins: [preact(), ssr()],
   optimizeDeps: {
-    include: ['preact/devtools', 'preact/debug', 'preact/jsx-dev-runtime'],
+    include: ['preact', 'preact/devtools', 'preact/debug', 'preact/jsx-dev-runtime', 'preact/hooks'],
   },
 }
 
