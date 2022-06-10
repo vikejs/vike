fcf8b5cd399c9ffc904f4178d7b09b44375d0c11
Romuald Brillout <git@brillout.com>
2022-06-10 14:45:56 +0200

    add docs test

--- docs/.testRun.ts
+++ docs/.testRun.ts
@@ -30,4 +30,43 @@ function testRun(cmd: 'pnpm run dev' | 'pnpm run preview') {
     await page.locator('h2:has-text("Control")').click()
     await page.waitForSelector(selector, { state: 'hidden' })
   })
+
+  test('Layout', async () => {
+    const layout = await page.evaluate(() => {
+      return {
+        html: getWidths(document.documentElement),
+        body: getWidths(document.body),
+        page: getWidths(document.querySelector('#page-view')),
+        left: getWidths(document.querySelector('#navigation-wrapper')),
+        right: getWidths(document.querySelector('#page-wrapper')),
+      }
+      function getWidths(elem: Element | null): Widths {
+        if (!elem) throw new Error('Elem missing')
+        return {
+          clientWidth: elem.clientWidth,
+          scrollWidth: elem.scrollWidth,
+        }
+      }
+    })
+
+    // Default viewport size: 1280x720
+    //  - https://playwright.dev/docs/api/class-testoptions#test-options-viewport
+    testWidth(layout.html, 1280)
+    testWidth(layout.body, 1280)
+    testWidth(layout.page, 1280)
+    testWidth(layout.left, 300)
+    testWidth(layout.right, 980)
+
+    return
+
+    type Widths = {
+      clientWidth: number
+      scrollWidth: number
+    }
+
+    function testWidth(widths: Widths, width: number) {
+      expect(widths.clientWidth).toBe(width)
+      expect(widths.scrollWidth).toBe(width)
+    }
+  })
 }
