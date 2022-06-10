15711e676bbe5edefa79940e870eb87ba8a43c98
Romuald Brillout <git@brillout.com>
2022-06-10 10:19:50 +0200

    add docs test

--- docs/.testRun.ts
+++ docs/.testRun.ts
@@ -1,6 +1,6 @@
 export { testRun }
 
-import { run, fetchHtml } from '../libframe/test/setup'
+import { page, run, fetchHtml, partRegex, urlBase } from '../libframe/test/setup'
 
 function testRun(cmd: 'pnpm run dev' | 'pnpm run preview') {
   run(cmd)
@@ -16,5 +16,18 @@ function testRun(cmd: 'pnpm run dev' | 'pnpm run preview') {
       '<meta name="description" content="Like Next.js / Nuxt but as do-one-thing-do-it-well Vite plugin." />',
     )
     expect(html).toContain('integrate tools manually')
+    expect(html).toMatch(partRegex`<h2>${/[^\/]+/}Control</h2>`)
+    expect(html).toContain('<h2>🔧<!-- --> Control</h2>')
+  })
+
+  test('Learn more collapsible', async () => {
+    await page.goto(urlBase + '/')
+    const text = 'you keep control over how your pages are rendered'
+    const selector = `p:has-text("${text}")`
+    await page.waitForSelector(selector, { state: 'hidden' })
+    await page.locator('h2:has-text("Control")').click()
+    await page.waitForSelector(selector, { state: 'visible' })
+    await page.locator('h2:has-text("Control")').click()
+    await page.waitForSelector(selector, { state: 'hidden' })
   })
 }
