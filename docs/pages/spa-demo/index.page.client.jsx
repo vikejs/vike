209e78364aafc6d5ccbabbb708d4b2fd9bcde926
Romuald Brillout <git@brillout.com>
2022-06-10 20:14:20 +0200

    docs: remove render-modes demo

--- docs/pages/spa-demo/index.page.client.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-import { Page } from './index.page'
-
-hydrate()
-
-async function hydrate() {
-  ReactDOM.render(<Page />, document.getElementById('page-view'))
-}
