77a3d25d6645766a551019bce00d8d77138a27d8
Romuald Brillout <git@brillout.com>
2022-06-15 17:45:57 +0200

    add examples/layouts-react

---
+++ examples/layouts-react/pages/about.page.jsx
@@ -0,0 +1,15 @@
+export { Page }
+
+function Page() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        <b>
+          This page uses the default layout <code>/renderer/LayouDefault.jsx</code>.
+        </b>
+      </p>
+      <p>This app showcases (nested) layouts.</p>
+    </>
+  )
+}
---
+++ examples/layouts-react/readme.md
@@ -0,0 +1,10 @@
+Example of defining (nested) layouts.
+
+To run it:
+
+```bash
+git clone git@github.com:brillout/vite-plugin-ssr
+cd vite-plugin-ssr/examples/layouts-react/
+npm install
+npm run dev
+```
