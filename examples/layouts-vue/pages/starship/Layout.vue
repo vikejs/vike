3ec05312ef82c86c87158eebb3374892b0c5f7ed
Romuald Brillout <git@brillout.com>
2022-06-17 17:43:22 +0200

    improve examples/layouts-vue

--- examples/layouts-vue/pages/starship/Layout.vue
+++ examples/layouts-vue/pages/starship/Layout.vue
@@ -1,31 +1,14 @@
 <template>
   <LayoutDefault>
-    <h1>Starship 🚀</h1>
-    <p>
-      <b>
-        This page uses the default layout <code>/renderer/LayouDefault.jsx</code>.
-        <br />
-        With the nested layout <code>/pages/starship/Layout.jsx</code>.
-      </b>
-    </p>
-    <p>Outer layout is preserved during nested layout navigation: <Counter /></p>
-    <div class="sub-navigation">
-      <a href="/starship/">Overview</a> <a href="/starship/reviews">Reviews</a>
-      <a href="/starship/spec">Tech Spec</a>
-    </div>
-    <div style="margin-top: 20px"><slot /></div>
+    <PageOuter>
+      <slot />
+    </PageOuter>
   </LayoutDefault>
 </template>
 
 <script>
-import Counter from './Counter.vue'
 import LayoutDefault from '../../renderer/LayoutDefault.vue'
-const components = { Counter, LayoutDefault }
+import PageOuter from './PageOuter.vue'
+const components = { PageOuter, LayoutDefault }
 export default { components }
 </script>
-
-<style>
-.sub-navigation a {
-  margin-right: 10px;
-}
-</style>
---
+++ examples/layouts-vue/pages/starship/PageOuter.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>Starship 🚀</h1>
+  <p>
+    <b>
+      This page uses the default layout <code>/renderer/LayouDefault.vue</code>.
+      <br />
+      With the nested layout <code>/pages/starship/Layout.vue</code>.
+    </b>
+  </p>
+  <p>
+    <b> Outer layout is preserved when navigating the nested layout: <Counter /> </b>
+  </p>
+  <br />
+  <div class="sub-navigation">
+    <a keep-scroll-position href="/starship">Overview</a> <a keep-scroll-position href="/starship/reviews">Reviews</a>
+    <a keep-scroll-position href="/starship/spec">Tech Spec</a>
+  </div>
+  <div style="margin-top: 20px; border: 1px solid black; padding: 10px 50px"><slot /></div>
+  <br />
+  <p>
+    <b>The scroll position is preserved when navigating the nested layout.</b>
+  </p>
+  <DummyText />
+</template>
+
+<style>
+.sub-navigation a {
+  margin-right: 10px;
+}
+</style>
+
+<script>
+import Counter from './Counter.vue'
+import DummyText from './DummyText.vue'
+const components = { Counter, DummyText }
+export default { components }
+</script>
