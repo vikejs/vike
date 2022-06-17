d6c0c5f33c8db85298c77bb5e02d6d722cd14d59
Romuald Brillout <git@brillout.com>
2022-06-17 17:24:04 +0200

    improve examples/layouts-react

--- examples/layouts-react/pages/starship/Layout.jsx
+++ examples/layouts-react/pages/starship/Layout.jsx
@@ -1,29 +1,13 @@
 export { Layout }
 
-import './Layout.css'
 import { LayoutDefault } from '../../renderer/LayoutDefault'
-import { Counter } from './Counter'
+import { PageOuter } from './PageOuter'
 
 function Layout({ children }) {
   return (
     <>
       <LayoutDefault>
-        <h1>Starship 🚀</h1>
-        <p>
-          <b>
-            This page uses the default layout <code>/renderer/LayouDefault.jsx</code>.
-            <br />
-            With the nested layout <code>/pages/starship/Layout.jsx</code>.
-          </b>
-        </p>
-        <p>
-          Outer layout is preserved during nested layout navigation: <Counter />
-        </p>
-        <div className="sub-navigation">
-          <a href="/starship/">Overview</a> <a href="/starship/reviews">Reviews</a>{' '}
-          <a href="/starship/spec">Tech Spec</a>
-        </div>
-        <div style={{ marginTop: 20 }}>{children}</div>
+        <PageOuter>{children}</PageOuter>
       </LayoutDefault>
     </>
   )
