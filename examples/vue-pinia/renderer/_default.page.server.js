465c89589309cac2513f8a3a1efd0607459771eb
Chris-Robin Ennen <chris-robin@ennen.dev>
2022-06-20 15:37:05 +0200

    fix hydration mismatch by invoking onBeforeRenderPageHook before app creation

--- examples/vue-pinia/renderer/_default.page.server.js
+++ examples/vue-pinia/renderer/_default.page.server.js
@@ -19,10 +19,10 @@ async function render(pageContext) {
 }
 
 async function onBeforeRender(pageContext) {
-  const { Page, routeParams } = pageContext
-  const { app, store } = createApp({ Page, routeParams })
-
   const { pageContext: addendum } = await pageContext.runOnBeforeRenderPageHook(pageContext)
+
+  Object.assign(pageContext, addendum)
+  const { app, store } = createApp(pageContext)
 
   const stream = renderToNodeStream(app)
 
