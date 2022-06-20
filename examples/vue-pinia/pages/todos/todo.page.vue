c56ae0c22f4fee17e3f689ded28e380f756308ca
Chris-Robin Ennen <chris@ncteam.de>
2022-06-20 14:39:50 +0200

    update example to showcase accessing route parameters

--- examples/vue-pinia/pages/todos/todo.page.vue
+++ examples/vue-pinia/pages/todos/todo.page.vue
@@ -1,25 +1,33 @@
 <template>
   <h1>To-do</h1>
-  <span>{{ text }}</span>
+  <span>{{ todo?.text }}</span>
   <br />
   <a href="/">Back</a>
 </template>
 
 <script setup>
-import { onServerPrefetch, computed } from 'vue'
+import { onServerPrefetch, ref, onMounted } from 'vue'
+import { usePageContext } from '../../renderer/usePageContext'
 import { useTodos } from '../../stores/useTodos'
 
 const props = defineProps(['todoId'])
 
-onServerPrefetch(async () => {
-  const todosStore = useTodos()
-  await todosStore.fetchTodoList()
-})
-const todosStore = useTodos()
+const todo = ref()
 
-const text = computed(() => {
-  const todo = todosStore.todoById(parseInt(props.todoId))
-  console.log('todo.text', todo?.text)
-  return todo?.text
+const loadTodo = async (id) => {
+  const idNum = parseInt(id)
+  const todosStore = useTodos()
+  await todosStore.fetchTodoById(idNum)
+  todo.value = todosStore.todoById(idNum)
+}
+
+onServerPrefetch(async () => {
+  // for this to work we need to pass routeParams (or whatever else we may need) to createApp in _default.page.server.js
+  const { routeParams } = usePageContext()
+  await loadTodo(routeParams.todoId)
+})
+
+onMounted(async () => {
+  await loadTodo(props.todoId)
 })
 </script>
