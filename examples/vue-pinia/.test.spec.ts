b2c9608900f98c14a0398b72b779cf9016cd1dc0
Romuald Brillout <git@brillout.com>
2022-06-20 12:19:48 +0200

    add client-side test

--- examples/vue-pinia/.test.spec.ts
+++ examples/vue-pinia/.test.spec.ts
@@ -1,13 +1,44 @@
-import { fetchHtml, run } from '../../libframe/test/setup'
+import { fetchHtml, run, page, urlBase, autoRetry } from '../../libframe/test/setup'
 
 run('npm run start')
 
-test('page content is rendered to HTML', async () => {
+test('server-side', async () => {
   const html = await fetchHtml('/')
+  expect(html).toContain('<h1>Pinia Example</h1>')
   expect(html).toContain('<h2>To-do List</h2>')
   expect(html).toContain('Buy milk')
   expect(html).toContain('Buy chocolate')
+  expect(html).toContain('Counter 0')
   expect(html).toContain(
     '"INITIAL_STATE":{"todos":{"todoList":[{"id":0,"text":"Buy milk"},{"id":1,"text":"Buy chocolate"}]},"counter":{"count":0}}',
   )
+})
+
+test('client-side', async () => {
+  await page.goto(urlBase + '/')
+
+  // Interactive button
+  expect(await page.textContent('button')).toBe('Counter 0')
+  // `autoRetry` for waiting on hydration
+  await autoRetry(async () => {
+    await page.click('button')
+    expect(await page.textContent('button')).toContain('Counter 1')
+  })
+
+  expect(await page.textContent('h1')).toBe('Pinia Example')
+  expect(await page.textContent('h2')).toBe('To-do List')
+  const firstTodoSelector = 'a[href="/todos/0"]'
+  expect(await page.textContent(firstTodoSelector)).toBe('Buy milk')
+
+  await page.click(firstTodoSelector)
+  await autoRetry(async () => {
+    expect(await page.textContent('h1')).toBe('To-do')
+  })
+  expect(await page.textContent('span')).toContain('Buy milk')
+
+  await page.click('a[href="/"]')
+  await autoRetry(async () => {
+    expect(await page.textContent('h1')).toBe('Pinia Example')
+  })
+  expect(await page.textContent('button')).toContain('Counter 1')
 })
