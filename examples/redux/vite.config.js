0e7ffc5a3981fa356075db8d06bbe658b04787fa
Romuald Brillout <git@brillout.com>
2022-06-22 16:23:02 +0200

    add examples/redux optimizeDeps.include

--- examples/redux/vite.config.js
+++ examples/redux/vite.config.js
@@ -3,4 +3,6 @@ import ssr from 'vite-plugin-ssr/plugin'
 
 export default {
   plugins: [react(), ssr()],
+  // Only needed for the vite-plugin-ssr CI
+  optimizeDeps: { include: ['react', 'react-dom/client', 'react-redux', 'redux'] },
 }
