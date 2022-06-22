f31e15bda7d32845f3c670adedaa699f16746fd6
Romuald Brillout <git@brillout.com>
2022-06-22 22:00:43 +0200

    improve workaround when CI cannot access server

--- vite-plugin-ssr/node/plugin/plugins/commonConfig.ts
+++ vite-plugin-ssr/node/plugin/plugins/commonConfig.ts
@@ -23,9 +23,9 @@ function setDefaultPort(config: ResolvedConfig) {
   config.preview.port ??= 3000
 }
 
-// Workaround GitHub Action running on MacOS failing to access the server
+// Workaround GitHub Action failing to access the server
 function workaroundCI(config: ResolvedConfig) {
-  if (process.env.CI && process.platform === 'darwin') {
+  if (process.env.CI) {
     config.server.host ??= true
     config.preview.host ??= true
   }
