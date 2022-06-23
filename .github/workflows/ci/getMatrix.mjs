dbb5a9368469973c29952683b169abd49e95e8ec
Romuald Brillout <git@brillout.com>
2022-06-23 12:07:27 +0200

    add getMatrix.mjs test

--- .github/workflows/ci/getMatrix.mjs
+++ .github/workflows/ci/getMatrix.mjs
@@ -5,156 +5,178 @@ import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 const testRE = /\.(test|spec)\./
 const testCmdE2e = 'pnpm run test:e2e'
+const args = process.argv
+
+export { getMatrix }
+
+if (args.includes('--ci')) {
+  const matrix = getMatrix()
+  if (args.includes('--debug')) {
+    console.log(JSON.stringify(matrix, null, 2))
+    console.log(matrix.length)
+  } else {
+    console.log(`{"include":${JSON.stringify(matrix)}}`)
+  }
+}
 
 /** @typedef { ({ name: string, TEST_FILES: string, testCmd: string } & Setup)[] } MatrixEntry */
-
-const root = cmd('git rev-parse --show-toplevel')
-const projectFiles = cmd(`git ls-files`, { cwd: root }).split(' ')
-
-/** @type string[] */
-const testFiles = projectFiles.filter((file) => testRE.test(file))
 /** @typedef {{os: string, node_version: string}} Setup */
-/** @type Setup[] */
-const setupExamples = [
-  {
-    os: 'ubuntu-latest',
-    node_version: '16',
-  },
-  {
-    os: 'windows-latest',
-    node_version: '14',
-  },
-]
-/** @type Record<string, { testFiles: null | string[], setups: Setup[], testCmd: string }> */
-const testJobs = {
-  // Unit tests
-  'Unit Tests': {
-    testFiles: [],
-    setups: [{
+
+function getMatrix() {
+  const root = cmd('git rev-parse --show-toplevel')
+  const projectFiles = cmd(`git ls-files`, { cwd: root }).split(' ')
+
+  /** @type string[] */
+  const testFiles = projectFiles.filter((file) => testRE.test(file))
+  /** @type Setup[] */
+  const setupExamples = [
+    {
+      os: 'ubuntu-latest',
+      node_version: '16',
+    },
+    {
       os: 'windows-latest',
       node_version: '14',
-    }],
-    testCmd: 'pnpm run test:units',
-  },
+    },
+  ]
+  /** @type Record<string, { testFiles: null | string[], setups: Setup[], testCmd: string }> */
+  const testJobs = {
+    // Unit tests
+    'Unit Tests': {
+      testFiles: [],
+      setups: [
+        {
+          os: 'windows-latest',
+          node_version: '14',
+        },
+      ],
+      testCmd: 'pnpm run test:units',
+    },
 
-  // Check all types
-  TypeScript: {
-    testFiles: null,
-    setups: [{
-  os: 'ubuntu-latest',
-  node_version: '18',
-    }],
-    testCmd: 'pnpm run test:types',
-  },
+    // Check all types
+    TypeScript: {
+      testFiles: null,
+      setups: [
+        {
+          os: 'ubuntu-latest',
+          node_version: '18',
+        },
+      ],
+      testCmd: 'pnpm run test:types',
+    },
 
-  // E2e tests
-  'Examples React': {
-    testFiles: [],
-    setups: setupExamples,
-    testCmd: testCmdE2e,
-  },
-  'Examples Vue/Others': {
-    testFiles: [],
-    setups: setupExamples,
-    testCmd: testCmdE2e,
-  },
-  Boilerplates: {
-    testFiles: [],
-    setups: [{
-      os: 'macos-latest',
-      node_version: '17',
-    }],
-    testCmd: testCmdE2e,
-  },
-  'Cloudflare + esbuild': {
-    testFiles: [],
-    setups: [{
-      os: 'ubuntu-latest',
-      node_version: '16',
-    }],
-    testCmd: testCmdE2e,
-  },
-  'Cloudflare + webpack': {
-    testFiles: [],
-    setups: [{
-      os: 'ubuntu-latest',
-      node_version: '16',
-    }],
-    testCmd: testCmdE2e,
-  },
-  'https://vite-plugin-ssr.com': {
-    testFiles: [],
-    setups: [{
-      os: 'ubuntu-latest',
-      node_version: '17',
-    }],
-    testCmd: testCmdE2e,
+    // E2e tests
+    'Examples React': {
+      testFiles: [],
+      setups: setupExamples,
+      testCmd: testCmdE2e,
+    },
+    'Examples Vue/Others': {
+      testFiles: [],
+      setups: setupExamples,
+      testCmd: testCmdE2e,
+    },
+    Boilerplates: {
+      testFiles: [],
+      setups: [
+        {
+          os: 'macos-latest',
+          node_version: '17',
+        },
+      ],
+      testCmd: testCmdE2e,
+    },
+    'Cloudflare + esbuild': {
+      testFiles: [],
+      setups: [
+        {
+          os: 'ubuntu-latest',
+          node_version: '16',
+        },
+      ],
+      testCmd: testCmdE2e,
+    },
+    'Cloudflare + webpack': {
+      testFiles: [],
+      setups: [
+        {
+          os: 'ubuntu-latest',
+          node_version: '16',
+        },
+      ],
+      testCmd: testCmdE2e,
+    },
+    'https://vite-plugin-ssr.com': {
+      testFiles: [],
+      setups: [
+        {
+          os: 'ubuntu-latest',
+          node_version: '17',
+        },
+      ],
+      testCmd: testCmdE2e,
+    },
   }
-}
 
-testFiles.forEach((testFile) => {
-  const category = getCategory(testFile)
-  assert(category in testJobs, "Following category doesn't exist: " + category)
-  const job = testJobs[category]
-  assert(job.testFiles)
-  job.testFiles.push(testFile)
-})
+  testFiles.forEach((testFile) => {
+    const category = getCategory(testFile)
+    assert(category in testJobs, "Following category doesn't exist: " + category)
+    const job = testJobs[category]
+    assert(job.testFiles)
+    job.testFiles.push(testFile)
+  })
 
-/** @type { (testFile: string) => string } */
-function getCategory(testFile) {
-  if (testFile.includes('.spec.')) {
-    return 'Unit Tests'
-  }
-  if (testFile.startsWith('boilerplates/')) {
-    return 'Boilerplates'
-  }
-  if (testFile.startsWith('docs/')) {
-    return 'https://vite-plugin-ssr.com'
-  }
-  if (testFile.startsWith('examples/')) {
-    if (testFile.includes('cloudflare')) {
-      if (testFile.includes('webpack')) {
-        return 'Cloudflare + webpack'
+  /** @type { (testFile: string) => string } */
+  function getCategory(testFile) {
+    if (testFile.includes('.spec.')) {
+      return 'Unit Tests'
+    }
+    if (testFile.startsWith('boilerplates/')) {
+      return 'Boilerplates'
+    }
+    if (testFile.startsWith('docs/')) {
+      return 'https://vite-plugin-ssr.com'
+    }
+    if (testFile.startsWith('examples/')) {
+      if (testFile.includes('cloudflare')) {
+        if (testFile.includes('webpack')) {
+          return 'Cloudflare + webpack'
+        } else {
+          return 'Cloudflare + esbuild'
+        }
+      }
+      if (isReactExample(testFile)) {
+        return 'Examples React'
       } else {
-        return 'Cloudflare + esbuild'
+        return 'Examples Vue/Others'
       }
     }
-    if (isReactExample(testFile)) {
-      return 'Examples React'
-    } else {
-      return 'Examples Vue/Others'
-    }
+    assert(false, 'Cannot find category for test file: ' + testFile)
   }
-  assert(false, 'Cannot find category for test file: ' + testFile)
-}
 
-/** @type { (testFile: string) => boolean } */
-function isReactExample(testFile) {
-  const pkgJsonFile = path.join(root, path.dirname(testFile), './package.json')
-  // await import(pkgJsonFile, { assert: { type: 'json'}})
-  const pkgJson = require(pkgJsonFile)
-  return !!pkgJson.dependencies['react']
-}
+  /** @type { (testFile: string) => boolean } */
+  function isReactExample(testFile) {
+    const pkgJsonFile = path.join(root, path.dirname(testFile), './package.json')
+    // await import(pkgJsonFile, { assert: { type: 'json'}})
+    const pkgJson = require(pkgJsonFile)
+    return !!pkgJson.dependencies['react']
+  }
 
-/** @type MatrixEntry */
-const matrix = []
-Object.entries(testJobs).map(([name, { testFiles, setups, testCmd }]) => {
-  setups.forEach((setup) => {
-    matrix.push({
-      testCmd,
-      name: name + getJobName(setup),
-      TEST_FILES: (testFiles ?? []).join(' '),
-      ...setup,
+  /** @type MatrixEntry */
+  const matrix = []
+  Object.entries(testJobs).map(([name, { testFiles, setups, testCmd }]) => {
+    setups.forEach((setup) => {
+      matrix.push({
+        testCmd,
+        name: name + getJobName(setup),
+        TEST_FILES: (testFiles ?? []).join(' '),
+        ...setup,
+      })
     })
   })
-})
 
-/*
-console.log(JSON.stringify(matrix, null, 2))
-console.log(matrix.length)
-/*/
-console.log(`{"include":${JSON.stringify(matrix)}}`)
-//*/
+  return matrix
+}
 
 /** @type { (setup: Setup) => string } */
 function getJobName(setup) {
---
+++ .github/workflows/ci/getMatrix.spec.ts
@@ -0,0 +1,82 @@
+import { getMatrix } from './getMatrix.mjs'
+import { expect, describe, it } from 'vitest'
+
+describe('getMatrix()', () => {
+  it('basics', () => {
+    const matrix = getMatrix()
+    expect(matrix).toMatchInlineSnapshot(`
+      [
+        {
+          "TEST_FILES": ".github/workflows/ci/getMatrix.spec.ts test/renderPage.spec.ts vite-plugin-ssr/shared/route/resolveFilesystemRoute.spec.ts vite-plugin-ssr/shared/route/resolvePrecedence/overall.spec.ts vite-plugin-ssr/shared/route/resolvePrecedence/route-strings.spec.ts vite-plugin-ssr/shared/route/resolveRouteString.spec.ts vite-plugin-ssr/utils/parseUrl.spec.ts vite-plugin-ssr/utils/pathHandling.spec.ts",
+          "name": "Unit Tests - Win - Node.js 14",
+          "node_version": "14",
+          "os": "windows-latest",
+          "testCmd": "pnpm run test:units",
+        },
+        {
+          "TEST_FILES": "",
+          "name": "TypeScript - Ubuntu - Node.js 18",
+          "node_version": "18",
+          "os": "ubuntu-latest",
+          "testCmd": "pnpm run test:types",
+        },
+        {
+          "TEST_FILES": "examples/base-url-cdn/.test.ts examples/base-url/.dev-base-url.test.ts examples/base-url/.dev.test.ts examples/base-url/.prod-ssg.test.ts examples/base-url/.prod-ssr.test.ts examples/custom-server-render-integration/.dev.test.ts examples/custom-server-render-integration/.prod.test.ts examples/file-structure-domain-driven/.dev.test.ts examples/file-structure-domain-driven/.prod.test.ts examples/graphql-apollo/.test.ts examples/i18n-prerender/.dev.test.ts examples/i18n-prerender/.prod.test.ts examples/i18n/.dev.test.ts examples/layouts-react/.dev.test.ts examples/layouts-react/.preview.test.ts examples/path-aliases/.dev.test.ts examples/path-aliases/.prod-static.test.ts examples/path-aliases/.prod.test.ts examples/react-17/.dev.test.ts examples/react-17/.prod.test.ts examples/react-full/.dev.test.ts examples/react-full/.preview.test.ts examples/react-router/.test.ts examples/react/.dev.test.ts examples/react/.prod.test.ts examples/redux/.test.ts examples/render-modes/.dev.test.ts examples/render-modes/.prod.test.ts examples/urql/.dev.test.ts examples/urql/.prod.test.ts",
+          "name": "Examples React - Ubuntu - Node.js 16",
+          "node_version": "16",
+          "os": "ubuntu-latest",
+          "testCmd": "pnpm run test:e2e",
+        },
+        {
+          "TEST_FILES": "examples/base-url-cdn/.test.ts examples/base-url/.dev-base-url.test.ts examples/base-url/.dev.test.ts examples/base-url/.prod-ssg.test.ts examples/base-url/.prod-ssr.test.ts examples/custom-server-render-integration/.dev.test.ts examples/custom-server-render-integration/.prod.test.ts examples/file-structure-domain-driven/.dev.test.ts examples/file-structure-domain-driven/.prod.test.ts examples/graphql-apollo/.test.ts examples/i18n-prerender/.dev.test.ts examples/i18n-prerender/.prod.test.ts examples/i18n/.dev.test.ts examples/layouts-react/.dev.test.ts examples/layouts-react/.preview.test.ts examples/path-aliases/.dev.test.ts examples/path-aliases/.prod-static.test.ts examples/path-aliases/.prod.test.ts examples/react-17/.dev.test.ts examples/react-17/.prod.test.ts examples/react-full/.dev.test.ts examples/react-full/.preview.test.ts examples/react-router/.test.ts examples/react/.dev.test.ts examples/react/.prod.test.ts examples/redux/.test.ts examples/render-modes/.dev.test.ts examples/render-modes/.prod.test.ts examples/urql/.dev.test.ts examples/urql/.prod.test.ts",
+          "name": "Examples React - Win - Node.js 14",
+          "node_version": "14",
+          "os": "windows-latest",
+          "testCmd": "pnpm run test:e2e",
+        },
+        {
+          "TEST_FILES": "examples/graphql-apollo-vue/.dev.test.ts examples/graphql-apollo-vue/.prod.test.ts examples/html-fragments/.test.ts examples/layouts-vue/.dev.test.ts examples/layouts-vue/.preview.test.ts examples/preact-client-routing/.dev.test.ts examples/preact-client-routing/.prod.test.ts examples/preact-server-routing/.dev.test.ts examples/preact-server-routing/.prod.test.ts examples/vue-full/.dev.test.ts examples/vue-full/.prod.test.ts examples/vue-router/.test.ts examples/vue/.dev.test.ts examples/vue/.prod.test.ts examples/vuex/.test.ts",
+          "name": "Examples Vue/Others - Ubuntu - Node.js 16",
+          "node_version": "16",
+          "os": "ubuntu-latest",
+          "testCmd": "pnpm run test:e2e",
+        },
+        {
+          "TEST_FILES": "examples/graphql-apollo-vue/.dev.test.ts examples/graphql-apollo-vue/.prod.test.ts examples/html-fragments/.test.ts examples/layouts-vue/.dev.test.ts examples/layouts-vue/.preview.test.ts examples/preact-client-routing/.dev.test.ts examples/preact-client-routing/.prod.test.ts examples/preact-server-routing/.dev.test.ts examples/preact-server-routing/.prod.test.ts examples/vue-full/.dev.test.ts examples/vue-full/.prod.test.ts examples/vue-router/.test.ts examples/vue/.dev.test.ts examples/vue/.prod.test.ts examples/vuex/.test.ts",
+          "name": "Examples Vue/Others - Win - Node.js 14",
+          "node_version": "14",
+          "os": "windows-latest",
+          "testCmd": "pnpm run test:e2e",
+        },
+        {
+          "TEST_FILES": "boilerplates/boilerplate-react-ts/.dev.test.ts boilerplates/boilerplate-react-ts/.prod.test.ts boilerplates/boilerplate-react/.dev.test.ts boilerplates/boilerplate-react/.prod.test.ts boilerplates/boilerplate-vue-ts/.dev.test.ts boilerplates/boilerplate-vue-ts/.prod.test.ts boilerplates/boilerplate-vue/.dev.test.ts boilerplates/boilerplate-vue/.prod.test.ts",
+          "name": "Boilerplates - Mac - Node.js 17",
+          "node_version": "17",
+          "os": "macos-latest",
+          "testCmd": "pnpm run test:e2e",
+        },
+        {
+          "TEST_FILES": "examples/cloudflare-workers-vue/.dev.test.ts examples/cloudflare-workers-vue/.miniflare.test.ts examples/cloudflare-workers-vue/.wrangler.test.ts examples/cloudflare-workers/.dev.test.ts examples/cloudflare-workers/.miniflare.test.ts examples/cloudflare-workers/.wrangler.test.ts",
+          "name": "Cloudflare + esbuild - Ubuntu - Node.js 16",
+          "node_version": "16",
+          "os": "ubuntu-latest",
+          "testCmd": "pnpm run test:e2e",
+        },
+        {
+          "TEST_FILES": "examples/cloudflare-workers-webpack/.dev.test.ts examples/cloudflare-workers-webpack/.miniflare.test.ts examples/cloudflare-workers-webpack/.wrangler.test.ts",
+          "name": "Cloudflare + webpack - Ubuntu - Node.js 16",
+          "node_version": "16",
+          "os": "ubuntu-latest",
+          "testCmd": "pnpm run test:e2e",
+        },
+        {
+          "TEST_FILES": "docs/.dev.test.ts docs/.preview.test.ts",
+          "name": "https://vite-plugin-ssr.com - Ubuntu - Node.js 17",
+          "node_version": "17",
+          "os": "ubuntu-latest",
+          "testCmd": "pnpm run test:e2e",
+        },
+      ]
+    `)
+  })
+})
