492175ffa062538237330c10b5eb2de56ea37929
Romuald Brillout <git@brillout.com>
2022-06-29 10:47:26 +0200

    update pinia example & docs for vps 0.4 & Vikepress

--- examples/vue-pinia/renderer/_default.page.client.js
+++ examples/vue-pinia/renderer/_default.page.client.js
@@ -1,18 +1,16 @@
-import { useClientRouter } from 'vite-plugin-ssr/client/router'
+export { render }
+export const clientRouting = true
+
 import { createApp } from './app'
 
 let app
-useClientRouter({
-  render(pageContext) {
-    if (!app) {
-      const instance = createApp(pageContext)
-      app = instance.app
-      instance.store.state.value = pageContext.initialStoreState
-      app.mount('#app')
-    } else {
-      app.changePage(pageContext)
-    }
-  },
-  // Vue needs the first render to be a hydration
-  ensureHydration: true
-})
+function render(pageContext) {
+  if (!app) {
+    const instance = createApp(pageContext)
+    app = instance.app
+    instance.store.state.value = pageContext.initialStoreState
+    app.mount('#app')
+  } else {
+    app.changePage(pageContext)
+  }
+}
