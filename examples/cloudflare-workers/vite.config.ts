2e6b885c3cdc26cd5636e3a8fec7796b77b433d4
Romuald Brillout <git@brillout.com>
2022-07-06 21:13:55 +0200

    remove now unecessary optimizeDeps entries

--- examples/cloudflare-workers/vite.config.ts
+++ examples/cloudflare-workers/vite.config.ts
@@ -10,5 +10,5 @@ export default {
     preserveSymlinks: true,
   },
   // We manually add a list of dependencies to be pre-bundled, in order to avoid a page reload at dev start which breaks vite-plugin-ssr's CI
-  optimizeDeps: { include: ['react-streaming', 'react-streaming/client'] },
+  optimizeDeps: { include: ['react-streaming'] },
 } as UserConfig
--- examples/redux/vite.config.js
+++ examples/redux/vite.config.js
@@ -2,7 +2,5 @@ import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
 
 export default {
-  plugins: [react(), ssr()],
-  // We manually add a list of dependencies to be pre-bundled, in order to avoid a page reload at dev start which breaks vite-plugin-ssr's CI
-  optimizeDeps: { include: ['react', 'react-dom/client', 'react-redux', 'redux'] },
+  plugins: [react(), ssr()]
 }
