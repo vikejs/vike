5d9b1f7d2dc809937cef6f9aaf55fc75b70daf0f
Romuald Brillout <git@brillout.com>
2022-07-06 12:58:39 +0200

    revert: [extractStyles] set includeAssetsImportedByServer default to true during beta

--- vite-plugin-ssr/node/plugin/plugins/config/resolveConfigVps.ts
+++ vite-plugin-ssr/node/plugin/plugins/config/resolveConfigVps.ts
@@ -14,7 +14,7 @@ function resolveConfigVps(fromPluginOptions: unknown, fromViteConfig: unknown): 
     },
     prerender: resolvePrerenderOptions(fromPluginOptions, fromViteConfig),
     includeCSS: fromPluginOptions.includeCSS ?? fromViteConfig.includeCSS ?? [],
-    includeAssetsImportedByServer: fromPluginOptions.includeAssetsImportedByServer ?? fromViteConfig.includeAssetsImportedByServer ?? true,
+    includeAssetsImportedByServer: fromPluginOptions.includeAssetsImportedByServer ?? fromViteConfig.includeAssetsImportedByServer ?? false,
   }
 
   assertConfigVpsResolved({ vitePluginSsr })
