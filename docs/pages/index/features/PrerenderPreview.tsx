2264ceea847b3e4ff5ecdacb940806d24cf81c29
Romuald Brillout <git@brillout.com>
2022-07-08 14:47:02 +0200

    docs: update pre-rendering docs

--- docs/pages/index/features/PrerenderPreview.tsx
+++ docs/pages/index/features/PrerenderPreview.tsx
@@ -10,12 +10,14 @@ function PrerenderPreview() {
   return (
     <pre>
     <code className="language-custom" style={{backgroundColor: '#262626', color: 'white', overflowX: 'auto'}}>
-      $ npx vite-plugin-ssr prerender{'\n'}
-<span style={{color: 'rgb(3, 191, 200)'}}>vite-plugin-ssr</span> <span style={{color: colorAction}}>pre-rendering HTML...</span>{'\n'}
-<span style={{color: colorAction}}>✓</span> 3 HTML documents pre-rendered.{'\n'}
-<span style={{color: colorPath}}>dist/client/</span><span style={{color: colorFile}}>index.html</span>{'\n'}
-<span style={{color: colorPath}}>dist/client/</span><span style={{color: colorFile}}>about/index.html</span>{'\n'}
-<span style={{color: colorPath}}>dist/client/</span><span style={{color: colorFile}}>404.html</span>
+      <span style={{color: '#aaa'}}>$</span> vite build {'\n'}
+      <span style={{color: 'rgb(3, 191, 200)'}}>vite</span> <span style={{color: colorAction}}>building for production...</span>{'\n'}
+      <span style={{color: colorAction}}>✓</span> 42 modules transformed.{'\n'}
+      <span style={{color: 'rgb(3, 191, 200)'}}>vite-plugin-ssr</span> <span style={{color: colorAction}}>pre-rendering HTML...</span>{'\n'}
+      <span style={{color: colorAction}}>✓</span> 3 HTML documents pre-rendered.{'\n'}
+      <span style={{color: colorPath}}>dist/client/</span><span style={{color: colorFile}}>index.html</span>{'\n'}
+      <span style={{color: colorPath}}>dist/client/</span><span style={{color: colorFile}}>about/index.html</span>{'\n'}
+      <span style={{color: colorPath}}>dist/client/</span><span style={{color: colorFile}}>404.html</span>
     </code>
     </pre>
   )
