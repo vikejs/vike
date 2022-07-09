889d691148ccee656b3d63e7acd5ac4e4c2f9788
Romuald Brillout <git@brillout.com>
2022-07-09 12:21:45 +0200

    update boilerplates

--- boilerplates/boilerplate-react-ts/server/index.ts
+++ boilerplates/boilerplate-react-ts/server/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import compression from 'compression'
-import { createPageRenderer } from 'vite-plugin-ssr'
+import { renderPage } from 'vite-plugin-ssr'
 
 const isProduction = process.env.NODE_ENV === 'production'
 const root = `${__dirname}/..`
@@ -12,19 +12,20 @@ async function startServer() {
 
   app.use(compression())
 
-  let viteDevServer
   if (isProduction) {
-    app.use(express.static(`${root}/dist/client`))
+    const sirv = require('sirv')
+    app.use(sirv(`${root}/dist/client`))
   } else {
     const vite = require('vite')
-    viteDevServer = await vite.createServer({
-      root,
-      server: { middlewareMode: 'ssr' },
-    })
-    app.use(viteDevServer.middlewares)
+    const viteDevMiddleware = (
+      await vite.createServer({
+        root,
+        server: { middlewareMode: 'ssr' },
+      })
+    ).middlewares
+    app.use(viteDevMiddleware)
   }
 
-  const renderPage = createPageRenderer({ viteDevServer, isProduction, root })
   app.get('*', async (req, res, next) => {
     const url = req.originalUrl
     const pageContextInit = {
