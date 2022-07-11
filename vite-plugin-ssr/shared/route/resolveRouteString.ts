ceb397dbe8d3304abafdbae3180a8c3b92613294
Romuald Brillout <git@brillout.com>
2022-07-11 10:21:26 +0200

    minor refactor

--- vite-plugin-ssr/shared/route/resolveRouteString.ts
+++ vite-plugin-ssr/shared/route/resolveRouteString.ts
@@ -1,7 +1,6 @@
 export { resolveRouteString }
 export { isStaticRouteString }
 export { analyzeRouteString }
-export { PARAM_TOKENS }
 export { PARAM_TOKEN_NEW }
 
 import { assertWarning } from '../utils'
@@ -9,7 +8,6 @@ import { assert, assertUsage } from './utils'
 
 const PARAM_TOKEN_NEW = '@'
 const PARAM_TOKEN_OLD = ':'
-const PARAM_TOKENS = [PARAM_TOKEN_NEW, PARAM_TOKEN_OLD]
 
 function resolveRouteString(routeString: string, urlPathname: string): null | { routeParams: Record<string, string> } {
   assertUsage(
@@ -20,8 +18,8 @@ function resolveRouteString(routeString: string, urlPathname: string): null | { 
   )
   assert(urlPathname.startsWith('/'))
 
-  const routeParts = routeString.split('/')
-  const urlParts = urlPathname.split('/')
+  const routeSegments = routeString.split('/')
+  const urlSegments = urlPathname.split('/')
 
   const routeParams: Record<string, string> = {}
 
@@ -32,26 +30,26 @@ function resolveRouteString(routeString: string, urlPathname: string): null | { 
   }
 
   //console.log(routeString, urlPathname)
-  for (let i = 0; i < Math.max(routeParts.length, urlParts.length); i++) {
-    const routeDir = routeParts[i]
-    const urlDir = urlParts[i]
-    if (routeDir === '*') {
-      routeParams['*'] = urlParts.slice(Math.max(1, i)).join('/')
+  for (let i = 0; i < Math.max(routeSegments.length, urlSegments.length); i++) {
+    const routeSegment = routeSegments[i]
+    const urlSegment = urlSegments[i]
+    if (routeSegment === '*') {
+      routeParams['*'] = urlSegments.slice(Math.max(1, i)).join('/')
       return { routeParams }
-    } else if (routeDir && isParameterized(routeDir)) {
+    } else if (routeSegment && isParam(routeSegment)) {
       assertWarning(
-        !routeDir.startsWith(PARAM_TOKEN_OLD),
+        !routeSegment.startsWith(PARAM_TOKEN_OLD),
         `Outdated route string \`${routeString}\`, use \`${routeString
           .split(PARAM_TOKEN_OLD)
           .join(PARAM_TOKEN_NEW)}\` instead.`,
         { onlyOnce: true },
       )
-      if (!urlDir) {
+      if (!urlSegment) {
         return null
       }
-      routeParams[routeDir.slice(1)] = urlDir
+      routeParams[routeSegment.slice(1)] = urlSegment
     } else {
-      if ((routeDir || '') !== (urlDir || '')) {
+      if ((routeSegment || '') !== (urlSegment || '')) {
         return null
       }
     }
@@ -72,25 +70,25 @@ function assertGlob(routeString: string) {
   )
 }
 function analyzeRouteString(routeString: string) {
-  const pathSegments = routeString.split('/').filter((path) => path !== '' && path !== '*')
+  const routeSegments = routeString.split('/').filter((routeSegment) => routeSegment !== '' && routeSegment !== '*')
 
   let numberOfStaticSegmentsBeginning = 0
-  for (const path of pathSegments) {
-    if (isParameterized(path)) {
+  for (const routeSegment of routeSegments) {
+    if (isParam(routeSegment)) {
       break
     }
     numberOfStaticSegmentsBeginning++
   }
 
-  const numberOfStaticSegements = pathSegments.filter((p) => !isParameterized(p)).length
-  const numberOfParameterSegments = pathSegments.filter((p) => isParameterized(p)).length
+  const numberOfStaticSegements = routeSegments.filter((s) => !isParam(s)).length
+  const numberOfParameterSegments = routeSegments.filter((s) => isParam(s)).length
 
   const isCatchAll = routeString.endsWith('*')
 
   return { numberOfParameterSegments, numberOfStaticSegmentsBeginning, numberOfStaticSegements, isCatchAll }
 }
 
-function isParameterized(routeSegment: string) {
+function isParam(routeSegment: string) {
   return routeSegment.startsWith(PARAM_TOKEN_NEW) || routeSegment.startsWith(PARAM_TOKEN_OLD)
 }
 
