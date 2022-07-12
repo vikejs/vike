3c5fa83aff61ff7528ba837274449aaf5e6b4a69
Romuald Brillout <git@brillout.com>
2022-07-12 19:52:26 +0200

    fix: add page files to optimizeDeps.entries

--- vite-plugin-ssr/node/plugin/plugins/devConfig.ts
+++ vite-plugin-ssr/node/plugin/plugins/devConfig.ts
@@ -3,8 +3,6 @@ export { devConfig }
 import type { Plugin } from 'vite'
 import { apply, addSsrMiddleware, javascriptFileExtensions } from '../utils'
 import { getGlobRoots } from './generateImportGlobs/getGlobRoots'
-import * as vite from 'vite'
-const viteVersion = (vite as { version?: string }).version || '2.?.?'
 
 function devConfig(): Plugin[] {
   return [
@@ -14,16 +12,7 @@ function devConfig(): Plugin[] {
       config: () => ({
         ssr: { external: ['vite-plugin-ssr'] },
         optimizeDeps: {
-          entries:
-            process.env.CI && viteVersion.startsWith('2.')
-              ? [`**/*.page.${javascriptFileExtensions}`, `**/*.page.client.${javascriptFileExtensions}`]
-              : // Ideally we should use `fast-glob` to determine the index page files
-                [
-                  `/pages/index.page.${javascriptFileExtensions}`,
-                  `/pages/index.page.client.${javascriptFileExtensions}`,
-                  `/pages/index/index.page.${javascriptFileExtensions}`,
-                  `/pages/index/index.page.client.${javascriptFileExtensions}`,
-                ],
+          entries: [`**/*.page.${javascriptFileExtensions}`, `**/*.page.client.${javascriptFileExtensions}`],
           exclude: [
             // We exclude the client code to support `import.meta.glob()`
             'vite-plugin-ssr/client',
