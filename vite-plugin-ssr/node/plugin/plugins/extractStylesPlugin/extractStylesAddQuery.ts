4a821d6781d57acc204bc573f6ed440fe0939189
Romuald Brillout <git@brillout.com>
2022-07-15 15:31:20 +0200

    minor

--- vite-plugin-ssr/node/plugin/plugins/extractStylesPlugin/extractStylesAddQuery.ts
+++ vite-plugin-ssr/node/plugin/plugins/extractStylesPlugin/extractStylesAddQuery.ts
@@ -1,8 +1,8 @@
+export { extractStylesAddQuery }
+
 // We don't import from `../../../plugin/utils` because `extractStylesAddQuery()` is imported by the server-side runtime; we don't want the server-side runtime to import `plugin/utils/*`.
 import { assert } from '../../../../utils/assert'
 import { getFileExtension } from '../../../../utils/getFileExtension'
-
-export { extractStylesAddQuery }
 
 function extractStylesAddQuery(id: string) {
   const fileExtension = getFileExtension(id)
