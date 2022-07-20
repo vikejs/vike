2dda94badeb8fc941ccf4b05afbef489a66f690b
Alessia Bellisario <alessia@apollographql.com>
2022-07-20 12:37:03 -0400

    chore: update apollo vue example, test and pnpm lockfile

--- examples/graphql-apollo-vue/.testRun.ts
+++ examples/graphql-apollo-vue/.testRun.ts
@@ -1,19 +1,9 @@
 export { testRun }
 
 import { fetchHtml, run } from '../../libframe/test/setup'
-const viteVersion = '3.?.?'
 
 function testRun(cmd: 'npm run dev' | 'npm run prod') {
   run(cmd)
-
-  if (cmd === 'npm run prod' && viteVersion.startsWith('3')) {
-    // https://github.com/apollographql/apollo-client/issues/9833
-    // https://github.com/apollographql/apollo-client/issues/9834
-    const msg = 'SKIPPED Apollo GrpahQL production test until it supports Vite 3.'
-    console.log(msg)
-    test(msg, () => {})
-    return
-  }
 
   test('page is rendered to HTML', async () => {
     const html = await fetchHtml('/')
