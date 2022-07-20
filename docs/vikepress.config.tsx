dcee0caf098154b176da27380878fffb2431967e
Romuald Brillout <git@brillout.com>
2022-07-20 20:24:01 +0200

    docs: fix social banner

--- docs/vikepress.config.tsx
+++ docs/vikepress.config.tsx
@@ -2,7 +2,6 @@ import type { Config } from 'vikepress'
 import { headings, headingsWithoutLink } from './headings'
 import { projectInfo } from './utils'
 import faviconUrl from './images/icons/vite-plugin-ssr.svg'
-import bannerUrl from './images/banner.png'
 import React from 'react'
 import { NavHeader, NavHeaderMobile } from './NavHeader'
 
@@ -20,7 +19,7 @@ const config: Config = {
   headingsWithoutLink,
   tagline: 'Like Next.js/Nuxt but as do-one-thing-do-it-well Vite plugin.',
   titleNormalCase: false,
-  bannerUrl,
+  bannerUrl: 'https://vite-plugin-ssr.com/banner.png',
   websiteUrl: 'https://vite-plugin-ssr.com/',
 }
 export default config
