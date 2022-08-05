5af40d1c8075087a9cc282e703bdc6d451155710
Arthur Gailes <agailes1@gmail.com>
2022-08-05 16:36:39 -0400

    update exports type

--- boilerplates/boilerplate-react-ts/renderer/types.ts
+++ boilerplates/boilerplate-react-ts/renderer/types.ts
@@ -4,8 +4,10 @@ export type PageContext = {
   Page: (pageProps: PageProps) => React.ReactElement
   pageProps: PageProps
   urlPathname: string
-  documentProps?: {
-    title?: string
-    description?: string
+  exports: {
+    documentProps?: {
+      title?: string;
+      description?: string;
+    }
   }
 }
