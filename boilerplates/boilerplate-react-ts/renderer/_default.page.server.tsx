97543a8f9861968571945c8346b15de714fa843c
Arthur Gailes <agailes1@gmail.com>
2022-08-05 16:33:56 -0400

    update defaultProps
    comes from pageContext.exports

--- boilerplates/boilerplate-react-ts/renderer/_default.page.server.tsx
+++ boilerplates/boilerplate-react-ts/renderer/_default.page.server.tsx
@@ -19,7 +19,7 @@ async function render(pageContext: PageContextBuiltIn & PageContext) {
   )
 
   // See https://vite-plugin-ssr.com/head
-  const { documentProps } = pageContext
+  const { documentProps } = pageContext.exports
   const title = (documentProps && documentProps.title) || 'Vite SSR app'
   const desc = (documentProps && documentProps.description) || 'App using Vite + vite-plugin-ssr'
 
