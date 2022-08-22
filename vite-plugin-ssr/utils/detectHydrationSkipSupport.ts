bbd39c644e9433075cb5685479c03857fd50e310
Romuald Brillout <git@brillout.com>
2022-08-22 18:01:16 +0200

    fix: improve React detection heuristic (#423)

--- vite-plugin-ssr/utils/detectHydrationSkipSupport.ts
+++ vite-plugin-ssr/utils/detectHydrationSkipSupport.ts
@@ -1,6 +1,6 @@
 export { detectHydrationSkipSupport }
 
-import { assertWarning } from './assert'
+import { assert, assertWarning } from './assert'
 
 function detectHydrationSkipSupport(): boolean {
   const isReact = isReactApp()
@@ -10,22 +10,42 @@ function detectHydrationSkipSupport(): boolean {
   return false
 }
 
-// https://stackoverflow.com/questions/73156433/detect-with-javascript-whether-the-website-is-using-react
+// There doesn't seem to be a reliable way to detect React:
+//  - https://stackoverflow.com/questions/73156433/detect-with-javascript-whether-the-website-is-using-react
 function isReactApp() {
-  const win = window as any
-  // I don't remember where I got this trick from. I guess `__REACT_DEVTOOLS_GLOBAL_HOOK__` is set by React, but I don't know why it's `undefined` at https://reactjs.org/ which doesn't seem to make sense.
-  const isReact1 = !!win.__REACT_DEVTOOLS_GLOBAL_HOOK__
-  // Set by `@vitejs/plugin-react`
-  const isReact2 = !!win.__vite_plugin_react_preamble_installed__
-  // Set by `@vitejs/plugin-react` as well
-  const isReact3 = !!win.$RefreshReg$
-  if (!isReact1 && !isReact2 && !isReact3) {
-    return false
+  // Heursitic using values set by React (Dev Tools)
+  // - `window.__REACT_DEVTOOLS_GLOBAL_HOOK__` is an internal API for React Dev Tools
+  //   - https://stackoverflow.com/questions/46807826/how-does-react-developer-tools-determine-that-the-webpage-is-using-react/46808361#46808361
+  //   - In principle, `window.__REACT_DEVTOOLS_GLOBAL_HOOK__` should exist only if React Dev Tools is installed. (Although it seems that it's sometimes defined even when React Dev Tools isn't installed; I don't know why.)
+  // - We use `isReact1`/`isReact2` merely to validate `isReact3` and `isReact4`. Beacuse `isReact1`/`isReact2` can be false negatives:
+  //   - React populates `__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers` fairly late => false negative early in the rendering phase
+  //   - `window.__REACT_DEVTOOLS_GLOBAL_HOOK__` is `undefined` if React Dev Tools isn't installed => false negative
+  const isReact1 = !!(window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__?.renderers?.size
+  const isReact2 = !!(window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__?.rendererInterfaces?.size
+
+  // Heursitic using values set by `@vitejs/plugin-react`
+  const isReact3 = !!(window as any).__vite_plugin_react_preamble_installed__
+  const isReact4 = !!(window as any).$RefreshReg$
+
+  const assertHeuristic = (condition: boolean) => {
+    assertWarning(
+      condition,
+      'An internal heuristic needs to be updated, see https://github.com/brillout/vite-plugin-ssr/issues/423',
+      { onlyOnce: true },
+    )
   }
-  assertWarning(
-    isReact1 && isReact2 && isReact3,
-    'An internal heuristic needs to be updated, see https://github.com/brillout/vite-plugin-ssr/issues/423',
-    { onlyOnce: true },
-  )
-  return true
+
+  if (isReact1 || isReact2) {
+    assertHeuristic(isReact1 && isReact2 && isReact3 && isReact4)
+    return true
+  }
+
+  if (isReact3 || isReact4) {
+    // See explanation above of why `isReact1` and `isReact2` are `false`
+    assert(isReact1 === false && isReact2 === false)
+    assertHeuristic(isReact3 && isReact4)
+    return true
+  }
+
+  return false
 }
