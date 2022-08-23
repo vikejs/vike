3b2ba9d9d9ee3f2039b34c596a5c21cdd6ec6958
Romuald Brillout <git@brillout.com>
2022-08-23 21:32:05 +0200

    remove utils/pathHandling.ts

--- vite-plugin-ssr/node/plugin/plugins/distEntries.ts
+++ vite-plugin-ssr/node/plugin/plugins/distEntries.ts
@@ -2,7 +2,8 @@ export { distEntriesPlugin }
 
 import type { Plugin, ResolvedConfig } from 'vite'
 import { importBuildPlugin } from 'vite-plugin-import-build/plugin'
-import { getOutDirs, projectInfo, pathRelative, pathJoin } from '../utils'
+import { getOutDirs, projectInfo, toPosixPath } from '../utils'
+import path from 'path'
 
 function distEntriesPlugin(): Plugin[] {
   let config: ResolvedConfig
@@ -26,10 +27,12 @@ function distEntriesPlugin(): Plugin[] {
 
 function getImporterCode(config: ResolvedConfig, pageFilesEntry: string) {
   // Current directory: vite-plugin-ssr/dist/cjs/node/plugin/plugins/
-  const importPathAbsolute = require.resolve(`../../../../../dist/cjs/node/plugin/plugins/distEntries/loadDistEntries`)
+  const importPathAbsolute = toPosixPath(
+    require.resolve(`../../../../../dist/cjs/node/plugin/plugins/distEntries/loadDistEntries`)
+  )
   const { outDirServer } = getOutDirs(config.build.outDir)
-  const outDirServerAbsolute = pathJoin(config.root, outDirServer)
-  const importPath = pathRelative(outDirServerAbsolute, importPathAbsolute)
+  const outDirServerAbsolute = path.posix.join(config.root, outDirServer)
+  const importPath = path.posix.relative(outDirServerAbsolute, importPathAbsolute)
   const importerCode = [
     `const { setDistEntries } = require('${importPath}');`,
     'setDistEntries({',
