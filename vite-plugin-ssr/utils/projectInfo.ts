6d249347210c75ecc5f9b7ae9273419b517b82fe
Romuald Brillout <git@brillout.com>
2022-08-24 21:52:14 +0200

    fix: track instances for improved debuggability

--- vite-plugin-ssr/utils/projectInfo.ts
+++ vite-plugin-ssr/utils/projectInfo.ts
@@ -7,3 +7,10 @@ export const projectInfo = {
   githubRepository: 'https://github.com/brillout/vite-plugin-ssr' as const,
   discordInviteToolChannel: 'https://discord.com/invite/qTq92FQzKb' as const
 }
+
+// Trick: since `utils/asserts.ts` depends on this file (`utils/projectInfo.ts`), we can have confidence that this file is always instantiated. So that we don't have to initialize this code snippet at every possible entry. (There are a *lot* of entries: `client/router/`, `client/`, `node/`, `node/plugin/`, `node/cli`, etc.)
+globalThis.__vite_plugin_ssr__instances = globalThis.__vite_plugin_ssr__instances || []
+globalThis.__vite_plugin_ssr__instances.push(projectInfo.projectVersion)
+declare global {
+  var __vite_plugin_ssr__instances: undefined | string[]
+}
