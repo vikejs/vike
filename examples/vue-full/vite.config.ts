7e3f5e2f327fff5fd06d5595b071f9c9bd58f793
Romuald Brillout <git@brillout.com>
2022-08-24 22:58:05 +0200

    improve vue-full example

--- examples/vue-full/vite.config.ts
+++ examples/vue-full/vite.config.ts
@@ -11,15 +11,6 @@ const config: UserConfig = {
     md(),
     ssr({ prerender: true })
   ],
-  clearScreen: false,
-  // Neeed if using an ESM-only library. This is not the case of this example and it's, in general, a rare case. But such situation will increasingly occur as ESM-only libraries emerge.
-  build: {
-    rollupOptions: {
-      output: {
-        format: 'es' // Transpile to ESM instead of CJS
-      }
-    }
-  },
   // We manually add a list of dependencies to be pre-bundled, in order to avoid a page reload at dev start which breaks vite-plugin-ssr's CI
   optimizeDeps: { include: ['cross-fetch'] }
 }
