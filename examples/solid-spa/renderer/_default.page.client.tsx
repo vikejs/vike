55678b17b7e6894f0672455a949eedb8253d6c09
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-09-03 10:58:43 +0200

    fix: disposing of previous page

--- examples/solid-spa/renderer/_default.page.client.tsx
+++ examples/solid-spa/renderer/_default.page.client.tsx
@@ -3,7 +3,22 @@ export { render }
 import { render as renderSolid } from 'solid-js/web'
 import type { PageContextClient } from './types'
 
+/**
+ * A function that disposes previously rendered pages.
+ *
+ * If the function is not executed, each route change will
+ * append a page to the DOM without clearing (disposing)
+ * the previous one.
+ */
+let disposePreviousPage: () => void
+
 async function render(pageContext: PageContextClient) {
   const { Page } = pageContext
-  return renderSolid(() => <Page />, document.getElementById('root') as HTMLElement)
+
+  if (disposePreviousPage) {
+    disposePreviousPage()
+  }
+
+  // render the page and save the dispose function of that page
+  disposePreviousPage = renderSolid(() => <Page />, document.getElementById('root'))
 }
