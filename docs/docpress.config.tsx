594511d936c54cd0a77ba13d6bd350aa3f008dca
Romuald Brillout <git@brillout.com>
2022-09-27 17:37:52 +0200

    minor

--- docs/docpress.config.tsx
+++ docs/docpress.config.tsx
@@ -8,19 +8,19 @@ import { NavHeader, NavHeaderMobile } from './NavHeader'
 const config: Config = {
   projectInfo,
   faviconUrl,
+  navHeader: <NavHeader />,
+  navHeaderMobile: <NavHeaderMobile />,
+  headings,
+  headingsWithoutLink,
+  tagline: 'Like Next.js/Nuxt but as do-one-thing-do-it-well Vite plugin.',
+  titleNormalCase: false,
+  twitterHandle: '@brillout',
+  websiteUrl: 'https://vite-plugin-ssr.com',
   algolia: {
     appId: 'MUXG1ZE9F6',
     apiKey: '8d5986fca9ba9110bcbbfc51263de88b',
     indexName: 'vite-pluginssr'
   },
-  navHeaderMobile: <NavHeaderMobile />,
-  navHeader: <NavHeader />,
-  headings,
-  headingsWithoutLink,
-  tagline: 'Like Next.js/Nuxt but as do-one-thing-do-it-well Vite plugin.',
-  titleNormalCase: false,
   bannerUrl: 'https://vite-plugin-ssr.com/banner.png',
-  twitterHandle: '@brillout',
-  websiteUrl: 'https://vite-plugin-ssr.com/'
 }
 export default config
