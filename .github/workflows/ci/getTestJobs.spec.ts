807bbc1e3b192ca217b01652b49e5313a56ddd56
Romuald Brillout <git@brillout.com>
2022-10-01 15:42:13 +0200

    skip getTestJobs() test

--- .github/workflows/ci/getTestJobs.spec.ts
+++ .github/workflows/ci/getTestJobs.spec.ts
@@ -1,14 +1,14 @@
 import { getTestJobs } from './getTestJobs.mjs'
 import { expect, describe, it } from 'vitest'
 
-/*/
+//*/
+// We only use this `getTestJobs()` test for developing the getTestJobs() function. (Because, otherwise, the fixture down below would need to be updated everytime there is a new/(re)moved test file.)
 const SKIP = true
 /*/
 const SKIP = false
 //*/
 
 describe('getTestJobs()', () => {
-  // We only use this `getTestJobs()` test for developing the getTestJobs() function. (Because, otherwise, the fixture down below would need to be updated everytime there is a new/(re)moved test file.)
   if (SKIP) {
     const msg = 'SKIPPED getTestJobs() test'
     it(msg, () => {})
