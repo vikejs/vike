18b09b36f480b99c4310a34a799674ddc2162777
Romuald Brillout <git@brillout.com>
2022-10-05 22:46:34 +0200

    revert "improve includeAssetsImportedByServer test"

--- test/extractAssets/pages/index/index.page.server.jsx
+++ test/extractAssets/pages/index/index.page.server.jsx
@@ -1,15 +1,12 @@
 import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr'
 import logo1Url from './logo-1.svg?url'
 import logo2Url from './logo-2.svg?raw'
-import scriptUrl from './script.js?url'
-
 export async function render() {
   return escapeInject`<!DOCTYPE html>
     <html>
       <body>
         <img src="${logo1Url}" />
         ${dangerouslySkipEscape(logo2Url)}
-        <script src="${scriptUrl}"></script>
       </body>
     </html>`
 }
