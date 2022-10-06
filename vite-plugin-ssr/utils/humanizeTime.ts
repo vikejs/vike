667aedcf58d6e0ddc21c74fdbb8167eff62bd71d
Romuald Brillout <git@brillout.com>
2022-10-06 21:09:06 +0200

    minor

--- vite-plugin-ssr/utils/humanizeTime.ts
+++ vite-plugin-ssr/utils/humanizeTime.ts
@@ -1,16 +1,24 @@
 export { humanizeTime }
 
-function humanizeTime(milliseconds: number) {
+function humanizeTime(milliseconds: number): string {
   const seconds = milliseconds / 1000
   if (seconds < 120) {
-    return `${round(seconds)} seconds`
+    const n = round(seconds)
+    return `${n} second${plural(n)}`
   }
-  const minutes = seconds / 60
-  return `${round(minutes)} minutes`
+  {
+    const minutes = seconds / 60
+    const n = round(minutes)
+    return `${n} minute${plural(n)}`
+  }
 }
 
 function round(n: number): string {
   let rounded = n.toFixed(1)
   if (rounded.endsWith('.0')) rounded = rounded.slice(0, -2)
   return rounded
+}
+
+function plural(n: string): string {
+  return n === '1' ? '' : 's'
 }
