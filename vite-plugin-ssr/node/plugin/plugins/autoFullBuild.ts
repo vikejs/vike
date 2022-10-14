70d90a26a21bf526520b0fc77abd84007f77e4ca
Romuald Brillout <git@brillout.com>
2022-10-14 19:14:16 +0200

    fix: avoid Rollup handling chained build errors (#472)

--- vite-plugin-ssr/node/plugin/plugins/autoFullBuild.ts
+++ vite-plugin-ssr/node/plugin/plugins/autoFullBuild.ts
@@ -25,18 +25,22 @@ function autoFullBuild(): Plugin {
       if (config.build.ssr || config.vitePluginSsr.disableAutoFullBuild || !isViteCliCall()) {
         return
       }
+      try {
+        const { configFile, root } = config
 
-      const { configFile, root } = config
+        const configSSR = {
+          build: { ssr: true },
+          configFile,
+          root
+        }
+        await build(configSSR)
 
-      const configSSR = {
-        build: { ssr: true },
-        configFile,
-        root
-      }
-      await build(configSSR)
-
-      if (config.vitePluginSsr.prerender && !config.vitePluginSsr.prerender.disableAutoRun) {
-        await prerender({ viteConfig: { configFile, root } })
+        if (config.vitePluginSsr.prerender && !config.vitePluginSsr.prerender.disableAutoRun) {
+          await prerender({ viteConfig: { configFile, root } })
+        }
+      } catch (err) {
+        // Avoid Rollup prefixing the error with `[vite-plugin-ssr:autoFullBuild]`, for example see https://github.com/brillout/vite-plugin-ssr/issues/472#issuecomment-1276274203
+        console.error(err)
       }
     }
   }
