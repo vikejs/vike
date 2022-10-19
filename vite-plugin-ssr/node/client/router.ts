3c58410c7785467456b40a92243b15142b49d726
Romuald Brillout <git@brillout.com>
2022-10-19 22:37:34 +0200

    fix: improve error message upon environment loading wrong exports entry (fix #481)

--- vite-plugin-ssr/node/client/router.ts
+++ vite-plugin-ssr/node/client/router.ts
@@ -1,12 +1,14 @@
-import { assertUsage, isBrowser } from '../utils'
-
 export { navigate }
 
+import { assertUsage } from '../../utils/assert'
+import { isBrowser } from '../../utils/isBrowser'
+
+assertUsage(
+  !isBrowser(),
+  '[`navigate(url)`] Something is wrong with your environment (it loads the wrong `vite-plugin-ssr/client/router` entry). This may be happening if you use Jest or Babel. Open a new GitHub issue to discuss a solution.'
+)
+
 function navigate(): never {
-  assertUsage(
-    !isBrowser(),
-    '[`navigate(url)`] Something is wrong with your environment (it loads the wrong `vite-plugin-ssr/client/router` entry). This may be happening if you use Jest or Babel. Open a new GitHub issue to discuss a solution.'
-  )
   assertUsage(
     false,
     '[`navigate(url)`] The `navigate(url)` function is only callable in the browser but you are calling it in Node.js.'
--- vite-plugin-ssr/utils/isBrowser.ts
+++ vite-plugin-ssr/utils/isBrowser.ts
@@ -1,6 +1,4 @@
-export { isBrowser }
-
-function isBrowser() {
+export function isBrowser() {
   // Using `typeof window !== 'undefined'` alone is not enough because some users use https://www.npmjs.com/package/ssr-window
   return typeof window !== 'undefined' && typeof window.scrollY === 'number'
 }
