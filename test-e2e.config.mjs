da536c312d2c3d08a1f6e50b7bb632e92e999f10
Romuald Brillout <git@brillout.com>
2022-10-19 22:17:46 +0200

    update test-e2e

--- test-e2e.config.mjs
+++ test-e2e.config.mjs
@@ -6,7 +6,7 @@ export default {
 }
 
 function tolerateError(log) {
-  return isSlowHookWarning() || isFetchExperimentalWarning() || isViteBug()
+  return isSlowHookWarning() || isNoErrorPageWarning() || isFetchExperimentalWarning() || isViteEsbuildBug()
 
   // [vite-plugin-ssr@0.4.42][Warning] The onBeforeRender() hook of /pages/star-wars/index/index.page.server.ts is taking more than 4 seconds
   function isSlowHookWarning() {
@@ -15,6 +15,16 @@ function tolerateError(log) {
       log.logText.includes('[Warning]') &&
       log.logText.includes('hook of') &&
       log.logText.includes('is taking more than 4 seconds')
+    )
+  }
+
+  // [vite-plugin-ssr@0.4.42][Warning] No `_error.page.js` found. We recommend creating a `_error.page.js` file. (This warning is not shown in production.)
+  function isNoErrorPageWarning() {
+    return (
+      log.logSource === 'stderr' &&
+      log.logText.includes(
+        'No `_error.page.js` found. We recommend creating a `_error.page.js` file. (This warning is not shown in production.)'
+      )
     )
   }
 
@@ -27,12 +37,24 @@ function tolerateError(log) {
     )
   }
 
+  // ```bash
   // [16:10:29.456][\examples\preact-client-routing][npm run preview][stderr] 4:10:29 PM [vite] Internal server error: The service is no longer running
-  function isViteBug() {
+  // ```
+  // ```bash
+  // [16:38:26.428][\examples\i18n][npm run dev][stderr] Error: The service was stopped
+  //     at D:\a\vite-plugin-ssr\vite-plugin-ssr\node_modules\.pnpm\esbuild@0.14.47\node_modules\esbuild\lib\main.js:1381:29
+  // ```
+  // ```bash
+  // [16:41:56.607][\examples\preact-server-routing][npm run preview][stderr] 4:41:56 PM [vite] Internal server error: The service was stopped
+  //   Plugin: vite:esbuild
+  //   File: D:/a/vite-plugin-ssr/vite-plugin-ssr/examples/preact-server-routing/renderer/_error.page.jsx
+  //       at D:\a\vite-plugin-ssr\vite-plugin-ssr\node_modules\.pnpm\esbuild@0.14.47\node_modules\esbuild\lib\main.js:1381:29
+  // ```
+  function isViteEsbuildBug() {
     return (
+      process.platform === 'win32' &&
       log.logSource === 'stderr' &&
-      log.logText.includes('vite') &&
-      log.logText.includes('Internal server error: The service is no longer running')
+      (log.logText.includes('The service is no longer running') || log.logText.includes('The service was stopped'))
     )
   }
 }
