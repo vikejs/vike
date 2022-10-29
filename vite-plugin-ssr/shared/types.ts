7b227173eab346dcf780442db01df4a13fafbfc4
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-10-29 17:24:00 +0200

    use jsdoc deprecated

--- vite-plugin-ssr/shared/types.ts
+++ vite-plugin-ssr/shared/types.ts
@@ -9,12 +9,12 @@ export type PageContextBuiltIn<Page = any> = {
   exports: Record<string, unknown>
   /** Same as `pageContext.exports` but cumulative */
   exportsAll: Record<string, { exportValue: unknown }[]>
-  /** Outdated. Don't use. */
+  /** @deprecated Don't use. */
   url: string
   /** The URL of the current page */
   urlOriginal: string
   /** If an error occurs, whether the error is a `404 Page Not Found` or a `500 Internal Server Error`, see https://vite-plugin-ssr.com/error-page */
   is404?: boolean
-  /** Outdated, do not use */
+  /** @deprecated do not use */
   pageExports: Record<string, unknown>
 } & PageContextUrls
