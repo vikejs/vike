d1ccd25a50509cceb86fb27d5e4afd0a90e3b2c7
Romuald Brillout <git@brillout.com>
2022-11-01 18:19:58 +0100

    fix: tolerate navigate to be loaded and called in tests (fix #496)

--- vite-plugin-ssr/node/client/router.ts
+++ vite-plugin-ssr/node/client/router.ts
@@ -1,16 +1,10 @@
 export { navigate }
 
-import { assertUsage } from '../../utils/assert'
-import { isBrowser } from '../../utils/isBrowser'
+import { assertWarning } from '../../utils/assert'
 
-assertUsage(
-  !isBrowser(),
-  'Something is wrong with your environment (it loads the wrong `node_modules/vite-plugin-ssr/package.json#exports["./client/router"]` entry). This may be happening if you use Jest or Babel. Open a new GitHub issue to discuss a solution.'
-)
-
-function navigate(): never {
-  assertUsage(
-    false,
-    '[`navigate(url)`] The `navigate(url)` function is only callable in the browser but you are calling it in Node.js.'
-  )
+function navigate() {
+  assertWarning(false, 'Calling navigate() on the server-side has no effect', {
+    showStackTrace: true,
+    onlyOnce: false
+  })
 }
