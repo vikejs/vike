4af167eaa03f3451088e7e1598562dd81a8bc013
Romuald Brillout <git@brillout.com>
2022-11-05 09:10:57 +0100

    minor

--- examples/redux/renderer/_default.page.client.jsx
+++ examples/redux/renderer/_default.page.client.jsx
@@ -7,6 +7,8 @@ import { getStore } from './store'
 
 async function render(pageContext) {
   const { Page } = pageContext
+  // We initilaize the store on every render because we use Server Routing. If we use Client Routing, then we should initialize the store only once instead.
+  // (See https://vite-plugin-ssr.com/server-routing-vs-client-routing for more information about Client Routing and Server Routing.)
   const store = getStore(pageContext.PRELOADED_STATE)
   hydrateRoot(
     document.getElementById('react-root'),
