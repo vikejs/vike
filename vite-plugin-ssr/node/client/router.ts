22f40af8bc99c7cb76c33e340ce06c1fc445891e
Romuald Brillout <git@brillout.com>
2022-11-07 10:15:57 +0100

    improve type export fix

--- vite-plugin-ssr/node/client/router.ts
+++ vite-plugin-ssr/node/client/router.ts
@@ -1,10 +1,11 @@
 export { navigate }
 
 import { assertWarning } from '../../utils/assert'
+import type { navigate as navigateOriginal } from '../../client/router'
 
-function navigate() {
+const navigate = (() => {
   assertWarning(false, 'Calling navigate() on the server-side has no effect', {
     showStackTrace: true,
     onlyOnce: false
   })
-}
+}) as any as typeof navigateOriginal
