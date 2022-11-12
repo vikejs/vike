d4d9e8797b57be29ca9dc7139bc3dbfad692c0b1
Romuald Brillout <git@brillout.com>
2022-11-12 18:47:05 +0100

    minor refactor

--- vite-plugin-ssr/node/plugin/plugins/config.ts
+++ vite-plugin-ssr/node/plugin/plugins/config.ts
@@ -1,8 +1,9 @@
 export { setVitePluginSsrConfig }
 
 import type { Plugin } from 'vite'
-import type { ConfigVpsUserProvided } from './config/ConfigVps'
-import { resolveConfigVps } from './config/resolveConfigVps'
+import type { ConfigVpsUserProvided, ConfigVpsResolved } from './config/ConfigVps'
+import { checkConfigVpsUserProvided, assertConfigVpsResolved } from './config/assertConfigVps'
+import { assertUsage } from '../utils'
 
 function setVitePluginSsrConfig(vpsConfig: unknown) {
   return {
@@ -16,4 +17,54 @@ function setVitePluginSsrConfig(vpsConfig: unknown) {
       return { vitePluginSsr }
     }
   } as Plugin
+}
+
+function resolveConfigVps(
+  fromPluginOptions: ConfigVpsUserProvided,
+  fromViteConfig: ConfigVpsUserProvided
+): ConfigVpsResolved {
+  {
+    const validationErr = checkConfigVpsUserProvided(fromPluginOptions)
+    if (validationErr)
+      assertUsage(false, `vite.config.js > vite-plugin-ssr option ${validationErr.prop} ${validationErr.errMsg}`)
+  }
+  {
+    const validationErr = checkConfigVpsUserProvided(fromViteConfig)
+    if (validationErr) assertUsage(false, `vite.config.js#vitePluginSsr.${validationErr.prop} ${validationErr.errMsg}`)
+  }
+
+  const vitePluginSsr: ConfigVpsResolved = {
+    disableAutoFullBuild: fromPluginOptions.disableAutoFullBuild ?? fromViteConfig.disableAutoFullBuild ?? false,
+    pageFiles: {
+      include: [...(fromPluginOptions.pageFiles?.include ?? []), ...(fromViteConfig.pageFiles?.include ?? [])],
+      addPageFiles: [
+        ...(fromPluginOptions.pageFiles?.addPageFiles ?? []),
+        ...(fromViteConfig.pageFiles?.addPageFiles ?? [])
+      ]
+    },
+    prerender: resolvePrerenderOptions(fromPluginOptions, fromViteConfig),
+    includeCSS: fromPluginOptions.includeCSS ?? fromViteConfig.includeCSS ?? [],
+    includeAssetsImportedByServer:
+      fromPluginOptions.includeAssetsImportedByServer ?? fromViteConfig.includeAssetsImportedByServer ?? false
+  }
+
+  assertConfigVpsResolved({ vitePluginSsr })
+  return vitePluginSsr
+}
+
+function resolvePrerenderOptions(fromPluginOptions: ConfigVpsUserProvided, fromViteConfig: ConfigVpsUserProvided) {
+  let prerender: ConfigVpsResolved['prerender'] = false
+
+  if (fromPluginOptions.prerender || fromViteConfig.prerender) {
+    const prerenderUserOptions =
+      typeof fromPluginOptions.prerender === 'boolean' ? {} : fromPluginOptions.prerender ?? {}
+    const prerenderViteConfig = typeof fromViteConfig.prerender === 'boolean' ? {} : fromViteConfig.prerender ?? {}
+    prerender = {
+      partial: prerenderUserOptions.partial ?? prerenderViteConfig.partial ?? false,
+      noExtraDir: prerenderUserOptions.noExtraDir ?? prerenderViteConfig.noExtraDir ?? false,
+      parallel: prerenderUserOptions.parallel ?? prerenderViteConfig.parallel ?? true,
+      disableAutoRun: prerenderUserOptions.disableAutoRun ?? prerenderViteConfig.disableAutoRun ?? false
+    }
+  }
+  return prerender
 }
