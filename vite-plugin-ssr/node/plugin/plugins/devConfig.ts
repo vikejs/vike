90840e5b218cf484841eaa7b71846400ded2e1f1
Romuald Brillout <git@brillout.com>
2022-11-14 11:04:20 +0100

    improve optimizeDeps comments

--- vite-plugin-ssr/node/plugin/plugins/devConfig.ts
+++ vite-plugin-ssr/node/plugin/plugins/devConfig.ts
@@ -18,11 +18,16 @@ function devConfig(): Plugin[] {
         ssr: { external: ['vite-plugin-ssr'] },
         optimizeDeps: {
           exclude: [
-            // We exclude the client code to support `import.meta.glob()`
+            // We exclude the vite-plugin-ssr client to be able to use `import.meta.glob()`
             'vite-plugin-ssr/client',
             'vite-plugin-ssr/client/router',
             'vite-plugin-ssr/routing',
-            // We also have to exclude these because of `pnpm`
+            // - We also exclude @brillout/json-serializer to avoid:
+            //   ```
+            //   9:28:58 AM [vite] ✨ new dependencies optimized: @brillout/json-serializer/parse
+            //   9:28:58 AM [vite] ✨ optimized dependencies changed. reloading
+            //   ```
+            // - Previously, we had to exclude @brillout/json-serializer because of pnpm, but this doesn't seem to be the case anymore
             '@brillout/json-serializer/parse',
             '@brillout/json-serializer/stringify'
           ]
