0b7c9bc3935f1df2430ce4939134d910d773a935
Romuald Brillout <git@brillout.com>
2022-11-17 18:28:27 +0100

    fix: improve error message upon wrong Vite dev server usage (fix #516)

--- vite-plugin-ssr/node/plugin/plugins/retrieveDevServer.ts
+++ vite-plugin-ssr/node/plugin/plugins/retrieveDevServer.ts
@@ -1,12 +1,22 @@
 export { retrieveDevServer }
 
-import type { Plugin } from 'vite'
+import type { Plugin, ResolvedConfig } from 'vite'
 import { setViteDevServer } from '../../globalContext'
+import { assertUsage } from '../utils'
 
 function retrieveDevServer(): Plugin {
+  let config: ResolvedConfig
   return {
     name: 'vite-plugin-ssr:retrieveDevServer',
+    configResolved(config_) {
+      config = config_
+    },
     configureServer(viteDevServer) {
+      const { mode } = config
+      assertUsage(
+        mode === 'development',
+        `When using Vite's development server (\`vite.createServer()\`), \`mode\` is expected to be \`development\` (it's \`${mode}\` instead).`
+      )
       setViteDevServer(viteDevServer)
     }
   } as Plugin
