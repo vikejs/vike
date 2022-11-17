77308016a23713ec75deaeb609cf87228d1573fa
Romuald Brillout <git@brillout.com>
2022-11-17 15:55:44 +0100

    improve <Link>

--- examples/stem-react/pages/_default/Link.tsx
+++ examples/stem-react/pages/_default/Link.tsx
@@ -5,8 +5,10 @@ import React from 'react'
 
 function Link({ href, children }: { href: string; children: string }) {
   const pageContext = usePageContext() as { urlPathname: string } // TODO
+  const { urlPathname } = pageContext
+  const isActive = href === '/' ? urlPathname === href : urlPathname.startsWith(href)
   return (
-    <a href={href} className={pageContext.urlPathname.startsWith(href) ? 'is-active' : undefined}>
+    <a href={href} className={isActive ? 'is-active' : undefined}>
       {children}
     </a>
   )
