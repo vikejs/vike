1e4dbe65cf552665b388eac54e4dd3ad3f50db00
Romuald Brillout <git@brillout.com>
2022-11-17 22:36:02 +0100

    fix: refine wrong usage message (#516)

--- vite-plugin-ssr/node/plugin/plugins/retrieveDevServer.ts
+++ vite-plugin-ssr/node/plugin/plugins/retrieveDevServer.ts
@@ -15,7 +15,7 @@ function retrieveDevServer(): Plugin {
       const { mode } = config
       assertUsage(
         mode === 'development',
-        `When using Vite's development server (\`vite.createServer()\`), \`mode\` is expected to be \`development\` (it's \`${mode}\` instead).`
+        `When using Vite's development server (\`vite.createServer()\`), \`mode\` is expected to be \`development\` (it's \`${mode}\` instead). Note that Vite's development server isn't supposed to be used in production.`
       )
       setViteDevServer(viteDevServer)
     }
