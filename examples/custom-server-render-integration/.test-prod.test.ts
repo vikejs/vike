5c22186d307e52d6580cba292b8c445d00ec98e3
Romuald Brillout <git@brillout.com>
2022-11-22 18:16:08 +0100

    remove examples/custom-server-render-integration/

--- examples/custom-server-render-integration/.test-prod.test.ts
+++
@@ -1,2 +0,0 @@
-import { testRun } from './.testRun'
-testRun('npm run prod')
--- examples/custom-server-render-integration/renderer/_default.page.client.jsx
+++
@@ -1,9 +0,0 @@
-export { render }
-
-import React from 'react'
-import { hydrateRoot } from 'react-dom/client'
-
-async function render(pageContext) {
-  const { Page } = pageContext
-  hydrateRoot(document.getElementById('react-root'), <Page />)
-}
