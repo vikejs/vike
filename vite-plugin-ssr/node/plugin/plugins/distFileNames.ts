f3d956e3881f7add6fb1d5a609e762e5f383cfa1
Romuald Brillout <git@brillout.com>
2022-11-25 13:29:12 +0100

    revert "refactor distFileNames"

--- vite-plugin-ssr/node/plugin/plugins/distFileNames.ts
+++ vite-plugin-ssr/node/plugin/plugins/distFileNames.ts
@@ -4,6 +4,7 @@ import { assertPosixPath, assert, isCallable, assertUsage } from '../utils'
 import type { Plugin, ResolvedConfig } from 'vite'
 import path from 'path'
 import { determinePageId } from '../../../shared/determinePageId'
+import { deduceRouteStringFromFilesystemPath } from '../../../shared/route/deduceRouteStringFromFilesystemPath'
 import { extractAssetsRE } from './extractAssetsPlugin'
 
 function distFileNames(): Plugin {
@@ -30,50 +31,35 @@ function getAssetFileName(
   assetFileName: string | undefined,
   config: ResolvedConfig
 ): string {
-  console.log('a1', assetInfo.name)
-  if (assetFileName) return assetFileName
-
-  const nameOriginal = assetInfo.name
   const assetsDir = getAssetsDir(config)
-  if( !nameOriginal ) return `${assetsDir}/[name]-[hash][extname]`
-  const { name, ext } = getExtAsset(nameOriginal)
 
   // Not sure when/why this happens
-  if (BLACK_LIST.includes(name)) {
-    assetFileName = `${assetsDir}/chunk-[hash]${ext}`
-    console.log('a2', assetFileName)
-    return assetFileName
+  if (assetInfo.name && BLACK_LIST.includes(assetInfo.name)) {
+    assetFileName ??= `${assetsDir}/chunk-[hash][extname]`
   }
 
   // dist/client/assets/index.page.server.jsx_extractAssets_lang.e4e33422.css
   // => dist/client/assets/index.page.server.e4e33422.css
   if (
     // Vite 2
-    name.endsWith('_extractAssets_lang.css') ||
+    assetInfo.name?.endsWith('_extractAssets_lang.css') ||
     // Vite 3
-    name.endsWith('?extractAssets&lang.css')
+    assetInfo.name?.endsWith('?extractAssets&lang.css')
   ) {
-    // TODO
-    //const nameBase = name.split('.').slice(0, -2).join('.')
-    assetFileName = `${assetsDir}/${name}-[hash]${ext}`
-    console.log('a3', assetFileName)
-    return assetFileName
+    const nameBase = assetInfo.name.split('.').slice(0, -2).join('.')
+    assetFileName ??= `${assetsDir}/${nameBase}.[hash][extname]`
   }
 
-  assetFileName = `${assetsDir}/${name}-[hash]${ext}`
-  console.log('a4', assetFileName)
+  assetFileName ??= `${assetsDir}/[name].[hash][extname]`
   return assetFileName
 }
 
 function getChunkFileName(chunkInfo: PreRenderedChunk, chunkFileName: string | undefined, config: ResolvedConfig) {
-  console.log('c1', chunkInfo.facadeModuleId)
-  if (chunkFileName) return chunkFileName
-
   const { root } = config
   assertPosixPath(root)
   const assetsDir = getAssetsDir(config)
 
-  const { facadeModuleId: id } = chunkInfo
+  const id = chunkInfo.facadeModuleId
 
   if (id) {
     assertPosixPath(id)
@@ -85,58 +71,34 @@ function getChunkFileName(chunkInfo: PreRenderedChunk, chunkFileName: string | u
     !id ||
     id.includes('/node_modules/') ||
     !id.startsWith(root) ||
-    extractAssetsRE.test(id)
+    (id.includes('.page.server.') && extractAssetsRE.test(id))
   ) {
-    chunkFileName = `${assetsDir}/chunk-[hash].js`
-    console.log('c2', chunkFileName)
+    chunkFileName ??= `${assetsDir}/chunk-[hash].js`
     return chunkFileName
   }
 
-  const { ext, name } = getExtChunk(chunkInfo.name)
+  chunkFileName ??= `${assetsDir}/[name].[hash].js`
 
-  if (id.includes('.page.')) {
-    const chunkPath = deduceChunkPath(id, root)
-    chunkFileName = [assetsDir, chunkPath, `${name}-[hash].${ext}`].filter(Boolean).join('/')
-    console.log('c3', chunkFileName)
-    return chunkFileName
+  const { name } = chunkInfo
+  if (name.startsWith('index.page.') || name === 'index.page') {
+    const chunkName = deduceChunkNameFromFilesystemRouting(id, root)
+    if (chunkName) {
+      chunkFileName = chunkFileName.replace('[name]', name.replace('index', chunkName))
+      return chunkFileName
+    }
   }
-
-  chunkFileName = `${assetsDir}/${name}-[hash].${ext}`
-  console.log('c4', chunkFileName)
   return chunkFileName
 }
 
-function getExtChunk(nameOriginal: string): { name: string, ext: string } {
-  const [name, ...exts] = nameOriginal.split('.')
-  assert(name)
-  // "[extname]" is not a valid placeholder in the "output.chunkFileNames" pattern.
-  const ext = [...exts, 'js'].join('.')
-  return { name, ext }
-}
-function getExtAsset(nameOriginal: string): { name: string, ext: string } {
-  const [name, ...exts] = nameOriginal.split('.')
-  assert(name)
-  const ext = ['', ...exts].join('.')
-  return { name, ext }
-}
-
-function deduceChunkPath(id: string, root: string): string {
+function deduceChunkNameFromFilesystemRouting(id: string, root: string): string | null {
   assert(id?.startsWith(root), { id, root })
   const pathRelative = path.posix.relative(root, id)
   assert(!pathRelative.startsWith('.') && !pathRelative.startsWith('/'), { id, root })
   const pageId = determinePageId('/' + pathRelative)
-  assert(!pageId.endsWith('.'), id)
-  assert(!pageId.endsWith('.page'), id)
-  let chunkPath = pageId
-    .split('/')
-  // Remove file name
-    .slice(0, -1)
-    .join('/')
-  assert(chunkPath.startsWith('/') || chunkPath==='')
-  chunkPath = chunkPath.slice(1)
-  assert(!chunkPath.startsWith('/'))
-  assert(!chunkPath.endsWith('/'))
-  return chunkPath
+  const routeString = deduceRouteStringFromFilesystemPath(pageId, [])
+  const dirS = routeString.split('/')
+  const pageFileName = dirS[dirS.length - 1]
+  return pageFileName ?? null
 }
 
 function setChunkFileNames(
