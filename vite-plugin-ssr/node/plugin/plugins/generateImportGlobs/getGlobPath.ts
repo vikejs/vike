474d3f1462de77c039806c93448c18c9a964c8a0
Romuald Brillout <git@brillout.com>
2022-12-01 17:52:16 +0100

    minor

--- vite-plugin-ssr/node/plugin/plugins/generateImportGlobs/getGlobPath.ts
+++ vite-plugin-ssr/node/plugin/plugins/generateImportGlobs/getGlobPath.ts
@@ -1,19 +1,10 @@
 export { getGlobPath }
 
-import { assertPosixPath, toPosixPath, scriptFileExtensions } from '../../utils'
-import path from 'path'
+import { assertPosixPath, scriptFileExtensions } from '../../utils'
 
-function getGlobPath(
-  globRoot: string,
-  fileSuffix: 'page' | 'page.client' | 'page.server' | 'page.route',
-  root?: string
-): string {
+function getGlobPath(globRoot: string, fileSuffix: 'page' | 'page.client' | 'page.server' | 'page.route'): string {
   assertPosixPath(globRoot)
   let globPath = [...globRoot.split('/'), '**', `*.${fileSuffix}.${scriptFileExtensions}`].filter(Boolean).join('/')
-  if (root) {
-    globPath = toPosixPath(path.posix.join(root, globPath))
-  } else {
-    globPath = '/' + globPath
-  }
+  globPath = '/' + globPath
   return globPath
 }
