bdaf70db92ed857462bac26f29e67b1c94960f96
Romuald Brillout <git@brillout.com>
2022-12-05 17:23:47 +0100

    minor

--- vite-plugin-ssr/node/page-files/setup.ts
+++ vite-plugin-ssr/node/page-files/setup.ts
@@ -1,36 +1,16 @@
 import { setPageFilesAsync } from '../../shared/getPageFiles'
 import { assert, debugGlob } from '../utils'
 import { getViteDevServer } from '../globalContext'
-//import { pathToFileURL } from 'url'
-import { ViteDevServer } from 'vite'
 import { virtualModuleIdPageFilesServer } from '../plugin/plugins/generateImportGlobs/virtualModuleIdPageFiles'
 
 setPageFilesAsync(getPageFilesExports)
 
 async function getPageFilesExports(): Promise<unknown> {
   const viteDevServer = getViteDevServer()
-  if (viteDevServer) {
-    const pageFilesExports = await dev(viteDevServer)
-    debugGlob('Page files found: ', pageFilesExports)
-    return pageFilesExports
-  }
-  //const pageFilesExports = await distAutoImporterDisabled()
-  assert(false)
-}
-
-async function dev(viteDevServer: ViteDevServer) {
   assert(viteDevServer)
   const moduleExports = await viteDevServer.ssrLoadModule(virtualModuleIdPageFilesServer)
   const pageFilesExports: unknown = (moduleExports as any).default || (moduleExports as any)
+  debugGlob('Page files found: ', pageFilesExports)
   assert(pageFilesExports)
   return pageFilesExports
 }
-
-/*
-async function distAutoImporterDisabled(): Promise<unknown> {
-}
-
-async function dynamicImport(filePath: string): Promise<unknown> {
-  return new Function('file', 'return import(file)')(pathToFileURL(filePath).href)
-}
-*/
