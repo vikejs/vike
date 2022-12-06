46671ba76ddd1a1d2e015af6f6286c998af76dbc
Romuald Brillout <git@brillout.com>
2022-12-06 15:42:35 +0100

    minor

---
+++ vite-plugin-ssr/node/renderPage/assertArguments.ts
@@ -0,0 +1,64 @@
+export { assertArguments }
+
+import { assert, assertUsage, assertWarning, hasProp, isPlainObject } from '../utils'
+
+function assertArguments(...args: unknown[]): void {
+  const prefix = '[renderPage(pageContextInit)]'
+
+  const pageContextInit = args[0]
+  assertUsage(pageContextInit, prefix + ' argument `pageContextInit` is missing')
+  const len = args.length
+  assertUsage(len === 1, `${prefix} You passed ${len} arguments but \`renderPage()\` accepts only one argument.'`)
+
+  assertUsage(
+    isPlainObject(pageContextInit),
+    `${prefix} \`pageContextInit\` should be a plain JavaScript object, but \`pageContextInit.constructor === ${
+      (pageContextInit as any).constructor
+    }\``
+  )
+
+  if ('url' in pageContextInit) {
+    assertWarning(
+      false,
+      '`pageContext.url` has been renamed to `pageContext.urlOriginal`: replace `renderPage({ url })` with `renderPage({ urlOriginal })`. (See https://vite-plugin-ssr.com/migration/0.4.23 for more information.)',
+      { showStackTrace: false, onlyOnce: true }
+    )
+    pageContextInit.urlOriginal = pageContextInit.url
+    delete pageContextInit.url
+  }
+  assert(!('url' in pageContextInit))
+
+  assertUsage(
+    hasProp(pageContextInit, 'urlOriginal'),
+    prefix + ' `pageContextInit` is missing the property `pageContextInit.urlOriginal`'
+  )
+  assertUsage(
+    typeof pageContextInit.urlOriginal === 'string',
+    prefix +
+      ' `pageContextInit.urlOriginal` should be a string but `typeof pageContextInit.urlOriginal === "' +
+      typeof pageContextInit.urlOriginal +
+      '"`.'
+  )
+  assertUsage(
+    pageContextInit.urlOriginal.startsWith('/') || pageContextInit.urlOriginal.startsWith('http'),
+    prefix +
+      ' `pageContextInit.urlOriginal` should start with `/` (e.g. `/product/42`) or `http` (e.g. `http://example.org/product/42`) but `pageContextInit.urlOriginal === "' +
+      pageContextInit.urlOriginal +
+      '"`'
+  )
+
+  try {
+    const { urlOriginal } = pageContextInit
+    const urlWithOrigin = urlOriginal.startsWith('http') ? urlOriginal : 'http://fake-origin.example.org' + urlOriginal
+    // We use `new URL()` to validate the URL. (`new URL(url)` throws an error if `url` isn't a valid URL.)
+    new URL(urlWithOrigin)
+  } catch (err) {
+    assertUsage(
+      false,
+      prefix +
+        ' `pageContextInit.urlOriginal` should be a URL but `pageContextInit.urlOriginal==="' +
+        pageContextInit.urlOriginal +
+        '"`.'
+    )
+  }
+}
--- vite-plugin-ssr/node/renderPage/index.ts
+++ vite-plugin-ssr/node/renderPage/index.ts
@@ -46,6 +46,7 @@ import { handlePageContextRequestUrl } from './handlePageContextRequestUrl'
 import type { PreloadFilter } from '../html/injectAssets/getHtmlTags'
 import { createHttpResponseObject, HttpResponse } from './createHttpResponseObject'
 import { assertError, logError, logErrorIfDifferentFromOriginal } from './logError'
+import { assertArguments } from './assertArguments'
 
 type GlobalRenderingContext = {
   _allPageIds: string[]
@@ -737,68 +738,7 @@ async function executeRenderHook(
   return { htmlRender, renderFilePath }
 }
 
-function assertArguments(...args: unknown[]) {
-  const prefix = '[renderPage(pageContextInit)]'
-
-  const pageContextInit = args[0]
-  assertUsage(pageContextInit, prefix + ' argument `pageContextInit` is missing')
-  const len = args.length
-  assertUsage(len === 1, `${prefix} You passed ${len} arguments but \`renderPage()\` accepts only one argument.'`)
-
-  assertUsage(
-    isPlainObject(pageContextInit),
-    `${prefix} \`pageContextInit\` should be a plain JavaScript object, but \`pageContextInit.constructor === ${
-      (pageContextInit as any).constructor
-    }\``
-  )
-
-  if ('url' in pageContextInit) {
-    assertWarning(
-      false,
-      '`pageContext.url` has been renamed to `pageContext.urlOriginal`: replace `renderPage({ url })` with `renderPage({ urlOriginal })`. (See https://vite-plugin-ssr.com/migration/0.4.23 for more information.)',
-      { showStackTrace: false, onlyOnce: true }
-    )
-    pageContextInit.urlOriginal = pageContextInit.url
-    delete pageContextInit.url
-  }
-  assert(!('url' in pageContextInit))
-
-  assertUsage(
-    hasProp(pageContextInit, 'urlOriginal'),
-    prefix + ' `pageContextInit` is missing the property `pageContextInit.urlOriginal`'
-  )
-  assertUsage(
-    typeof pageContextInit.urlOriginal === 'string',
-    prefix +
-      ' `pageContextInit.urlOriginal` should be a string but `typeof pageContextInit.urlOriginal === "' +
-      typeof pageContextInit.urlOriginal +
-      '"`.'
-  )
-  assertUsage(
-    pageContextInit.urlOriginal.startsWith('/') || pageContextInit.urlOriginal.startsWith('http'),
-    prefix +
-      ' `pageContextInit.urlOriginal` should start with `/` (e.g. `/product/42`) or `http` (e.g. `http://example.org/product/42`) but `pageContextInit.urlOriginal === "' +
-      pageContextInit.urlOriginal +
-      '"`'
-  )
-
-  try {
-    const { urlOriginal } = pageContextInit
-    const urlWithOrigin = urlOriginal.startsWith('http') ? urlOriginal : 'http://fake-origin.example.org' + urlOriginal
-    // We use `new URL()` to validate the URL. (`new URL(url)` throws an error if `url` isn't a valid URL.)
-    new URL(urlWithOrigin)
-  } catch (err) {
-    assertUsage(
-      false,
-      prefix +
-        ' `pageContextInit.urlOriginal` should be a URL but `pageContextInit.urlOriginal==="' +
-        pageContextInit.urlOriginal +
-        '"`.'
-    )
-  }
-}
-
-function warnMissingErrorPage() {
+function warnMissingErrorPage(): void {
   const globalContext = getGlobalContext()
   if (!globalContext.isProduction) {
     assertWarning(
