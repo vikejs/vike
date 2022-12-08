e50e815bdc1ae53c4ecc137c1f82a74fbf04286d
Romuald Brillout <git@brillout.com>
2022-12-08 16:16:29 +0100

    fix test

--- examples/base-url-server/.test.ts
+++ examples/base-url-server/.test.ts
@@ -1,3 +1,3 @@
 import { testRun } from '../base-url/.testRun'
 
-testRun('npm run start', { base: '/some/base-url/', baseAssets: 'http://localhost:8080/cdn/' })
+testRun('npm run start', { baseServer: '/some/base-url/', baseAssets: 'http://localhost:8080/cdn/' })
