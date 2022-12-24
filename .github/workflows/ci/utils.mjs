f4ddfbfbdb34ab988d54689f4e3f1101c7fd6d05
Romuald Brillout <git@brillout.com>
2022-12-24 22:12:18 +0100

    minor

--- .github/workflows/ci/utils.mjs
+++ .github/workflows/ci/utils.mjs
@@ -4,7 +4,7 @@ export { isObject }
 import { execSync } from 'child_process'
 
 /** @type { (command: string, options?: { cwd?: string }) => string } */
-function cmd(command, { cwd } = {}) {
+function cmd(command, { cwd } = { cwd: undefined }) {
   let stdout = execSync(command, { encoding: 'utf8', cwd })
   stdout = stdout.split(/\s/).filter(Boolean).join(' ')
   return stdout
