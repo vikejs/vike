0e7a53f889500e0f7b293645ac5a5b7a08dcf0c8
Romuald Brillout <git@brillout.com>
2023-01-04 20:49:54 +0100

    move node/helpers to node/runtime/helpers

--- vite-plugin-ssr/node/helpers/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './assets'
-export * from './serializePageContextClientSide'
-export * from './viteManifest'
---
+++ vite-plugin-ssr/node/runtime/helpers.ts
@@ -0,0 +1,3 @@
+export * from '../helpers/assets'
+export * from '../helpers/serializePageContextClientSide'
+export * from '../helpers/viteManifest'
