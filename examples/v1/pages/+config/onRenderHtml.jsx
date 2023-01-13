5fc2e29e1705cdec8ce015b8d986b6b6372c56b1
Romuald Brillout <git@brillout.com>
2023-01-13 16:10:16 +0100

    simplify v1 example

--- examples/v1/pages/+config/onRenderHtml.jsx
+++ examples/v1/pages/+config/onRenderHtml.jsx
@@ -1,23 +1,24 @@
 export default onRenderHtml
 
 import React from 'react'
-import { renderToStream } from 'react-streaming/server'
-import { escapeInject } from 'vite-plugin-ssr'
+import { renderToString } from 'react-dom/server'
+import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr'
 import { PageLayout } from './PageLayout'
 
 async function onRenderHtml(pageContext) {
   const { Page, pageProps, userAgent } = pageContext
-  const stream = await renderToStream(
-    <PageLayout>
-      <Page {...pageProps} />
-    </PageLayout>,
-    { userAgent }
+  const viewHtml = dangerouslySkipEscape(
+    renderToString(
+      <PageLayout>
+        <Page {...pageProps} />
+      </PageLayout>
+    )
   )
 
   return escapeInject`<!DOCTYPE html>
     <html>
       <body>
-        <div id="page-view">${stream}</div>
+        <div id="page-view">${viewHtml}</div>
       </body>
     </html>`
 }
