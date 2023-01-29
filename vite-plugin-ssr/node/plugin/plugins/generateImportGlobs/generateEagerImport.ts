6eda464fbfa4c5064f14e25700c4b2ca57e6b611
Romuald Brillout <git@brillout.com>
2023-01-30 00:17:04 +0100

    minor fix

--- vite-plugin-ssr/node/plugin/plugins/generateImportGlobs/generateEagerImport.ts
+++ vite-plugin-ssr/node/plugin/plugins/generateImportGlobs/generateEagerImport.ts
@@ -3,7 +3,7 @@ export { generateEagerImport }
 let varCounterGlobal = 0
 
 function generateEagerImport(importPath: string, varCounter?: number): { importVar: string; importStatement: string } {
-  if (!varCounter) varCounter = varCounterGlobal++
+  if (varCounter === undefined) varCounter = varCounterGlobal++
   const importVar = `__import_${varCounter}`
   const importStatement = `import * as ${importVar} from '${importPath}';`
   return { importVar, importStatement }
