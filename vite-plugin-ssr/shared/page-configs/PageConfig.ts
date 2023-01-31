2162b06ab4ca9105e654c268437ddce08dc5690e
Romuald Brillout <git@brillout.com>
2023-01-31 15:44:11 +0100

    clean up

--- vite-plugin-ssr/shared/page-configs/PageConfig.ts
+++ vite-plugin-ssr/shared/page-configs/PageConfig.ts
@@ -8,6 +8,37 @@ type c_Env = 'client-only' | 'server-only' | 'server-and-client' | 'routing'
 
 type ConfigName = string
 
+type PageConfigData = {
+  pageId2: string // TODO: rename
+  pageConfigFilePath: string
+  routeFilesystem: null | string
+  codeFilesImporter: string
+  configSources: Record<
+    ConfigName,
+    {
+      configFilePath: string
+      c_env: c_Env
+      configValue?: unknown
+      codeFilePath?: string
+    }
+  >
+}
+
+// TODO: rename
+type PageConfig2 = PageConfigData & {
+  loadCodeFiles: () => Promise<
+    {
+      configName: string
+      codeFilePath: string
+      codeFileExports: Record<string, unknown>
+    }[]
+  >
+}
+
+type PageConfigLoaded = PageConfig2 & {
+  configValues: Record<string, unknown>
+}
+
 type PageConfigGlobal = Record<
   ConfigName,
   {
@@ -15,34 +46,3 @@ type PageConfigGlobal = Record<
     configValue: unknown
   }
 >
-
-type ConfigSource = {
-  configFilePath: string
-  c_env: c_Env
-  configValue?: unknown
-  codeFilePath?: string
-}
-
-type PageConfigData = {
-  pageId2: string
-  codeFilesImporter: string
-  routeFilesystem: null | string
-  pageConfigFilePath: string
-  configSources: Record<ConfigName, ConfigSource>
-}
-
-type CodeFile = { configName: string; codeFilePath: string; codeFileExports: Record<string, unknown> }
-
-// TODO: rename
-type PageConfig2 = {
-  pageId2: string // TODO: rename
-  pageConfigFilePath: string
-  routeFilesystem: null | string
-  codeFilesImporter: string
-  configSources: Record<ConfigName, ConfigSource>
-  loadCodeFiles: () => Promise<CodeFile[]>
-}
-
-type PageConfigLoaded = PageConfig2 & {
-  configValues: Record<string, unknown> // TODO: rename?
-}
