a22c3152439166889fe323476efd7812df49e46b
Romuald Brillout <git@brillout.com>
2023-02-05 14:23:57 +0100

    minor

--- examples/react-full-v1/pages/star-wars/@id/+config.ts
+++ examples/react-full-v1/pages/star-wars/@id/+config.ts
@@ -1,4 +1,11 @@
 export default {
   Page: './Page.tsx',
   onBeforeRender: './onBeforeRender.ts'
+  /* TODO
+  configDefinitions: {
+    onBeforeRender: {
+      c_env: ['server-and-client']
+    }
+  }
+  */
 }
