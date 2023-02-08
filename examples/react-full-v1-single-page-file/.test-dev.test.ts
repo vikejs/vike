32a2bd8760055f5a8fce4af6eeae7d92e8974b97
Romuald Brillout <git@brillout.com>
2023-02-08 13:01:16 +0100

    revert "add examples/react-full-v1-single-page-file/"

--- examples/react-full-v1-single-page-file/.test-dev.test.ts
+++
@@ -1,2 +0,0 @@
-import { testRun } from '../react-full/.testRun'
-testRun('react', 'npm run dev')
--- examples/react-full-v1-single-page-file/pages/hello/onPrerender.ts
+++
@@ -1,7 +0,0 @@
-export default onPrerender
-
-import { names } from './names'
-
-function onPrerender(): string[] {
-  return ['/hello', ...names.map((name) => `/hello/${name}`)]
-}
