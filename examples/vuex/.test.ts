45796bddc8f23d29c0b97d49e36572ed1443a0b0
Romuald Brillout <git@brillout.com>
2023-02-13 16:36:21 +0100

    update examples/vuex/ deps

--- examples/vuex/.test.ts
+++ examples/vuex/.test.ts
@@ -1,4 +1,4 @@
-import { fetchHtml, run, test, expect, skip } from '@brillout/test-e2e'
+import { fetchHtml, run, test, expect } from '@brillout/test-e2e'
 run('npm run dev')
 
 test('page content is rendered to HTML', async () => {
