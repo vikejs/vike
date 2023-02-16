86944ebbfaed769c1d4fe97321726d7126a5f546
Romuald Brillout <git@brillout.com>
2023-02-17 00:39:30 +0100

    fix: update link to https://vite-plugin-ssr.com/route-function#cannot-provide-pagecontext

--- vite-plugin-ssr/shared/route/resolveRouteFunction.ts
+++ vite-plugin-ssr/shared/route/resolveRouteFunction.ts
@@ -58,7 +58,7 @@ async function resolveRouteFunction(
 
   assertUsage(
     !('pageContext' in result),
-    'Providing `pageContext` in Route Functions is forbidden, see https://vite-plugin-ssr.com/route-function#async'
+    'Providing `pageContext` in Route Functions is forbidden, see https://vite-plugin-ssr.com/route-function#cannot-provide-pagecontext'
   )
 
   assert(isPlainObject(routeParams))
