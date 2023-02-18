8c5d10956bb72a8f0668a3f4d16cdf6062a198ad
Romuald Brillout <git@brillout.com>
2023-02-18 15:49:56 +0100

    minor

--- examples/i18n/renderer/_default.page.route.js
+++ examples/i18n/renderer/_default.page.route.js
@@ -3,19 +3,13 @@ export { onBeforeRoute }
 import { extractLocale } from '../locales'
 
 function onBeforeRoute(pageContext) {
-  let urlMod = pageContext.urlOriginal
-
-  const { urlWithoutLocale, locale } = extractLocale(urlMod)
-  urlMod = urlWithoutLocale
-
+  const { urlWithoutLocale, locale } = extractLocale(pageContext.urlOriginal)
   return {
     pageContext: {
-      // We make `locale` available as `pageContext.locale`.
-      // We then use https://vite-plugin-ssr.com/pageContext-anywhere
-      // to access `pageContext.locale` in any React/Vue component.
+      // We make `locale` available as `pageContext.locale`. We can then use https://vite-plugin-ssr.com/pageContext-anywhere to access pageContext.locale in any React/Vue component.
       locale,
       // We overwrite the original URL
-      urlOriginal: urlMod
+      urlOriginal: urlWithoutLocale
     }
   }
 }
