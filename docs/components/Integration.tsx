c175518acc4f0ca82654ca64a4989f6cf8ebb0e6
Romuald Brillout <git@brillout.com>
2023-02-21 16:06:12 +0100

    minor refactor

--- docs/components/Integration.tsx
+++ docs/components/Integration.tsx
@@ -2,7 +2,7 @@ export { Integration }
 
 import React from 'react'
 import { assert } from '@brillout/docpress'
-import { Invitation } from './Invitation'
+import { projectInfo } from '../utils'
 
 function Integration({ toolTypeName }: { toolTypeName: string }) {
   assert(toolTypeName)
@@ -12,6 +12,15 @@ function Integration({ toolTypeName }: { toolTypeName: string }) {
         In principle, we can use <code>vite-plugin-ssr</code> with any {toolTypeName}. If you're having difficulties
         integrating {toolTypeName.startsWith('a') ? 'an' : 'a'} {toolTypeName}, <Invitation />.
       </p>
+    </>
+  )
+}
+
+function Invitation() {
+  return (
+    <>
+      <a href={projectInfo.discordInvite}>join our Discord server</a> or{' '}
+      <a href={projectInfo.githubIssues}>open a new GitHub issue</a>
     </>
   )
 }
