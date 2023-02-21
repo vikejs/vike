8ca067ea68d9efc4b9c879b80c85e79448235017
Romuald Brillout <git@brillout.com>
2023-02-21 16:34:20 +0100

    docs: minor

--- docs/components/Integration.tsx
+++ docs/components/Integration.tsx
@@ -9,8 +9,9 @@ function Integration({ toolTypeName }: { toolTypeName: string }) {
   return (
     <>
       <p>
-        In principle, we can use <code>vite-plugin-ssr</code> with any {toolTypeName}. If you're having difficulties
-        integrating {toolTypeName.startsWith('a') ? 'an' : 'a'} {toolTypeName}, <a href={projectInfo.githubDiscussions}>create a new discussion on GitHub</a>.
+        In principle, we can use <code>vite-plugin-ssr</code> with any {toolTypeName}.
+        <a href={projectInfo.githubDiscussions}>Create a new discussion on GitHub</a> if you have questions or if you
+        want help with integrating {toolTypeName.startsWith('a') ? 'an' : 'a'} {toolTypeName},
       </p>
     </>
   )
