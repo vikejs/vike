c32cac0d0a65c2a6e3b9829e6b4b2c663145fcce
Szabolcs Csizmadia <Szabolcs.Csizmadia-ext@querplex.biz>
2023-02-24 13:15:36 +0100

    inject assets after meta charset (#638)

--- vite-plugin-ssr/node/runtime/html/injectAssets/injectHtmlTags.spec.ts
+++ vite-plugin-ssr/node/runtime/html/injectAssets/injectHtmlTags.spec.ts
@@ -1,4 +1,4 @@
-import { injectAtOpeningTag, injectAtClosingTag } from './injectHtmlTags'
+import { injectHtmlFragments, injectAtOpeningTag, injectAtClosingTag } from './injectHtmlTags'
 import { expect, describe, it } from 'vitest'
 
 const htmlHead1 = '<html><head></head></html>'
@@ -7,6 +7,29 @@ const htmlHead2 = `<html>
 </html>`
 const htmlHead3 = `<html>
   <head>
+  </head>
+</html>`
+
+const htmlHeadMetaCharsetWithoutWhitespace = '<html><head><meta charset="utf-8"></head></html>'
+const htmlHeadMetaCharsetWithWhitespace = `<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+</html>`
+const htmlHeadMetaCharsetWithTagBetween = `<html>
+  <head>
+    <link href="/foo.css">
+    <meta charset="utf-8">
+  </head>
+</html>`
+const htmlHeadMetaCharsetWithClosingTag = `<html>
+  <head>
+    <meta charset="utf-8"></meta>
+  </head>
+</html>`
+const htmlHeadMetaCharsetWithClosingTagAndWhitespace = `<html>
+  <head>
+    <meta charset="utf-8"> </ meta>
   </head>
 </html>`
 
@@ -23,14 +46,64 @@ const tag1 = '<link href="/foo.ttf">'
 const tag2 = '<script src="/foo.js"></script>'
 
 describe('injectHtmlTags', () => {
-  it('injectAtOpeningTag()', () => {
-    expect(injectAtOpeningTag('head', htmlHead1, tag1)).toMatchInlineSnapshot(
+  it('injectHtmlFragments()', () => {
+    expect(injectHtmlFragments('HTML_BEGIN', tag1, htmlHead1, null)).toMatchInlineSnapshot(
       '"<html><head><link href=\\"/foo.ttf\\"></head></html>"'
     )
-    expect(injectAtOpeningTag('head', htmlHead1, `${tag1}${tag1}`)).toMatchInlineSnapshot(
+    expect(injectHtmlFragments('HTML_BEGIN', tag1, htmlHeadMetaCharsetWithoutWhitespace, null)).toMatchInlineSnapshot(
+      '"<html><head><meta charset=\\"utf-8\\"><link href=\\"/foo.ttf\\"></head></html>"'
+    )
+    expect(injectHtmlFragments('HTML_BEGIN', tag1, htmlHeadMetaCharsetWithWhitespace, null)).toMatchInlineSnapshot(
+      `
+      "<html>
+        <head>
+          <meta charset=\\"utf-8\\">
+          <link href=\\"/foo.ttf\\">
+        </head>
+      </html>"
+    `
+    )
+    expect(injectHtmlFragments('HTML_BEGIN', tag1, htmlHeadMetaCharsetWithTagBetween, null)).toMatchInlineSnapshot(
+      `
+      "<html>
+        <head>
+          <link href=\\"/foo.ttf\\">
+          <link href=\\"/foo.css\\">
+          <meta charset=\\"utf-8\\">
+        </head>
+      </html>"
+    `
+    )
+    expect(injectHtmlFragments('HTML_BEGIN', tag1, htmlHeadMetaCharsetWithClosingTag, null)).toMatchInlineSnapshot(
+      `
+      "<html>
+        <head>
+          <meta charset=\\"utf-8\\"></meta>
+          <link href=\\"/foo.ttf\\">
+        </head>
+      </html>"
+    `
+    )
+    expect(injectHtmlFragments('HTML_BEGIN', tag1, htmlHeadMetaCharsetWithClosingTagAndWhitespace, null)).toMatchInlineSnapshot(
+      `
+      "<html>
+        <head>
+          <meta charset=\\"utf-8\\"> </ meta>
+          <link href=\\"/foo.ttf\\">
+        </head>
+      </html>"
+    `
+    )   
+  })
+  
+  it('injectAtOpeningTag()', () => {
+    expect(injectAtOpeningTag('head','', htmlHead1, tag1)).toMatchInlineSnapshot(
+      '"<html><head><link href=\\"/foo.ttf\\"></head></html>"'
+    )
+    expect(injectAtOpeningTag('head', '', htmlHead1, `${tag1}${tag1}`)).toMatchInlineSnapshot(
       '"<html><head><link href=\\"/foo.ttf\\"><link href=\\"/foo.ttf\\"></head></html>"'
     )
-    expect(injectAtOpeningTag('head', htmlHead2, `${tag1}${tag1}`)).toMatchInlineSnapshot(
+    expect(injectAtOpeningTag('head', '', htmlHead2, `${tag1}${tag1}`)).toMatchInlineSnapshot(
       `
       "<html>
         <head>
@@ -40,7 +113,7 @@ describe('injectHtmlTags', () => {
       </html>"
     `
     )
-    expect(injectAtOpeningTag('head', htmlHead3, `${tag2}${tag2}${tag1}`)).toMatchInlineSnapshot(
+    expect(injectAtOpeningTag('head', '', htmlHead3, `${tag2}${tag2}${tag1}`)).toMatchInlineSnapshot(
       `
       "<html>
         <head>
--- vite-plugin-ssr/node/runtime/html/injectAssets/injectHtmlTags.ts
+++ vite-plugin-ssr/node/runtime/html/injectAssets/injectHtmlTags.ts
@@ -4,8 +4,9 @@ export { createHtmlHeadIfMissing }
 // Export for unit testing
 export { injectAtOpeningTag }
 export { injectAtClosingTag }
+export { injectHtmlFragments }
 
-import { assert, slice } from '../../../utils'
+import { assert, assertWarning, slice } from '../../../utils'
 import type { HtmlTag } from './getHtmlTags'
 import type { InjectToStream } from '../stream/react-streaming'
 
@@ -39,7 +40,37 @@ function injectHtmlFragments(
 ): string {
   if (position === 'HTML_BEGIN') {
     assert(tagOpeningExists('head', htmlString))
-    htmlString = injectAtOpeningTag('head', htmlString, htmlFragment)
+
+    const metaCharsetState = getMetaCharsetState(htmlString)
+
+    assertWarning(
+      metaCharsetState === 'missing',
+      'The <meta charset="${ENCODING}"> tag is missing from the html source. You may consider adding it as the first child tag of the head tag as it is a best practice to always include it in the HTML source. Not doing so could lead to a variety of issues. For further details see: https://github.com/brillout/vite-plugin-ssr/issues/638',
+      {
+        onlyOnce: true, showStackTrace: false
+      }
+    ) 
+
+    assertWarning(
+      metaCharsetState === 'exists',
+      'The <meta charset="${ENCODING}"> tag is not the first child tag of the head tag in the html source. The tag should occur as soon as possible inside the html source, to avoid running into a variety of issues. For further details see: https://github.com/brillout/vite-plugin-ssr/issues/638',
+      {
+        onlyOnce: true, showStackTrace: false
+      }
+    )
+
+    let htmlBeginInjectAtTag: 'head' | 'meta' = 'head'
+    let htmlBeginInjectAtAttribute: Attribute | '' = ''
+
+    // Inject the html fragments targeted at head after the meta charset tag
+    // if it the first child of the html head element.
+    // Fixes: https://github.com/brillout/vite-plugin-ssr/issues/638
+    if(metaCharsetState === 'first-tag') {
+      htmlBeginInjectAtTag = 'meta'
+      htmlBeginInjectAtAttribute = 'charset'
+    }
+
+    htmlString = injectAtOpeningTag(htmlBeginInjectAtTag, htmlBeginInjectAtAttribute, htmlString, htmlFragment)
     return htmlString
   }
   if (position === 'HTML_END') {
@@ -81,8 +112,15 @@ function resolveHtmlTag(htmlTag: HtmlTag['htmlTag']) {
   return typeof htmlTag !== 'string' ? htmlTag() : htmlTag
 }
 
-function injectAtOpeningTag(tag: 'head' | 'html' | '!doctype', htmlString: string, htmlFragment: string): string {
-  const openingTag = getTagOpening(tag)
+function injectAtOpeningTag(
+  tag: 'head' | 'meta' | 'html' | '!doctype',
+  attribute: Attribute | '',
+  htmlString: string, 
+  htmlFragment: string
+): string {
+  const openingTag = attribute 
+    ? getTagOpeningWithAttribute(tag, attribute)
+    : getTagOpening(tag)
   const matches = htmlString.match(openingTag)
   assert(matches && matches.length >= 1)
   const tagInstance = matches[0]
@@ -94,7 +132,7 @@ function injectAtOpeningTag(tag: 'head' | 'html' | '!doctype', htmlString: strin
   const before = slice(htmlParts, 0, 1)[0]! + tagInstance
   const after = slice(htmlParts, 1, 0).join(tagInstance)
 
-  htmlFragment = injectBreakLines(htmlFragment, before, after)
+  htmlFragment = injectBreakLines(htmlFragment, isSelfClosing(tag), before, after)
 
   return before + htmlFragment + after
 }
@@ -112,12 +150,12 @@ function injectAtClosingTag(tag: 'body' | 'html', htmlString: string, htmlFragme
   const before = slice(htmlParts, 0, -1).join(tagInstance)
   const after = tagInstance + slice(htmlParts, -1, 0)
 
-  htmlFragment = injectBreakLines(htmlFragment, before, after)
+  htmlFragment = injectBreakLines(htmlFragment, isSelfClosing(tag), before, after)
 
   return before + htmlFragment + after
 }
 
-function injectBreakLines(htmlFragment: string, before: string, after: string) {
+function injectBreakLines(htmlFragment: string, selfClosingTag: boolean, before: string, after: string) {
   assert(htmlFragment.trim() === htmlFragment)
 
   const currentLineBefore = before.split('\n').slice(-1)[0]!
@@ -128,7 +166,7 @@ function injectBreakLines(htmlFragment: string, before: string, after: string) {
   }
   if (!paddingParent) return htmlFragment
 
-  const whitespaceExtra = paddingParent ? '  ' : ''
+  const whitespaceExtra = paddingParent && !selfClosingTag ? '  ' : ''
   const whitespace = `${paddingParent}${whitespaceExtra}`
   const padding = `\n${whitespace}`
 
@@ -157,13 +195,13 @@ function createHtmlHeadIfMissing(htmlString: string): string {
   const htmlFragment = '<head></head>'
 
   if (tagOpeningExists('html', htmlString)) {
-    htmlString = injectAtOpeningTag('html', htmlString, htmlFragment)
+    htmlString = injectAtOpeningTag('html', '', htmlString, htmlFragment)
     assertion()
     return htmlString
   }
 
   if (tagOpeningExists('!doctype', htmlString)) {
-    htmlString = injectAtOpeningTag('!doctype', htmlString, htmlFragment)
+    htmlString = injectAtOpeningTag('!doctype', '', htmlString, htmlFragment)
     assertion()
     return htmlString
   }
@@ -173,7 +211,8 @@ function createHtmlHeadIfMissing(htmlString: string): string {
   return htmlString
 }
 
-type Tag = 'html' | 'head' | 'body' | '!doctype'
+type Tag = 'html' | 'head' | 'body' | '!doctype' | 'meta'
+type Attribute = 'charset'
 // Pay attention to performance when searching for tags
 // Use the most effective way to test or match tag existence
 // Use tag existence checking with caution as it is costly operation
@@ -192,4 +231,29 @@ function getTagOpening(tag: Tag) {
 function getTagClosing(tag: Tag) {
   const tagClosing = new RegExp(`</${tag}>`, 'i')
   return tagClosing
+}
+function getTagOpeningWithAttribute(tag: Tag, attribute: Attribute) {
+  const tagWithAttribute = new RegExp(`<${tag} ${attribute}=['"].*['"](>| [^>]*>)(\\s*<\/[\\s]*${tag}>)?`, 'i') 
+  return tagWithAttribute
+}
+function isSelfClosing(tag: Tag) {
+  const selfClosingTags: Tag[] = ['meta']
+  return selfClosingTags.includes(tag)
+}
+
+function getMetaCharsetState(htmlString: string) {
+  const metaCharsetExistsRE = /<head>([\s\S]*)<meta charset=/i
+  const metaCharsetMatches = metaCharsetExistsRE.exec(htmlString)
+
+  if(!metaCharsetMatches) {
+    return 'missing'
+  }
+
+  const [, textBetweenHeadAndMetaCharset] = metaCharsetMatches
+  const onlyWhitespaceRE = /^\s*$/
+
+  // Capture group is always defined
+  return onlyWhitespaceRE.test(textBetweenHeadAndMetaCharset as string)
+    ? 'first-tag'
+    : 'exists'
 }
