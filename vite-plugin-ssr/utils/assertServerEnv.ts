a0535fcd757ba0b41efd1ec04d64ea0e3bc8d5e0
Romuald Brillout <git@brillout.com>
2023-02-26 21:22:39 +0100

    skip assertServerEnv() if running in Vitest

--- vite-plugin-ssr/utils/assertServerEnv.ts
+++ vite-plugin-ssr/utils/assertServerEnv.ts
@@ -12,18 +12,23 @@ const state = getGlobalObject<{
   isVitePreview?: true
 }>('utils/assertServerEnv.ts', {})
 
-function assertServerEnv() {
+function assertServerEnv(): void | undefined {
+  if (isVitest()) return
   if (state.isNotProduction) {
     assert(state.isDev || state.isVitePreview)
   }
 }
 
-function assertServerEnv_setIsNotProduction() {
+function assertServerEnv_setIsNotProduction(): void | undefined {
   state.isNotProduction = true
 }
-function assertServerEnv_setIsDev() {
+function assertServerEnv_setIsDev(): void | undefined {
   state.isDev = true
 }
-function assertServerEnv_setIsVitePreview() {
+function assertServerEnv_setIsVitePreview(): void | undefined {
   state.isVitePreview = true
+}
+
+function isVitest(): boolean {
+  return typeof process !== 'undefined' && typeof process.env !== 'undefined' && 'VITEST' in process.env
 }
