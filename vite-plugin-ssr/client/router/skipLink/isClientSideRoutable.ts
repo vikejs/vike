705eda7550d17cb6b21b3540cfed70e19ad5705f
Romuald Brillout <git@brillout.com>
2023-03-01 17:39:03 +0100

    fix isClientSideRoutable()

--- vite-plugin-ssr/client/router/skipLink/isClientSideRoutable.ts
+++ vite-plugin-ssr/client/router/skipLink/isClientSideRoutable.ts
@@ -25,7 +25,9 @@ function analyze(
 ): { isClientSideRenderable: boolean; isClientRouting: boolean } {
   if (pageConfig) {
     const isClientRouting = getConfigValue(pageConfig, 'clientRouting', 'boolean') ?? false
-    const isClientSideRenderable = !!getCodeFilePath(pageConfig, 'onRenderClient')
+    const onRenderClientExists: boolean = !!getCodeFilePath(pageConfig, 'onRenderClient')
+    const PageExists: boolean = !!getCodeFilePath(pageConfig, 'Page') && pageConfig.configSources.Page!.c_env !== 'server-only'
+    const isClientSideRenderable = onRenderClientExists && PageExists
     return { isClientSideRenderable, isClientRouting }
   } else {
     // TOOD: globally rename isHtmlOnly to !isClientSideRenderable
