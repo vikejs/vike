a318328729806519b6458b7f2341f2ad76d6477b
Romuald Brillout <git@brillout.com>
2023-03-09 13:46:34 +0100

    minor

--- vite-plugin-ssr/node/plugin/plugins/importUserCode/v1-design/debug.ts
+++ vite-plugin-ssr/node/plugin/plugins/importUserCode/v1-design/debug.ts
@@ -1,3 +1,2 @@
 import { createDebugger } from '../../../utils'
-// TODO remove old debug vps:glob
 export const debug = createDebugger('vps:virtual-files')
--- vite-plugin-ssr/utils/debugGlob.ts
+++ vite-plugin-ssr/utils/debugGlob.ts
@@ -1,2 +1,3 @@
 import { createDebugger } from './debug'
+// TODO/v1-release: remove (or rename?)
 export const debugGlob = createDebugger('vps:glob')
