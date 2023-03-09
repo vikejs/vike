d032d69ffd6a5494638eac78841d62326cb42530
Romuald Brillout <git@brillout.com>
2023-03-09 15:34:44 +0100

    improve makeFilePathAsbolute

--- vite-plugin-ssr/utils/isNodejs.ts
+++
@@ -1,9 +0,0 @@
-import { assert } from './assert'
-
-export { isNodejs }
-
-// We don't use `isNodejs()` anymore because it doesn't work for Cloudflare Workers
-function isNodejs() {
-  assert(false)
-  // return typeof process !== 'undefined' && typeof process.versions.node !== 'undefined'
-}
--- vite-plugin-ssr/utils/makeFilePathAbsolute.ts
+++ vite-plugin-ssr/utils/makeFilePathAbsolute.ts
@@ -4,13 +4,27 @@ import path from 'path'
 import type { ResolvedConfig } from 'vite'
 import { toPosixPath, assertPosixPath } from './filesystemPathHandling'
 import { assert } from './assert'
+import { isNodeJS } from './isNodeJS'
+
+// This util should/is only used by node/plugin/utils.ts
+assert(isNodeJS())
 
 // Vite handles paths such as `/pages/index.page.js` which are relative to `config.root`.
 // Make them absolute starting from the filesystem route `/`.
 function makeFilePathAbsolute(filePathRelative: string, config: ResolvedConfig): string {
   assertPosixPath(filePathRelative)
   assert(filePathRelative.startsWith('/'))
-  const cwd = config.root
-  const filePathAbsolute = toPosixPath(require.resolve(path.join(cwd, filePathRelative)))
+  const { root } = config
+  assert(!filePathRelative.startsWith(root))
+  assert(!isFilesystemAbsolute(filePathRelative))
+  const filePathAbsolute = toPosixPath(require.resolve(path.join(root, filePathRelative)))
   return filePathAbsolute
+}
+
+function isFilesystemAbsolute(filePath: string) {
+  return getRootDir(filePath) === getRootDir(toPosixPath(process.cwd()))
+}
+function getRootDir(filePath: string) {
+  assertPosixPath(filePath)
+  return filePath.split('/').filter(Boolean)[0]
 }
