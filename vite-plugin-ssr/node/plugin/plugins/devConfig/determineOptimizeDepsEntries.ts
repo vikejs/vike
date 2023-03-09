d20f92a604814a05a665a10677229d72a5991c17
Romuald Brillout <git@brillout.com>
2023-03-09 15:54:11 +0100

    fix: aggressively add all page files to optimizeDeps.entries

--- vite-plugin-ssr/node/plugin/plugins/devConfig/determineOptimizeDepsEntries.ts
+++ vite-plugin-ssr/node/plugin/plugins/devConfig/determineOptimizeDepsEntries.ts
@@ -1,10 +1,6 @@
 export { determineOptimizeDepsEntries }
 
 import type { ResolvedConfig } from 'vite'
-import { getPageFileObject } from '../../../../shared/getPageFiles/getPageFileObject'
-import { deduceRouteStringFromFilesystemPath } from '../../../../shared/route/deduceRouteStringFromFilesystemPath'
-import { findPageRouteFile } from '../../../../shared/route/loadPageRoutes'
-import { getUrlFromRouteString } from '../../../../shared/route/resolveRouteString'
 import { findPageFiles } from '../../helpers'
 import { makeFilePathAbsolute } from '../../utils'
 import { getConfigData } from '../importUserCode/v1-design/getConfigData'
@@ -22,30 +18,8 @@ async function determineOptimizeDepsEntries(config: ResolvedConfig, isDev: boole
 
   // V0.4 design
   {
-    const pageFilesAll = (await findPageFiles(config, ['.page', '.page.client', '.page.route'], isDev)).map((p) =>
-      getPageFileObject(p)
-    )
-    let pageFiles = pageFilesAll.filter((p) => p.fileType === '.page' || p.fileType === '.page.client')
-    if (pageFiles.length > 10) {
-      pageFiles = pageFiles.filter((p) => {
-        if (p.isErrorPageFile || p.isDefaultPageFile || p.isRendererPageFile) {
-          return true
-        }
-        if (findPageRouteFile(p.pageId, pageFilesAll)) {
-          return false
-        }
-        {
-          const routeString = deduceRouteStringFromFilesystemPath(
-            p.pageId,
-            // There is no easy way to get the `filesystemRoots` at Vite config resolve time
-            []
-          )
-          const url = getUrlFromRouteString(routeString)
-          return url === '/'
-        }
-      })
-    }
-    pageFiles.slice(0, 10).forEach(({ filePath }) => {
+    const pageFiles = await findPageFiles(config, ['.page', '.page.client'], isDev)
+    pageFiles.forEach((filePath) => {
       const entry = makeFilePathAbsolute(filePath, config)
       entries.push(entry)
     })
