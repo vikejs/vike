d4a905bdc2db76e8281f0a671cb77c021e570697
Romuald Brillout <git@brillout.com>
2023-03-12 23:40:40 +0100

    update /examples/graphql-apollo-vue/ and re-add it to CI

--- examples/graphql-apollo-vue/.testRun.ts
+++ examples/graphql-apollo-vue/.testRun.ts
@@ -1,15 +1,9 @@
 export { testRun }
 
-import { fetchHtml, run, test, expect, skip } from '@brillout/test-e2e'
+import { fetchHtml, run, test, expect } from '@brillout/test-e2e'
 
 function testRun(cmd: 'npm run dev' | 'npm run prod') {
-  {
-    skip('SKIPPED until it supports Vite 4')
-    return
-  }
-
   run(cmd)
-
   test('page is rendered to HTML', async () => {
     const html = await fetchHtml('/')
     expect(html).toContain('<p>List of countries.')
