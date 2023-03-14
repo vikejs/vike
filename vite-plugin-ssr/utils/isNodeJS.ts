2ce1d1812556e69d0ff7ab3925ccbb11f8a9a40c
Romuald Brillout <git@brillout.com>
2023-03-14 16:41:02 +0100

    remove unused utils/isNodeJS.ts

--- vite-plugin-ssr/utils/isNodeJS.ts
+++
@@ -1,9 +0,0 @@
-export function isNodeJS(): boolean {
-  if (typeof process === 'undefined') return false
-  if (!process.cwd) return false
-  // https://stackoverflow.com/questions/4224606/how-to-check-whether-a-script-is-running-under-node-js/35813135#35813135
-  if (!process.versions || typeof process.versions.node === 'undefined') return false
-  // https://stackoverflow.com/questions/4224606/how-to-check-whether-a-script-is-running-under-node-js/35813135#comment92529277_35813135
-  if (!process.release || process.release.name !== 'node') return false
-  return true
-}
