5cffe2b3bde128b9f865f84c070f97dbda54a5ba
Romuald Brillout <git@brillout.com>
2023-03-16 19:22:40 +0100

    simplify types re-exporting

--- vite-plugin-ssr/node/runtime/index.ts
+++ vite-plugin-ssr/node/runtime/index.ts
@@ -5,7 +5,7 @@ export { pipeWebStream, pipeNodeStream, pipeStream, stampPipe } from './html/str
 export { injectAssets__public as _injectAssets } from './html/injectAssets/injectAssets__public'
 export { RenderErrorPage } from './renderPage/RenderErrorPage'
 
-export type { PageContextBuiltIn } from '../types'
+export type { PageContextBuiltIn } from '../../shared/types'
 export type { InjectFilterEntry } from './html/injectAssets/getHtmlTags'
 
 // Help Telefunc detect the user's stack
--- vite-plugin-ssr/types.d.ts
+++ vite-plugin-ssr/types.d.ts
@@ -1,4 +1,4 @@
 // TODO/v1-release: remove this file
 // Legacy way of importing types: `import type { PageContextBuiltIn, PageContextBuiltInClient } from 'vite-plugin-ssr/types'`
-export type { PageContextBuiltIn } from './dist/cjs/node/types'
-export type { PageContextBuiltInClient } from './dist/esm/client/types'
+export type { PageContextBuiltIn } from './dist/cjs/node/runtime/'
+export type { PageContextBuiltInClient } from './dist/esm/client/'
