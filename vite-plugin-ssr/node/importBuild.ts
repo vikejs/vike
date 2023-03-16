e64000ba12f1987dd7ca8aff3507cf681214b2c5
Romuald Brillout <git@brillout.com>
2023-03-16 19:10:17 +0100

    minor

--- vite-plugin-ssr/node/importBuild.ts
+++ vite-plugin-ssr/node/importBuild.ts
@@ -1,4 +1,4 @@
-// TODO: remove upon next major release
+// TODO/v1-release: remove
 console.warn(
   "[vite-plugin-ssr][Warning] importBuild.js is not needed anymore; it has no effect. Remove `import 'dist/server/importBuild.js'` to remove this warning. More infos at https://vite-plugin-ssr.com/importBuild.js"
 )
