ae47db55b0679303472665b4e6e5b9e3f795f09f
Romuald Brillout <git@brillout.com>
2023-03-16 19:52:54 +0100

    minor

--- vite-plugin-ssr/node/client/router.ts
+++ vite-plugin-ssr/node/client/router.ts
@@ -7,5 +7,5 @@ const navigate = (() => {
     showStackTrace: true,
     onlyOnce: false
   })
-  // `as never` because package.json#exports["./client/router"].types points to type defined by the client-side code
+  // `never` because package.json#exports["./client/router"].types points to type defined by the client-side code
 }) as never
