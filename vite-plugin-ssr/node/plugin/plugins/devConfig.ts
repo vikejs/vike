f4af809fee096a47ae6bb510fa8bc566fd277e1c
Romuald Brillout <git@brillout.com>
2023-03-19 21:13:38 +0100

    minor

--- vite-plugin-ssr/node/plugin/plugins/devConfig.ts
+++ vite-plugin-ssr/node/plugin/plugins/devConfig.ts
@@ -43,7 +43,6 @@ function devConfig(): Plugin[] {
         }
       },
       async configResolved(config) {
-        config.command
         assertRoot(root, config)
         const configVps = await getConfigVps(config)
         addExtensionsToOptimizeDeps(config, configVps)
