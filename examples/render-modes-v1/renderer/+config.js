c81ec8114e1473b0893c81f95ba148283a8d7cf4
Romuald Brillout <git@brillout.com>
2023-03-20 19:45:32 +0100

    minor

--- examples/render-modes-v1/renderer/+config.js
+++ examples/render-modes-v1/renderer/+config.js
@@ -8,16 +8,18 @@ export default {
     renderMode: {
       valueEnv: 'c_config',
       sideEffect({ configDefinedBy, configValue }) {
-        let valueEnv
-        if (configValue == 'HTML') valueEnv = 'server-only'
-        if (configValue == 'SPA') valueEnv = 'client-only'
-        if (configValue == 'SSR') valueEnv = 'server-and-client'
+        let PageEnv
+        if (configValue == 'HTML') PageEnv = 'server-only'
+        if (configValue == 'SPA') PageEnv = 'client-only'
+        if (configValue == 'SSR') PageEnv = 'server-and-client'
         // TODO: rename configDefinedBy
-        if (!valueEnv)
+        if (!PageEnv)
           throw new Error(`${configDefinedBy} has an invalid value, make to set its value to 'SSR', 'SPA', or 'HTML'`)
         return {
           configDefinitions: {
-            Page: { valueEnv }
+            Page: {
+              valueEnv: PageEnv
+            }
           }
         }
       }
