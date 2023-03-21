c124be63d8ac7765780994022298521c4f2fa5ac
MDReal32 <veysaliyev00@gmail.com>
2023-03-21 14:27:50 -0400

    development run with support vite server and hmr

---
+++ boilerplates/boilerplate-react-ts/server/dev.ts
@@ -0,0 +1,45 @@
+import { existsSync, readFileSync } from 'node:fs'
+import { fileURLToPath } from 'node:url'
+import { resolve } from 'node:path'
+
+import express from 'express'
+import compression from 'compression'
+import { UserConfig, createServer } from 'vite'
+import { renderPage } from 'vite-plugin-ssr'
+import merge from 'lodash/merge'
+import { format } from 'prettier'
+
+import viteConfig from '../vite.config.js'
+
+const prettierConfigFile = `${process.env.PROJECT_CWD || process.cwd()}/.prettierrc`
+let prettierConfig = {}
+if (existsSync(prettierConfigFile)) prettierConfig = JSON.parse(readFileSync(prettierConfigFile, 'utf-8'))
+
+const port = (viteConfig as UserConfig).server?.port || (process.env.PORT ? +process.env.PORT : 3001)
+
+const root = resolve(fileURLToPath(import.meta.url), '../..')
+
+async function startServer() {
+  const app = express()
+  app.use(compression())
+
+  const viteDevServer = await createServer(merge(viteConfig, { root, server: { middlewareMode: true } }))
+  app.use(viteDevServer.middlewares)
+
+  app.get('*', async (req, res, next) => {
+    const pageContextInit = { urlOriginal: req.originalUrl }
+    const pageContext = await renderPage(pageContextInit)
+    const { httpResponse } = pageContext
+    if (!httpResponse) return next()
+    const { body, statusCode, contentType, earlyHints } = httpResponse
+    if (res.writeEarlyHints) res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) })
+    const updatedBody = format(body, { parser: 'html', ...prettierConfig })
+    res.status(statusCode).type(contentType).send(updatedBody)
+  })
+
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+  })
+}
+
+startServer().then(null)
