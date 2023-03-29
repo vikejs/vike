5ee6cbf2e995e65f0d1cc013d6954b47d1b62749
Romuald Brillout <git@brillout.com>
2023-03-29 22:55:57 +0200

    minor

--- examples/v1/pages/+config.ts
+++ examples/v1/pages/+config.ts
@@ -1,8 +1,9 @@
 import onRenderHtml from './+config/onRenderHtml'
 import onRenderClient from './+config/onRenderClient'
+import type { Config } from 'vite-plugin-ssr'
 
 export default {
   onRenderClient,
   onRenderHtml,
   passToClient: ['pageProps']
-}
+} satisfies Config
