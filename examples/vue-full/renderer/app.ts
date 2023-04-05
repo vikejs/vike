8589bf1a4588ae95bc51cd05228c01b6a65c6726
Romuald Brillout <git@brillout.com>
2023-04-05 15:23:00 +0200

    fix ts type

--- examples/vue-full/renderer/app.ts
+++ examples/vue-full/renderer/app.ts
@@ -52,6 +52,9 @@ function createApp(pageContext: PageContext) {
 }
 
 // Same as `Object.assign()` but with type inference
-function objectAssign<Obj, ObjAddendum>(obj: Obj, objAddendum: ObjAddendum): asserts obj is Obj & ObjAddendum {
+function objectAssign<Obj extends object, ObjAddendum>(
+  obj: Obj,
+  objAddendum: ObjAddendum
+): asserts obj is Obj & ObjAddendum {
   Object.assign(obj, objAddendum)
 }
