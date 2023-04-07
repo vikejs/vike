fed7021bd56f61c957070eaad3d9274dde327759
Romuald Brillout <git@brillout.com>
2023-04-07 14:13:56 +0200

    solid-devtools

--- examples/solid-ssr/vite.config.ts
+++ examples/solid-ssr/vite.config.ts
@@ -1,9 +1,17 @@
 import solid from 'vite-plugin-solid'
 import ssr from 'vite-plugin-ssr/plugin'
 import { UserConfig } from 'vite'
+import devtools from 'solid-devtools/vite'
 
 const config: UserConfig = {
-  plugins: [solid({ ssr: true }), ssr()],
+  plugins: [
+    devtools({
+      /* additional options */
+      autoname: true // e.g. enable autoname
+    }),
+    solid({ ssr: true }),
+    ssr()
+  ],
   build: {
     // @ts-ignore
     polyfillDynamicImport: false
