e94ca4112fae0482ab31a5ef136a5cee72c0dbe1
Romuald Brillout <git@brillout.com>
2023-04-11 20:28:24 +0200

    cp -r base-url-server/ base-url-server-v1

---
+++ examples/base-url-server-v1/base.js
@@ -0,0 +1,2 @@
+export const baseServer = '/some/base-url/'
+export const baseAssets = 'http://localhost:8080/cdn/'
---
+++ examples/base-url-server-v1/pages/about/index.page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export { Page }
+
+function Page() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Example of setting the Base URL.</p>
+    </>
+  )
+}
