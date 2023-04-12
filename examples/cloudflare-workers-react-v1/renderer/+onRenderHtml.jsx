f6ee710927ccb2f4429c3fc652ace53083dfe0f9
Romuald Brillout <git@brillout.com>
2023-04-12 20:37:04 +0200

    migrate /examples/cloudflare-workers-react-v1/ to V1 design

--- examples/cloudflare-workers-react-v1/renderer/+onRenderHtml.jsx
+++ examples/cloudflare-workers-react-v1/renderer/+onRenderHtml.jsx
@@ -1,15 +1,11 @@
+export default onRenderHtml
+
 import ReactDOMServer from 'react-dom/server'
 import React from 'react'
 import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr/server'
 import { PageLayout } from './PageLayout'
 
-export { render }
-export { passToClient }
-
-// See https://vite-plugin-ssr.com/data-fetching
-const passToClient = ['pageProps']
-
-function render(pageContext) {
+function onRenderHtml(pageContext) {
   const { Page, pageProps } = pageContext
   const pageHtml = ReactDOMServer.renderToString(
     <PageLayout>
