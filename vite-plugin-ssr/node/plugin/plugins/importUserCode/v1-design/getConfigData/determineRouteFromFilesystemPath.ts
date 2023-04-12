0b42eba256a8765068b50bc6cc74be628f9f8318
Romuald Brillout <git@brillout.com>
2023-04-12 15:04:49 +0200

    fix: [V1 design] implement filesystem route for npm package imports

--- vite-plugin-ssr/node/plugin/plugins/importUserCode/v1-design/getConfigData/determineRouteFromFilesystemPath.ts
+++ vite-plugin-ssr/node/plugin/plugins/importUserCode/v1-design/getConfigData/determineRouteFromFilesystemPath.ts
@@ -1,12 +1,18 @@
 export { determineRouteFromFilesystemPath }
 
-import { assert } from '../../../../utils'
+import { assert, getNpmPackageImportPath, isNpmPackageImportPath } from '../../../../utils'
 import { determinePageId } from './determinePageId'
 
-function determineRouteFromFilesystemPath(filesystemPath: string): string {
-  const pageId = determinePageId(filesystemPath)
+function determineRouteFromFilesystemPath(dirOrFilePath: string): string {
+  const pageId = determinePageId(dirOrFilePath)
 
   let routeString = pageId
+  if (isNpmPackageImportPath(routeString)) {
+    const importPath = getNpmPackageImportPath(routeString) ?? ''
+    assert(!importPath.startsWith('/'))
+    routeString = '/' + importPath
+  }
+  assert(routeString.startsWith('/'))
 
   {
     let paths = routeString.split('/')
@@ -19,7 +25,7 @@ function determineRouteFromFilesystemPath(filesystemPath: string): string {
     routeString = '/'
   }
 
-  assert(routeString.startsWith('/'))
+  assert(routeString.startsWith('/') || isNpmPackageImportPath(routeString))
   assert(!routeString.endsWith('/') || routeString === '/')
 
   return routeString
