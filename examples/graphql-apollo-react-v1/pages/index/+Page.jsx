044159058d1a0e98bfcad58cd800c7a1816a02f0
Romuald Brillout <git@brillout.com>
2023-04-17 14:08:07 +0200

    migrate /examples/graphql-apollo-react/ to V1 Design

--- examples/graphql-apollo-react-v1/pages/index/+Page.jsx
+++ examples/graphql-apollo-react-v1/pages/index/+Page.jsx
@@ -1,7 +1,7 @@
+export default Page
+
 import React, { useState } from 'react'
 import { gql, useQuery } from '@apollo/client'
-
-export { Page }
 
 function Page() {
   const { data } = useQuery(gql`
