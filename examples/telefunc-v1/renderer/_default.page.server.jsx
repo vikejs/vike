56bd201f5c3922bdc0386fc157e232b6a4966d20
Romuald Brillout <git@brillout.com>
2023-04-18 17:31:56 +0200

    copy examples/telefunc/ for V1 design

---
+++ examples/telefunc-v1/renderer/_default.page.server.jsx
@@ -0,0 +1,28 @@
+// See https://vite-plugin-ssr.com/render
+export { render }
+// See https://vite-plugin-ssr.com/data-fetching
+export { passToClient }
+
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr/server'
+import { PageLayout } from './PageLayout'
+
+const passToClient = ['pageProps']
+
+async function render(pageContext) {
+  const { Page, pageProps } = pageContext
+  const page = (
+    <PageLayout>
+      <Page {...pageProps} />
+    </PageLayout>
+  )
+  const pageHtml = renderToString(page)
+
+  return escapeInject`<!DOCTYPE html>
+    <html>
+      <body>
+        <div id="page-view">${dangerouslySkipEscape(pageHtml)}</div>
+      </body>
+    </html>`
+}
---
+++ examples/telefunc-v1/server/index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const { renderPage } = require('vite-plugin-ssr/server')
+const { telefunc } = require('telefunc')
+
+const isProduction = process.env.NODE_ENV === 'production'
+const root = `${__dirname}/..`
+
+startServer()
+
+async function startServer() {
+  const app = express()
+
+  if (isProduction) {
+    app.use(express.static(`${root}/dist/client`))
+  } else {
+    const vite = require('vite')
+    const viteDevMiddleware = (
+      await vite.createServer({
+        root,
+        server: { middlewareMode: true }
+      })
+    ).middlewares
+    app.use(viteDevMiddleware)
+  }
+
+  app.use(express.text()) // Parse & make HTTP request body available at `req.body`
+  app.all('/_telefunc', async (req, res) => {
+    const context = {}
+    const httpResponse = await telefunc({ url: req.originalUrl, method: req.method, body: req.body, context })
+    const { body, statusCode, contentType } = httpResponse
+    res.status(statusCode).type(contentType).send(body)
+  })
+
+  app.get('*', async (req, res, next) => {
+    const pageContextInit = {
+      urlOriginal: req.originalUrl
+    }
+    const pageContext = await renderPage(pageContextInit)
+    const { httpResponse } = pageContext
+    if (!httpResponse) return next()
+    const { statusCode, contentType } = httpResponse
+    res.status(statusCode).type(contentType)
+    httpResponse.pipe(res)
+  })
+
+  const port = process.env.PORT || 3000
+  app.listen(port)
+  console.log(`Server running at http://localhost:${port}`)
+}
