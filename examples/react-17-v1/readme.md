4069bfb2a18253f3368da44b098197d5f85746e5
Romuald Brillout <git@brillout.com>
2023-04-18 13:40:24 +0200

    copy examples/react-17/ for V1 design

---
+++ examples/react-17-v1/readme.md
@@ -0,0 +1,8 @@
+Example of using `vite-plugin-ssr` with React 17.
+
+```bash
+git clone git@github.com:brillout/vite-plugin-ssr
+cd vite-plugin-ssr/examples/react-17/
+npm install
+npm run dev
+```
---
+++ examples/react-17-v1/renderer/_default.page.server.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr/server'
+import { PageLayout } from './PageLayout'
+
+export { render }
+export { passToClient }
+
+// See https://vite-plugin-ssr.com/data-fetching
+const passToClient = ['pageProps']
+
+async function render(pageContext) {
+  const { Page, pageProps } = pageContext
+  const pageHtml = renderToString(
+    <PageLayout>
+      <Page {...pageProps} />
+    </PageLayout>
+  )
+
+  return escapeInject`<!DOCTYPE html>
+    <html>
+      <body>
+        <div id="page-view">${dangerouslySkipEscape(pageHtml)}</div>
+      </body>
+    </html>`
+}
