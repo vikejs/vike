02d16111a3da5bf2cbcef8696ce4b2a25928301c
Romuald Brillout <git@brillout.com>
2023-04-18 16:41:48 +0200

    copy examples/react-streaming/ for V1 design

---
+++ examples/react-streaming-v1/server/index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const compression = require('compression')
+const { renderPage } = require('vite-plugin-ssr/server')
+
+const isProduction = process.env.NODE_ENV === 'production'
+const root = `${__dirname}/..`
+
+startServer()
+
+async function startServer() {
+  const app = express()
+
+  app.use(compression())
+
+  if (isProduction) {
+    const sirv = require('sirv')
+    app.use(sirv(`${root}/dist/client`))
+  } else {
+    const vite = require('vite')
+    const viteDevMiddleware = (
+      await vite.createServer({
+        root,
+        server: { middlewareMode: true }
+      })
+    ).middlewares
+    app.use(viteDevMiddleware)
+  }
+
+  app.get('*', async (req, res, next) => {
+    const userAgent = req.headers['user-agent']
+    const pageContextInit = {
+      urlOriginal: req.originalUrl,
+      userAgent
+    }
+    const pageContext = await renderPage(pageContextInit)
+    const { httpResponse } = pageContext
+    if (!httpResponse) return next()
+    const { statusCode, contentType, earlyHints } = httpResponse
+    if (res.writeEarlyHints) res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) })
+    res.status(statusCode).type(contentType)
+    httpResponse.pipe(res)
+  })
+
+  const port = process.env.PORT || 3000
+  app.listen(port)
+  console.log(`Server running at http://localhost:${port}`)
+}
