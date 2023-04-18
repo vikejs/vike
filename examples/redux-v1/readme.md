9efdfbf02514878c466628b7d43647c25a498624
Romuald Brillout <git@brillout.com>
2023-04-18 16:49:53 +0200

    copy examples/redux/ for V1 design

---
+++ examples/redux-v1/readme.md
@@ -0,0 +1,8 @@
+Example of using `vite-plugin-ssr` with React and Redux.
+
+```bash
+git clone git@github.com:brillout/vite-plugin-ssr
+cd vite-plugin-ssr/examples/redux/
+npm install
+npm run dev
+```
---
+++ examples/redux-v1/renderer/_default.page.server.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { Provider } from 'react-redux'
+import { getStore } from './store'
+import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr/server'
+
+export { render }
+export { onBeforeRender }
+export { passToClient }
+
+const passToClient = ['PRELOADED_STATE']
+
+async function render(pageContext) {
+  const { pageHtml } = pageContext
+  return escapeInject`<!DOCTYPE html>
+    <html>
+      <body>
+        <div id="react-root">${dangerouslySkipEscape(pageHtml)}</div>
+      </body>
+    </html>`
+}
+
+async function onBeforeRender(pageContext) {
+  const store = getStore()
+
+  const { Page } = pageContext
+  const pageHtml = renderToString(
+    <Provider store={store}>
+      <Page />
+    </Provider>
+  )
+
+  // Grab the initial state from our Redux store
+  const PRELOADED_STATE = store.getState()
+
+  return {
+    pageContext: {
+      PRELOADED_STATE,
+      pageHtml
+    }
+  }
+}
