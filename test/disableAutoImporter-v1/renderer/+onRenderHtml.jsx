adae991e0a64d0afc02e07768f7d534f42beb1ee
Romuald Brillout <git@brillout.com>
2023-04-22 22:52:09 +0200

    migrate /test/disableAutoImporter/ to V1 design

--- test/disableAutoImporter-v1/renderer/+onRenderHtml.jsx
+++ test/disableAutoImporter-v1/renderer/+onRenderHtml.jsx
@@ -1,11 +1,11 @@
-export { render }
+export default onRenderHtml
 
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr/server'
 import { PageLayout } from './PageLayout'
 
-async function render(pageContext) {
+async function onRenderHtml(pageContext) {
   const { Page } = pageContext
   const viewHtml = dangerouslySkipEscape(
     renderToString(
