70a7895a3c8750b3520408938819d05002aca8a6
Romuald Brillout <git@brillout.com>
2023-04-24 00:23:49 +0200

    update tests

--- test/preload/utils/stabilizeHashs.spec.ts
+++ test/preload/utils/stabilizeHashs.spec.ts
@@ -5,6 +5,6 @@ describe('preload tags', () => {
   it('Default preload strategy', async () => {
     expect(stabilizeHashs('/assets/_default.page.client.81bbaf22.js')).toBe('/assets/_default.page.client.$HASH.js')
     expect(stabilizeHashs('/assets/_default.page.client.81bbaf22.css')).toBe('/assets/_default.page.client.$HASH.css')
-    expect(stabilizeHashs('/assets/chunks/87271e60.js')).toBe('/assets/chunks/$HASH.js')
+    expect(stabilizeHashs('/assets/chunks/chunk-87271e60.js')).toBe('/assets/chunks/chunk-$HASH.js')
   })
 })
