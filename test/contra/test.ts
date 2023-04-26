55830c4bf795e2d29a5d766571011561a15433b9
Romuald Brillout <git@brillout.com>
2023-04-26 14:22:01 +0200

    minor

--- test/contra/test.ts
+++ test/contra/test.ts
@@ -5,12 +5,17 @@ import { renderPage } from '../../vite-plugin-ssr/node/runtime'
 
 function test() {
   describe('Contra Simulation', () => {
-    it('private properties', async () => {
-      const pageContext: any = await renderPage({ urlOriginal: '/' })
-      expect(pageContext._pageId).toBe('/pages/index')
-      expect(pageContext._pageContextHtmlTag).toBe(
-        '<script id="vite-plugin-ssr_pageContext" type="application/json">{"pageContext":{"_pageId":"/pages/index"}}</script>'
-      )
-    })
+    it(
+      'private properties',
+      async () => {
+        const pageContext: any = await renderPage({ urlOriginal: '/' })
+        expect(pageContext.httpResponse).not.toBe(null)
+        expect(pageContext._pageId).toBe('/pages/index')
+        expect(pageContext._pageContextHtmlTag).toBe(
+          '<script id="vite-plugin-ssr_pageContext" type="application/json">{"pageContext":{"_pageId":"/pages/index"}}</script>'
+        )
+      },
+      { timeout: 20 * 1000 }
+    )
   })
 }
