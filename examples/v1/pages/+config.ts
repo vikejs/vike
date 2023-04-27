f46e7b4376a6d4a20bc6676630e7922cd2386b96
Romuald Brillout <git@brillout.com>
2023-04-27 11:38:08 +0200

    remove examples/v1

--- examples/v1/pages/+config.ts
+++
@@ -1,9 +0,0 @@
-import onRenderHtml from './+config/onRenderHtml'
-import onRenderClient from './+config/onRenderClient'
-import type { Config } from 'vite-plugin-ssr/types'
-
-export default {
-  onRenderClient,
-  onRenderHtml,
-  passToClient: ['pageProps']
-} satisfies Config
