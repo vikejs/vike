4495f4a7f7bb41d7a548bbb21c2a53478676a8ee
Romuald Brillout <git@brillout.com>
2023-04-27 16:46:49 +0200

    minor

--- examples/preact-client-routing-v1/renderer/+onRenderClient.jsx
+++ examples/preact-client-routing-v1/renderer/+onRenderClient.jsx
@@ -22,11 +22,6 @@ async function onRenderClient(pageContext) {
 }
 
 function getPageTitle(pageContext) {
-  const title =
-    // For static titles (defined in the `export { documentProps }` of the page's `.page.js`)
-    (pageContext.exports.documentProps || {}).title ||
-    // For dynamic tiles (defined in the `export addContextProps()` of the page's `.page.server.js`)
-    (pageContext.documentProps || {}).title ||
-    'Demo'
+  const title = (pageContext.config.documentProps || {}).title || (pageContext.documentProps || {}).title || 'Demo'
   return title
 }
