d19c67d8613266ab4b607ac55477b9acfd26ace2
Romuald Brillout <git@brillout.com>
2023-05-10 18:54:30 +0200

    minor

--- examples/vike-react-simple/vite.config.js
+++ examples/vike-react-simple/vite.config.js
@@ -2,5 +2,7 @@ import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
 
 export default {
-  plugins: [react(), ssr()]
+  plugins: [react(), ssr()],
+  // TODO: why is this needed?
+  optimizeDeps: { include: ['vike-react-simple/onRenderClient'] }
 }
