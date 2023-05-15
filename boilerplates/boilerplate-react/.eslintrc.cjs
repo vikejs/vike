c840607d928f66ae1f086a751c0225d31a2cfb13
Romuald Brillout <git@brillout.com>
2023-05-15 22:50:38 +0200

    minor

--- boilerplates/boilerplate-react/.eslintrc.cjs
+++ boilerplates/boilerplate-react/.eslintrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-  env: { browser: true, es2020: true, node: true },
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: 'detect' } },
   rules: {
     'react-refresh/only-export-components': 'warn'
   },
   reportUnusedDisableDirectives: true,
   ignorePatterns: ['dist/*'],
+  env: { browser: true, es2020: true, node: true },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: 'detect' } },
   plugins: ['react-refresh'],
   extends: [
     'eslint:recommended',
