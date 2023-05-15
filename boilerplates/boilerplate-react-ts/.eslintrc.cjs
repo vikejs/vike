35f6ef5df720df6f8f48924cfb943713475328e0
Romuald Brillout <git@brillout.com>
2023-05-15 22:24:54 +0200

    use detect

--- boilerplates/boilerplate-react-ts/.eslintrc.cjs
+++ boilerplates/boilerplate-react-ts/.eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    settings: { react: { version: 'detect' } },
     plugins: ['react-refresh'],
     rules: {
       'react-refresh/only-export-components': 'warn',
