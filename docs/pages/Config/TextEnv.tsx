d311541abec41e3797607fb0fdb747ebe2723566
Romuald Brillout <git@brillout.com>
2023-05-17 16:39:11 +0200

    docs: minor

--- docs/pages/Config/TextEnv.tsx
+++ docs/pages/Config/TextEnv.tsx
@@ -3,5 +3,5 @@ export { TextEnv }
 import React from 'react'
 
 function TextEnv({ children }: { children: any }) {
-  return <span style={{ color: '#888' }}>{children}</span>
+  return <span style={{ color: '#888', fontSize: '0.94em', verticalAlign: 'middle' }}>{children}</span>
 }
