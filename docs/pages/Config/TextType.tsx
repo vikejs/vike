fa123e89164b6f3360fbfb7c947ed773bd4c9ba9
Romuald Brillout <git@brillout.com>
2023-05-18 20:26:02 +0200

    prettier

--- docs/pages/Config/TextType.tsx
+++ docs/pages/Config/TextType.tsx
@@ -3,5 +3,9 @@ export { TextType }
 import React from 'react'
 
 function TextType({ children }: { children: string }) {
-  return <span style={{ opacity: 0.85, fontSize: '0.9em', verticalAlign: 'middle' }}><code>{children}</code></span>
+  return (
+    <span style={{ opacity: 0.85, fontSize: '0.9em', verticalAlign: 'middle' }}>
+      <code>{children}</code>
+    </span>
+  )
 }
