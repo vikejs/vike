a6799d1cd9f9c372cfb9625e982cd30ac8b76f13
Romuald Brillout <git@brillout.com>
2023-05-25 23:51:11 +0200

    fix: improve error message upon malformed HTML (fix #913)

--- vite-plugin-ssr/client/getPageContextSerializedInHtml.ts
+++ vite-plugin-ssr/client/getPageContextSerializedInHtml.ts
@@ -1,5 +1,5 @@
 import { parse } from '@brillout/json-serializer/parse'
-import { hasProp, objectAssign, assert } from './utils'
+import { hasProp, objectAssign, assert, assertUsage } from './utils'
 
 export { getPageContextSerializedInHtml }
 
@@ -8,7 +8,13 @@ function getPageContextSerializedInHtml(): {
   _pageContextRetrievedFromServer: Record<string, unknown>
   _comesDirectlyFromServer: true
 } {
-  const pageContextJson = document.getElementById('vite-plugin-ssr_pageContext')?.textContent
+  const id = 'vite-plugin-ssr_pageContext'
+  const elem = document.getElementById(id)
+  assertUsage(
+    elem,
+    `The element #${id} (which vite-plugin-ssr automatically injects into the HTML) is missing from the DOM. This may happen if your HTML is malformed. Make sure your HTML isn't malformed, and make sure you don't remove #${id} from the HTML nor from the DOM.`
+  )
+  const pageContextJson = elem.textContent
   assert(pageContextJson)
 
   const parseResult = parse(pageContextJson)
