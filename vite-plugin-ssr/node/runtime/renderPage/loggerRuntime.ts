0289b2776e69b4a428940613041baa2107cc1972
Romuald Brillout <git@brillout.com>
2023-06-13 13:53:06 +0200

    fix var init access

--- vite-plugin-ssr/node/runtime/renderPage/loggerRuntime.ts
+++ vite-plugin-ssr/node/runtime/renderPage/loggerRuntime.ts
@@ -1,6 +1,6 @@
 // Logger used by the the server runtime. (Which is also used when pre-rendering.)
 
-export let logError: LogError = logErrorProd
+export let logError: LogError
 export let logInfo: null | LogInfo = null
 export { setRuntimeLogger }
 
@@ -8,7 +8,13 @@ import { logErrorProd } from './loggerProd'
 import type { LogError } from './loggerProd'
 import type { LogInfo } from '../../plugin/shared/loggerNotProd'
 
-function setRuntimeLogger(logError_: LogError, logInfo_: LogInfo) {
+logError =
+  // @ts-ignore
+  logError ??
+  // Default
+  logErrorProd
+
+function setRuntimeLogger(logError_: LogError, logInfo_: LogInfo | null) {
   logError = logError_
   logInfo = logInfo_
 }
