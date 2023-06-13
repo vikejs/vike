eee8bc1c81de886756721200cfda3c1ba99609a6
Romuald Brillout <git@brillout.com>
2023-06-13 13:53:12 +0200

    prettier

--- vite-plugin-ssr/node/plugin/shared/loggerTranspile/formatFrameError.spec.ts
+++ vite-plugin-ssr/node/plugin/shared/loggerTranspile/formatFrameError.spec.ts
@@ -93,7 +93,8 @@ describe('getErrMsg()', () => {
   it('real use case - @vitejs/plugin-react (3)', () => {
     const message =
       "/home/rom/code/vite-plugin-ssr/examples/react-full-v1/components/Counter.tsx: Missing semicolon. (1:7)\n\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 1 |\u001b[39m iemport \u001b[33mReact\u001b[39m\u001b[33m,\u001b[39m { useState } \u001b[36mfrom\u001b[39m \u001b[32m'react'\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m   |\u001b[39m        \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 2 |\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 3 |\u001b[39m \u001b[36mexport\u001b[39m { \u001b[33mCounter\u001b[39m }\u001b[0m\n\u001b[0m \u001b[90m 4 |\u001b[39m\u001b[0m"
-    const frame = "> 1  |  iemport React, { useState } from 'react'\n   |         ^\n  2  |  \n  3  |  export { Counter }  4 |"
+    const frame =
+      "> 1  |  iemport React, { useState } from 'react'\n   |         ^\n  2  |  \n  3  |  export { Counter }  4 |"
     const err = { message, id, frame }
     const codeSnippet = message.split('semicolon. (1:7)')[1]
     expect(getErrMsg(err)).toBe(`Missing semicolon. ${codeSnippet}`)
