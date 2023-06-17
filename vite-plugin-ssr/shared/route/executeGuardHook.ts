49aa5d8d746ee658d11e944e535a5e3081ef1aab
Romuald Brillout <git@brillout.com>
2023-06-17 11:41:17 +0200

    require V1 design for guard()

--- vite-plugin-ssr/shared/route/executeGuardHook.ts
+++ vite-plugin-ssr/shared/route/executeGuardHook.ts
@@ -1,16 +1,19 @@
 export { executeGuardHook }
 
 import { getHook } from '../getHook'
-import { assertUsage, executeHook } from './utils'
-import type { PageContextExports } from '../getPageFiles'
+import { assert, assertUsage, executeHook } from './utils'
+import type { PageContextExports, PageFile } from '../getPageFiles'
+import type { PageConfig } from '../page-configs/PageConfig'
 
-async function executeGuardHook<T extends PageContextExports>(
+async function executeGuardHook<T extends PageContextExports & PageData>(
   pageContext: T,
   prepareForUserConsumption: (pageConfig: T) => T | void
-) {
+): Promise<void> {
   const hook = getHook(pageContext, 'guard')
   if (!hook) return
   const guard = hook.hookFn
+
+  assertUsage(isV1design(pageContext), 'guard() only works with the V1 design')
 
   let pageContextForUserConsumption = pageContext
   const res = prepareForUserConsumption(pageContext)
@@ -21,4 +24,15 @@ async function executeGuardHook<T extends PageContextExports>(
     hookResult === undefined,
     `The guard() hook of ${hook.hookFilePath} returns a value, but guard() doesn't accept any return value`
   )
+}
+
+type PageData = {
+  _pageFilesAll: PageFile[]
+  _pageConfigs: PageConfig[]
+}
+function isV1design(pageContext: PageData): boolean {
+  const oldDesign = pageContext._pageFilesAll.length
+  const newDesign = pageContext._pageConfigs.length
+  assert(oldDesign === 0 || newDesign === 0)
+  return oldDesign === 0
 }
