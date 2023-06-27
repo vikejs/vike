fa47af4fcb5b1d938170ce61a3b2a6daffd5c8bb
Romuald Brillout <git@brillout.com>
2023-06-27 12:37:39 +0200

    refactor isNpmPackage()

---
+++ vite-plugin-ssr/utils/isNpmPackage.spec.ts
@@ -0,0 +1,41 @@
+import { expect, describe, it } from 'vitest'
+import { parse } from './isNpmPackage'
+
+describe('parse()', () => {
+  it('basics', () => {
+    expect(parse('some-pkg')).toStrictEqual({ pkgName: 'some-pkg', importPath: null })
+    expect(parse('@scope/name')).toStrictEqual({ pkgName: '@scope/name', importPath: null })
+    expect(parse('@scope/name/path')).toStrictEqual({ pkgName: '@scope/name', importPath: 'path' })
+    expect(parse('@scope/name/some/deep/path.js')).toStrictEqual({
+      pkgName: '@scope/name',
+      importPath: 'some/deep/path.js'
+    })
+    expect(parse('./some/path')).toBe(null)
+    expect(parse('/some-path')).toBe(null)
+    expect(parse('\\some/path')).toBe(null)
+    expect(parse('.\\some-path')).toBe(null)
+    expect(parse('#alias')).toBe(null)
+    expect(parse('a!bc')).toBe(null)
+  })
+  it('edge cases', () => {
+    expect(parse('')).toBe(null)
+    expect(parse('a')).toStrictEqual({ pkgName: 'a', importPath: null })
+    expect(parse('0')).toStrictEqual({ pkgName: '0', importPath: null }) // https://www.npmjs.com/package/0
+    expect(parse('-')).toBe(null) // Actually wrong: https://www.npmjs.com/package/-
+    expect(parse('_')).toBe(null)
+    expect(parse('.')).toBe(null)
+    expect(parse('.a')).toBe(null)
+    expect(parse('a.js')).toStrictEqual({ pkgName: 'a.js', importPath: null }) // https://www.npmjs.com/package/a.js
+    expect(parse('a.')).toStrictEqual({ pkgName: 'a.', importPath: null }) // https://www.npmjs.com/package/a.js
+    expect(parse('a-')).toStrictEqual({ pkgName: 'a-', importPath: null })
+    expect(parse('a_')).toStrictEqual({ pkgName: 'a_', importPath: null })
+    expect(parse('@')).toBe(null)
+    expect(parse('@/')).toBe(null)
+    expect(parse('@/a')).toBe(null)
+    expect(parse('@a')).toBe(null)
+    expect(parse('@a/')).toBe(null)
+    expect(parse('@a/b')).toStrictEqual({ pkgName: '@a/b', importPath: null })
+    expect(parse('@a!b/c')).toBe(null)
+    expect(parse('@a/b/c!')).toStrictEqual({ pkgName: '@a/b', importPath: 'c!' })
+  })
+})
--- vite-plugin-ssr/utils/isNpmPackage.ts
+++ vite-plugin-ssr/utils/isNpmPackage.ts
@@ -3,66 +3,60 @@ export { isNpmPackageName }
 export { getNpmPackageName }
 export { getNpmPackageImportPath }
 
-import { assert } from './assert'
-const invalidNameRE = /[^a-zA-Z-_]/
+// For ./isNpmPackage.spec.ts
+export { parse }
 
 function isNpmPackageImport(str: string): boolean {
-  const res = parseNpmPath(str)
+  const res = parse(str)
   return res !== null
 }
 
 function isNpmPackageName(str: string | undefined): boolean {
-  const res = parseNpmPath(str)
+  const res = parse(str)
   return res !== null && res.importPath === null
 }
 
 function getNpmPackageName(str: string): null | string {
-  const res = parseNpmPath(str)
+  const res = parse(str)
   if (!res) return null
-  return res.npmPackageName
+  return res.pkgName
 }
 
 function getNpmPackageImportPath(str: string): null | string {
-  const res = parseNpmPath(str)
+  const res = parse(str)
   if (!res) return null
   return res.importPath
 }
 
-function parseNpmPath(str: string | undefined): null | { npmPackageName: string; importPath: null | string } {
-  if (str === undefined || str.includes('\\') || str.startsWith('/')) {
-    return null
-  }
+// The logic down below is wrong, for example:
+//  - https://www.npmjs.com/package/-
+// The correct logic is complex, see https://github.com/npm/validate-npm-package-name
+// We don't need to be accurate: are there npm packages with weird names that are actually being used?
+function parse(str: string | undefined): null | { pkgName: string; importPath: null | string } {
+  if (!str) return null
 
   let scope: string | null = null
   if (str.startsWith('@')) {
-    if (!str.includes('/')) {
-      return null
-    }
-    const [first, ...rest] = str.split('/')
-    scope = first!
+    if (!str.includes('/')) return null
+    const [scope_, ...rest] = str.split('/')
+    scope = scope_!
     str = rest.join('/')
-    assert(scope && scope.startsWith('@'))
+    if (!str) return null
+    if (scope === '@' || invalid(scope.slice(1))) return null
   }
 
-  if (str === '') {
-    return null
-  }
-
-  const [first, ...rest] = str.split('/')
-  const name = first
-  const importPath = rest.length === 0 ? null : rest.join('/')
-  assert(name)
-  assert(importPath === null || !importPath.startsWith('/'))
-
-  if (invalidNameRE.test(name) || (scope && invalidNameRE.test(scope.slice(1)))) {
-    return null
-  }
-
-  assert(!name.startsWith('/'))
-  const npmPackageName = scope ? `${scope}/${name}` : name
+  const [name, ...importPathParts] = str.split('/')
+  if (!name || invalid(name)) return null
+  const importPath = importPathParts.length === 0 ? null : importPathParts.join('/')
 
   return {
-    npmPackageName,
+    pkgName: scope ? `${scope}/${name}` : name,
     importPath
   }
+}
+function invalid(s: string) {
+  const firstLetter = s[0]
+  if (!firstLetter || !/[a-z0-9]/.test(firstLetter)) return true
+  if (/[^a-z0-9_\-\.]/.test(s)) return true
+  return false
 }
