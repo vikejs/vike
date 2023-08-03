6494246612d7c6923867cd392ad4f5989065726e
Romuald Brillout <git@brillout.com>
2023-08-03 12:44:13 +0200

    docs: improve generic integration docs

--- docs/components/static-host.tsx
+++ docs/components/static-host.tsx
@@ -1,52 +1,63 @@
-import React from 'react'
-
 export { StaticHostDocIntro }
+export { StaticHostDocStrategies }
 export { StaticHostDocOutro }
+
+import React from 'react'
 
 function StaticHostDocIntro({ staticHostLink }: { staticHostLink?: JSX.Element }) {
   return (
     <p>
-      By <a href="/pre-rendering">pre-rendering</a> our pages, we can remove the need for a Node.js server. We can then
-      deploy our app to any static host
+      By <a href="/pre-rendering">pre-rendering</a> your pages, you can remove the need for a production server. You can
+      then deploy your app to any static host
       {staticHostLink}.
     </p>
   )
 }
-function StaticHostDocOutro({ baseUrlAddendum }: { baseUrlAddendum?: JSX.Element }) {
+function StaticHostDocStrategies({ name = 'the static host' }: { name?: string }) {
   return (
-    <div>
-      <p>
-        If we don't deploy our app at the URL root <code>/</code>, we can{' '}
-        <a href="/base-url">change our app's Base URL</a>.{baseUrlAddendum}
-      </p>
-      <p>In general, we can choose between following deploy strategies:</p>
+    <>
+      <p>You can choose between following deploy strategies:</p>
       <ul>
         <li>
-          We build locally and upload <code>dist/client/</code> to the static host.
+          Build locally and upload <code>dist/client/</code> to {name}.
         </li>
         <li>
-          We let a <a href="https://github.com/features/actions">GitHub action</a> build and upload{' '}
-          <code>dist/client/</code> to the static host.
+          Let a <a href="https://github.com/features/actions">GitHub action</a> build and upload{' '}
+          <code>dist/client/</code> to {name}.
         </li>
-        <li>We let the static host run the build for us.</li>
+        <li>Let {name} build your app.</li>
       </ul>
+    </>
+  )
+}
+function StaticHostDocOutro({ baseUrlAddendum }: { baseUrlAddendum?: JSX.Element }) {
+  return (
+    <>
+      <p>
+        {' '}
+        You can try out your production build locally with{' '}
+        <a href="https://vitejs.dev/guide/cli.html#vite-preview">
+          <code>$ vite preview</code>
+        </a>
+        , or any other tool such as{' '}
+        <a href="https://www.npmjs.com/package/serve">
+          <code>$ serve dist/client/</code>
+        </a>
+        .
+      </p>
       <blockquote>
         <p>
-          The <code>$ vite build</code> command generates a directory <code>dist/client/</code> that contains all our
-          static assets.
-        </p>
-        <p>
-          {' '}
-          We can locally try our deploy with <a href="https://www.npmjs.com/package/serve">serve</a> by running{' '}
-          <code>$ serve dist/client/</code>.
+          The <code>$ vite build</code> command generates a directory <code>dist/client/</code> that contains all static
+          assets.
         </p>
       </blockquote>
-    </div>
-  )
-  /*
-  return <>
-
-
+      <blockquote>
+        <p>
+          If you don't deploy your app at your domain root <code>https://my-domain.com</code>, but at{' '}
+          <code>https://my-domain.com/somewhere/nested</code> instead, then{' '}
+          <a href="/base-url">change your app's Base URL</a>.{baseUrlAddendum}
+        </p>
+      </blockquote>
     </>
-    */
+  )
 }
