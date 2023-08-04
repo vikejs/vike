8ed654edb23305590a6eabd589d016e55dbb7ecf
Tim Nguyen <140995877+timnghg@users.noreply.github.com>
2023-08-04 15:56:34 +0700

    fix: oven-sh/bun#3743 - Support Bun

--- vite-plugin-ssr/node/runtime/renderPage/getPageAssets.ts
+++ vite-plugin-ssr/node/runtime/renderPage/getPageAssets.ts
@@ -142,7 +142,7 @@ function resolveClientEntriesDev(
     // VPS client entry
     assert(clientEntry.endsWith('.js'))
     const req = require // Prevent webpack from bundling client code
-    const res = req.resolve
+    const res = typeof Bun != "undefined" ? (toPath: string) => Bun.resolveSync(toPath, __dirname) : req.resolve;
     try {
       // For Vitest
       // [RELATIVE_PATH_FROM_DIST] Current file: node_modules/vite-plugin-ssr/node/runtime/html/injectAssets.js
