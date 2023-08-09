b017fd90935c845cb7444ebff758a97cf08b0fc4
Li.Wen <7763134303@qq.com>
2023-08-09 17:30:24 +0800

    ts

--- examples/vue-router/pages/app.ts
+++ examples/vue-router/pages/app.ts
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: ./routertjs
+ * @version: v1.0
+ * @Author: LiWen
+ * @Date: 2023-08-09 17:25:26
+ * @LastEditors: LiWen
+ * @LastEditTime: 2023-08-09 17:29:21
+ */
 import { createSSRApp } from 'vue'
 import { createRouter } from './router'
 
