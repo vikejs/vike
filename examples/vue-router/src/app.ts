e21c1213a328de19587dc34aee74f12451d78537
Li.Wen <7763134303@qq.com>
2023-08-10 17:41:05 +0800

    架构调整

--- examples/vue-router/src/app.ts
+++ examples/vue-router/src/app.ts
@@ -4,16 +4,17 @@
  * @Author: LiWen
  * @Date: 2023-08-09 17:25:26
  * @LastEditors: LiWen
- * @LastEditTime: 2023-08-10 17:36:10
+ * @LastEditTime: 2023-08-10 17:40:18
  */
 import { createSSRApp } from 'vue'
 import { createRouter } from './router'
 import painia from './stores'
-import type { PageContext } from '../src/entity/types'
+import type { PageContext } from '@/entity/types'
 
 function createApp(pageContext: PageContext) {
   const app = createSSRApp(pageContext.Page)
   const router = createRouter()
+
   app.use(router)
   app.use(painia)
   
