242662e72b34be536147e7e24cfecf3e9b8045bd
Li.Wen <7763134303@qq.com>
2023-08-10 17:46:10 +0800

    调整页面加载方式

--- examples/vue-router/src/layout/all.page.server.ts
+++ examples/vue-router/src/layout/all.page.server.ts
@@ -1,13 +1,5 @@
-/*
- * @Descripttion: 
- * @version: v1.0
- * @Author: LiWen
- * @Date: 2023-08-09 17:25:26
- * @LastEditors: LiWen
- * @LastEditTime: 2023-08-10 16:25:23
- */
-import { renderToNodeStream } from '@vue/server-renderer'
-import { escapeInject } from 'vite-plugin-ssr/server'
+import { renderToString } from '@vue/server-renderer'
+import {  dangerouslySkipEscape } from 'vite-plugin-ssr/server'
 import { createApp } from '../app'
 import type { PageContext } from '../entity/types'
 import type { PageContextBuiltInClientWithClientRouting as PageContextBuiltInClient } from 'vite-plugin-ssr/types'
@@ -19,15 +11,12 @@ async function render(pageContext: PageContextBuiltInClient & PageContext) {
   router.push(pageContext.urlOriginal)
   await router.isReady()
 
-  const stream = renderToNodeStream(app)
+  const appHtml = await renderToString(app)
 
-  const documentHtml = escapeInject`<!DOCTYPE html>
+  const documentHtml = `<!DOCTYPE html>
     <html>
-      <head>
-        <title>标题待定</title>
-      </head>
       <body>
-        <div id="app">${stream}</div>
+        <div id="app">${dangerouslySkipEscape(appHtml)}</div>
       </body>
     </html>`
 
