2b59694ac4c3449d1ed857510cf70b0cc05f7ddf
Romuald Brillout <git@brillout.com>
2023-08-14 16:34:24 +0200

    minor refactor

--- vite-plugin-ssr/utils/filesystemPathHandling.ts
+++ vite-plugin-ssr/utils/filesystemPathHandling.ts
@@ -1,45 +1,19 @@
 export { toPosixPath }
 export { assertPosixPath }
-export { toSystemPath }
-export { isPosixPath }
 
 import { assert } from './assert'
 
-const sepPosix = '/'
-const sepWin32 = '\\'
-
-function isPosixPath(path: string) {
-  return !path.includes(sepWin32)
-}
-
-function toPosixPath(path: string) {
-  const pathPosix = path.split(sepWin32).join(sepPosix)
+function toPosixPath(path: string): string {
+  const pathPosix = path.split('\\').join('/')
   assertPosixPath(pathPosix)
   return pathPosix
 }
 
-function assertPosixPath(path: string) {
+function assertPosixPath(path: string): void {
   const errMsg = (msg: string) => `Not a posix path: ${msg}`
   assert(path !== null, errMsg('null'))
   assert(typeof path === 'string', errMsg(`typeof path === '${typeof path}'`))
   assert(path !== '', errMsg('(empty string)'))
   assert(path)
-  assert(!path.includes(sepWin32), errMsg(path))
-}
-
-function toSystemPath(path: string) {
-  if (isPosixEnv()) {
-    return toPosixPath(path)
-  }
-  if (isWin32Env()) {
-    return path.split(sepPosix).join(sepWin32)
-  }
-  assert(false)
-}
-
-function isWin32Env() {
-  return process.platform === 'win32'
-}
-function isPosixEnv() {
-  return !isWin32Env()
+  assert(!path.includes('\\'), errMsg(path))
 }
