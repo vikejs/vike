88a5213c4a4f6cfb6baf276905f579df21c75ace
Romuald Brillout <git@brillout.com>
2023-08-17 17:57:47 +0200

    enable source code to target both CJS and ESM

--- vite-plugin-ssr/node/plugin/index.ts
+++ vite-plugin-ssr/node/plugin/index.ts
@@ -54,9 +54,12 @@ function plugin(vpsConfig?: ConfigVpsUserProvided): any {
   return plugins
 }
 
-// Enable `const ssr = require('vite-plugin-ssr/plugin')`
-// This lives at the end of the file to ensure it happens after all assignments to `exports`
-module.exports = Object.assign(exports.default, exports)
+// Enable `const ssr = require('vite-plugin-ssr/plugin')`.
+//  - This lives at the end of the file to ensure it happens after all assignments to `exports`.
+//  - This is only used for the CJS build; we wrap it in a try-catch for the ESM build.
+try {
+  module.exports = Object.assign(exports.default, exports)
+} catch {}
 
 // Error upon wrong usage
 Object.defineProperty(plugin, 'apply', {
