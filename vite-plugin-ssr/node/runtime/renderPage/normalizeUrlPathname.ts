e91732b5c838157dfde4210555504edcb03267b7
Romuald Brillout <git@brillout.com>
2023-08-18 10:49:50 +0200

    minor refactor

--- vite-plugin-ssr/node/runtime/renderPage/normalizeUrlPathname.ts
+++ vite-plugin-ssr/node/runtime/renderPage/normalizeUrlPathname.ts
@@ -1,6 +1,6 @@
 export { normalizeUrlPathname }
 
-import { assert, parseUrl } from '../utils.js'
+import { assert, assertUrlComponents, createUrl, parseUrl } from '../utils.js'
 
 function normalizeUrlPathname(urlOriginal: string): string | null {
   const urlParsed = parseUrl(urlOriginal, '/')
@@ -8,13 +8,7 @@ function normalizeUrlPathname(urlOriginal: string): string | null {
   assert(pathnameOriginal.startsWith('/'))
   const pathnameNormalized = '/' + pathnameOriginal.split('/').filter(Boolean).join('/')
   if (pathnameOriginal === pathnameNormalized) return null
-  assert(urlOriginal === `${urlParsed.origin || ''}${recreateUrl(pathnameOriginal, urlParsed)}`)
-  const urlNormalized = recreateUrl(pathnameNormalized, urlParsed)
+  assertUrlComponents(urlOriginal, urlParsed.origin, pathnameOriginal, urlParsed.searchOriginal, urlParsed.hashOriginal)
+  const urlNormalized = createUrl('', pathnameNormalized, urlParsed.searchOriginal, urlParsed.hashOriginal)
   return urlNormalized
-}
-
-// Copied & adapted from https://github.com/brillout/vite-plugin-ssr/blob/bdfa8acd68a3575bfa1c8a4a3821ffc02da55ade/vite-plugin-ssr/utils/parseUrl.ts#L85-L86
-function recreateUrl(pathname: string, urlParsed: ReturnType<typeof parseUrl>) {
-  const urlRecreated = `${pathname}${urlParsed.searchOriginal || ''}${urlParsed.hashOriginal || ''}`
-  return urlRecreated
 }
