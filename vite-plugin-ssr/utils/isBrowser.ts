ceea33b780d286b9daff2551e30dfcdb269c824b
Romuald Brillout <git@brillout.com>
2023-08-23 09:11:01 +0200

    comment

--- vite-plugin-ssr/utils/isBrowser.ts
+++ vite-plugin-ssr/utils/isBrowser.ts
@@ -1,4 +1,5 @@
 export function isBrowser() {
   // Using `typeof window !== 'undefined'` alone is not enough because some users use https://www.npmjs.com/package/ssr-window
   return typeof window !== 'undefined' && typeof window.scrollY === 'number'
+  // Alternatively, test whether environment is a *real* browser: https://github.com/brillout/picocolors/blob/d59a33a0fd52a8a33e4158884069192a89ce0113/picocolors.js#L87-L89
 }
