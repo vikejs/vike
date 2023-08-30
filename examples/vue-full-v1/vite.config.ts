c6525b2c85e1666bb4bacab612e4451be12479e8
Aurelien Lourot <software@lourot.dev>
2023-08-30 15:20:00 +0200

    qual: fix 'pnpm run test:types'

--- examples/vue-full-v1/vite.config.ts
+++ examples/vue-full-v1/vite.config.ts
@@ -9,7 +9,7 @@ const config: UserConfig = {
     vue({
       include: [/\.vue$/, /\.md$/]
     }),
-    md()
+    md({})
   ],
   // We manually add a list of dependencies to be pre-bundled, in order to avoid a page reload at dev start which breaks vite-plugin-ssr's CI
   optimizeDeps: { include: ['cross-fetch'] }
