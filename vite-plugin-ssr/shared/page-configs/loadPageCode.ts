fad7020d427472a375b8c42318e62d03cd74d30a
Romuald Brillout <git@brillout.com>
2023-09-05 19:39:21 +0200

    align with test

--- vite-plugin-ssr/shared/page-configs/loadPageCode.ts
+++ vite-plugin-ssr/shared/page-configs/loadPageCode.ts
@@ -96,6 +96,6 @@ function assertIsNotNull(configValue: unknown, configName: string, codeFilePath:
   assert(!codeFilePath.includes('+config.'))
   assertUsage(
     configValue !== null,
-    `Set ${pc.cyan(configName)} to ${pc.cyan('null')} in a '+config.js' file instead of ${codeFilePath}`
+    `Set ${pc.cyan(configName)} to ${pc.cyan('null')} in a +config.h.js file instead of ${codeFilePath}`
   )
 }
