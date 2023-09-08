bdbcb00529ab486acce0b79e69e32cac2bab8135
Romuald Brillout <git@brillout.com>
2023-09-08 07:22:59 +0200

    minor refactor

--- vite-plugin-ssr/shared/page-configs/Config.ts
+++ vite-plugin-ssr/shared/page-configs/Config.ts
@@ -33,131 +33,130 @@ type ConfigNameBuiltIn = ConfigNameBuiltInPublic | ConfigNameBuiltInInternal
  *
  * https://vite-plugin-ssr.com/config
  */
-type Config<Page = unknown> = Partial<{
-  /** The root UI component of the page */
-  Page: Page | ImportString
+type Config<Page = unknown> = {
+  Page?: Page
 
   /** The page's URL(s).
    *
    *  https://vite-plugin-ssr.com/route
    */
-  route: string | Function | ImportString
+  route?: string | Function | ImportString
 
   /** Protect page(s), e.g. forbid unauthorized access.
    *
    *  https://vite-plugin-ssr.com/guard
    */
-  guard: Function | ImportString
+  guard?: Function | ImportString
   /**
    * Whether to pre-render the page(s).
    *
    * https://vite-plugin-ssr.com/pre-rendering
    */
-  prerender: boolean | ImportString
+  prerender?: boolean | ImportString
 
   /**
    * Inherit from other configurations.
    *
    * https://vite-plugin-ssr.com/extends
    */
-  extends: Config | Config[] | ImportString | ImportString[]
+  extends?: Config | Config[] | ImportString | ImportString[]
 
   /** Hook called before the page is rendered, usually for fetching data.
    *
    *  https://vite-plugin-ssr.com/onBeforeRender
    */
-  onBeforeRender: Function | ImportString | null
+  onBeforeRender?: Function | ImportString | null
 
   /** Determines what pageContext properties are sent to the client-side.
    *
    * https://vite-plugin-ssr.com/passToClient
    */
-  passToClient: string[] | ImportString
+  passToClient?: string[] | ImportString
 
   /** Hook called when page is rendered on the client-side.
    *
    * https://vite-plugin-ssr.com/onRenderClient
    */
-  onRenderClient: Function | ImportString
+  onRenderClient?: Function | ImportString
   /** Hook called when page is rendered to HTML on the server-side.
    *
    * https://vite-plugin-ssr.com/onRenderHtml
    */
-  onRenderHtml: Function | ImportString
+  onRenderHtml?: Function | ImportString
 
   /** Enable async Route Functions.
    *
    * https://vite-plugin-ssr.com/route-function#async
    */
-  iKnowThePerformanceRisksOfAsyncRouteFunctions: boolean | ImportString
+  iKnowThePerformanceRisksOfAsyncRouteFunctions?: boolean | ImportString
 
   /** Change the URL root of Filesystem Routing.
    *
    * https://vite-plugin-ssr.com/filesystemRoutingRoot
    */
-  filesystemRoutingRoot: string | ImportString
+  filesystemRoutingRoot?: string | ImportString
 
   /** Page Hook called when pre-rendering starts.
    *
    * https://vite-plugin-ssr.com/onPrerenderStart
    */
-  onPrerenderStart: Function | ImportString
+  onPrerenderStart?: Function | ImportString
   /** Global Hook called before the whole pre-rendering process starts.
    *
    * https://vite-plugin-ssr.com/onBeforePrerenderStart
    */
-  onBeforePrerenderStart: Function | ImportString
+  onBeforePrerenderStart?: Function | ImportString
 
   /** Hook called before the URL is routed to a page.
    *
    * https://vite-plugin-ssr.com/onBeforeRoute
    */
-  onBeforeRoute: Function | ImportString
+  onBeforeRoute?: Function | ImportString
 
   /** Hook called after the page is hydrated.
    *
    * https://vite-plugin-ssr.com/clientRouting
    */
-  onHydrationEnd: Function | ImportString
+  onHydrationEnd?: Function | ImportString
   /** Hook called before the user navigates to a new page.
    *
    * https://vite-plugin-ssr.com/clientRouting
    */
-  onPageTransitionStart: Function | ImportString
+  onPageTransitionStart?: Function | ImportString
   /** Hook called after the user navigates to a new page.
    *
    * https://vite-plugin-ssr.com/clientRouting
    */
-  onPageTransitionEnd: Function | ImportString
+  onPageTransitionEnd?: Function | ImportString
 
   /** Whether the UI framework (React/Vue/Solid/...) allows the page's hydration to be aborted.
    *
    * https://vite-plugin-ssr.com/clientRouting
    */
-  hydrationCanBeAborted: boolean | ImportString
+  hydrationCanBeAborted?: boolean | ImportString
   /** Additional client code.
    *
    * https://vite-plugin-ssr.com/client
    */
-  client: string | ImportString
+  client?: string | ImportString
   /** Enable Client Routing.
    *
    * https://vite-plugin-ssr.com/clientRouting
    */
-  clientRouting: boolean | ImportString
+  clientRouting?: boolean | ImportString
 
   /** Create new or modify existing configurations.
    *
    * https://vite-plugin-ssr.com/meta
    */
-  meta: ConfigMeta | ImportString
+  meta?: ConfigMeta | ImportString
 
   /** Prefetch links.
    *
    * https://vite-plugin-ssr.com/clientRouting#link-prefetching
    */
-  prefetchStaticAssets: PrefetchStaticAssets | ImportString
-}>
+  prefetchStaticAssets?: PrefetchStaticAssets | ImportString
+}
 
 type ConfigMeta = Record<string, ConfigDefinition>
 
