3e97e1db47bb19edaa6fce7ba64baa3d13836fce
Romuald Brillout <git@brillout.com>
2023-09-08 07:18:53 +0200

    revert "experiment with interface merging"

--- test/config-file-paths-v1/renderer/Config.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'vite-plugin-ssr/types' {
-  export interface Config {
-    Page?: `import:${string}`
-  }
-}
-
-// Useless import in order to make this file non-ambient
-import { Config as _ } from 'vite-plugin-ssr/types'
