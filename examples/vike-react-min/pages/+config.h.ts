fdb306f7208b95e1af8f8c106534930092dc6764
Romuald Brillout <git@brillout.com>
2023-09-25 20:20:30 +0200

    add /examples/vike-react-min

---
+++ examples/vike-react-min/pages/+config.h.ts
@@ -0,0 +1,6 @@
+import vikeReact from 'vike-react'
+import type { Config } from 'vike/types'
+
+export default {
+  extends: vikeReact
+} satisfies Config
---
+++ examples/vike-react-min/pages/index/+config.h.ts
@@ -0,0 +1,6 @@
+import Page from './Page'
+import type { Config } from 'vike/types'
+
+export default {
+  Page
+} satisfies Config
