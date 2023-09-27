715ceb960e1e7724f4a94c6da6c66f4d0bdf488f
Romuald Brillout <git@brillout.com>
2023-09-27 14:21:30 +0200

    minor refactor

--- docs/pages/banner.page.server.tsx
+++ docs/pages/banner.page.server.tsx
@@ -28,32 +28,30 @@ function render(pageContext: PageContext) {
 
 function Page() {
   return (
-    <>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-        <div style={{ display: 'flex', height: 320 }}>
-          <img src={iconPlugin} style={{ marginRight: 50, marginTop: 0, height: '100%' }} />
-          <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', height: '100%' }}>
-            <div>
-              <span
-                style={{
-                  fontSize: '8.8em',
-                  display: 'inline-block',
-                  verticalAlign: 'middle',
-                  fontWeight: 500,
-                  lineHeight: '1.1em'
-                }}
-              >
-                Vike
-              </span>
-            </div>
-            <div style={{ fontSize: '4.2em', paddingLeft: 4, paddingTop: 0, paddingBottom: 0, lineHeight: '1.2em' }}>
-              Next Generation
-              <br />
-              Frontend Framework
-            </div>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <div style={{ display: 'flex', height: 320 }}>
+        <img src={iconPlugin} style={{ marginRight: 50, marginTop: 0, height: '100%' }} />
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', height: '100%' }}>
+          <div>
+            <span
+              style={{
+                fontSize: '8.8em',
+                display: 'inline-block',
+                verticalAlign: 'middle',
+                fontWeight: 500,
+                lineHeight: '1.1em'
+              }}
+            >
+              Vike
+            </span>
+          </div>
+          <div style={{ fontSize: '4.2em', paddingLeft: 4, paddingTop: 0, paddingBottom: 0, lineHeight: '1.2em' }}>
+            Next Generation
+            <br />
+            Frontend Framework
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
