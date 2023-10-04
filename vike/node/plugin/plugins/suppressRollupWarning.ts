acfc1590f7fa3a7a4fa230f4b7d997bf65623cc0
Romuald Brillout <git@brillout.com>
2023-10-04 15:33:24 +0200

    fix: improve suppressing of expected Rollup warnings

--- vike/node/plugin/plugins/suppressRollupWarning.ts
+++ vike/node/plugin/plugins/suppressRollupWarning.ts
@@ -44,17 +44,23 @@ function suppressUnusedImport(warning: RollupWarning & { ids?: string[] }): bool
   if (warning.code !== 'UNUSED_EXTERNAL_IMPORT') return false
 
   // I guess it's expected that JSX contains unsused `import React from 'react'`
-  if (warning.exporter === 'react' && warning.names?.includes('default')) return true
+  if (warning.exporter === 'react' && warning.names?.includes('default')) {
+    warning.names = warning.names.filter((n) => n !== 'default')
+    if (warning.names.length === 0) return true
+  }
 
   // Suppress:
   // ```
   // "untrack" is imported from external module "solid-js" but never used in "pages/star-wars/index/+Page.tsx", "pages/index/Counter.tsx", "components/Link.tsx", "pages/todo/TodoList.tsx" and "pages/todo/+Page.tsx".
   // ```
   // I'm guessing Solid's transformer injects `import { untrack } from 'solid-js'`
-  if (warning.exporter === 'solid-js' && warning.names?.includes('untrack')) return true
+  if (warning.exporter === 'solid-js' && warning.names?.includes('untrack')) {
+    warning.names = warning.names.filter((n) => n !== 'untrack')
+    if (warning.names.length === 0) return true
+  }
 
   // If some library does something unexpected, we suppress since it isn't actionable
-  if (warning.ids?.some((id) => id.includes('/node_modules/'))) return true
+  if (warning.ids?.every((id) => id.includes('/node_modules/'))) return true
 
   return false
 }
