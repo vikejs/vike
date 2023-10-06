86abfd54091baae5a8140adb35524eb29599d653
Romuald Brillout <git@brillout.com>
2023-10-06 13:06:23 +0200

    fix: remove vike from ssr.noExternal (fix #1163)

--- vike/node/plugin/plugins/commonConfig.ts
+++ vike/node/plugin/plugins/commonConfig.ts
@@ -17,12 +17,7 @@ function commonConfig(): Plugin[] {
     {
       name: 'vike-commonConfig-1',
       config: () => ({
-        appType: 'custom',
-        ssr: {
-          // Needed as long as Vike is published as CJS.
-          // TODO: can we remove this once Vike is published as ESM?
-          external: ['vike', 'vike/server']
-        }
+        appType: 'custom'
       }),
       configResolved(config) {
         installRequireShim_setUserRootDir(config.root)
