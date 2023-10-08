10be9d090e28cbf4774e2df14cbcfcd81c181334
Romuald Brillout <git@brillout.com>
2023-10-08 13:57:10 +0200

    improve route globs [wip] (#1167)

--- vike/shared/route/resolveRouteString.spec.ts
+++ vike/shared/route/resolveRouteString.spec.ts
@@ -1,6 +1,6 @@
 import { resolveRouteString } from './resolveRouteString.js'
 import { stripAnsi } from '../../utils/stripAnsi.js'
-import { expect, describe, it } from 'vitest'
+import { expect, describe, it, assert } from 'vitest'
 
 const r: typeof resolveRouteString = (a, b) => resolveRouteString(a, b)
 
@@ -9,17 +9,14 @@ describe('resolveRouteString', () => {
     expect(r('/a', '/b')).toEqual(null)
     expect(r('/a', '/a')).toEqual({ routeParams: {} })
     expect(r('/', '/')).toEqual({ routeParams: {} })
-
-    expectErr(() => r('', '/a/b/c'), `[vike][Wrong Usage] Invalid Route String '' (empty string): set it to / instead`)
-    expectErr(
-      () => r('a', '/a/b/c'),
-      `[vike][Wrong Usage] Invalid Route String a: Route Strings should start with a leading slash / (or be *)`
-    )
   })
 
   it('parameterized routes', () => {
     expect(r('/@p', '/a')).toEqual({ routeParams: { p: 'a' } })
     expect(r('/@p', '/a/')).toEqual({ routeParams: { p: 'a' } })
+    expect(r('/@p/b', '/a/b')).toEqual({ routeParams: { p: 'a' } })
+    expect(r('/c/@p/b', '/c/a/b')).toEqual({ routeParams: { p: 'a' } })
+
     expect(r('/@p', '/a/b')).toEqual(null)
     expect(r('/@p', '/')).toEqual(null)
     expect(r('/@p', '/@p')).toEqual({ routeParams: { p: '@p' } })
@@ -41,6 +38,7 @@ describe('resolveRouteString', () => {
     expect(r('/a/b/@p', '/a/c')).toEqual(null)
     expect(r('/a/b/@p', '/a')).toEqual(null)
 
+    return
     expect(r('/@p1/@p2', '/a/b')).toEqual({ routeParams: { p1: 'a', p2: 'b' } })
     expect(r('/@p1/@p2', '/a/b/')).toEqual({ routeParams: { p1: 'a', p2: 'b' } })
     expect(r('/@p1/@p2', '/a/b/c/d')).toEqual(null)
@@ -48,26 +46,17 @@ describe('resolveRouteString', () => {
   })
 
   it('glob', () => {
-    expect(r('*', '/')).toEqual({ routeParams: { '*': '' } })
+    expect(r('*', '/')).toEqual({ routeParams: { '*': '/' } })
     expect(r('/*', '/')).toEqual({ routeParams: { '*': '' } })
     expect(r('/*', '/a')).toEqual({ routeParams: { '*': 'a' } })
-    expect(r('*', '/a')).toEqual({ routeParams: { '*': 'a' } })
+    expect(r('*', '/a')).toEqual({ routeParams: { '*': '/a' } })
     expect(r('/*', '/a/b')).toEqual({ routeParams: { '*': 'a/b' } })
-    expect(r('*', '/a/b')).toEqual({ routeParams: { '*': 'a/b' } })
+    expect(r('*', '/a/b')).toEqual({ routeParams: { '*': '/a/b' } })
     expect(r('/*', '/a/b/c')).toEqual({ routeParams: { '*': 'a/b/c' } })
-    expect(r('*', '/a/b/c')).toEqual({ routeParams: { '*': 'a/b/c' } })
+    expect(r('*', '/a/b/c')).toEqual({ routeParams: { '*': '/a/b/c' } })
     expect(r('/a/*', '/a/b')).toEqual({ routeParams: { '*': 'b' } })
     expect(r('/a/*', '/a/b/c/d')).toEqual({ routeParams: { '*': 'b/c/d' } })
     expect(r('/a/*', '/b/c')).toEqual(null)
-
-    expectErr(
-      () => r('/a/*/c/*', '/a/b/c'),
-      `[vike][Wrong Usage] Invalid Route String /a/*/c/*: Route Strings aren't allowed to contain more than one glob * (use a Route Function instead)`
-    )
-    expectErr(
-      () => r('/a/*/c', '/a/b/c'),
-      `[vike][Wrong Usage] Invalid Route String /a/*/c: make sure it ends with * or use a Route Function`
-    )
   })
 
   it('special characters', () => {
@@ -81,7 +70,71 @@ describe('resolveRouteString', () => {
     expect(r('/@p', '/¥')).toEqual({ routeParams: { p: '¥' } })
     expect(r('/*', '/¥')).toEqual({ routeParams: { '*': '¥' } })
   })
+
+  return
+  it('invalid route string', () => {
+    expectErr(() => r('', '/a/b/c'), `[vike][Wrong Usage] Invalid Route String '' (empty string): set it to / instead`)
+    expectErr(
+      () => r('a', '/a/b/c'),
+      `[vike][Wrong Usage] Invalid Route String a: Route Strings should start with a leading slash / (or be *)`
+    )
+    expectErr(
+      () => r('/a/*/c/*', '/a/b/c'),
+      `[vike][Wrong Usage] Invalid Route String /a/*/c/*: Route Strings aren't allowed to contain more than one glob * (use a Route Function instead)`
+    )
+    expectErr(
+      () => r('/a/*/c', '/a/b/c'),
+      `[vike][Wrong Usage] Invalid Route String /a/*/c: make sure it ends with * or use a Route Function`
+    )
+  })
 })
+describe('resolveRouteString - advanced globbing', () => {
+  it('basics', () => {
+    expect(test('/a', '/a')).toEqual([])
+    expect(test('/', '/')).toEqual([])
+    expect(test('/a', '/b')).toBe(false)
+    expect(test('/a/b', '/a/b')).toEqual([])
+  })
+  it('globbing', () => {
+    expect(test('/a/*/c', '/a/b/c')).toEqual(['b'])
+    expect(test('/a/*/c', '/a/b/d')).toEqual(false)
+    expect(test('/a/*', '/a/b/c')).toEqual(['b/c'])
+    expect(test('/*', '/a')).toEqual(['a'])
+    expect(test('/*', '/a/b/c/d')).toEqual(['a/b/c/d'])
+    expect(test('/a/*', '/a/b/c/d')).toEqual(['b/c/d'])
+    expect(test('/a/b/*', '/a/b/c/d')).toEqual(['c/d'])
+    expect(test('/a/b/c/d', '/a/b/c/d')).toEqual([])
+    expect(test('/a/*/c/*/e', '/a/b/c/d/e')).toEqual(['b', 'd'])
+    expect(test('/a/*/c/*', '/a/b/c/d/e')).toEqual(['b', 'd/e'])
+    expect(test('/a/*/e', '/a/b/c/d/e')).toEqual(['b/c/d'])
+  })
+  it('globbing - inside segment', () => {
+    expect(test('/a*e', '/a/b/c/d/e')).toEqual(['/b/c/d/'])
+  })
+  it('BurdaForward', () => {
+    // Use case 1
+    expect(test('/some/*.html', '/some/a.html')).toEqual(['a'])
+    expect(test('/some/*.html', '/some/a/b/c.html')).toEqual(['a/b/c'])
+    expect(test('/some/*.html', '/some/a/b/c.html')).toEqual(['a/b/c'])
+    // Use case 2
+    expect(test('/some/route*.html', '/some/route.html')).toEqual([''])
+    expect(test('/some/route*.html', '/some/route-a.html')).toEqual(['-a'])
+    expect(test('/some/route*.html', '/some/routea/b/c.html')).toEqual(['a/b/c'])
+  })
+})
+
+function test(routeString: string, urlPathname: string): false | string[] {
+  const res = resolveRouteString(routeString, urlPathname)
+  if (!res) return false
+  const globs: string[] = []
+  Object.entries(res.routeParams).forEach(([key, val]) => {
+    assert(key.startsWith('*'))
+    key = key.slice(1)
+    const i = key === '' ? 0 : parseInt(key, 10) - 1
+    globs[i] = val
+  })
+  return globs
+}
 
 function expectErr(fn: Function, errMsg: string) {
   {
--- vike/shared/route/resolveRouteString.ts
+++ vike/shared/route/resolveRouteString.ts
@@ -26,7 +26,154 @@ function assertRouteString(routeString: string, errPrefix: `${string}Invalid` | 
   )
 }
 
+// TODO: precedence
 function resolveRouteString(routeString: string, urlPathname: string): null | { routeParams: Record<string, string> } {
+  const routeSegments = splitRouteString(routeString)
+  //*
+  console.log()
+  console.log('routeString', routeString)
+  console.log('urlPathname', urlPathname)
+  console.log('routeSegments', routeSegments)
+  //*/
+  let urlRest = urlPathname
+
+  const routeParams: Record<string, string> = {}
+  const globs: string[] = []
+  let isGlobbing:
+    | false
+    | {
+        match: string[]
+        isTrailingGlob?: true
+      } = false
+  let isParam: false | string = false
+  const pushGlob = () => {
+    if (isGlobbing) {
+      globs.push(isGlobbing.match.join('/'))
+      isGlobbing = false
+    }
+  }
+  const pushParam = (match: string) => {
+    if (match!.includes('/')) return true
+    assert(isParam)
+    routeParams[isParam] = match!
+    isParam = false
+  }
+
+  for (const routeIndex in routeSegments) {
+    const routeSegment = routeSegments[routeIndex]!
+    /*
+    console.log('routeSegment', routeSegment)
+    console.log('urlRest', urlRest)
+    */
+    if (routeSegment.static) {
+      const { s } = routeSegment
+      if (!isGlobbing && !isParam) {
+        if (!urlRest.startsWith(s)) return null
+        urlRest = urlRest.slice(s.length)
+      } else {
+        if (!urlRest.includes(s)) return null
+        const [match, ...rest] = urlRest.split(s)
+        if (isParam) {
+          const failed = pushParam(match!)
+          if (failed) return null
+        }
+        if (isGlobbing) {
+          isGlobbing.match.push(match!)
+          pushGlob()
+        }
+        urlRest = rest.join(s)
+      }
+    } else if (routeSegment.param) {
+      isParam = routeSegment.s
+    } else {
+      assert(routeSegment.glob)
+      pushGlob()
+      isGlobbing = { match: [] }
+      // TODO: remove
+      if (routeIndex === String(routeSegments.length - 1)) {
+        isGlobbing.isTrailingGlob = true
+      }
+    }
+  }
+
+  if (urlRest && !isGlobbing && !isParam) {
+    return null
+  }
+  if (isParam) {
+    if (!urlRest) return null
+    if (urlRest.endsWith('/') && urlRest !== '/') urlRest = urlRest.slice(0, -1)
+    const failed = pushParam(urlRest)
+    if (failed) return null
+  }
+  if (isGlobbing) {
+    isGlobbing.match.push(urlRest)
+    pushGlob()
+  }
+
+  globs.forEach((val, i) => {
+    const idx = `*${globs.length !== 1 ? i + 1 : ''}` as const
+    routeParams[idx] = val
+  })
+
+  return { routeParams }
+}
+// TODO: simplify
+type Segment =
+  | {
+      glob: true
+      static?: undefined
+      param?: undefined
+    }
+  | {
+      glob?: undefined
+      static: true
+      param?: undefined
+      s: string
+    }
+  | {
+      glob?: undefined
+      static?: undefined
+      param: true
+      s: string
+    }
+function splitRouteString(routeString: string) {
+  const segments: Segment[] = []
+  const parts = routeString.split('/')
+  parts.forEach((s, i) => {
+    if (isParam(s)) {
+      assertWarning(
+        !s.startsWith(PARAM_TOKEN_OLD),
+        `Outdated Route String ${pc.cyan(routeString)}, use ${pc.cyan(
+          routeString.split(PARAM_TOKEN_OLD).join(PARAM_TOKEN_NEW)
+        )} instead.`,
+        { onlyOnce: true }
+      )
+      segments.push({ param: true, s: s.slice(1) })
+    } else {
+      // TODO: simplify?
+      if (i !== parts.length - 1) s += '/'
+      if (segments[segments.length - 1]?.param) s = '/' + s
+      const parts2 = s.split('*')
+      parts2.forEach((s, i) => {
+        if (i !== 0) segments.push({ glob: true })
+        if (s !== '') {
+          const segmentLast = segments[segments.length - 1]
+          if (segmentLast?.static) {
+            segmentLast.s += s
+          } else {
+            segments.push({ static: true, s })
+          }
+        }
+      })
+    }
+  })
+  return segments
+}
+
+function resolveRouteStringOld(
+  routeString: string,
+  urlPathname: string
+): null | { routeParams: Record<string, string> } {
   assertRouteString(routeString)
   assert(urlPathname.startsWith('/'))
 
