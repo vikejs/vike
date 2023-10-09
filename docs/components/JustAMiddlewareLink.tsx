34ae98dfff563e3890ee33fe75cd54375bb4b90a
Romuald Brillout <git@brillout.com>
2023-10-09 12:01:49 +0200

    docs: minor

--- docs/components/JustAMiddlewareLink.tsx
+++ docs/components/JustAMiddlewareLink.tsx
@@ -6,8 +6,8 @@ import { Link } from '@brillout/docpress'
 function JustAMiddlewareLink() {
   return (
     <>
-      From the perspective of the server, vike app is{' '}
-      <Link text="just a middleware" href="/integration#server-side-tools" />.
+      From the perspective of the server, Vike is{' '}
+      <Link text="just a server middleware" href="/integration#server-side-tools" />.
     </>
   )
 }
