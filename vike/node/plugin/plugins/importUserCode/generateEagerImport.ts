1c0a39e838f91970b97063a4bbfe40ec5548464f
Romuald Brillout <git@brillout.com>
2023-10-13 17:27:49 +0200

    minor refactor

--- vike/node/plugin/plugins/importUserCode/generateEagerImport.ts
+++ vike/node/plugin/plugins/importUserCode/generateEagerImport.ts
@@ -5,18 +5,18 @@ let varCounterGlobal = 0
 function generateEagerImport(
   importPath: string,
   varCounter?: number,
-  importName?: string
+  exportName?: string
 ): { importVar: string; importStatement: string } {
   if (varCounter === undefined) varCounter = varCounterGlobal++
   const importVar = `import_${varCounter}` as const
   const importLiteral = (() => {
-    if (!importName || importName === '*') {
+    if (!exportName || exportName === '*') {
       return `* as ${importVar}` as const
     }
-    if (importName === 'default') {
+    if (exportName === 'default') {
       return importVar
     }
-    return `{ ${importName} as ${importVar} }` as const
+    return `{ ${exportName} as ${importVar} }` as const
   })()
   const importStatement = `import ${importLiteral} from '${importPath}';`
   return { importVar, importStatement }
