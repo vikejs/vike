ff73ee95b2b4689c5f33e7ea10b30086a611ef7c
Romuald Brillout <git@brillout.com>
2023-10-13 15:35:40 +0200

    comment

--- vike/node/plugin/plugins/importUserCode/v1-design/getFilePathToShowToUser.ts
+++ vike/node/plugin/plugins/importUserCode/v1-design/getFilePathToShowToUser.ts
@@ -4,10 +4,18 @@ export type { FilePath }
 import { assert } from '../../../utils.js'
 
 type FilePath = {
+  /** The file's path relative to the filesystem root.
+   *
+   * Example: `/home/rom/code/my-app/pages/some-page/Page.js`
+   */
   filePathAbsolute: string
+  /** The file's path relative to the Vite's root (i.e. the user's project root directory).
+   *
+   * Example: `/pages/some-page/Page.js`
+   */
   filePathRelativeToUserRootDir: null | string
 }
-function getFilePathToShowToUser(filePath: FilePath) {
+function getFilePathToShowToUser(filePath: FilePath): string {
   const filePathToShowToUser = filePath.filePathRelativeToUserRootDir ?? filePath.filePathAbsolute
   assert(filePathToShowToUser)
   return filePathToShowToUser
