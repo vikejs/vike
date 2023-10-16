8463abb8d611a1684ea742f626e392c956201ff6
Romuald Brillout <git@brillout.com>
2023-10-16 15:58:48 +0200

    fix HookTypeScriptHints (usage)

--- docs/components/HookTypeScriptHints.tsx
+++ docs/components/HookTypeScriptHints.tsx
@@ -4,13 +4,22 @@ import React from 'react'
 import { assert, Link, Warning } from '@brillout/docpress'
 
 function HookTypeScriptHints({ hookTypeName }: { hookTypeName: `${string}Sync` | `${string}Async` }) {
-  assert(hookTypeName.endsWith('Sync') || hookTypeName.endsWith('Async'))
-  const isSync = hookTypeName.endsWith('Sync')
+  let hookName: string
+  let isSync: boolean
+  if (hookTypeName.endsWith('Sync')) {
+    hookName = hookTypeName.slice('Sync'.length)
+    isSync = true
+  } else if (hookTypeName.endsWith('Async')) {
+    hookName = hookTypeName.slice('Async'.length)
+    isSync = false
+  } else {
+    assert(false, hookTypeName)
+  }
   return (
     <>
       <Warning>
         Don't omit <code>ReturnType&lt;{hookTypeName}&gt;</code> (don't write{' '}
-        <code>{`const onRenderHtml: ${hookTypeName} = ${isSync ? '' : 'async '}(pageContext) => {`}</code>), otherwise
+        <code>{`const ${hookName}: ${hookTypeName} = ${isSync ? '' : 'async '}(pageContext) => {`}</code>), otherwise
         TypeScript won't strictly check the return type for unknown extra properties: see this TypeScript{' '}
         <a href="https://www.typescriptlang.org/play?#code/C4TwDgpgBAYgdlAvFAFGAhgJ3QWwFxQDOwmAlnAOYCUSAfFAN5ToFwCuOARhJlAL4AoAQHphUQDLkUAOqYA9pShtC6ChAEBjecSgAzOAEYC8JKgzYcNRPRQMBUZgX0AaO1FESoAFXAQAyurIwYCgAE1kIQjgAcmDNHDAAG3RyV04CACYBPiohd0BQcigAYVlMTAh1YKUVNU04bT10owRkNCxcKgIAJQhgNkw4b0gAHnh6K1RbexYoZ1d8rx9-QNjZeKTyQlSMrKogA">
           playground
