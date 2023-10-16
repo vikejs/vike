3f9ae4fbd1024f094d335886fda80e28417d3173
Romuald Brillout <git@brillout.com>
2023-10-16 15:38:00 +0200

    udpate example

--- examples/react-full-v1/pages/hello/+route.ts
+++ examples/react-full-v1/pages/hello/+route.ts
@@ -1,11 +1,11 @@
 // https://vike.dev/route
 export { route }
 
-import type { Route } from 'vike/types'
+import type { RouteSync } from 'vike/types'
 import { resolveRoute } from 'vike/routing'
 
 // Route Functions enables advanced routing logic
-const route: Route = (pageContext): ReturnType<Route> => {
+const route: RouteSync = (pageContext): ReturnType<RouteSync> => {
   if (pageContext.urlPathname === '/hello' || pageContext.urlPathname === '/hello/') {
     const name = 'anonymous'
     return { routeParams: { name } }
