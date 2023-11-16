9550e04c123f5fa8f4f9956e8e9e7a3b95419dc6
Romuald Brillout <git@brillout.com>
2023-11-16 17:19:47 +0100

    Revert "Revert "update examples and tests""
    This reverts commit 71fbb1b6bc61986320bafded911530947a1e9b88.

--- examples/react-full-v1/renderer/+config.h.ts
+++ examples/react-full-v1/renderer/+config.h.ts
@@ -9,11 +9,11 @@ export default {
   meta: {
     // Create new config 'title'
     title: {
-      env: 'server-and-client'
+      env: { server: true, client: true }
     },
     // Create new config 'onBeforeRenderIsomorph'
     onBeforeRenderIsomorph: {
-      env: 'config-only',
+      env: { config: true },
       effect({ configDefinedAt, configValue }) {
         if (typeof configValue !== 'boolean') {
           throw new Error(`${configDefinedAt} should be a boolean`)
@@ -24,7 +24,7 @@ export default {
               onBeforeRender: {
                 // We override Vike's default behavior of always loading/executing onBeforeRender() on the server-side.
                 // If we set onBeforeRenderIsomorph to true, then onBeforeRender() is loaded/executed in the browser as well, allowing us to fetch data direcly from the browser upon client-side navigation (without involving our Node.js/Edge server at all).
-                env: 'server-and-client'
+                env: { server: true, client: true }
               }
             }
           }
