bb484e3b7061ec6df2b4e879b2e705310964b23c
Romuald Brillout <git@brillout.com>
2023-11-30 23:53:08 +0100

    docs: re-use screenshot fixture

--- docs/.testRun.ts
+++ docs/.testRun.ts
@@ -58,10 +58,8 @@ function testRun(cmd: 'pnpm run dev' | 'pnpm run preview') {
     expect(await locator.isHidden()).toBe(false)
   })
 
-  /* TODO: re-use
   test('screenshot fixture', async () => {
     await page.locator('#version-number').evaluate((element) => (element.innerHTML = 'v9.9.99'))
     await testScreenshotFixture({ doNotTestLocally: true })
   })
-  */
 }
