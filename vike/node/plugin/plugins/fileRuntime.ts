86b3cd489194ec05a28c039db6b8879c0591aa59
Yusaku <hitoyasablue@gmail.com>
2023-12-01 23:31:35 +0900

    use resolveId instead of transform

--- vike/node/plugin/plugins/fileRuntime.ts
+++ vike/node/plugin/plugins/fileRuntime.ts
@@ -5,28 +5,15 @@ import { assertUsage } from '../utils.js'
 
 function assertFileRuntime(): Plugin {
   return {
-    name: 'vike:assert-file-environment',
+    name: 'vike:assert-file-runtime',
     enforce: 'pre',
-    async transform(code, id, options) {
-      if (!id.match(/\.[cm]?[jt]sx?$/)) return
-
-      const importRegex = /import\s+.*\s+from\s+['"]([^'"]+)['"]/g
-      let match
-      while ((match = importRegex.exec(code)) !== null) {
-        const importPath = match[1]
-
-        if (options?.ssr && importPath?.includes('.client')) {
-          assertUsage(
-            false,
-            `Import Error: Client-only module "${importPath}" included in server bundle (imported by "${id}").`
-          )
-        }
-        if (!options?.ssr && importPath?.includes('.server')) {
-          assertUsage(
-            false,
-            `Import Error: Server-only module "${importPath}" included in client bundle (imported by "${id}").`
-          )
-        }
+    async resolveId(source, importer, options) {
+      const additionalMessage = importer ? ` (imported by ${importer.split('?')[0]}) ` : ''
+      if (options.ssr && source.includes('.client')) {
+        assertUsage(false, `Client-only module "${source}" included in server bundle${additionalMessage}.`)
+      }
+      if (!options.ssr && source.includes('.server')) {
+        assertUsage(false, `Server-only module "${source}" included in client bundle${additionalMessage}.`)
       }
     }
   }
