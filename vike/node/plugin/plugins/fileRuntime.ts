8afa577c29ee199946d4ed1ed8b1aad7cfcb50e3
Yusaku <hitoyasablue@gmail.com>
2023-12-03 00:49:36 +0900

    also use this.resolve for detecting aliases

--- vike/node/plugin/plugins/fileRuntime.ts
+++ vike/node/plugin/plugins/fileRuntime.ts
@@ -8,12 +8,16 @@ function assertFileRuntime(): Plugin {
     name: 'vike:assert-file-runtime',
     enforce: 'pre',
     async resolveId(source, importer, options) {
+      const resolved = await this.resolve(source, importer, { skipSelf: true, ...options })
+      if (!resolved) return
+
       const additionalMessage = importer ? ` (imported by ${importer.split('?')[0]}) ` : ''
-      if (options?.ssr && source.includes('.client')) {
-        assertUsage(false, `Client-only module "${source}" included in server bundle${additionalMessage}.`)
+      const modulePath = resolved.id.split('?')[0] || ''
+      if (options?.ssr && modulePath.includes('.client')) {
+        assertUsage(false, `Client-only module "${modulePath}" included in server bundle${additionalMessage}.`)
       }
-      if (!options?.ssr && source.includes('.server')) {
-        assertUsage(false, `Server-only module "${source}" included in client bundle${additionalMessage}.`)
+      if (!options?.ssr && modulePath.includes('.server')) {
+        assertUsage(false, `Server-only module "${modulePath}" included in client bundle${additionalMessage}.`)
       }
     }
   }
