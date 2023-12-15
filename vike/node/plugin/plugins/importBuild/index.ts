28656442eba77806e02ea160d3846e72b51ec0dc
Romuald Brillout <git@brillout.com>
2023-12-15 13:15:19 +0100

    use latest @brillout/vite-plugin-import-build pre-release

--- vike/node/plugin/plugins/importBuild/index.ts
+++ vike/node/plugin/plugins/importBuild/index.ts
@@ -1,16 +1,21 @@
 export { importBuild }
 
-import type { Plugin, ResolvedConfig } from 'vite'
-import { importBuild as importBuild_ } from '@brillout/vite-plugin-import-build/plugin.js'
-import { getOutDirs, projectInfo, toPosixPath } from '../../utils.js'
+import type { Plugin, ResolvedConfig, Rollup } from 'vite'
+import { importBuild as importBuild_, findImportBuildBundleEntry } from '@brillout/vite-plugin-import-build/plugin.js'
+import { assert, getOutDirs, projectInfo, toPosixPath, viteIsSSR } from '../../utils.js'
 import path from 'path'
 import { createRequire } from 'module'
 import { getConfigVike } from '../../../shared/getConfigVike.js'
 import type { ConfigVikeResolved } from '../../../../shared/ConfigVike.js'
 import { getVikeManifest } from './getVikeManifest.js'
+import fs from 'fs/promises'
+import { virtualFileIdImportUserCodeServer } from '../../../shared/virtual-files/virtualFileImportUserCode.js'
 // @ts-ignore Shimmed by dist-cjs-fixup.js for CJS build.
 const importMetaUrl: string = import.meta.url
 const require_ = createRequire(importMetaUrl)
+type Bundle = Rollup.OutputBundle
+type Options = Rollup.NormalizedOutputOptions
+const ASSETS_MAP = '__VITE_ASSETS_MAP__'
 
 function importBuild(): Plugin[] {
   let config: ResolvedConfig
@@ -22,38 +27,52 @@ function importBuild(): Plugin[] {
       async configResolved(config_) {
         config = config_
         configVike = await getConfigVike(config)
+      },
+      async writeBundle(options, bundle) {
+        if (!viteIsSSR(config)) return
+        await replace_ASSETS_MAP(options, bundle)
       }
     },
     importBuild_({
-      getImporterCode: ({ findBuildEntry }) => {
-        const pageFilesEntry = findBuildEntry('pageFiles')
-        return getImporterCode(pageFilesEntry, config, configVike)
+      getImporterCode: () => {
+        return getEntryCode(config, configVike)
       },
       libraryName: projectInfo.projectName
     })
   ]
 }
 
-function getImporterCode(pageFilesEntry: string, config: ResolvedConfig, configVike: ConfigVikeResolved) {
+function getEntryCode(config: ResolvedConfig, configVike: ConfigVikeResolved): string {
   const importPath = getImportPath(config)
-
   const vikeManifest = getVikeManifest(config, configVike)
-
-  // The only reason we went for using CJS require() instead of ESM import() is because import() doesn't support .json files
   const importerCode = [
-    '(async () => {',
-    `  const { setImportBuildGetters } = await import('${importPath}');`,
+    `  import { setImportBuildGetters } from '${importPath}';`,
+    `  import * as pageFiles from '${virtualFileIdImportUserCodeServer}';`,
     '  setImportBuildGetters({',
-    `    pageFiles: () => import('./${pageFilesEntry}'),`,
+    `    pageFiles: () => pageFiles,`,
     // TODO: rename clientManifest -> assetManifest
-    "    clientManifest: () => require('../assets.json'),",
+    `    clientManifest: () => { return ${ASSETS_MAP} },`,
     // TODO: rename pluginManifest -> vikeManifest
     `    pluginManifest: () => (${JSON.stringify(vikeManifest, null, 2)}),`,
     '  });',
-    '})()',
     ''
   ].join('\n')
   return importerCode
+}
+async function replace_ASSETS_MAP(options: Options, bundle: Bundle) {
+  const { dir } = options
+  assert(dir)
+  // I guess importBuild won't be found in the bundle when using @vitejs/plugin-legacy
+  const importBuildEntry = findImportBuildBundleEntry(bundle)
+  const importBuildFilePath = path.join(dir, importBuildEntry.fileName)
+  const assetsJsonFilePath = path.join(dir, '..', 'assets.json')
+  const [assetsJsonString, importBuildFileContent] = await Promise.all([
+    await fs.readFile(assetsJsonFilePath, 'utf8'),
+    await fs.readFile(importBuildFilePath, 'utf8')
+  ])
+  const importBuildFileContentFixed = importBuildFileContent.replace(ASSETS_MAP, assetsJsonString)
+  assert(importBuildFileContentFixed !== importBuildFileContent)
+  await fs.writeFile(importBuildFilePath, importBuildFileContentFixed)
 }
 function getImportPath(config: ResolvedConfig) {
   // We resolve filePathAbsolute even if we don't use it: we use require.resolve() as an assertion that the relative path is correct
