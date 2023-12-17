245e306d1a5989b9ca0c8e5484415b893d4852d6
Dániel Horváth <horvath.daniel@lexunit.hu>
2023-12-17 20:36:36 +0100

    improve

--- vike/node/plugin/plugins/ssrEmitAssets.ts
+++ vike/node/plugin/plugins/ssrEmitAssets.ts
@@ -6,7 +6,7 @@ import fs from 'fs/promises'
 import { existsSync } from 'fs'
 import { ViteManifestEntry } from '../../shared/ViteManifest.js'
 import fg from 'fast-glob'
-import { getGlobalObject } from '../utils.js'
+import { assert, getGlobalObject } from '../utils.js'
 
 const globalObject = getGlobalObject('ssrEmitAssets.ts', {
   outDirServerAbs: '',
@@ -35,12 +35,12 @@ function ssrEmitAssetsPlugin(): Plugin {
       globalObject.outDirClientAbs = path.posix.resolve(globalObject.outDirServerAbs, '..', 'client')
     },
     async closeBundle() {
-      const clientManifestPath = path.posix.join(globalObject.outDirClientAbs, "..", "assets.json")
       const assetsDirServerAbs = path.posix.join(globalObject.outDirServerAbs, 'assets')
       const assetsDirClientAbs = path.posix.join(globalObject.outDirClientAbs, 'assets')
       if (!existsSync(assetsDirServerAbs)) {
         return
       }
+      //TODO: This can create duplicates
       await fs.cp(assetsDirServerAbs, assetsDirClientAbs, { recursive: true, force: true })
       await fs.rm(assetsDirServerAbs, { recursive: true })
     }
@@ -48,32 +48,26 @@ function ssrEmitAssetsPlugin(): Plugin {
 }
 
 async function injectCssAfterPrerender() {
+  const clientManifestPath = path.posix.join(globalObject.outDirClientAbs, '..', 'assets.json')
+  const clientManifestParsed = Object.entries<ViteManifestEntry>(
+    JSON.parse(await fs.readFile(clientManifestPath, 'utf-8'))
+  )
   const serverManifestPath = path.posix.join(globalObject.outDirServerAbs, 'manifest.json')
-  const serverManifest = JSON.parse(await fs.readFile(serverManifestPath, 'utf-8'))
-  const serverManifestParsed = Object.entries<ViteManifestEntry>(serverManifest)
-  const findCss = (entry: ViteManifestEntry) => {
-    const css = []
-    const chunks = new Set([entry.src])
-    for (let chunkPath of chunks) {
-      const chunk = serverManifestParsed.find(([key]) => key === chunkPath)![1]
-      for (const import_ of chunk.imports ?? []) {
-        chunks.add(import_)
-      }
-      for (const css_ of chunk.css ?? []) {
-        css.push({ src: css_, hash: css_.split('.').at(-2)! })
-      }
-    }
-    return css
-  }
-  const entries = serverManifestParsed.filter(([, v]) => v.isEntry).map(([, v]) => v)
-  for (const entry of entries) {
+  const serverManifestParsed = Object.entries<ViteManifestEntry>(
+    JSON.parse(await fs.readFile(serverManifestPath, 'utf-8'))
+  )
+
+  // Use the css src from client manifest, if the same css file is included in both
+  const joinedManifest = [...clientManifestParsed, ...serverManifestParsed]
+
+  for (const [, entry] of serverManifestParsed.filter(([, { isEntry }]) => isEntry)) {
     const pageId = entry.src!.split(':').pop()!.replace('/pages', '')
-    const css = findCss(entry)
+    const css = collectCssForEntry(joinedManifest, entry)
     if (!css.length) {
       continue
     }
-    const hasParameter = pageId.endsWith('@id')
-    const globs = [`${pageId.substring(1).replace('/@id', '')}${hasParameter ? '/*' : ''}/index.html`]
+
+    const globs = [`${pageId.substring(1).replace('@id', '*')}/index.html`]
 
     if (pageId === '/index') {
       globs.push('index.html')
@@ -83,19 +77,10 @@ async function injectCssAfterPrerender() {
       cwd: globalObject.outDirClientAbs
     })
 
-    // console.log({
-    //     globs,
-    //     css,
-    //     hasParameter,
-    //     htmlFiles,
-    //     pageId,
-    //     entry
-    // });
-
     for (const htmlFile of htmlFiles) {
       const htmlFileAbs = path.posix.join(globalObject.outDirClientAbs, htmlFile)
       const file = await fs.readFile(htmlFileAbs, 'utf-8')
-      const notImportedCss = css.filter((css) => !file.includes(css.hash))
+      const notImportedCss = css.filter((css) => !file.includes(`href="/${css.src}"`))
       const scriptTags = notImportedCss
         .map((css) => `<link rel="stylesheet" type="text/css" href="/${css.src}">`)
         .join('\n')
@@ -103,4 +88,25 @@ async function injectCssAfterPrerender() {
       await fs.writeFile(htmlFileAbs, newFile, 'utf-8')
     }
   }
+}
+
+function collectCssForEntry(manifestEntries: [string, ViteManifestEntry][], entry: ViteManifestEntry) {
+  const css = []
+  const chunks = new Set([entry.src])
+  for (const chunkSrc of chunks) {
+    const chunk = manifestEntries.find(([key]) => key === chunkSrc)?.[1]
+    assert(chunk)
+    for (const import_ of chunk.imports ?? []) {
+      chunks.add(import_)
+    }
+    for (let css_ of chunk.css ?? []) {
+      const hash = css_.split('.').at(-2)
+      assert(hash)
+      const cssEntry = manifestEntries.find(([, manifestEntry]) => manifestEntry.file.includes(hash))
+      assert(cssEntry)
+
+      css.push({ src: cssEntry[1].file, hash })
+    }
+  }
+  return css
 }
