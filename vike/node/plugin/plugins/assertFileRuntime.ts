963f9d66597916a5eddb536e50cf726bd2ed55b4
Romuald Brillout <git@brillout.com>
2023-12-20 18:22:42 +0100

    support old 0.4 design and workaround Vite funky behavior

--- vike/node/plugin/plugins/assertFileRuntime.ts
+++ vike/node/plugin/plugins/assertFileRuntime.ts
@@ -2,12 +2,27 @@ export { assertFileRuntime }
 
 import { Plugin } from 'vite'
 import { assertUsage } from '../utils.js'
+import { extractAssetsRE } from './extractAssetsPlugin.js'
+import { extractExportNamesRE } from './extractExportNamesPlugin.js'
 
 function assertFileRuntime(): Plugin {
   return {
     name: 'vike:assertFileRuntime',
     enforce: 'pre',
     async resolveId(source, importer, options) {
+      // TODO/v1-release: remove
+      if (extractAssetsRE.test(source) || extractExportNamesRE.test(source)) return
+
+      // Seems like Vite is doing some funky stuff here.
+      if (importer?.endsWith('.html')) {
+        return
+      }
+
+      // I don't know why and who sets importer to '<stdin>' (I guess Vite?).
+      if (importer === '<stdin>') {
+        importer = undefined
+      }
+
       const resolved = await this.resolve(source, importer, { skipSelf: true, ...options })
       if (!resolved) return
 
