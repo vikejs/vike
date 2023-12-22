a01ee53639553c70adf6009e16ec42add682f194
Romuald Brillout <git@brillout.com>
2023-12-22 12:27:22 +0100

    docs: improve React/Vue Router doc page

--- docs/components/RecommendationRouterLibraries.tsx
+++ docs/components/RecommendationRouterLibraries.tsx
@@ -13,11 +13,11 @@ function RecommendationRouterLibraries({ libraryName, githubRepo }: { libraryNam
     <>
       <Warning>
         <p>
-          While it's possible to use {libraryName} (and deeply integrate it using {onBeforeRouteLink}) we recommend
-          against it: Vike's built-in router has many features that {libraryName} doesn't (and cannot) offer.
+          While it's possible to use {libraryName} we recommend against it: Vike's built-in router has many features
+          that {libraryName} doesn't (and cannot) offer.
         </p>
         <p>
-          That said, if you have an existing app that uses {libraryName} that you want to migrate to Vike, then it makes
+          That said, if you have an existing app using {libraryName} that you want to migrate to Vike, then it makes
           sense to start using Vike with {libraryName} and, after your migration is done, to progressively migrate from{' '}
           {libraryName} to Vike's router.
         </p>
@@ -25,16 +25,23 @@ function RecommendationRouterLibraries({ libraryName, githubRepo }: { libraryNam
           Create a new feature request if you want a feature provided by {libraryName} that Vike's router is missing.
         </p>
       </Warning>
+      <blockquote>
+        <p>
+          We believe that the frontend routing should be completely owned by the frontend framework. Trying to seperate
+          concerns in that regard leads to a frankenstein architecture.
+        </p>
+      </blockquote>
       <p>
         Example of using Vike with {libraryName} (shallow integration without using {onBeforeRouteLink}):
-        <ul>
-          <li>
-            <a href={'https://github.com/' + githubRepo}>
-              GitHub &gt; <code>{githubRepo}</code>
-            </a>
-          </li>
-        </ul>
       </p>
+      <ul>
+        <li>
+          <a href={'https://github.com/' + githubRepo}>
+            GitHub &gt; <code>{githubRepo}</code>
+          </a>
+        </li>
+      </ul>
+      <p>Contribution welcome to explore deep integration using {onBeforeRouteLink}.</p>
     </>
   )
 }
