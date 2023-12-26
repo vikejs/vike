d6b310ddb8a388d179feab30defd0f6453c3838d
Aurelien Lourot <software@lourot.dev>
2023-12-26 18:12:29 +0100

    qual: remove obsolete `react-data-fetching` example
    Merged into `react-full`.

--- examples/react-data-fetching/renderer/+onPageTransitionEnd.ts
+++
@@ -1,9 +0,0 @@
-// https://vike.dev/onPageTransitionEnd
-export { onPageTransitionEnd }
-
-import type { OnPageTransitionEndAsync } from 'vike/types'
-
-const onPageTransitionEnd: OnPageTransitionEndAsync = async (): ReturnType<OnPageTransitionEndAsync> => {
-  console.log('Page transition end')
-  document.querySelector('body')!.classList.remove('page-is-transitioning')
-}
--- examples/react-data-fetching/renderer/useData.tsx
+++
@@ -1,9 +0,0 @@
-// https://vike.dev/useData
-export { useData }
-
-import { usePageContext } from './usePageContext'
-
-function useData<Data>() {
-  const { data } = usePageContext()
-  return data as Data
-}
