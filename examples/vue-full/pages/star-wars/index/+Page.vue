0a389a37406b6ec4b07f5f5c7fa9f23deac4246d
Aurelien Lourot <software@lourot.dev>
2023-12-27 18:26:58 +0100

    examples: fix vue-full's build

--- examples/vue-full/pages/star-wars/index/+Page.vue
+++ examples/vue-full/pages/star-wars/index/+Page.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Star Wars Movies</h1>
   <ol>
-    <li v-for="item in movies" :key="item.id">
+    <li v-for="item in data.movies" :key="item.id">
       <a :href="'/star-wars/' + item.id">{{ item.title }}</a> ({{ item.release_date }})
     </li>
   </ol>
@@ -12,5 +12,5 @@
 <script lang="ts" setup>
 import type { Data } from './+data'
 import { useData } from '../../../renderer/useData'
-const { movies } = useData<Data>()
+const data = useData<Data>()
 </script>
