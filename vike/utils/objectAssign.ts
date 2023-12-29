ad840404ca5bf6756aba7d5fa930354450150001
Romuald Brillout <git@brillout.com>
2023-12-29 15:30:49 +0100

    global assertion

--- vike/utils/objectAssign.ts
+++ vike/utils/objectAssign.ts
@@ -1,13 +1,17 @@
 export { objectAssign }
 
+import { assert } from './assert.js'
+
 // Same as Object.assign() but:
 //  - With type inference
 //  - Preserves property descriptors, which we need for preserving the getters added by addUrlComputedProps()
-function objectAssign<Obj extends object, ObjAddendum>(
+function objectAssign<Obj extends object, ObjAddendum extends object | null>(
   obj: Obj,
   objAddendum: ObjAddendum
 ): asserts obj is Obj & ObjAddendum {
   if (objAddendum) {
+    // pageContext.urlOriginal should never be modified
+    assert(!('urlOriginal' in objAddendum))
     Object.defineProperties(obj, Object.getOwnPropertyDescriptors(objAddendum))
   }
 }
