c410699680a70de59abec3d90fe36bc88d1acd99
Romuald Brillout <git@brillout.com>
2023-12-29 13:44:14 +0100

    fix: improve handling of redundant errors

--- vike/utils/isSameError.ts
+++ vike/utils/isSameError.ts
@@ -1,18 +1,8 @@
 import { isObject } from './isObject.js'
-import { deepEqual } from './deepEqual.js'
 
 export { isSameError }
 
-function isSameError(err1: unknown, err2: unknown) {
-  return (
-    isObject(err1) &&
-    isObject(err2) &&
-    err1.constructor === err2.constructor &&
-    deepEqual({ ...err1, stack: null }, { ...err2, stack: null }) &&
-    // 'message' and 'stack' are usually non-emurable
-    err2.message === err2.message
-    /* Doesn't work because: the stack trace isn't exactly the same between the original page rendering and the fallback error page rendering
-    err2.stack === err2.stack
-    */
-  )
+function isSameError(err1: unknown, err2: unknown): boolean {
+  if (!isObject(err1) || !isObject(err2)) return false
+  return err1.message === err2.message
 }
