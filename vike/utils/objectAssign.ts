4e74deb309b8832156d3a0e797c32e2b035d7187
Romuald Brillout <git@brillout.com>
2023-12-29 15:39:26 +0100

    Revert "global assertion"
    This reverts commit ad840404ca5bf6756aba7d5fa930354450150001.

--- vike/utils/objectAssign.ts
+++ vike/utils/objectAssign.ts
@@ -1,17 +1,13 @@
 export { objectAssign }
 
-import { assert } from './assert.js'
-
 // Same as Object.assign() but:
 //  - With type inference
 //  - Preserves property descriptors, which we need for preserving the getters added by addUrlComputedProps()
-function objectAssign<Obj extends object, ObjAddendum extends object | null>(
+function objectAssign<Obj extends object, ObjAddendum>(
   obj: Obj,
   objAddendum: ObjAddendum
 ): asserts obj is Obj & ObjAddendum {
   if (objAddendum) {
-    // pageContext.urlOriginal should never be modified
-    assert(!('urlOriginal' in objAddendum))
     Object.defineProperties(obj, Object.getOwnPropertyDescriptors(objAddendum))
   }
 }
