d93322427d273867932f4654c77a2c715134e3dd
Richard Unterberg <34207253+richard-unterberg@users.noreply.github.com>
2024-01-01 14:01:24 +0100

    ✏️ fix typo in comment

--- examples/react-full/renderer/+onRenderHtml.tsx
+++ examples/react-full/renderer/+onRenderHtml.tsx
@@ -15,7 +15,7 @@ const onRenderHtml: OnRenderHtmlAsync = async (pageContext): ReturnType<OnRender
     <PageShell pageContext={pageContext}>
       <Page />
     </PageShell>,
-    // We don't need react-streaming for this app. (We use it merely to showcase that Vike can handle react-streaming with a pre-rendered app. Note that using react-streaming with pre-rendering can make sense if we want to be able to use React's latest <Suspsense> techniques.)
+    // We don't need react-streaming for this app. (We use it merely to showcase that Vike can handle react-streaming with a pre-rendered app. Note that using react-streaming with pre-rendering can make sense if we want to be able to use React's latest <Suspense> techniques.)
     { disable: true }
   )
 
