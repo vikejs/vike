a547fcbd68958a29ea9288f4f911e745a4527142
Romuald Brillout <git@brillout.com>
2024-01-03 16:24:35 +0100

    add test case

--- vike/node/runtime/renderPage/logHintForCjsEsmError.spec.ts
+++ vike/node/runtime/renderPage/logHintForCjsEsmError.spec.ts
@@ -401,6 +401,34 @@ const { ApolloClient } = pkg;
       }
     )
 
+    t(
+      'react-live-runner',
+      // https://github.com/vikejs/vike/discussions/571#discussioncomment-6144329
+    {
+  message: "Named export 'LiveEditor' not found. The requested module 'react-live-runner' is a CommonJS module, which may not support all module.exports as named exports.\nCommonJS modules can always be imported via the default export, for example using:\n\nimport pkg from 'react-live-runner';\nconst { LiveProvider, LiveEditor } = pkg;\n",
+  code: undefined,
+  stack: `
+file:///home/romu/tmp/vite-ssr-redux-react-live-runner-example/dist/server/entries/pages_index-page.mjs:4
+import { LiveProvider, LiveEditor } from "react-live-runner";
+                       ^^^^^^^^^^
+SyntaxError: Named export 'LiveEditor' not found. The requested module 'react-live-runner' is a CommonJS module, which may not support all module.exports as named exports.
+CommonJS modules can always be imported via the default export, for example using:
+
+import pkg from 'react-live-runner';
+const { LiveProvider, LiveEditor } = pkg;
+
+    at ModuleJob._instantiate (node:internal/modules/esm/module_job:131:21)
+    at async ModuleJob.run (node:internal/modules/esm/module_job:213:5)
+    at async ModuleLoader.import (node:internal/modules/esm/loader:316:24)
+    at async pageFile.loadFile (/home/romu/tmp/vite-ssr-redux-react-live-runner-example/node_modules/.pnpm/vite-plugin-ssr@0.4.131_vite@4.3.9/node_modules/vite-plugin-ssr/dist/cjs/shared/getPageFiles/parseGlobResults.js:35:40)
+    at async Promise.all (index 0)
+    at async loadPageFilesServerSide (/home/romu/tmp/vite-ssr-redux-react-live-runner-example/node_modules/.pnpm/vite-plugin-ssr@0.4.131_vite@4.3.9/node_modules/vite-plugin-ssr/dist/cjs/shared/getPageFiles/analyzePageServerSide/loadPageFilesServerSide.js:10:5)
+    at async Promise.all (index 0)
+    at async loadPageFilesServer (/home/romu/tmp/vite-ssr-redux-react-live-runner-example/node_modules/.pnpm/vite-plugin-ssr@0.4.131_vite@4.3.9/node_modules/vite-plugin-ssr/dist/cjs/node/runtime/renderPage/loadPageFilesServer.js:15:110)
+    at async /home/romu/tmp/vite-ssr-redux-react-live-runner-example/node_modules/.pnpm/vite-plugin-ssr@0.4.131_vite@4.3.9/node_modules/vite-plugin-ssr/dist/cjs/node/prerender/runPrerender.js:247:48
+`
+})
+
     tPartial(
       'vue-i18n',
       // https://github.com/vikejs/vike/discussions/635
