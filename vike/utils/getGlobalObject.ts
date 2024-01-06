017911429d8894ad35ce7334b20bb33df0f1e9a2
Romuald Brillout <git@brillout.com>
2024-01-06 15:12:06 +0100

    minor refactor

--- vike/utils/getGlobalObject.ts
+++ vike/utils/getGlobalObject.ts
@@ -1,12 +1,11 @@
 export function getGlobalObject<T extends Record<string, unknown> = never>(
-  // We use the filename as key; each `getGlobalObject()` call should live in a unique filename.
+  // We use the filename as key; each `getGlobalObject()` call should live inside a file with a unique filename.
   key: `${string}.ts`,
   defaultValue: T
 ): T {
-  const allGlobalObjects = (globalThis.__vike = globalThis.__vike || {})
-  const globalObject = (allGlobalObjects[key] = (allGlobalObjects[key] as T) || defaultValue)
+  // @ts-ignore
+  const globalObjectsAll = (globalThis[projectKey] = globalThis[projectKey] || {})
+  const globalObject = (globalObjectsAll[key] = globalObjectsAll[key] || defaultValue)
   return globalObject
 }
-declare global {
-  var __vike: undefined | Record<string, Record<string, unknown>>
-}
+const projectKey = '_vike'
