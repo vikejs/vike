ce657273a3c00b355bb331395e95d81389e78a0b
Romuald Brillout <git@brillout.com>
2024-01-09 15:51:32 +0100

    minor refactor

--- docs/components/VikeExtensionsUiFrameworks.tsx
+++ docs/components/VikeExtensionsUiFrameworks.tsx
@@ -1,6 +1,7 @@
 export { VikeExtensionsUiFrameworks }
 export { VikeExtensionsUiFrameworksNames }
 export { VikeExtensionsUiFrameworksNamesEnglish }
+export { ProvidedByVikeExtensionUiFramework }
 
 import React from 'react'
 import { Link } from '@brillout/docpress'
@@ -26,5 +27,20 @@ function VikeExtensionsUiFrameworksNamesEnglish() {
     <>
       <code>vike-react</code>, <code>vike-vue</code>, and <code>vike-solid</code>
     </>
+  )
+}
+
+function ProvidedByVikeExtensionUiFramework({ what, manual }: { what: JSX.Element; manual: JSX.Element }) {
+  return (
+    <blockquote>
+      <p>
+        The {what} is provided by{' '}
+        <Link href="/extensions#ui-frameworks">
+          <code>vike-react</code>/<code>vike-vue</code>/<code>vike-solid</code>
+        </Link>
+        . If you don't use one of these Vike extension, you can implement the {what} yourself as shown at {manual}
+        .
+      </p>
+    </blockquote>
   )
 }
