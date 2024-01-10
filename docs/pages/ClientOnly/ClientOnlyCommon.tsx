e8315bf499580e355f952b27f5ef8f96e25b0287
Chris-Robin Ennen <chris-robin@ennen.dev>
2024-01-10 19:06:44 +0100

    add docs for vike-vue/ClientOnly
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- docs/pages/ClientOnly/ClientOnlyCommon.tsx
+++ docs/pages/ClientOnly/ClientOnlyCommon.tsx
@@ -1,27 +1,35 @@
 import React from 'react'
 import ReactUsage from './ReactUsage.mdx'
 import SolidUsage from './SolidUsage.mdx'
+import VueUsage from './VueUsage.mdx'
 
 export const ClientOnlyCommon = ({ packageName }: { packageName: string }) => {
   const isReact = packageName === 'vike-react'
   const isSolid = packageName === 'vike-solid'
+  const isVue = packageName === 'vike-vue'
+
   return (
     <div>
       <h3>Usage</h3>
       {isReact && <ReactUsage />}
       {isSolid && <SolidUsage />}
+      {isVue && <VueUsage />}
       <h3>Props</h3>
       <ul>
         <li>
           <strong>load</strong>: A function that returns a promise resolving to import the specified component's path,
           dynamically loading it.
         </li>
-        <li>
-          <strong>children</strong>: A function that takes the loaded component and returns the JSX to be rendered.
-        </li>
-        <li>
-          <strong>fallback</strong>: A JSX element that is displayed while the dynamic component is being loaded.
-        </li>
+        {!isVue && (
+          <>
+            <li>
+              <strong>children</strong>: A function that takes the loaded component and returns the JSX to be rendered.
+            </li>
+            <li>
+              <strong>fallback</strong>: A JSX element that is displayed while the dynamic component is being loaded.
+            </li>
+          </>
+        )}
         {isReact && (
           <li>
             <strong>deps</strong> (optional): An array of dependencies that, when changed, will trigger a re-render of
