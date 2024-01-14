1b7b3c531035d3c4735a343f450a29e52bc87f15
Romuald Brillout <git@brillout.com>
2024-01-14 10:46:02 +0100

    fix: temporarily don't make ssr.external true by default

--- vike/node/plugin/plugins/commonConfig.ts
+++ vike/node/plugin/plugins/commonConfig.ts
@@ -1,7 +1,6 @@
 export { commonConfig }
 
 import type { Plugin, ResolvedConfig } from 'vite'
-import { version } from 'vite'
 import { assert, assertWarning, findUserPackageJsonPath } from '../utils.js'
 import { assertRollupInput } from './buildConfig.js'
 import { installRequireShim_setUserRootDir } from '@brillout/require-shim'
@@ -28,7 +27,9 @@ function commonConfig(): Plugin[] {
         order: 'post',
         handler(config) {
           overrideViteDefaultPort(config)
+          /* TODO: do this after implementing vike.config.js and new setting transformLinkedDependencies (or probably a better name like transpileLinkedDependencies/bundleLinkedDependencies or something else)
           overrideViteDefaultSsrExternal(config)
+          */
           workaroundCI(config)
           assertRollupInput(config)
           assertResolveAlias(config)
@@ -47,11 +48,22 @@ function overrideViteDefaultPort(config: ResolvedConfig) {
   config.preview ??= {}
   config.preview.port ??= 3000
 }
+/*
 function overrideViteDefaultSsrExternal(config: ResolvedConfig) {
   if (!isViteVersionWithSsrExternalTrue()) return
   // @ts-ignore Not released yet: https://github.com/vitejs/vite/pull/10939/files#diff-5a3d42620df2c6b17e25f440ffdb67683dee7ef57317674d19f41d5f30502310L5
   config.ssr.external ??= true
 }
+import { version } from 'vite'
+function isViteVersionWithSsrExternalTrue(): boolean {
+  const versionParts = version.split('.').map((s) => parseInt(s, 10)) as [number, number, number]
+  assert(versionParts.length === 3)
+  if (versionParts[0] > 5) return true
+  if (versionParts[1] > 0) return true
+  if (versionParts[2] >= 12) return true
+  return false
+}
+*/
 
 // Workaround GitHub Action failing to access the server
 function workaroundCI(config: ResolvedConfig) {
@@ -77,13 +89,4 @@ function assertEsm(userViteRoot: string) {
     `We recommend setting ${dir}package.json#type to "module", see https://vike.dev/CJS`,
     { onlyOnce: true }
   )
-}
-
-function isViteVersionWithSsrExternalTrue(): boolean {
-  const versionParts = version.split('.').map((s) => parseInt(s, 10)) as [number, number, number]
-  assert(versionParts.length === 3)
-  if (versionParts[0] > 5) return true
-  if (versionParts[1] > 0) return true
-  if (versionParts[2] >= 12) return true
-  return false
 }
