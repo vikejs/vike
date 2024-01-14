98fcd2e5b3bdd61c78471a03ad2a1136486f85e3
Dániel Horváth <horvath.daniel@lexunit.hu>
2024-01-14 23:09:52 +0100

    add viteConfig to prerender

--- vike/node/api/prerender.ts
+++ vike/node/api/prerender.ts
@@ -4,16 +4,16 @@ import type { PrerenderOptions } from '../prerender/runPrerender.js'
 
 async function prerender(options: PrerenderOptions) {
   const { resolve } = await import('path')
-  const { partial, noExtraDir, base, parallel, outDir, configFile } = options
+  const { partial, noExtraDir, base, parallel, outDir, configFile, viteConfig } = options
   const root = options.root && resolve(options.root)
 
   const { isCliCall } = await import('./utils.js')
   if (isCliCall) {
     const { runPrerender_forceExit, runPrerenderFromCLI } = await import('../prerender/runPrerender.js')
-    await runPrerenderFromCLI({ partial, noExtraDir, base, root, parallel, outDir, configFile })
+    await runPrerenderFromCLI({ partial, noExtraDir, base, root, parallel, outDir, configFile, viteConfig })
     runPrerender_forceExit()
   } else {
     const { runPrerenderFromAPI } = await import('../prerender/runPrerender.js')
-    await runPrerenderFromAPI({ partial, noExtraDir, base, root, parallel, outDir, configFile })
+    await runPrerenderFromAPI({ partial, noExtraDir, base, root, parallel, outDir, configFile, viteConfig })
   }
 }
