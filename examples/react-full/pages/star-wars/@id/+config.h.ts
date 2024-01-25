3eecd0c8c30343511b0dfc8b20befa66b6e7671d
Romuald Brillout <git@brillout.com>
2024-01-25 11:26:45 +0100

    reproduce faulty meta merging (#1458)

---
+++ examples/react-full/pages/star-wars/@id/+config.h.ts
@@ -0,0 +1,7 @@
+export const config = {
+  meta: {
+    data: {
+      env: { server: true, client: true }
+    }
+  },
+}
--- examples/react-full/renderer/+config.h.ts
+++ examples/react-full/renderer/+config.h.ts
@@ -11,25 +11,8 @@ export const config = {
     title: {
       env: { server: true, client: true }
     },
-    // Define new setting 'dataIsomorph'
-    dataIsomorph: {
-      env: { config: true },
-      effect({ configDefinedAt, configValue }) {
-        if (typeof configValue !== 'boolean') {
-          throw new Error(`${configDefinedAt} should be a boolean`)
-        }
-        if (configValue) {
-          return {
-            meta: {
-              data: {
-                // We override Vike's default behavior of always loading/executing data() on the server-side.
-                // If we set dataIsomorph to true, then data() is loaded/executed in the browser as well, allowing us to fetch data direcly from the browser upon client-side navigation (without involving our Node.js/Edge server at all).
-                env: { server: true, client: true }
-              }
-            }
-          }
-        }
-      }
+    data: {
+      env: { server: true, client: false }
     }
   },
   hooksTimeout: {
