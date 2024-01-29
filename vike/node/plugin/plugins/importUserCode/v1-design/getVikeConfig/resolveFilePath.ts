a13edf1a1ab6b1ebbb89f1e8d322ccccbf08e4fa
Romuald Brillout <git@brillout.com>
2024-01-29 23:57:40 +0100

    minor refactor

--- vike/node/plugin/plugins/importUserCode/v1-design/getVikeConfig/resolveFilePath.ts
+++ vike/node/plugin/plugins/importUserCode/v1-design/getVikeConfig/resolveFilePath.ts
@@ -1,7 +1,8 @@
 export { resolveFilePathRelativeToUserRootDir }
+export { resolveFilePathAbsoluteFilesystem }
 
 import path from 'path'
-import { assertPosixPath } from '../../../../utils.js'
+import { assert, assertPosixPath } from '../../../../utils.js'
 import type { FilePathResolved } from '../../../../../../shared/page-configs/PageConfig.js'
 
 function resolveFilePathRelativeToUserRootDir(
@@ -9,7 +10,24 @@ function resolveFilePathRelativeToUserRootDir(
   userRootDir: string
 ): FilePathResolved {
   assertPosixPath(filePathRelativeToUserRootDir)
+  assertPosixPath(userRootDir)
   const filePathAbsoluteFilesystem = path.posix.join(userRootDir, filePathRelativeToUserRootDir)
+  return getFilePathResolved(filePathRelativeToUserRootDir, filePathAbsoluteFilesystem)
+}
+
+function resolveFilePathAbsoluteFilesystem(filePathAbsoluteFilesystem: string, userRootDir: string): FilePathResolved {
+  assertPosixPath(filePathAbsoluteFilesystem)
+  assertPosixPath(userRootDir)
+  let filePathRelativeToUserRootDir = path.posix.relative(userRootDir, filePathAbsoluteFilesystem)
+  assert(!filePathRelativeToUserRootDir.startsWith('.') && !filePathRelativeToUserRootDir.startsWith('/'))
+  filePathRelativeToUserRootDir = '/' + filePathRelativeToUserRootDir
+  return getFilePathResolved(filePathRelativeToUserRootDir, filePathAbsoluteFilesystem)
+}
+
+function getFilePathResolved(
+  filePathRelativeToUserRootDir: string,
+  filePathAbsoluteFilesystem: string
+): FilePathResolved {
   return {
     filePathRelativeToUserRootDir,
     filePathAbsoluteVite: filePathRelativeToUserRootDir,
