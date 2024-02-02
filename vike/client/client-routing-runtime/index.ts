224ae953d23fb84619b7ae9141f76d1d23b45ecf
Romuald Brillout <git@brillout.com>
2024-02-02 07:38:03 +0100

    fix: rename `export { PROJECT_VERSION }` to `export { version }`

--- vike/client/client-routing-runtime/index.ts
+++ vike/client/client-routing-runtime/index.ts
@@ -6,7 +6,7 @@
 
 export { navigate, reload } from './navigate.js'
 export { prefetch } from './prefetch.js'
-export { PROJECT_VERSION } from './utils.js'
+export { PROJECT_VERSION as version } from './utils.js'
 
 // TODO/v1-release: remove this
 import type { PageContextBuiltInClientWithClientRouting } from '../../shared/types.js'
