6e859387c03b121bc22e64315121b128ef83b4a9
Romuald Brillout <git@brillout.com>
2024-02-08 23:52:25 +0100

    comment

--- test-e2e.config.mjs
+++ test-e2e.config.mjs
@@ -84,7 +84,7 @@ function tolerateError({ logSource, logText }) {
     isNodeExperimentalLoader() ||
     isNotV1Design() ||
     [
-      // TODO: remove once all examples/tests use >=5.1.0
+      // TODO: remove once all examples/tests use Vite >=5.1.0
       'The glob option "as" has been deprecated in favour of "query"'
       // TODO: move everything to this array
     ].some((t) => logText.includes(t))
