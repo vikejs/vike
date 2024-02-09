ef2f5563e6f2e0560a3bdad3ce3d1be228bb8d7a
Romuald Brillout <git@brillout.com>
2024-02-09 21:06:22 +0100

    minor refactor

--- vike/__internal/index.ts
+++ vike/__internal/index.ts
@@ -5,7 +5,7 @@ export type { PageRoutes, PageFile, PageConfigRuntime as PageConfig }
 import { route as routeInternal, type PageRoutes } from '../shared/route/index.js'
 import { type PageFile } from '../shared/getPageFiles.js'
 import { getGlobalContext, initGlobalContext } from '../node/runtime/globalContext.js'
-import { setNodeEnvToProduction } from '../utils/nodeEnv.js'
+import { setNodeEnvToProduction } from '../utils/assertNodeEnv.js'
 import { assert } from '../utils/assert.js'
 import { getRenderContext } from '../node/runtime/renderPage/renderPageAlreadyRouted.js'
 import { PageConfigRuntime } from '../shared/page-configs/PageConfig.js'
