fe5558e12e07f1b17703114919b37a221782cbef
Romuald Brillout <git@brillout.com>
2024-02-12 23:00:23 +0100

    update examples/** to use +Head.js isntead of +config.js pointer import

--- examples/vike-react/pages/+config.ts
+++ examples/vike-react/pages/+config.ts
@@ -1,12 +1,10 @@
 import type { Config } from 'vike/types'
-import Head from '../layouts/HeadDefault'
 import logoUrl from '../assets/logo.svg'
 import vikeReact from 'vike-react/config'
 
 // Default configs (can be overriden by pages)
 export default {
   ssr: true,
-  Head,
   // <title>
   title: 'My Vike + React App',
   // <link rel="icon" href="${favicon}" />
