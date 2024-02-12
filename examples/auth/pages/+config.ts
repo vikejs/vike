4295a62e2f47592b223ceffb516e14b3a9fb6b47
Romuald Brillout <git@brillout.com>
2024-02-12 22:59:28 +0100

    update examples/** to use +Layout.js isntead of +config.js pointer import

--- examples/auth/pages/+config.ts
+++ examples/auth/pages/+config.ts
@@ -1,9 +1,7 @@
 import type { Config } from 'vike/types'
-import Layout from '../layouts/LayoutDefault'
 import vikeReact from 'vike-react/config'
 
 export default {
-  Layout,
   passToClient: ['userFullName'],
   extends: vikeReact
 } satisfies Config
--- examples/vike-react/pages/+config.ts
+++ examples/vike-react/pages/+config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'vike/types'
-import Layout from '../layouts/LayoutDefault'
 import Head from '../layouts/HeadDefault'
 import logoUrl from '../assets/logo.svg'
 import vikeReact from 'vike-react/config'
@@ -7,7 +6,6 @@ import vikeReact from 'vike-react/config'
 // Default configs (can be overriden by pages)
 export default {
   ssr: true,
-  Layout,
   Head,
   // <title>
   title: 'My Vike + React App',
