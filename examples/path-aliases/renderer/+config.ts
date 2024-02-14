d53a74dd0a62caeadb241c216601286edffd5708
Romuald Brillout <git@brillout.com>
2024-02-14 09:07:38 +0100

    update examples/path-aliases to use +onRender{Client,Html}.js instead of pointer import

--- examples/path-aliases/renderer/+config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from 'vike/types'
-import { onRenderClient } from '#root/renderer/onRenderClient'
-import { onRenderHtml } from '#root/renderer/onRenderHtml'
-
-export default {
-  onRenderClient,
-  onRenderHtml
-} satisfies Config
