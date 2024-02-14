80a5e4f30609e553544a8dbb291d19f55b557482
Romuald Brillout <git@brillout.com>
2024-02-14 12:10:06 +0100

    update deps vike-react and @brillout/docpress

--- docs/pages/+config.h.ts
+++ docs/pages/+config.h.ts
@@ -1,6 +1,6 @@
 export { config }
 
-import docpress from '@brillout/docpress/vike-config'
+import docpress from '@brillout/docpress/config'
 import type { Config } from 'vike/types'
 
 const config = {
