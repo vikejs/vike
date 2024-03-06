58d126fce358cef797e69fce0d6331e0d6caf6f4
Romuald Brillout <git@brillout.com>
2024-03-06 20:29:44 +0100

    avoid hmr connection error

--- test/preload/dev.spec.ts
+++ test/preload/dev.spec.ts
@@ -11,6 +11,18 @@ testRun(true)
 async function devApp() {
   await createServer({
     root: __dirname,
-    server: { middlewareMode: true }
+    server: {
+      middlewareMode: true,
+      /* Doesn't seem to work
+      hmr: false
+      */
+      hmr: {
+        // Avoid:
+        // ```
+        // Error: listen EADDRINUSE: address already in use :::24678
+        // ```
+        port: 11323
+      }
+    }
   })
 }
