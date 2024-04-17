8fa3acac956a8b310f8c7cbfa75e28bb42a93f7c
Romuald Brillout <git@brillout.com>
2024-04-17 16:45:07 +0200

    minor

--- vike/node/runtime/index-common.ts
+++ vike/node/runtime/index-common.ts
@@ -17,7 +17,12 @@ import './page-files/setup.js'
 // - Telefunc (to detect the user's stack https://github.com/brillout/telefunc/blob/8288310e88e06a42b710d39c39fb502364ca6d30/telefunc/utils/isVikeApp.ts#L4)
 function addEcosystemStamp() {
   const g = globalThis as Record<string, unknown>
-  g._isVikeApp = true
+  g._isVikeApp =
+    /* Don't set to true so that consumers do `!!globalThis._isVikeApp` instead of `globalThis._isVikeApp === true`.
+    true
+    */
+    // We use an object so that we can eventually, in the future, add helpful information as needed. (E.g. the Vike version, or global settings.)
+    {}
   // We keep the old stamp for older Telefunc versions
   g._isVitePluginSsr = true
 }
