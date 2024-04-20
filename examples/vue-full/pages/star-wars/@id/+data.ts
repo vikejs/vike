e17eab3cfc38ba67107da84cfaf0bc122071546f
Romuald Brillout <git@brillout.com>
2024-04-20 21:20:49 +0200

    align examples

--- examples/vue-full/pages/star-wars/@id/+data.ts
+++ examples/vue-full/pages/star-wars/@id/+data.ts
@@ -6,17 +6,23 @@ import fetch from 'cross-fetch'
 import { filterMovieData } from '../filterMovieData'
 import type { PageContextClient, PageContextServer } from 'vike/types'
 import type { MovieDetails } from '../types'
-import type { OnBeforeRenderAsync } from 'vike/types'
+import { render } from 'vike/abort'
 
-const data = async (pageContext: PageContextServer | PageContextClient) => {
-  const response = await fetch(`https://star-wars.brillout.com/api/films/${pageContext.routeParams!.movieId}.json`)
-  let movie = (await response.json()) as MovieDetails
+async function data(pageContext: PageContextServer | PageContextClient) {
+  const dataUrl = `https://star-wars.brillout.com/api/films/${pageContext.routeParams!.id}.json`
+  let movie: MovieDetails
+  try {
+    const response = await fetch(dataUrl)
+    movie = (await response.json()) as MovieDetails
+  } catch (err) {
+    console.error(err)
+    throw render(503, `Couldn't fetch data, because failed HTTP GET request to ${dataUrl}`)
+  }
 
   // We remove data we don't need because the data is passed to the client; we should
   // minimize what is sent over the network.
   movie = filterMovieData(movie)
 
-  // The page's <title>
   const { title } = movie
 
   return {
