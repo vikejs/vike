dbdaecdcef4f9a2c2b33a2facfb86f6faf372625
Robin Bühler <git+r@obin.ch>
2024-05-02 09:45:24 +0000

    add unit test

--- vike/node/runtime/renderPage/handlePageContextRequestUrl.spec.ts
+++ vike/node/runtime/renderPage/handlePageContextRequestUrl.spec.ts
@@ -4,7 +4,32 @@ import { expect, describe, it } from 'vitest'
 describe('removePageContextUrlSuffix()', () => {
   it('contain slash for root paths', () => {
     expect(
-      removePageContextUrlSuffix('/index.pageContext.json')
-    ).toMatchInlineSnapshot('"/"')
+      removePageContextUrlSuffix('https://example.com/index.pageContext.json')
+    ).toMatchInlineSnapshot('"https://example.com/"')
+  })
+  it('remove suffix', () => {
+    expect(
+      removePageContextUrlSuffix('https://example.com/test/index.pageContext.json')
+    ).toMatchInlineSnapshot('"https://example.com/test"')
+  })
+  it('remove suffix with query and hash', () => {
+    expect(
+      removePageContextUrlSuffix('https://example.com/test/index.pageContext.json?query=1#hash')
+    ).toMatchInlineSnapshot('"https://example.com/test?query=1#hash"')
+  })
+  it('contain slash for root paths with trailing slash', () => {
+    expect(
+      removePageContextUrlSuffix('https://example.com/index.pageContext.json/')
+    ).toMatchInlineSnapshot('"https://example.com/"')
+  })
+  it('remove suffix with trailing slash', () => {
+    expect(
+      removePageContextUrlSuffix('https://example.com/test/index.pageContext.json/')
+    ).toMatchInlineSnapshot('"https://example.com/test/"')
+  })
+  it('remove suffix with query and hash with trailing slash', () => {
+    expect(
+      removePageContextUrlSuffix('https://example.com/test/index.pageContext.json/?query=1#hash')
+    ).toMatchInlineSnapshot('"https://example.com/test/?query=1#hash"')
   })
 })
