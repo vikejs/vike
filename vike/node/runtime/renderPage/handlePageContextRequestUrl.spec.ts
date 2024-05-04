cff7dc3e9946cf4242d6f38c0a05ca6e29012c4d
Robin Bühler <git+r@obin.ch>
2024-05-04 08:45:12 +0000

    clean up unused tests

--- vike/node/runtime/renderPage/handlePageContextRequestUrl.spec.ts
+++ vike/node/runtime/renderPage/handlePageContextRequestUrl.spec.ts
@@ -17,19 +17,4 @@ describe('removePageContextUrlSuffix()', () => {
       removePageContextUrlSuffix('/test/index.pageContext.json?query=1#hash')
     ).toMatchInlineSnapshot('"/test?query=1#hash"')
   })
-  it('contain slash for root paths with trailing slash', () => {
-    expect(
-      removePageContextUrlSuffix('//index.pageContext.json')
-    ).toMatchInlineSnapshot('"/"')
-  })
-  it('remove suffix with trailing slash', () => {
-    expect(
-      removePageContextUrlSuffix('/test//index.pageContext.json')
-    ).toMatchInlineSnapshot('"/test/"')
-  })
-  it('remove suffix with trailing slash, query and hash', () => {
-    expect(
-      removePageContextUrlSuffix('/test//index.pageContext.json?query=1#hash')
-    ).toMatchInlineSnapshot('"/test/?query=1#hash"')
-  })
 })
--- vike/shared/getPageContextRequestUrl.spec.ts
+++ vike/shared/getPageContextRequestUrl.spec.ts
@@ -7,14 +7,4 @@ describe('getPageContextRequestUrl()', () => {
       getPageContextRequestUrl('/test')
     ).toMatchInlineSnapshot('"/test/index.pageContext.json"')
   })
-  it('return page context request url with trailing slash', () => {
-    expect(
-      getPageContextRequestUrl('/test/')
-    ).toMatchInlineSnapshot('"/test//index.pageContext.json"')
-  })
-  it('return page context request url with trailing slash, query and hash', () => {
-    expect(
-      getPageContextRequestUrl('/test/?query=1#hash')
-    ).toMatchInlineSnapshot('"/test//index.pageContext.json?query=1#hash"')
-  })
 })
