581d223b545252dea194ef7c997849be84e0c632
Romuald Brillout <git@brillout.com>
2024-05-08 11:11:54 +0200

    further use ts in /test/misc/

--- test/misc/renderer/onRenderHtml.tsx
+++ test/misc/renderer/onRenderHtml.tsx
@@ -4,10 +4,12 @@ export default onRenderHtml
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { escapeInject, dangerouslySkipEscape } from 'vike/server'
+// @ts-expect-error
 import { PageLayout } from './PageLayout'
 import { getTitle } from './getTitle'
+import type { PageContextServer } from 'vike/types'
 
-async function onRenderHtml(pageContext) {
+async function onRenderHtml(pageContext: PageContextServer) {
   const { Page, pageProps } = pageContext
   const title = getTitle(pageContext)
   const viewHtml = dangerouslySkipEscape(
