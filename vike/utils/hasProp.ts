b7551d8748a780052d671af370707877ab4ff91e
Romuald Brillout <git@brillout.com>
2024-05-08 17:04:52 +0200

    further use generic instead of overload

--- vike/utils/hasProp.ts
+++ vike/utils/hasProp.ts
@@ -1,7 +1,5 @@
 export { hasProp }
-
-// - https://2ality.com/2020/06/type-guards-assertion-functions-typescript.html
-// - https://www.typescriptlang.org/play?#code/GYVwdgxgLglg9mABDAzgFQJ4AcCmdgAUAbgIYA2IOAXIiWBgDSJTbWIDkARnHGTnewCUNUhRzIUibr35gA3AFgAUKEiwEEzLnzFylGnUbNWNdmBABbTjgBOQkXvGpE5q7cUrw0eElRa8hKL6tPRMLLimKFA2MGAA5vaIQU6SUTHxHqreGn6sOskGocYRHOAA1mBwAO5gickSiOWVNZle6r7oeYGOhUbhbGmxcYgAvKVgFdW1wlI8fHSIAN7KiMiExeIjW+OTNeyIgksrq4g2OFAgNkjRlMcAvsdnF1cb+EmOo9v9Hg9KyhAIKK0GhNKajRAAFgATMplCQUChbFACLltIQSEwzJZrHZBIJ-oCZAA6MhwOIEEj4v6eNQ+WgIpEEAFgAAmMHaIImzTAM3hiJsUEkzLZ7SOShOa0QTIQIp8hyelzAx1WUAAFjZqi4cFVEABRGwamwEdgAQQZArpADESDAyEJlHcgA
+export type { ResolveTypeAsString }
 
 import { isCallable } from './isCallable.js'
 import { isObject } from './isObject.js'
@@ -9,59 +7,72 @@ import { isArrayOfStrings } from './isArrayOfStrings.js'
 import { isObjectOfStrings } from './isObjectOfStrings.js'
 import { isArray } from './isArray.js'
 
-// prettier-ignore
-// biome-ignore format:
-function hasProp<ObjectType, PropName extends PropertyKey>(obj: ObjectType, prop: PropName, type: 'boolean'):  obj is ObjectType & Record<PropName, boolean>;
-// prettier-ignore
-// biome-ignore format:
-function hasProp<ObjectType, PropName extends PropertyKey>(obj: ObjectType, prop: PropName, type: 'number'):  obj is ObjectType & Record<PropName, number>;
-// prettier-ignore
-// biome-ignore format:
-function hasProp<ObjectType, PropName extends PropertyKey>(obj: ObjectType, prop: PropName, type: 'string'): obj is ObjectType & Record<PropName, string>;
-// prettier-ignore
-// biome-ignore format:
-function hasProp<ObjectType, PropName extends PropertyKey>(obj: ObjectType, prop: PropName, type: 'object'): obj is ObjectType & Record<PropName, Record<string, unknown>>;
-// prettier-ignore
-// biome-ignore format:
-function hasProp<ObjectType, PropName extends PropertyKey>(obj: ObjectType, prop: PropName, type: 'array'): obj is ObjectType & Record<PropName, unknown[]>;
-// prettier-ignore
-// biome-ignore format:
-function hasProp<ObjectType, PropName extends PropertyKey>(obj: ObjectType, prop: PropName, type: 'string[]'): obj is ObjectType & Record<PropName, string[]>;
-// prettier-ignore
-// biome-ignore format:
-function hasProp<ObjectType, PropName extends PropertyKey>(obj: ObjectType, prop: PropName, type: 'string{}'): obj is ObjectType & Record<PropName, Record<string, string>>;
-// prettier-ignore
-// biome-ignore format:
-function hasProp<ObjectType, PropName extends PropertyKey>(obj: ObjectType, prop: PropName, type: 'function'): obj is ObjectType & Record<PropName, (...args: any[]) => unknown>;
-// prettier-ignore
-// biome-ignore format:
-function hasProp<ObjectType, PropName extends PropertyKey>(obj: ObjectType, prop: PropName, type: 'undefined'): obj is ObjectType & Record<PropName, undefined>;
-// prettier-ignore
-// biome-ignore format:
-function hasProp<ObjectType, PropName extends PropertyKey>(obj: ObjectType, prop: PropName, type: 'null'): obj is ObjectType & Record<PropName, null>;
-// prettier-ignore
-// biome-ignore format:
-function hasProp<ObjectType, PropName extends PropertyKey>(obj: ObjectType, prop: PropName, type: 'true'): obj is ObjectType & Record<PropName, true>;
-// prettier-ignore
-// biome-ignore format:
-function hasProp<ObjectType, PropName extends PropertyKey>(obj: ObjectType, prop: PropName, type: 'false'): obj is ObjectType & Record<PropName, false>;
-// prettier-ignore
-// biome-ignore format:
-function hasProp<ObjectType, PropName extends PropertyKey, Enum>(obj: ObjectType, prop: PropName, type: Enum[]): obj is ObjectType & Record<PropName, Enum>;
-// prettier-ignore
-// biome-ignore format:
-function hasProp<ObjectType, PropName extends PropertyKey>(obj: ObjectType, prop: PropName): obj is ObjectType & Record<PropName, unknown>;
-function hasProp<ObjectType, PropName extends PropertyKey>(
+type TypeAsString =
+  | 'object'
+  | 'string{}'
+  | 'string[]'
+  | 'array'
+  | 'function'
+  | 'number'
+  | 'string'
+  | 'boolean'
+  | 'true'
+  | 'false'
+  | 'null'
+  | 'undefined'
+  | undefined
+
+type ResolveTypeAsString<Type extends TypeAsString = undefined> =
+  //
+  Type extends 'object'
+    ? Record<string, unknown>
+    : Type extends 'string{}'
+      ? Record<string, string>
+      : Type extends 'string[]'
+        ? string[]
+        : Type extends 'array'
+          ? unknown[]
+          : Type extends 'function'
+            ? (...args: any[]) => unknown
+            : Type extends 'number'
+              ? number
+              : Type extends 'string'
+                ? string
+                : Type extends 'boolean'
+                  ? boolean
+                  : Type extends 'true'
+                    ? true
+                    : Type extends 'false'
+                      ? false
+                      : Type extends 'null'
+                        ? null
+                        : Type extends 'undefined'
+                          ? undefined
+                          : Type extends undefined
+                            ? unknown
+                            : never
+
+function hasProp<ObjectType, PropName extends PropertyKey, Type extends TypeAsString = undefined>(
   obj: ObjectType,
   prop: PropName,
-  type: string | string[] = 'unknown'
+  type?: Type
+): obj is ObjectType & Record<PropName, ResolveTypeAsString<Type>>
+function hasProp<ObjectType, PropName extends PropertyKey, Enum>(
+  obj: ObjectType,
+  prop: PropName,
+  type: Enum[]
+): obj is ObjectType & Record<PropName, Enum>
+function hasProp(
+  obj: Record<string, unknown>,
+  prop: string,
+  type?:
+    | TypeAsString
+    //  Enum[]
+    | string[]
 ): boolean {
   if (!isObject(obj)) return false
   if (!(prop in obj)) {
     return type === 'undefined'
-  }
-  if (type === 'unknown') {
-    return true
   }
   const propValue = (obj as Record<any, unknown>)[prop]
   if (type === 'array') {
