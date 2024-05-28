e578ed45db99b5ea8dbb80941314f1bd324487fb
Romuald Brillout <git@brillout.com>
2024-05-28 13:39:30 +0200

    reproduce UTF8 file path (#1658)

---
+++ examples/react-minimal/pages/canções/+Page.jsx
@@ -0,0 +1,12 @@
+export default Page
+
+import React from 'react'
+
+function Page() {
+  return (
+    <>
+      <h1>UTF-8 file path</h1>
+      <p>Example of using a UTF-8 file path.</p>
+    </>
+  )
+}
