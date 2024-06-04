aaf463a35d8d6a5d50a0b866b742734229ed6f67
Romuald Brillout <git@brillout.com>
2024-06-04 07:50:44 +0200

    remove test

--- test/misc/vite.config.ts
+++ test/misc/vite.config.ts
@@ -2,18 +2,6 @@ import react from '@vitejs/plugin-react'
 import vike from 'vike/plugin'
 
 export default {
-  server: {
-    headers: {
-      'cross-origin-opener-policy': 'same-origin',
-      'cross-origin-embedder-policy': 'credentialless'
-    }
-  },
-  preview: {
-    headers: {
-      'cross-origin-opener-policy': 'same-origin',
-      'cross-origin-embedder-policy': 'require-corp'
-    }
-  },
   build: {
     outDir: `${__dirname}/../../test/misc/dist/nested`,
     assetsDir: '/nested-dir/assets'
