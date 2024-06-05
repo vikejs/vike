f675a3aa6a10ba63750ba9bfeb88068d00d47724
Romuald Brillout <git@brillout.com>
2024-06-05 14:35:59 +0200

    Reapply "simple & robust useData() impl"
    This reverts commit 631f674547a174e8ef1022e25d0076c19cdf04c1.

--- examples/vue-full/renderer/app.ts
+++ examples/vue-full/renderer/app.ts
@@ -4,7 +4,8 @@ import { createSSRApp, h, markRaw, reactive, ref } from 'vue'
 import PageLayout from './PageLayout.vue'
 import { setPageContext } from './usePageContext'
 import type { PageContext } from 'vike/types'
-import { objectAssign } from './utils'
+import { setData } from './useData'
+import { isObject, objectAssign } from './utils'
 
 function createApp(pageContext: PageContext) {
   const { Page } = pageContext
@@ -22,13 +23,24 @@ function createApp(pageContext: PageContext) {
   // app.changePage() is called upon navigation, see +onRenderClient.ts
   objectAssign(app, {
     changePage: (pageContext: PageContext) => {
+      const data = pageContext.data ?? {}
+      assertDataIsObject(data)
+      Object.assign(dataReactive, data)
       Object.assign(pageContextReactive, pageContext)
       pageRef.value = markRaw(pageContext.Page)
     }
   })
 
+  const data = pageContext.data ?? {}
+  assertDataIsObject(data)
+  const dataReactive = reactive(data)
   const pageContextReactive = reactive(pageContext)
   setPageContext(app, pageContextReactive)
+  setData(app, dataReactive)
 
   return app
+}
+
+function assertDataIsObject(data: unknown): asserts data is Record<string, unknown> {
+  if (!isObject(data)) throw new Error('Return value of data() hook should be an object, undefined, or null')
 }
--- examples/vue-full/renderer/utils.ts
+++ examples/vue-full/renderer/utils.ts
@@ -5,3 +5,7 @@ export function objectAssign<Obj extends object, ObjAddendum>(
 ): asserts obj is Obj & ObjAddendum {
   Object.assign(obj, objAddendum)
 }
+
+export function isObject(value: unknown): value is Record<string, unknown> {
+  return typeof value === 'object' && value !== null
+}
