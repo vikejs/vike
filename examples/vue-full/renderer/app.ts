c2efa44c4a1ba4761323708959ac7fc2101b6627
Romuald Brillout <git@brillout.com>
2024-06-05 14:18:36 +0200

    Revert "simple & robust useData() impl"
    This reverts commit e2e123ebc9f98cb3e54f378240a9eaabdbe0d2b0.

--- examples/vue-full/renderer/app.ts
+++ examples/vue-full/renderer/app.ts
@@ -4,8 +4,7 @@ import { createSSRApp, h, markRaw, reactive, ref } from 'vue'
 import PageLayout from './PageLayout.vue'
 import { setPageContext } from './usePageContext'
 import type { PageContext } from 'vike/types'
-import { setData } from './useData'
-import { isObject, objectAssign } from './utils'
+import { objectAssign } from './utils'
 
 function createApp(pageContext: PageContext) {
   const { Page } = pageContext
@@ -23,24 +22,13 @@ function createApp(pageContext: PageContext) {
   // app.changePage() is called upon navigation, see +onRenderClient.ts
   objectAssign(app, {
     changePage: (pageContext: PageContext) => {
-      const data = pageContext.data ?? {}
-      assertDataIsObject(data)
-      Object.assign(dataReactive, data)
       Object.assign(pageContextReactive, pageContext)
       pageRef.value = markRaw(pageContext.Page)
     }
   })
 
-  const data = pageContext.data ?? {}
-  assertDataIsObject(data)
-  const dataReactive = reactive(data)
   const pageContextReactive = reactive(pageContext)
   setPageContext(app, pageContextReactive)
-  setData(app, dataReactive)
 
   return app
-}
-
-function assertDataIsObject(data: unknown): asserts data is Record<string, unknown> {
-  if (!isObject(data)) throw new Error('Return value of data() hook should be an object, undefined, or null')
 }
--- examples/vue-full/renderer/useData.ts
+++ examples/vue-full/renderer/useData.ts
@@ -1,19 +1,10 @@
 // https://vike.dev/useData
 export { useData }
-export { setData }
 
-import { inject } from 'vue'
-import type { App } from 'vue'
-
-const key = Symbol()
+import { usePageContext } from './usePageContext'
 
 /** https://vike.dev/useData */
 function useData<Data>(): Data {
-  const data = inject(key)
-  if (!data) throw new Error('setData() not called')
+  const { data } = usePageContext()
   return data as any
-}
-
-function setData(app: App, data: unknown): void {
-  app.provide(key, data)
 }
