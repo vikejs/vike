fac6b8d3460a4fc5e11962c77bd49c4bec343075
Romuald Brillout <git@brillout.com>
2024-06-11 20:03:45 +0200

    minor refactor

--- vike/shared/page-configs/assertPlusFileExport.ts
+++ vike/shared/page-configs/assertPlusFileExport.ts
@@ -14,21 +14,21 @@ const EXPORTS_IGNORE = [
 const TOLERATE_SIDE_EXPORTS = ['.md', '.mdx'] as const
 
 function assertPlusFileExport(fileExports: Record<string, unknown>, filePathToShowToUser: string, configName: string) {
-  const exportsAll = Object.keys(fileExports).filter((exportName) => !EXPORTS_IGNORE.includes(exportName))
-  const exportsInvalid = exportsAll.filter((e) => e !== 'default' && e !== configName)
-  if (exportsInvalid.length === 0) {
-    if (exportsAll.length === 1) {
+  const exportNames = Object.keys(fileExports).filter((exportName) => !EXPORTS_IGNORE.includes(exportName))
+  const exportNamesInvalid = exportNames.filter((e) => e !== 'default' && e !== configName)
+  if (exportNamesInvalid.length === 0) {
+    if (exportNames.length === 1) {
       return
     }
     const exportDefault = pc.cyan('export default')
     const exportNamed = pc.cyan(`export { ${configName} }`)
-    if (exportsAll.length === 0) {
+    if (exportNames.length === 0) {
       assertUsage(
         false,
         `${filePathToShowToUser} doesn't export any value, but it should have a ${exportNamed} or ${exportDefault}`
       )
     } else {
-      assert(exportsAll.length === 2) // because `exportsInvalid.length === 0`
+      assert(exportNames.length === 2) // because `exportsInvalid.length === 0`
       assertWarning(
         false,
         `The exports of ${filePathToShowToUser} are ambiguous: remove ${exportDefault} or ${exportNamed}`,
@@ -37,12 +37,12 @@ function assertPlusFileExport(fileExports: Record<string, unknown>, filePathToSh
     }
   } else {
     if (TOLERATE_SIDE_EXPORTS.some((ext) => filePathToShowToUser.endsWith(ext))) return
-    exportsInvalid.forEach((exportInvalid) => {
+    exportNamesInvalid.forEach((exportInvalid) => {
       assertWarning(
         false,
         `${filePathToShowToUser} should have only one export: move ${pc.cyan(
           `export { ${exportInvalid} }`
-        )} to its own +${exportsInvalid}.js file`,
+        )} to its own +${exportNamesInvalid}.js file`,
         { onlyOnce: true }
       )
     })
