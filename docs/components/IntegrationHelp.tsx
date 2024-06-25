c38f226fd089a760db92b53b5d05f0377855db0e
Romuald Brillout <git@brillout.com>
2024-06-25 20:39:34 +0200

    fmt

--- docs/components/IntegrationHelp.tsx
+++ docs/components/IntegrationHelp.tsx
@@ -7,8 +7,8 @@ function IntegrationHelp() {
   return (
     <blockquote>
       <p>
-        <Link href="/faq#i-can-t-achieve-what-i-want-can-i-get-help">Feel free to reach out</Link> if you
-        run into an issue for integrating a tool.
+        <Link href="/faq#i-can-t-achieve-what-i-want-can-i-get-help">Feel free to reach out</Link> if you run into an
+        issue for integrating a tool.
       </p>
     </blockquote>
   )
