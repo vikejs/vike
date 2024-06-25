749131bb28c2a63ea4b2085d3d4016c906a33283
Romuald Brillout <git@brillout.com>
2024-06-25 15:55:57 +0200

    docs: polish FAQ

--- docs/components/IntegrationHelp.tsx
+++ docs/components/IntegrationHelp.tsx
@@ -7,7 +7,7 @@ function IntegrationHelp() {
   return (
     <blockquote>
       <p>
-        <Link href="/faq#i-ve-difficulties-integrating-a-tool-can-i-get-help">Feel free to reach out</Link> if you
+        <Link href="/faq#i-can-t-achieve-what-i-want-can-i-get-help">Feel free to reach out</Link> if you
         run into an issue for integrating a tool.
       </p>
     </blockquote>
