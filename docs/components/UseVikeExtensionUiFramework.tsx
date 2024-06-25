fc91fb7532bb6aaf8410422aa76363a69034c950
Romuald Brillout <git@brillout.com>
2024-06-25 19:53:57 +0200

    minor refactor

--- docs/components/UseVikeExtensionUiFramework.tsx
+++ docs/components/UseVikeExtensionUiFramework.tsx
@@ -1,5 +1,4 @@
 export { UseVikeExtensionUiFramework }
-export { UseVikeExtension }
 
 import React from 'react'
 import { UiFrameworkExtension } from '../components'
@@ -11,25 +10,6 @@ function UseVikeExtensionUiFramework({ featureName }: { featureName: string }) {
         Instead of manually integrating {featureName} yourself, you can use a <UiFrameworkExtension /> which already
         integrates {featureName}. And you can use <Bati /> to scaffold an app that uses{' '}
         <UiFrameworkExtension name noLink />.
-      </p>
-    </blockquote>
-  )
-}
-
-function UseVikeExtension({
-  children,
-  href
-}: { children: 'Pinia' | 'React Query' | string; href: `https://github.com/${string}` }) {
-  return (
-    <blockquote>
-      <p>
-        You can use{' '}
-        {
-          <a href={href}>
-            <code>{`vike-${children.toLowerCase().replaceAll(' ', '-')}`}</code>
-          </a>
-        }{' '}
-        instead of manually integrating {children} yourself.
       </p>
     </blockquote>
   )
