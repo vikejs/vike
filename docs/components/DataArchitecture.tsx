cb80668e242ac68aa8f4f955a2e4d0319e5e8658
Romuald Brillout <git@brillout.com>
2024-06-26 15:00:17 +0200

    docs: update data tool integration docs

--- docs/components/DataArchitecture.tsx
+++ docs/components/DataArchitecture.tsx
@@ -6,77 +6,42 @@ import { P, Link } from '@brillout/docpress'
 
 function DataArchitecture({
   toolName,
-  toolLink,
-  isGeneric,
-  toolType,
-  toolDocs
+  toolType
 }: {
-  toolName: string
-  toolLink?: string
-  isGeneric: boolean
+  toolName?: string
   toolType: 'data-store' | 'graphql' | 'data-fetching'
-  toolDocs: JSX.Element
 }) {
   return (
     <>
-      <DataRenderControl toolName={toolName} toolLink={toolLink} isGeneric={isGeneric} toolDocs={toolDocs} />
-      <DataPassToClient toolType={toolType} isGenericDoc={isGeneric} toolName={toolName} />
+      <DataPassToClient toolType={toolType} toolName={toolName} />
     </>
   )
 }
 
-function DataRenderControl({
-  toolName,
-  toolLink,
-  isGeneric,
-  toolDocs
-}: {
-  toolName: string
-  toolLink?: string
-  isGeneric: boolean
-  toolDocs: JSX.Element
-}) {
-  assert([true, false].includes(isGeneric), { isGeneric, isGenericType: typeof isGeneric })
-  assert(toolName)
-  assert(isGeneric === toolName.startsWith('any '), { isGeneric, toolName, toolLink })
-  assert(isGeneric === !toolLink, { isGeneric, toolName, toolLink })
-  const toolEl = toolLink ? <a href={toolLink}>{toolName}</a> : toolName
-  return (
-    <blockquote>
-      <p>
-        With Vike you have full control over rendering, data fetching, data management, and HTML streaming. Integrating{' '}
-        {toolEl}
-        {isGeneric ? ' you want' : ''} is mostly a matter of following its official SSR guide
-        {toolDocs && <>: {toolDocs}</>}. (Vike saves you from "fighting the framework".)
-      </p>
-    </blockquote>
-  )
-}
 function DataPassToClient({
   toolType,
-  isGenericDoc,
   toolName
 }: {
   toolType: 'data-store' | 'data-fetching' | 'graphql'
-  isGenericDoc?: boolean
-  toolName: string
+  toolName?: string
 }) {
   assert(toolType === 'data-store' || toolType === 'data-fetching' || toolType === 'graphql')
-  assert(isGenericDoc === undefined || isGenericDoc === true || isGenericDoc === false)
+  const isGenericDoc = !toolName
   const dataName = toolType === 'data-store' ? 'state' : 'data'
   const pageContextName = toolType === 'data-store' ? 'initialStoreState' : 'initialData'
   return (
     <>
       {toolType === 'data-store' && (
         <p>
-          When using a state management store such as {toolName}, your components don't access fetched data directly:
-          your components can only access the store. The fetched data merely determines the initial state of the store.
+          When using a state management store{toolName && ` such as ${toolName}`}, your components don't access fetched
+          data directly: your components only access the store. Fetched data merely determines the initial state of the
+          store.
         </p>
       )}
       {toolType === 'graphql' && (
         <p>
-          When using a GraphQL tool such as {toolName}, you define GraphQL queries/fragments on a component-level, while
-          fetching the GraphQL data in a single global hook (usually{' '}
+          When using a GraphQL tool{toolName && ` such as ${toolName}`}, you define GraphQL queries/fragments on a
+          component-level, while fetching the GraphQL data in a single global hook (usually{' '}
           <Link href="/onBeforeRender">
             <code>onBeforeRender()</code>
           </Link>
@@ -107,7 +72,7 @@ function DataPassToClient({
             <code>{`'${pageContextName}'`}</code> to <Link href="/passToClient" text={<code>passToClient</code>} />.
           </li>
           <li>
-            You initialize {toolType === 'data-store' ? 'the store' : toolName.replace(/^any /, 'your ')} on the
+            You initialize {toolType === 'data-store' ? 'the store' : toolName || 'your data fetching tool'} on the
             client-side using <code>pageContext.{pageContextName}</code>.
           </li>
         </ol>
