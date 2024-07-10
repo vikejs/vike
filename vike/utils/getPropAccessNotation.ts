a054e38cda44358ad13f01108f88ccc0b9c98f2a
Romuald Brillout <git@brillout.com>
2024-07-10 10:13:05 +0200

    minor refactor

--- vike/utils/getPropAccessNotation.ts
+++ vike/utils/getPropAccessNotation.ts
@@ -1,3 +1,6 @@
 export function getPropAccessNotation(key: unknown): `.${string}` | `[${string}]` {
-  return typeof key === 'string' && /^[a-z0-9\$_]+$/i.test(key) ? `.${key}` : `[${JSON.stringify(key)}]`
+  return typeof key === 'string' && isKeyDotNotationCompatible(key) ? `.${key}` : `[${JSON.stringify(key)}]`
+}
+function isKeyDotNotationCompatible(key: string): boolean {
+  return /^[a-z0-9\$_]+$/i.test(key)
 }
