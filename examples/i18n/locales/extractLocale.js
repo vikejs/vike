fbd3354637ff0326f1cf36fa3f1456f8b685150e
Romuald Brillout <git@brillout.com>
2024-07-18 23:58:02 +0200

    feat: modifyUrl()

--- examples/i18n/locales/extractLocale.js
+++ examples/i18n/locales/extractLocale.js
@@ -2,20 +2,20 @@ export { extractLocale }
 
 import { locales, localeDefault } from './locales'
 
-function extractLocale(url) {
-  const urlPaths = url.split('/')
+function extractLocale(urlPathname) {
+  const path = urlPathname.split('/')
 
   let locale
-  let urlWithoutLocale
+  let urlPathnameWithoutLocale
   // We remove the URL locale, for example `/de-DE/about` => `/about`
-  const firstPath = urlPaths[1]
-  if (locales.filter((locale) => locale !== localeDefault).includes(firstPath)) {
-    locale = firstPath
-    urlWithoutLocale = '/' + urlPaths.slice(2).join('/')
+  const first = path[1]
+  if (locales.filter((locale) => locale !== localeDefault).includes(first)) {
+    locale = first
+    urlPathnameWithoutLocale = '/' + path.slice(2).join('/')
   } else {
     locale = localeDefault
-    urlWithoutLocale = url
+    urlPathnameWithoutLocale = urlPathname
   }
 
-  return { locale, urlWithoutLocale }
+  return { locale, urlPathnameWithoutLocale }
 }
