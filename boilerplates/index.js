15e3f92a64a13bbd933a87e6c8c1fb36359d46c6
Romuald Brillout <git@brillout.com>
2024-07-22 10:30:54 +0200

    minor

--- boilerplates/index.js
+++ boilerplates/index.js
@@ -192,7 +192,7 @@ function initGitRepo(cwd) {
       [
         'git',
         '-c user.name="Vike"',
-        '-c user.email="noreply@vike.dev"',
+        '-c user.email="no-reply@vike.dev"',
         'commit',
         '--no-gpg-sign',
         '--message="scaffold Vike app"'
