aefd3ce6b82b1a0add5603010dd45ed80ee834af
Romuald Brillout <git@brillout.com>
2024-07-30 12:51:53 +0200

    fmt

--- docs/components/HookTypeScriptHints.tsx
+++ docs/components/HookTypeScriptHints.tsx
@@ -1,9 +1,9 @@
-export {HookTypeScriptHints}
+export { HookTypeScriptHints }
 
 import React from 'react'
-import {assert, Link} from '@brillout/docpress'
+import { assert, Link } from '@brillout/docpress'
 
-function HookTypeScriptHints({hookTypeName}: {hookTypeName: `${string}Sync` | `${string}Async`}) {
+function HookTypeScriptHints({ hookTypeName }: { hookTypeName: `${string}Sync` | `${string}Async` }) {
   // Ensure hookTypeName starts with an upper case
   assert(hookTypeName[0] !== hookTypeName[0].toLowerCase(), hookTypeName)
 
@@ -28,7 +28,8 @@ function HookTypeScriptHints({hookTypeName}: {hookTypeName: `${string}Sync` | `$
           Don't omit <code>ReturnType&lt;{hookTypeName}&gt;</code> otherwise TypeScript{' '}
           <a href="https://www.typescriptlang.org/play/?#code/PTAEEkDNQTwewK6jgWwJYBdQAMBKBTDBAJwDsAVGAB321AwAt9TRKaBlAY2LSqwHc4pAORZOTTgGt6TUMUIkWGavlCQ4xUAlKTScfi3wAPDMQCGoKsTg1iGNPgDOAOgBQIUAwwYqjgFwgAOaYDAgARs6cqMDo3HCOcJAYwGz4XDx8wGiOjghOwACsACwAbCUAjK6uyjSgAGIsALygABRUZuYofqCOpmikgQCUoI0AfKAA3qBm3aQIKGH4mgC+VR6AMuSgAOrWA1qOZoH4rlGkvWqk5d0NI63tncNjrROuoNPd5QA0r6AbrCrpXhYAAmcCcIjEqCoABszP0fmFugAmVzLQZrMCAUHJQABhDTyThYBAHI4nITnSCkJHXJp3DpmFCDboEIhkVIAHga4yeLRebxmoC+Pw82NSgL4oCiKBhcLOCORqMGQA">
             won't strictly check the return type
-          </a>.
+          </a>
+          .
         </p>
       </blockquote>
       <blockquote>
