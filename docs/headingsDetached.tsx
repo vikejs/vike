db9d03eb2ccf67804dee009aad1a33ae600392d6
Romuald Brillout <git@brillout.com>
2024-08-01 09:43:58 +0200

    document favicon

--- docs/headingsDetached.tsx
+++ docs/headingsDetached.tsx
@@ -5,6 +5,10 @@ import type { HeadingDetachedDefinition } from '@brillout/docpress'
 
 const headingsDetached = [
   {
+    title: '`favicon`',
+    url: '/favicon'
+  },
+  {
     title: '`<head>` tags without `vike-{react,vue,solid}`',
     url: '/head-manual'
   },
