f290ce7c091b21af0020b54a730b3fadb804f238
Romuald Brillout <git@brillout.com>
2024-08-19 19:07:33 +0200

    minor refactor

--- vike/utils/isFilePathAbsoluteFilesystem.ts
+++ vike/utils/isFilePathAbsoluteFilesystem.ts
@@ -6,29 +6,29 @@ import { assert } from './assert.js'
 import { assertPosixPath } from './filesystemPathHandling.js'
 
 /** Assert path is absolute starting from the filesystem root. */
-function assertFilePathAbsoluteFilesystem(p: string) {
+function assertFilePathAbsoluteFilesystem(filePath: string) {
   // The assert is "eventually reliable":
   // - For Windows users, the assert is correct.
   // - For Linux users assertFilePathAbsoluteFilesystem() will erroneously succeed if `p` is a path absolute from the user root dir.
   //   - But that's okay because the assertion will eventually fail for Windows users.
-  assert(isFilePathAbsoluteFilesystem(p))
+  assert(isFilePathAbsoluteFilesystem(filePath))
 }
 
 /** Whether path is absolute starting from the filesystem root. Isn't reliable for Linux users: isFilePathAbsoluteFilesystem() returns `true` for paths absolute from the user root dir. */
-function isFilePathAbsoluteFilesystem(p: string) {
-  assertPosixPath(p)
-  assert(!p.startsWith('/@fs/'))
+function isFilePathAbsoluteFilesystem(filePath: string) {
+  assertPosixPath(filePath)
+  assert(!filePath.startsWith('/@fs/'))
   if (process.platform !== 'win32') {
     // - For linux users, there doesn't seem to be a reliable way to distinguish between:
     //   - File path absolute starting from filesystem root, e.g. /home/rom/code/my-app/pages/about/+Page.js
     //   - File path absolute starting from user root dir (Vite's `config.root`), e.g. /pages/about/+Page.js
     // - Checking whether `p` starts with the first directory of process.cwd() (or `userRootDir`) can be erroneous, most notably when using docker: https://github.com/vikejs/vike/issues/703
     // - Using require.resolve() would be a solution but probably too slow?
-    return p.startsWith('/')
+    return filePath.startsWith('/')
   } else {
-    const yes = path.win32.isAbsolute(p)
+    const yes = path.win32.isAbsolute(filePath)
     // Ensure isFilePathAbsoluteFilesystem() returns `false` if path is absolute starting from the user root dir (see comments above).
-    if (yes) assert(!p.startsWith('/'))
+    if (yes) assert(!filePath.startsWith('/'))
     return yes
   }
 }
