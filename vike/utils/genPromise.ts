8d6b590cc58bd477a3f2dbdfcd45464477b5bf6e
Romuald Brillout <git@brillout.com>
2024-08-22 17:29:12 +0200

    minor

--- vike/utils/genPromise.ts
+++ vike/utils/genPromise.ts
@@ -1,4 +1,4 @@
-export function genPromise<T>() {
+export function genPromise<T>(): { promise: Promise<T>; resolve: (val: T) => void } {
   let resolve!: (val: T) => void
   const promise = new Promise<T>((r) => (resolve = r))
   return { promise, resolve }
