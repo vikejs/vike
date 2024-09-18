7b6503c65d30d3a7b835b3a7b676f8ae6557e290
Romuald Brillout <git@brillout.com>
2024-09-18 13:29:13 +0200

    improve example

--- test/misc/pages/dynamic-import-file-env/+Page.tsx
+++ test/misc/pages/dynamic-import-file-env/+Page.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { isBrowser } from './isBrowser'
 
-if (isBrowser) import('./hello.client')
+if (isBrowser) {
+  import('./hello.client')
+} else {
+  import('./hello.server')
+}
 
 export function Page() {
   return (
     <>
-      <p>dyanmic import() of .client.js file</p>
+      <p>dyanmic import() with .client.js and .server.js</p>
     </>
   )
 }
