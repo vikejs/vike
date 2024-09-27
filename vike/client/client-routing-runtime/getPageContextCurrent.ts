a11a96d94506c2c5d7a3d3bef1edcec8fe6eaa4d
Romuald Brillout <git@brillout.com>
2024-09-27 08:43:12 +0200

    minor refactor

--- vike/client/client-routing-runtime/getPageContextCurrent.ts
+++ vike/client/client-routing-runtime/getPageContextCurrent.ts
@@ -5,25 +5,25 @@ export { getPageContextCurrentAsync }
 import { getGlobalObject } from './utils.js'
 import type { PageContextExports } from '../../shared/getPageFiles.js'
 
-type PageContext = PageContextExports & {
+type PageContextCurrent = PageContextExports & {
   urlPathname: string
 }
 const globalObject = getGlobalObject('getPageContextCurrent.ts', {
-  pageContext: null as null | PageContext,
-  pageContextPromise: (() => {
-    let resolve: (pageContext: PageContext) => void
-    const promise = new Promise<PageContext>((r) => (resolve = r))
+  pageContextCurrent: null as null | PageContextCurrent,
+  pageContextCurrentPromise: (() => {
+    let resolve: (pageContextCurrent: PageContextCurrent) => void
+    const promise = new Promise<PageContextCurrent>((r) => (resolve = r))
     return { promise, resolve: resolve! }
   })()
 })
 
-function getPageContextCurrent(): null | PageContext {
-  return globalObject.pageContext
+function getPageContextCurrent(): null | PageContextCurrent {
+  return globalObject.pageContextCurrent
 }
-async function getPageContextCurrentAsync(): Promise<PageContext> {
-  return globalObject.pageContextPromise.promise
+async function getPageContextCurrentAsync(): Promise<PageContextCurrent> {
+  return globalObject.pageContextCurrentPromise.promise
 }
-function setPageContextCurrent(pageContext: PageContext): void {
-  globalObject.pageContext = pageContext
-  globalObject.pageContextPromise.resolve(pageContext)
+function setPageContextCurrent(pageContextCurrent: PageContextCurrent): void {
+  globalObject.pageContextCurrent = pageContextCurrent
+  globalObject.pageContextCurrentPromise.resolve(pageContextCurrent)
 }
