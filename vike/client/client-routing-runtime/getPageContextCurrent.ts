c28ab7a36c522ebabc036215e081368ebdd70e9b
Romuald Brillout <git@brillout.com>
2024-10-02 23:01:21 +0200

    remove geContextCurrentAsync()

--- vike/client/client-routing-runtime/getPageContextCurrent.ts
+++ vike/client/client-routing-runtime/getPageContextCurrent.ts
@@ -1,36 +1,20 @@
 export { setPageContextCurrent }
 export { getPageContextCurrent }
-export { getPageContextCurrentAsync }
 
-import { genPromise, getGlobalObject } from './utils.js'
+import { getGlobalObject } from './utils.js'
 import type { PageContextExports } from '../../shared/getPageFiles.js'
 
 type PageContextCurrent = PageContextExports & {
   urlPathname: string
 }
-const globalObject = getGlobalObject(
-  'getPageContextCurrent.ts',
-  (() => {
-    const { promise: pageContextCurrentPromise, resolve: pageContextCurrentPromiseResolve } =
-      genPromise<PageContextCurrent>()
-    return {
-      pageContextCurrent: null as null | PageContextCurrent,
-      pageContextCurrentPromise,
-      pageContextCurrentPromiseResolve
-    }
-  })()
-)
+const globalObject = getGlobalObject('getPageContextCurrent.ts', {
+  pageContextCurrent: null as null | PageContextCurrent
+})
 
 function getPageContextCurrent(): null | PageContextCurrent {
   const { pageContextCurrent } = globalObject
   return pageContextCurrent
 }
-// TODO/pageContext-prefetch: I think we can remove this? Once we use Vike's default if pageContextCurrent isn't defined yet.
-async function getPageContextCurrentAsync(): Promise<PageContextCurrent> {
-  const pageContextCurrent = await globalObject.pageContextCurrentPromise
-  return pageContextCurrent
-}
 function setPageContextCurrent(pageContextCurrent: PageContextCurrent): void {
   globalObject.pageContextCurrent = pageContextCurrent
-  globalObject.pageContextCurrentPromiseResolve(pageContextCurrent)
 }
