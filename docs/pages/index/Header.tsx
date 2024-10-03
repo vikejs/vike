6b9e344b24478daf409a44971376adfaeb05194f
Romuald Brillout <git@brillout.com>
2024-10-03 10:51:25 +0200

    docs: improve tagline

--- docs/pages/index/Header.tsx
+++ docs/pages/index/Header.tsx
@@ -9,8 +9,16 @@ function Header() {
     <div id="header-logo">
       <img src={iconVike} />
       <div>
-        <h1>Vike</h1>
-        <p id="header-tagline">Flexible, lean, community-driven, dependable, fast Vite-based frontend framework.</p>
+        <h1>
+          Next Generation
+          <br />
+          Frontend Framework
+        </h1>
+        <p id="header-tagline">
+          {/* &#8288; for non-breaking hyphen */}
+          Next.js/Nuxt alternative. Flexible, reliable, clutter&#8288;-&#8288;free, fast,
+          community&#8288;-&#8288;driven.
+        </p>
       </div>
     </div>
   )
