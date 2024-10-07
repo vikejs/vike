c2ca54f21307b13e6a694f7df103c5aad2d0810b
Romuald Brillout <git@brillout.com>
2024-10-07 13:42:41 +0200

    comment

--- docs/pages/index/Features.tsx
+++ docs/pages/index/Features.tsx
@@ -258,6 +258,9 @@ function useHeadingUnderlineAnimation() {
         const compromise = width_reference > width ? 0.7 : 0.3
         const duration_compromise = compromise * duration_adjusted + (1 - compromise) * duration_agnostic
         h.style.setProperty('--animation-duration', `${duration_compromise}s`)
+        // It's complex to only highlight only one heading at a time. We tried but failed:
+        // - https://github.com/vikejs/vike/commit/9ce5fd1ecc2413242fc5ee0669f272c31e2599c5
+        // - https://github.com/vikejs/vike/commit/e3a0e6457690782868405523ebca92524b1b48d7
         h.classList[isHighlighted ? 'add' : 'remove']('heading-highlight')
       })
     }
