a45775ccc5f57973740a41e181791f5e2faafdbd
Romuald Brillout <git@brillout.com>
2024-10-26 20:26:28 +0200

    website: fix video play logic

--- docs/pages/index/Header.tsx
+++ docs/pages/index/Header.tsx
@@ -63,7 +63,7 @@ function Header() {
   )
 }
 
-// - Source: https://github.com/brillout/vike-hammer-nitedani#animated
+// - Video source: https://github.com/brillout/vike-hammer-nitedani
 // - Spline Video export: https://www.youtube.com/watch?v=OgN8TZElx6M&t=130s
 // - Export as image sequence, then convert to video:
 //   ```bash
@@ -72,37 +72,40 @@ function Header() {
 //   For .webm see https://stackoverflow.com/questions/34974258/convert-pngs-to-webm-video-with-transparency
 function VikeNitedaniAnimated() {
   const ref = useRef<HTMLVideoElement>(null)
-  const is_firefox = isFirefox()
 
-  // We don't use `autoPlay` on mobile:
-  // - To save KBs.
-  // - To avoid weird mobile iOS Chrome bug(?): the video is automatically shown fullscren when client-side navigating from another page to the landing page.
   useEffect(() => {
-    // On Firefox, we use `autoPlay` to avoid the video poster from flickering.
-    if (is_firefox) return
+    // Avoid loading the video on mobile:
+    // - To save KBs.
+    // - To avoid weird mobile iOS Chrome bug(?): the video is automatically shown fullscren when client-side navigating from another page to the landing page.
     // 759 => same as https://github.com/vikejs/vike/blob/2c6325615390ae3be3afc6aa37ede6914b935702/docs/pages/index/HeaderLayout.css#L24
     if (screen.width > 759) {
+      const videoEl = ref.current!
+      videoEl.setAttribute('autoPlay', '')
+      videoEl.setAttribute(
+        'src',
+        'https://github.com/brillout/vike-hammer-nitedani/raw/refs/heads/main/vike-nitedani-animated.mp4'
+      )
+      /* We don't use play() because in Firefox it flickers
       // try-catch to suppress the following in the CI:
       // ```bash
       // Failed to load because no supported source was found.
       // ```
       // In practice, the video always seems to be successfully loading?
       try {
-        ref.current!.play()
+        videoEl.play()
       } catch (_) {}
+      */
     }
   })
 
   return (
     <video
       ref={ref}
-      src="https://github.com/brillout/vike-hammer-nitedani/raw/refs/heads/main/vike-nitedani-animated.mp4"
       poster={iconVikeAnimatedCover}
       height="250"
       width="182"
       muted
       loop
-      autoPlay={is_firefox}
       onContextMenu={(ev) => {
         navigate('/press#logo')
         ev.preventDefault()
@@ -130,10 +133,4 @@ function FeatureName({ children }: { children: string }) {
       {children}
     </a>
   )
-}
-
-function isFirefox() {
-  // https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browsers/9851769#9851769
-  // @ts-expect-error
-  return typeof InstallTrigger !== 'undefined'
 }
