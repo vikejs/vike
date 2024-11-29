b853374f337babb5e15c2c5385ac40bcdb78c612
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-11-29 14:14:04 +0100

    feat: add features section

---
+++ docs/pages/index/components/Chip/coreData.ts
@@ -0,0 +1,69 @@
+type coreDataType = {
+  title: string
+  href: string
+  posColLg: string
+  posRowLg: string
+}
+
+export const coreData: coreDataType[] = [
+  {
+    title: 'prefetching',
+    href: '/',
+    posColLg: '1 / span 3',
+    posRowLg: '1 / span 1'
+  },
+  {
+    title: 'preloading',
+    href: '/',
+    posColLg: '1 / span 3',
+    posRowLg: '2 / span 1'
+  },
+  {
+    title: 'url rewrites',
+    href: '/',
+    posColLg: '4 / span 3',
+    posRowLg: '1 / span 2'
+  },
+  {
+    title: 'url redirects',
+    href: '/',
+    posColLg: '7 / span 3',
+    posRowLg: '1 / span 2'
+  },
+  {
+    title: 'filesystem routing',
+    href: '/',
+    posColLg: '3 / span 5',
+    posRowLg: '3 / span 1'
+  },
+  {
+    title: 'pre-rendering',
+    href: '/',
+    posColLg: '1 / span 4',
+    posRowLg: '7 / span 2'
+  },
+  {
+    title: 'data fetching',
+    href: '/',
+    posColLg: '1 / span 4',
+    posRowLg: '9 / span 1'
+  },
+  {
+    title: 'route guard',
+    href: '/',
+    posColLg: '5 / span 2',
+    posRowLg: '7 / span 2'
+  },
+  {
+    title: 'layouts',
+    href: '/',
+    posColLg: '7 / span 3',
+    posRowLg: '7 / span 1'
+  },
+  {
+    title: 'html streaming',
+    href: '/',
+    posColLg: '7 / span 3',
+    posRowLg: '8 / span 2'
+  }
+]
--- docs/pages/index/sections/Features.tsx
+++ docs/pages/index/sections/Features.tsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import { TextBox } from '../components/TextBox'
 import { SectionTextCollection } from '../components/SectionTextCollection'
+import { Chip } from '../components/Chip/Chip'
 
 const data = {
   caption: 'Features',
-  title: 'A Core with all the Essentials.',
-  descriptions: [
-    'With Vike you have architectural freedom and you can choose any deployment strategy, any backend, any data-fetching tool, any UI library, and any render mode.'
-  ]
+  title: 'Core, all-included.'
 }
 
 export const Features = () => {
   return (
     <div
       style={{
-        width: '50%',
-        margin: '64px auto'
+        width: '100%',
+        margin: '80px 0'
       }}
     >
-      <TextBox>
-        <SectionTextCollection caption={data.caption} title={data.title} descriptions={data.descriptions} />
-      </TextBox>
+      <div
+        style={{
+          width: '50%',
+          margin: '0 auto'
+        }}
+      >
+        <TextBox>
+          <SectionTextCollection caption={data.caption} title={data.title} />
+        </TextBox>
+      </div>
+      <Chip />
     </div>
   )
 }
