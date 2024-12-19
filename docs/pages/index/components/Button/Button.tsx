03e1a2369a42050cf0be6aa7f834dc0ad8f1813c
Romuald Brillout <git@brillout.com>
2024-12-19 12:11:15 +0100

    fmt

--- docs/pages/index/components/Button/Button.tsx
+++ docs/pages/index/components/Button/Button.tsx
@@ -45,11 +45,7 @@ function ButtonLearnMore() {
   return (
     <div className="landingpage-button-learnmore">
       Learn More
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        style={{ width: 20, height: 20 }}
-        viewBox="0 0 24 24"
-      >
+      <svg xmlns="http://www.w3.org/2000/svg" style={{ width: 20, height: 20 }} viewBox="0 0 24 24">
         <path fill="currentColor" d="M10 6L8.59 7.41L13.17 12l-4.58 4.59L10 18l6-6z" />
       </svg>
     </div>
