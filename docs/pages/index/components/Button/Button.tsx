ca2d8f6a19d09da4a414b005b44a8aa221f6e39d
Romuald Brillout <git@brillout.com>
2024-12-19 11:58:53 +0100

    landingpage: minor

--- docs/pages/index/components/Button/Button.tsx
+++ docs/pages/index/components/Button/Button.tsx
@@ -47,7 +47,7 @@ function ButtonLearnMore() {
       Learn More
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        style={{ width: 20, height: 20, position: 'relative', top: 1 }}
+        style={{ width: 20, height: 20 }}
         viewBox="0 0 24 24"
       >
         <path fill="currentColor" d="M10 6L8.59 7.41L13.17 12l-4.58 4.59L10 18l6-6z" />
