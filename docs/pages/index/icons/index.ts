8302dae3c8f1b13c6aab1e5bef75dfac4e3fabc4
Romuald Brillout <git@brillout.com>
2024-12-22 10:19:27 +0100

    landinpage: use heart icon instead of compass icon

--- docs/pages/index/icons/index.ts
+++ docs/pages/index/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as earIcon } from './ear.svg'
 export { default as handshakeIcon } from './handshake.svg'
 export { default as windowIcon } from './window.svg'
+export { default as heartIcon } from './heart.svg'
