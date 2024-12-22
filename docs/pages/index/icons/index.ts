f49562501f3efb38bc5a0f90c904b49081254134
Romuald Brillout <git@brillout.com>
2024-12-22 10:19:32 +0100

    Revert "landinpage: use heart icon instead of compass icon"
    This reverts commit 8302dae3c8f1b13c6aab1e5bef75dfac4e3fabc4.

--- docs/pages/index/icons/index.ts
+++ docs/pages/index/icons/index.ts
@@ -1,4 +1,3 @@
 export { default as earIcon } from './ear.svg'
 export { default as handshakeIcon } from './handshake.svg'
 export { default as windowIcon } from './window.svg'
-export { default as heartIcon } from './heart.svg'
