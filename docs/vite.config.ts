e4fef6d43aecc76aeb909c55403da325f2863467
Romuald Brillout <git@brillout.com>
2024-12-23 17:06:12 +0100

    remove failed attempt to make currentColor work

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -7,27 +7,6 @@ export default {
   plugins: [
     //
     ...docpressConfig.plugins!,
-    svgr(),
-    pluginIconSvgInjectColor()
+    svgr()
   ]
 } satisfies UserConfig
-
-function pluginIconSvgInjectColor() {
-  return {
-    name: 'vike/docs:pluginIconSvgInjectColor',
-    // enforce: 'pre',
-    transform: {
-      // order: 'pre',
-      async handler(code: string, id: string) {
-        if (!id.endsWith('.svg')) return
-        // console.log('id', id)
-        if (!(id.endsWith('.svg') && id.includes('pages/index/icons/'))) {
-          return
-        }
-        console.log('-id', id)
-        console.log('-code', code)
-        return code.replace('currentColor', 'red')
-      }
-    }
-  } as const
-}
