452b43a0cb563b6364710a3ffd7cc95008ea0a69
Romuald Brillout <git@brillout.com>
2025-01-03 10:17:59 +0100

    landingpage: add used by tooltips

--- docs/pages/index/sections/hero/UsedBy.tsx
+++ docs/pages/index/sections/hero/UsedBy.tsx
@@ -10,34 +10,40 @@ import usedByName from './UsedBy/usedby-name.svg'
 import usedByChip from './UsedBy/usedby-chip.svg'
 import { IllustrationNote } from '../../components/IllustrationNote'
 
-const data: { website: `https://${string}`; logo: string; shrink?: number; offset?: number }[] = [
+const data: { website: `https://${string}`; logo: string; shrink?: number; offset?: number; desc: string }[] = [
   {
     website: 'https://name.com',
+    desc: 'One of the leading domain registrar',
     logo: usedByName,
     offset: 5
   },
   {
     website: 'https://contra.com',
+    desc: 'Disruptive Upwork alternative',
     logo: usedByContra,
     shrink: 40,
     offset: 4
   },
   {
     website: 'https://app.spline.design',
+    desc: 'Most advanced 3D design web app',
     logo: usedBySpline
   },
   {
     website: 'https://ecosia.org',
+    desc: 'Popular Google aternative',
     logo: usedByEcosia,
     shrink: 55,
     offset: -1
   },
   {
     website: 'https://bild.de',
+    desc: "Germany's most read news",
     logo: usedByBild
   },
   {
     website: 'https://chip.de',
+    desc: "Germany's most read consumer news",
     logo: usedByChip,
     shrink: 60
   }
@@ -58,6 +64,7 @@ function UsedBy() {
               padding: 10,
               marginLeft: i === 0 ? 0 : 30
             }}
+            aria-label={e.desc}
             className="hero-usedby colorize-on-hover"
           >
             <img
