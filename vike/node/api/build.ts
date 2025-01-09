c72ff3ddaf4f95e38a504d13ce3d37f43081ffed
Romuald Brillout <git@brillout.com>
2025-01-09 08:33:47 +0100

    minor refactor

--- vike/node/api/build.ts
+++ vike/node/api/build.ts
@@ -1,25 +1,32 @@
 export { build }
 
 import { enhanceViteConfig } from './enhanceViteConfig.js'
-import { build as buildVite } from 'vite'
+import { build as buildVite, type InlineConfig } from 'vite'
 
 async function build() {
   const { viteConfigEnhanced, configVike } = await enhanceViteConfig({}, 'build')
 
+  // Build client-side
   const outputClient = await buildVite(viteConfigEnhanced)
 
-  const outputServer = await buildVite({
-    ...viteConfigEnhanced,
-    build: {
-      ...viteConfigEnhanced.build,
-      ssr: true
-    }
-  })
+  // Build server-side
+  const outputServer = await buildVite(addSSR(viteConfigEnhanced))
 
+  // Pre-render
   if (configVike.prerender && !configVike.prerender.disableAutoRun && configVike.disableAutoFullBuild !== 'prerender') {
     const { runPrerenderFromAutoRun } = await import('../prerender/runPrerender.js')
     await runPrerenderFromAutoRun(viteConfigEnhanced)
   }
 
   return { outputClient, outputServer }
+}
+
+function addSSR(viteConfigEnhanced: InlineConfig): InlineConfig {
+  return {
+    ...viteConfigEnhanced,
+    build: {
+      ...viteConfigEnhanced.build,
+      ssr: true
+    }
+  }
 }
