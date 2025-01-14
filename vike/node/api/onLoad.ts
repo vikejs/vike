7e7fd40fc6babf5d2762f6242260ed5d55635456
Romuald Brillout <git@brillout.com>
2025-01-14 11:44:40 +0100

    further assert API/CLI env

--- vike/node/api/onLoad.ts
+++ vike/node/api/onLoad.ts
@@ -1,7 +1,9 @@
 export { onLoad }
 
 import { assertIsNotBrowser } from '../../utils/assertIsNotBrowser.js'
+import { assertIsNotProductionRuntime } from '../runtime/utils.js'
 
 function onLoad() {
   assertIsNotBrowser()
+  assertIsNotProductionRuntime()
 }
