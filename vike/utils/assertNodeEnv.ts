5fcc23b3cecfa2540d10eb39a5a154cd29324cf1
Romuald Brillout <git@brillout.com>
2025-01-15 13:10:58 +0100

    minor refactor

--- vike/utils/assertNodeEnv.ts
+++ vike/utils/assertNodeEnv.ts
@@ -15,27 +15,29 @@
 export { assertNodeEnv_build }
 export { assertNodeEnv_runtime }
 export { assertNodeEnv_onVikePluginLoad }
-// Vite already handles build
 export { handleNodeEnv_prerender }
 export { handleNodeEnv_vitePluginVercel }
+/* Vite already sets `process.env.NODE_ENV = 'production'`
+export { handleNodeEnv_build }
+*/
 
 import pc from '@brillout/picocolors'
 import { assertIsNotBrowser } from './assertIsNotBrowser.js'
-import { assertWarning } from './assert.js'
+import { assert, assertWarning } from './assert.js'
 import { vikeVitePluginLoadedInProductionError } from './assertIsNotProductionRuntime.js'
 assertIsNotBrowser()
 
 function assertNodeEnv_build() {
-  assertNodeEnvIsNotDev('building')
+  assertUsageNodeEnvIsNotDev('building')
 }
 
 function assertNodeEnv_runtime(isViteDev: boolean) {
-  const nodeEnv = getNodeEnv()
+  const nodeEnv = getNodeEnvValue()
   if (nodeEnv === null || nodeEnv === 'test') return
   const isNodeDev = isNodeEnvDev()
   // Calling Vite's createServer() is enough for hasViteDevServer to be true, even without actually adding Vite's development middleware to the server: https://github.com/vikejs/vike/issues/792#issuecomment-1516830759
   if (isViteDev === isNodeDev) return
-  const nodeEnvDesc = getNodeEnvDesc()
+  const nodeEnvDesc = getEnvDescription()
   // TODO: make it assertUsage() again once https://github.com/vikejs/vike/issues/1528 is implemented.
   const errMsg = `Running ${
     isViteDev ? pc.cyan('$ vike dev') : 'app in production'
@@ -44,19 +46,20 @@ function assertNodeEnv_runtime(isViteDev: boolean) {
 }
 
 function assertNodeEnv_onVikePluginLoad() {
-  const nodeEnv = getNodeEnv()
+  const nodeEnv = getNodeEnvValue()
   if (nodeEnv === 'test') return
+  //* Let's enable this function after support for Vite's CLI is removed. (We can then abort this function if the context is `$ vike build` or `import { build } from 'vike/api';build()`.)
+  if (true as boolean) return
+  /*/
+  if (getOperation() === 'build') return
+  //*/
   // TODO: make it assertUsage() again once https://github.com/vikejs/vike/issues/1528 is implemented.
   assertWarning(
-    /* We can enable this assertion after Vike's CLI is implemented and using Vite's CLI is deprecated (we can then check whether the context is a `$ vike build`).
-    isNodeEnvDev() || isVikeCliBuild(),
-    /*/
-    isNodeEnvDev() || (true as boolean),
-    ///*/
+    isNodeEnvDev(),
     [
       pc.cyan(`process.env.NODE_ENV === ${JSON.stringify(nodeEnv)}`),
       '(which Vike interprets as a non-development environment https://vike.dev/NODE_ENV)',
-      'while the vike/plugin module is loaded.',
+      'while vike/plugin is loaded.',
       vikeVitePluginLoadedInProductionError
     ].join(' '),
     { onlyOnce: true }
@@ -64,22 +67,21 @@ function assertNodeEnv_onVikePluginLoad() {
 }
 
 function handleNodeEnv_prerender() {
-  const assertNodeEnv = () => assertNodeEnvIsNotDev('pre-rendering')
-  if (getNodeEnv()) assertNodeEnv()
-  setNodeEnvToProduction()
-  assertNodeEnv()
+  if (getNodeEnvValue()) assertUsageNodeEnvIsNotDev('pre-rendering')
+  setNodeEnvProduction()
+  assert(getNodeEnvValue() === 'production')
 }
 
 function handleNodeEnv_vitePluginVercel() {
-  setNodeEnvToProduction()
+  setNodeEnvProduction()
 }
 
-function getNodeEnv(): null | undefined | string {
+function getNodeEnvValue(): null | undefined | string {
   if (typeof process === 'undefined') return null
   return process.env.NODE_ENV
 }
 
-function setNodeEnvToProduction(): void | undefined {
+function setNodeEnvProduction(): void | undefined {
   // The statement `process.env['NODE_ENV'] = 'production'` chokes webpack v4
   const proc = process
   const { env } = proc
@@ -87,15 +89,15 @@ function setNodeEnvToProduction(): void | undefined {
 }
 
 function isNodeEnvDev(): boolean {
-  const nodeEnv = getNodeEnv()
+  const nodeEnv = getNodeEnvValue()
   if (!nodeEnv) return true
   if (['development', 'dev'].includes(nodeEnv)) return true
   // That's quite aggressive, let's see if some user complains
   return false
 }
 
-function getNodeEnvDesc(): `environment is set to be a ${string} environment by process.env.NODE_ENV === ${string}` {
-  const nodeEnv = getNodeEnv()
+function getEnvDescription(): `environment is set to be a ${string} environment by process.env.NODE_ENV === ${string}` {
+  const nodeEnv = getNodeEnvValue()
   const isDev = isNodeEnvDev()
   const nodeEnvDesc = `environment is set to be a ${
     (isDev ? 'development' : 'production') as string
@@ -103,12 +105,12 @@ function getNodeEnvDesc(): `environment is set to be a ${string} environment by 
   return nodeEnvDesc
 }
 
-function assertNodeEnvIsNotDev(operation: 'building' | 'pre-rendering') {
-  const isDev = isNodeEnvDev()
-  if (!isDev) return
-  const nodeEnvDesc = getNodeEnvDesc()
+function assertUsageNodeEnvIsNotDev(operation: 'building' | 'pre-rendering') {
+  if (!isNodeEnvDev()) return
   // TODO: make it assertUsage() again once https://github.com/vikejs/vike/issues/1528 is implemented.
-  assertWarning(false, `The ${nodeEnvDesc} which is forbidden upon ${operation}, see https://vike.dev/NODE_ENV`, {
-    onlyOnce: true
-  })
+  assertWarning(
+    false,
+    `The ${getEnvDescription()} which is forbidden upon ${operation}, see https://vike.dev/NODE_ENV`,
+    { onlyOnce: true }
+  )
 }
