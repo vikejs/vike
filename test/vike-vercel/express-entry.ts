33e87e9bf98ed6c54f0857c26ce66d75f55d7e12
Romuald Brillout <git@brillout.com>
2025-01-16 20:27:07 +0100

    fmt

--- test/vike-vercel/express-entry.ts
+++ test/vike-vercel/express-entry.ts
@@ -1,55 +1,55 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-import { createHandler, createMiddleware } from "@universal-middleware/express";
-import express from "express";
-import { vikeHandler } from "./server/vike-handler";
+import { createHandler, createMiddleware } from '@universal-middleware/express'
+import express from 'express'
+import { vikeHandler } from './server/vike-handler'
 
-const __filename = globalThis.__filename ?? fileURLToPath(import.meta.url);
-const __dirname = globalThis.__dirname ?? dirname(__filename);
-const root = __dirname;
-const port = process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 3000;
-const hmrPort = process.env.HMR_PORT ? Number.parseInt(process.env.HMR_PORT, 10) : 24678;
+const __filename = globalThis.__filename ?? fileURLToPath(import.meta.url)
+const __dirname = globalThis.__dirname ?? dirname(__filename)
+const root = __dirname
+const port = process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 3000
+const hmrPort = process.env.HMR_PORT ? Number.parseInt(process.env.HMR_PORT, 10) : 24678
 
-export default await startServer();
+export default await startServer()
 
 async function startServer() {
-  const app = express();
+  const app = express()
 
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(`${root}/dist/client`));
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(`${root}/dist/client`))
   } else {
     // Instantiate Vite's development server and integrate its middleware to our server.
     // ! We should instantiate it *only* in development. (It isn't needed in production
     // and would unnecessarily bloat our server in production.)
-    const vite = await import("vite");
+    const vite = await import('vite')
     const viteDevMiddleware = (
       await vite.createServer({
         root,
-        server: { middlewareMode: true, hmr: { port: hmrPort } },
+        server: { middlewareMode: true, hmr: { port: hmrPort } }
       })
-    ).middlewares;
-    app.use(viteDevMiddleware);
+    ).middlewares
+    app.use(viteDevMiddleware)
   }
 
   app.get(
-    "/hello",
+    '/hello',
     createMiddleware(() => () => {
-      console.log("HELLO");
-      return new Response("Hello");
-    })(),
-  );
+      console.log('HELLO')
+      return new Response('Hello')
+    })()
+  )
 
   /**
    * Vike route
    *
    * @link {@see https://vike.dev}
    **/
-  app.all("*", createHandler(() => vikeHandler)());
+  app.all('*', createHandler(() => vikeHandler)())
 
   app.listen(port, () => {
-    console.log(`Server listening on http://localhost:${port}`);
-  });
+    console.log(`Server listening on http://localhost:${port}`)
+  })
 
-  return app;
+  return app
 }
--- test/vike-vercel/vite.config.ts
+++ test/vike-vercel/vite.config.ts
@@ -1,7 +1,7 @@
-import react from "@vitejs/plugin-react-swc";
-import vike from "vike/plugin";
-import type { UserConfig } from "vite";
-import vercel from "vite-plugin-vercel";
+import react from '@vitejs/plugin-react-swc'
+import vike from 'vike/plugin'
+import type { UserConfig } from 'vite'
+import vercel from 'vite-plugin-vercel'
 
 export default {
   plugins: [
@@ -9,19 +9,19 @@ export default {
     vike(),
     vercel({
       // You usually want the server to handle all routes
-      source: "/.*",
-    }),
+      source: '/.*'
+    })
   ],
   vercel: {
     additionalEndpoints: [
       {
         // entry file to the server. Default export must be a node server or a function
-        source: "express-entry.ts",
+        source: 'express-entry.ts',
         // replaces default Vike target
-        destination: "ssr_",
+        destination: 'ssr_',
         // already added by default Vike route
-        route: false,
-      },
-    ],
-  },
-} as UserConfig;
+        route: false
+      }
+    ]
+  }
+} as UserConfig
