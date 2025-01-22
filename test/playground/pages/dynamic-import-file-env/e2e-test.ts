ba1d8210689cf993fbf36e88e11b2fe76503dad7
Romuald Brillout <git@brillout.com>
2025-01-22 14:30:02 +0100

    fix test

--- test/playground/pages/dynamic-import-file-env/e2e-test.ts
+++ test/playground/pages/dynamic-import-file-env/e2e-test.ts
@@ -7,7 +7,8 @@ function testDynamicImportFileEnv({ isDev }: { isDev: boolean }) {
     await page.goto(getServerUrl() + '/dynamic-import-file-env')
     expect(await page.textContent('body')).toContain('Dyanmic import() of .client.js and .server.js')
     expectLog('hello from server', {
-      filter: (log) => log.logSource === (isDev ? 'stdout' : 'run()'),
+      filter: (log) => log.logSource === 'stdout',
+      // If `isDev===false` then log is printed upon pre-rendering (not when the test is running).
       allLogs: !isDev
     })
     await autoRetry(
