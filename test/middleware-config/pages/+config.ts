18457b0e8c6ac000896ddaf16909d5bb6a0f153d
Romuald Brillout <git@brillout.com>
2025-01-22 15:52:53 +0100

    wip

--- test/middleware-config/pages/+config.ts
+++ test/middleware-config/pages/+config.ts
@@ -1,6 +1,7 @@
 import vikeReact from 'vike-react/config'
 import type { Config } from 'vike/types'
 import Layout from '../layouts/LayoutDefault.js'
+import React from 'react'
 
 // Default config (can be overridden by pages)
 // https://vike.dev/config
