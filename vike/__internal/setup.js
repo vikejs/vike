5d388ee21a93c976bc1eaf2c780aa3ddaae5b4fe
Romuald Brillout <git@brillout.com>
2025-01-24 18:13:38 +0100

    fix: fix Vike extensions regression with __internal/setup

---
+++ vike/__internal/setup.js
@@ -0,0 +1,3 @@
+// Some tools still need this as of January 2025
+export * from '../dist/esm/node/runtime/page-files/setup.js'
+export { default } from '../dist/esm/node/runtime/page-files/setup.js'
---
+++ vike/node/runtime/page-files/setup.ts
@@ -0,0 +1,2 @@
+// TODO/v1-release: remove
+// This doesn't do anything anymore. (It's kept to avoid introducing a breaking change.)
