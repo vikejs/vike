d46be364450c7cfed075b058df70c55b23737713
Romuald Brillout <git@brillout.com>
2025-01-28 22:45:04 +0100

    comment (#2120)

--- vike/utils/isDev.ts
+++ vike/utils/isDev.ts
@@ -8,6 +8,8 @@ import type { ConfigEnv } from 'vite'
 function isDevCheck(configEnv: ConfigEnv): boolean {
   const { isPreview, command } = configEnv
   // Released at vite@5.1.0 which is guaranteed with `assertVersion('Vite', version, '5.1.0')`
+  // - Release: https://github.com/vitejs/vite/blob/6f7466e6211027686f40ad7e4ce6ec8477414546/packages/vite/CHANGELOG.md#510-beta4-2024-01-26:~:text=fix(preview)%3A-,set%20isPreview%20true,-(%2315695)%20(93fce55
+  // - Surprisingly, this assert() can fail: https://github.com/vikejs/vike/issues/2120
   assert(typeof isPreview === 'boolean')
   return command === 'serve' && !isPreview
 }
