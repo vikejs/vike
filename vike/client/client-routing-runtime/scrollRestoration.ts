fc5ae56df171eead4a5c5d60a3d0271e3a38dd21
Romuald Brillout <git@brillout.com>
2025-01-28 12:09:48 +0100

    comment

--- vike/client/client-routing-runtime/scrollRestoration.ts
+++ vike/client/client-routing-runtime/scrollRestoration.ts
@@ -1,14 +1,18 @@
 export { scrollRestoration_init }
 export { scrollRestoration_setInitialRenderIsDone }
 
-// Handle `window.history.scrollRestoration`
-// Explanation: https://github.com/cyco130/knave/blob/e9e1bc7687848504293197f1b314b7d12ad0d228/design.md#scroll-restoration
+// Using `window.history.scrollRestoration` to recover scroll position when user reloads the page or Cmd-Shift-T back to it.
+
+// We let the browser do it because it's fast.
+// - Alternatively we could inject an inline script `<script>scrollTo(history.state.scrollPosition)</script>` early, which seems to be equally fast. (See for example https://vike.dev/usePageContext which sets the main scroll position and the navigation scroll position equally fast.)
+
+// See also: https://github.com/cyco130/knave/blob/e9e1bc7687848504293197f1b314b7d12ad0d228/design.md#scroll-restoration
 
 import { getGlobalObject, onPageHide, onPageShow } from './utils.js'
 const globalObject = getGlobalObject<{ initialRenderIsDone?: true }>('scrollRestoration.ts', {})
 
-// We use the browser's native scroll restoration mechanism only for the first render
 function scrollRestoration_init() {
+  // Use the native scroll restoration mechanism only for the first render
   scrollRestoration_enable()
   onPageHide(scrollRestoration_enable)
   onPageShow(() => globalObject.initialRenderIsDone && scrollRestoration_disable())
@@ -25,6 +29,7 @@ function scrollRestoration_disable() {
 }
 function scrollRestoration_enable() {
   if ('scrollRestoration' in window.history) {
+    // Use the browser's native scroll restoration mechanism
     window.history.scrollRestoration = 'auto'
   }
 }
