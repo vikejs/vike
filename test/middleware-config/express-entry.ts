c3d9cec7e1b8620ed6a5f7207d804df79132ebc1
Joël Charles <joel.charles91@gmail.com>
2025-01-29 12:00:18 +0100

    +middleware example with enhance util (#2050)

--- test/middleware-config/express-entry.ts
+++ test/middleware-config/express-entry.ts
@@ -1,10 +1,10 @@
 import { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-import { createMiddleware } from '@universal-middleware/express'
+import type { EnhancedMiddleware } from '@universal-middleware/core'
+import { apply } from '@universal-middleware/express'
 import express from 'express'
 import { getMiddlewares } from 'vike/__internal'
-import type { Middleware } from './pages/Middleware'
 import { createDevMiddleware } from 'vike/server'
 
 const __filename = fileURLToPath(import.meta.url)
@@ -24,22 +24,13 @@ async function startServer() {
     app.use(devMiddleware)
   }
 
-  const middlewares = (await getMiddlewares()) as Middleware[]
-  middlewares.sort((m1, m2) => getOrder(m1.order) - getOrder(m2.order))
-  middlewares.forEach((middlewareSpec) => {
-    const middleware = middlewareSpec.value
-    app.all('*', createMiddleware(() => middleware)())
-  })
+  const middlewares = (await getMiddlewares()) as EnhancedMiddleware[]
+  // @ts-ignore Typescript seems to think that apply requires express 5
+  apply(app, middlewares)
 
   app.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`)
   })
 
   return app
-}
-
-function getOrder(order: Middleware['order']): number {
-  if (order === 'post') return 100
-  if (order === 'pre') return -100
-  return order
 }
--- test/middleware-config/pages/middlewareVike.ts
+++ test/middleware-config/pages/middlewareVike.ts
@@ -1,9 +1,8 @@
 export { middlewareVike }
 
-import { renderPage } from 'vike/server'
 // TODO: stop using universal-middleware and directly integrate server middlewares instead. (Bati generates boilerplates that use universal-middleware https://github.com/magne4000/universal-middleware to make Bati's internal logic easier. This is temporary and will be removed soon.)
-import type { UniversalMiddleware } from '@universal-middleware/core'
-import type { Middleware } from './Middleware'
+import { type UniversalMiddleware, enhance } from '@universal-middleware/core'
+import { renderPage } from 'vike/server'
 
 const vikeUniversalMiddleware: UniversalMiddleware = async (request, context, runtime) => {
   const pageContextInit = { ...context, ...runtime, urlOriginal: request.url, headersOriginal: request.headers }
@@ -19,10 +18,8 @@ const vikeUniversalMiddleware: UniversalMiddleware = async (request, context, ru
   })
 }
 
-const middlewareVike: Middleware[] = [
-  {
-    name: 'vike',
-    order: 'post',
-    value: vikeUniversalMiddleware
-  }
-]
+const middlewareVike = enhance(vikeUniversalMiddleware, {
+  name: 'vike',
+  method: 'GET',
+  path: '/**'
+})
