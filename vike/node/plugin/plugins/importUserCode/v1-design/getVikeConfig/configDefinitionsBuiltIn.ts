d1212ac4adda1bedf093b5b02cc53e2fcae3de69
Romuald Brillout <git@brillout.com>
2025-01-30 15:11:51 +0100

    fix global prop

--- vike/node/plugin/plugins/importUserCode/v1-design/getVikeConfig/configDefinitionsBuiltIn.ts
+++ vike/node/plugin/plugins/importUserCode/v1-design/getVikeConfig/configDefinitionsBuiltIn.ts
@@ -232,7 +232,7 @@ const configDefinitionsBuiltInAll: ConfigDefinitionsBuiltIn = {
     eager: true,
     global: true
   },
-  prerender: { env: { config: true }, global: (value) => typeof value !== 'object', type: ['boolean', 'object'] },
+  prerender: { env: { config: true }, global: (value) => typeof value === 'object', type: ['boolean', 'object'] },
   vite: { env: { config: true }, global: true, cumulative: true, type: 'object' },
   disableAutoFullBuild: {
     env: { config: true },
