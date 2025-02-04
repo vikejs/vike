caa3df7511a9d77272433a2f1f191f1102e76593
Romuald Brillout <git@brillout.com>
2025-02-04 16:35:07 +0100

    further assert

--- vike/utils/toPosixPath.ts
+++ vike/utils/toPosixPath.ts
@@ -2,14 +2,17 @@ export { toPosixPath }
 export { assertPosixPath }
 
 import { assert } from './assert.js'
+import { assertIsNotBrowser } from './assertIsNotBrowser.js'
 
 function toPosixPath(path: string): string {
+  assertIsNotBrowser()
   const pathPosix = path.split('\\').join('/')
   assertPosixPath(pathPosix)
   return pathPosix
 }
 
 function assertPosixPath(path: string): void {
+  assertIsNotBrowser()
   const errMsg = (msg: string) => `Not a posix path: ${msg}`
   assert(path !== null, errMsg('null'))
   assert(typeof path === 'string', errMsg(`typeof path === ${JSON.stringify(typeof path)}`))
