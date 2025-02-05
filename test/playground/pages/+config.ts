bf371ebe4579cd6f584de50b8d13ab204e4a9bc8
Romuald Brillout <git@brillout.com>
2025-02-05 19:05:50 +0100

    provoke global wrong location warning

--- test/playground/pages/+config.ts
+++ test/playground/pages/+config.ts
@@ -3,10 +3,6 @@ import vikeReact from 'vike-react/config'
 
 export default {
   title: 'Big Playground',
-  prerender: {
-    // TEST: prerender.noExtraDir
-    noExtraDir: true
-  },
   redirects: {
     // TEST: redirect to email
     '/mail': 'mailto:some@example.com'
