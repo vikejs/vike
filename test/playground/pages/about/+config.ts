af5ba16e1ee9b3d58f6c214401e775ce10ce9445
Romuald Brillout <git@brillout.com>
2025-02-05 19:05:56 +0100

    Revert "provoke global wrong location warning"
    This reverts commit 979f720ec06838897e57996c113364909b71aa61.

--- test/playground/pages/about/+config.ts
+++ test/playground/pages/about/+config.ts
@@ -4,8 +4,5 @@ export default {
   // TEST: define `Page` over import string at `+config.js > export default { Page }` instead of +Page.js file.
   // @ts-expect-error I ain't sure whether import strings should be part of the official type, but I think so.
   Page: 'import:./Page.jsx',
-  prerender: {
-    // TEST: prerender.noExtraDir
-    noExtraDir: true
-  }
+  prerender: false
 } satisfies Config
