d96cb16240993d9218c8b788fc952f03502f354e
Romuald Brillout <git@brillout.com>
2025-02-05 21:30:40 +0100

    comment

--- vike/utils/findFile.ts
+++ vike/utils/findFile.ts
@@ -7,6 +7,7 @@ import { assertPosixPath } from './path.js'
 
 type Filename = 'package.json' | 'vike.config.js' | 'vike.config.ts'
 
+// We need to be able to crawl the filesystem, regardless of Vike's `$ git ls-files` command call, because we need to fallback if the user didn't setup Git (e.g. we cannot remove the fast-glob fallback).
 function findFile(arg: Filename | Filename[], cwd: string): null | string {
   assertPosixPath(cwd)
   const filenames = isArray(arg) ? arg : [arg]
