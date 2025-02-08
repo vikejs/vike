2c54227defb7009719eff099b2dff1158c48dc62
Romuald Brillout <git@brillout.com>
2025-02-08 14:07:02 +0100

    minor refactor: unused code

--- vike/__internal/index.ts
+++ vike/__internal/index.ts
@@ -6,9 +6,8 @@ export { route, getPagesAndRoutes }
 export type { PageRoutes, PageFile, PageConfigRuntime as PageConfig }
 
 import { route as routeInternal, type PageRoutes } from '../shared/route/index.js'
-import { getPageFilesAll } from '../shared/getPageFiles/getPageFiles.js'
 import type { PageFile } from '../shared/getPageFiles/getPageFileObject.js'
-import { getGlobalContext, initGlobalContext_getGlobalContextAsync } from '../node/runtime/globalContext.js'
+import { getGlobalContext } from '../node/runtime/globalContext.js'
 import { setNodeEnvProduction } from '../utils/assertSetup.js'
 import { PageConfigRuntime } from '../shared/page-configs/PageConfig.js'
 
@@ -31,17 +30,6 @@ async function getPagesAndRoutes() {
     pageConfigs,
     allPageIds
   }
-}
-
-// TODO/eventually:
-//  - Make it cleaner once the internal refactoring about global configs is done.
-//  - Remove it in favor of https://vike.dev/getGlobalContext
-// Demo usage: https://github.com/vikejs/vike/pull/1823
-async function getPageFilesAllSafe(isProduction: boolean) {
-  await initGlobalContext_getGlobalContextAsync(isProduction)
-  const globalContext = getGlobalContext()
-  const pageFilesAll = await getPageFilesAll(false, globalContext.isProduction)
-  return pageFilesAll
 }
 
 async function route(pageContext: Parameters<typeof routeInternal>[0]) {
