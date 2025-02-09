24fb03939aeeb08ee2d1d6ede89d72bacf37f011
Romuald Brillout <git@brillout.com>
2025-02-09 11:18:26 +0100

    comment

--- vike/node/api/build.ts
+++ vike/node/api/build.ts
@@ -19,21 +19,21 @@ async function build(options: APIOptions = {}): Promise<{}> {
   // Pass it to autoFullBuild()
   if (viteConfigEnhanced) viteConfigEnhanced._viteConfigEnhanced = viteConfigEnhanced
 
-  // 1. Build client-side
-  // 2. Build server-side
-  //    > See: https://github.com/vikejs/vike/blob/c6c7533a56b3a16fc43ed644fc5c10c02d0ff375/vike/node/plugin/plugins/autoFullBuild.ts#L90
-  // 3. Pre-render (if enabled)
-  //    > See: https://github.com/vikejs/vike/blob/c6c7533a56b3a16fc43ed644fc5c10c02d0ff375/vike/node/plugin/plugins/autoFullBuild.ts#L98
-  //    > We purposely don't start the pre-rendering in this `build()` function but in a Rollup hook instead.
-  //    > Rationale: https://github.com/vikejs/vike/issues/2123
   if (vikeConfig.global.config.viteEnvironmentAPI) {
     assertVersion('Vite', version, '6.0.0')
     const { createBuilder } = await import('vite')
     const builder = await createBuilder(viteConfigEnhanced)
     await builder.buildApp()
   } else {
+    // This buildVite() call does everything:
+    // 1. Build client-side
+    // 2. Build server-side
+    //    > See: https://github.com/vikejs/vike/blob/c6c7533a56b3a16fc43ed644fc5c10c02d0ff375/vike/node/plugin/plugins/autoFullBuild.ts#L90
+    // 3. Pre-render (if enabled)
+    //    > See: https://github.com/vikejs/vike/blob/c6c7533a56b3a16fc43ed644fc5c10c02d0ff375/vike/node/plugin/plugins/autoFullBuild.ts#L98
+    //    > We purposely don't start the pre-rendering in this `build()` function but in a Rollup hook instead.
+    //    > Rationale: https://github.com/vikejs/vike/issues/2123
     await buildVite(viteConfigEnhanced)
-
     // When using the Vike CLI with pre-rendering the process is forcefully exited at the end of the buildVite() call above
     assert(!(isVikeCli() && isPrerendering()))
   }
