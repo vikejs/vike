475b7c41a491b938577c296f1ac6cbfab361659b
Romuald Brillout <git@brillout.com>
2025-02-09 12:07:00 +0100

    minor refactor: viteIsSSR()

--- vike/node/plugin/shared/viteIsSSR.ts
+++ vike/node/plugin/shared/viteIsSSR.ts
@@ -1,3 +1,4 @@
+import type { ResolvedConfig, UserConfig } from 'vite'
 import { assert } from '../../../utils/assert.js'
 import { isObject } from '../../../utils/isObject.js'
 
@@ -5,7 +6,7 @@ export { viteIsSSR }
 export { vite6IsSSR }
 export { viteIsSSR_options }
 
-function viteIsSSR(config: { build?: { ssr?: boolean | string } }): boolean {
+function viteIsSSR(config: ResolvedConfig | UserConfig): boolean {
   return !!config?.build?.ssr
 }
 
