bdfea9637de94809ea8ca74eef753c7f99541a41
Romuald Brillout <git@brillout.com>
2025-02-11 16:54:33 +0100

    update test

--- test/playground/pages/+Head.tsx
+++ test/playground/pages/+Head.tsx
@@ -1,12 +1,14 @@
 export { Head }
 
 import React from 'react'
+import favicon from './logo.svg'
 
 function Head() {
   return (
     <>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href={favicon} />
     </>
   )
 }
