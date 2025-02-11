63bc5ffb669e7738566cbd39a096ddb2a483fd5d
Romuald Brillout <git@brillout.com>
2025-02-11 17:17:12 +0100

    Revert "add test"
    This reverts commit 30238be528a49995d40191a0505a2fd2fca28c67.

--- test/playground/+vite.ts
+++
@@ -1,7 +0,0 @@
-import react from '@vitejs/plugin-react'
-import type { UserConfig } from 'vite'
-
-export default {
-  // TEST: use +vite to add Vite plugin
-  plugins: [react()]
-} satisfies UserConfig
--- test/playground/vite.config.ts
+++ test/playground/vite.config.ts
@@ -1,9 +1,11 @@
-import type { UserConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import vike from 'vike/plugin'
 
 export default {
   // TEST: funky build output directory
   build: {
     outDir: `${__dirname}/../../test/playground/dist/nested`,
     assetsDir: '/nested-dir/assets'
-  }
-} satisfies UserConfig
+  },
+  plugins: [vike(), react()]
+}
