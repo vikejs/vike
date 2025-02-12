f01871a1f49549db51309896da4ef0f962888f7e
Romuald Brillout <git@brillout.com>
2025-02-12 17:56:40 +0100

    website: update docpress

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,11 +1,8 @@
-import docpressConfig from '@brillout/docpress/vite-config'
 import svgr from 'vite-plugin-svgr'
 import type { UserConfig } from 'vite'
 
 export default {
-  ...docpressConfig,
   plugins: [
-    ...docpressConfig.plugins!,
     // Used by the landing page, see `.svg?react` imports
     svgr()
   ]
