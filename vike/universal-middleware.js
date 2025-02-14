8c8ff051a4527a1889ee3bf08f2b5e823fc5bbd2
Romuald Brillout <git@brillout.com>
2025-02-14 13:46:41 +0100

    minor refactor: rename shared\/universal-middleware => node\/runtime\/universal-middleware

--- vike/universal-middleware.js
+++ vike/universal-middleware.js
@@ -1,3 +1,3 @@
 // Some tools still need this as of January 2025
-export * from './dist/esm/shared/universal-middleware.js'
-export { default } from './dist/esm/shared/universal-middleware.js'
+export * from './dist/esm/node/runtime/universal-middleware.js'
+export { default } from './dist/esm/node/runtime/universal-middleware.js'
