1d30e12db1a66d003736055b817db6f6664b8dfc
Joël Charles <joel.charles91@gmail.com>
2025-02-14 14:41:47 +0100

    feat: universal-middleware (#2183)
    Co-authored-by: Romuald Brillout <git@brillout.com>

---
+++ vike/node/runtime/universal-middleware.ts
@@ -0,0 +1,16 @@
+import { renderPage } from './index.js'
+
+export default async function universalVikeHandler(
+  request: Request,
+  context: Record<string, unknown>,
+  runtime: Record<string, unknown>
+) {
+  const pageContextInit = { ...context, ...runtime, urlOriginal: request.url, headersOriginal: request.headers }
+  const pageContext = await renderPage(pageContextInit)
+  const response = pageContext.httpResponse
+  const readable = response.getReadableWebStream()
+  return new Response(readable, {
+    status: response.statusCode,
+    headers: response.headers
+  })
+}
---
+++ vike/universal-middleware.js
@@ -0,0 +1,3 @@
+// Some tools still need this as of January 2025
+export * from './dist/esm/node/runtime/universal-middleware.js'
+export { default } from './dist/esm/node/runtime/universal-middleware.js'
