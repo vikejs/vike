43839717cfb824a95ca3112bc921667528e1c0d6
Romuald Brillout <git@brillout.com>
2025-02-14 13:57:02 +0100

    use ts

--- vike/node/runtime/universal-middleware.ts
+++ vike/node/runtime/universal-middleware.ts
@@ -1,6 +1,10 @@
 import { renderPage } from './index.js'
 
-export default async function universalVikeHandler(request: Request, context: any, runtime: any) {
+export default async function universalVikeHandler(
+  request: Request,
+  context: Record<string, unknown>,
+  runtime: Record<string, unknown>
+) {
   const pageContextInit = { ...context, ...runtime, urlOriginal: request.url, headersOriginal: request.headers }
   const pageContext = await renderPage(pageContextInit)
   const response = pageContext.httpResponse
