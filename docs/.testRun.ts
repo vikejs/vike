96482e09a602d28a22d9e621673ea127652c305b
Romuald Brillout <git@brillout.com>
2025-02-17 11:39:18 +0100

    udapte test

--- docs/.testRun.ts
+++ docs/.testRun.ts
@@ -1,26 +1,22 @@
 export { testRun }
 
-import { page, test, expect, run, fetchHtml, getServerUrl, partRegex } from '@brillout/test-e2e'
+import { page, test, expect, run, fetchHtml, getServerUrl } from '@brillout/test-e2e'
 
-const textDOM = 'Next GenerationFrontend Framework'
-const textHTML = partRegex`<h1 ${/[^\>]+/}>Next Generation<br/>Frontend Framework</h1>`
+const text = 'Choose between stable and cutting-edge extensions'
 
 function testRun(cmd: 'pnpm run dev' | 'pnpm run preview') {
-  {
-    // Preview => `npm run preview` takes a long time
-    // Dev => `Learn more collapsible` takes a long time
-    const additionalTimeout = 120 * 1000
-    run(cmd, { additionalTimeout })
-  }
+  run(cmd, {
+    additionalTimeout: cmd === 'pnpm run dev' ? undefined : 120 * 1000
+  })
 
   test('HTML', async () => {
     const html = await fetchHtml('/')
-    expect(html).toMatch(textHTML)
+    expect(html).toContain(text)
   })
 
   test('DOM', async () => {
     await page.goto(getServerUrl() + '/')
-    await page.waitForFunction(() => (window as any).__docpress_hydrationFinished)
-    expect(await page.textContent('h1')).toBe(textDOM)
+    await page.waitForFunction(() => !!(window as any)._vike.fullyRenderedUrl)
+    expect(await page.textContent('body')).toContain(text)
   })
 }
