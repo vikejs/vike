ef98a268d2ca72d85bee55eefb2b5ad5a0aa72ae
Romuald Brillout <git@brillout.com>
2025-02-19 20:41:50 +0100

    minor refactor

--- vike/dist-cjs-fixup.mjs
+++ vike/dist-cjs-fixup.mjs
@@ -22,8 +22,8 @@ async function shimImportMetaUrl() {
 
 async function replaceImportMetaWithFilename(filePath) {
   const fileContent = await fs.readFile(filePath, 'utf8')
-  const modifiedContent = fileContent.replaceAll('import.meta.url', '`file://${__filename}`')
-  await fs.writeFile(filePath, modifiedContent, 'utf8')
+  const fileContentMod = fileContent.replaceAll('import.meta.url', '`file://${__filename}`')
+  await fs.writeFile(filePath, fileContentMod, 'utf8')
 }
 
 async function getFiles(directoryPath) {
