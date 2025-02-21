6786214bde9cf0d3c74316a02d503ab67f9aa91a
Romuald Brillout <git@brillout.com>
2025-02-21 15:27:47 +0100

    wip

--- vike/node/plugin/plugins/build/pluginAssetsManifest.ts
+++ vike/node/plugin/plugins/build/pluginAssetsManifest.ts
@@ -1,7 +1,7 @@
 export { pluginAssetsManifest }
 
 import { isV1Design } from '../importUserCode/v1-design/getVikeConfig.js'
-import type { ResolvedConfig, Plugin, UserConfig } from 'vite'
+import type { ResolvedConfig, Plugin, UserConfig, Rollup, Environment } from 'vite'
 import fs from 'fs/promises'
 import path from 'path'
 import {
@@ -12,15 +12,17 @@ import {
   writeManifestFile
 } from '../build/pluginAssetsManifest/fixServerAssets.js'
 import { set_macro_ASSETS_MANIFEST } from './pluginBuildEntry.js'
-import { getOutDirs, type OutDirs } from '../../shared/getOutDirs.js'
+import { getOutDirs, OutDirs } from '../../shared/getOutDirs.js'
 import { viteIsSSR } from '../../shared/viteIsSSR.js'
 import { getVikeConfigPublic } from '../commonConfig.js'
+import { assert, assertIsSingleModuleInstance } from '../../utils.js'
+type Bundle = Rollup.OutputBundle
+type Options = Rollup.NormalizedOutputOptions
 const manifestTempFile = '_temp_manifest.json'
 
 function pluginAssetsManifest(): Plugin[] {
   let isServerAssetsFixEnabled: boolean
   let config: ResolvedConfig
-  let assetsJsonFilePath: string
 
   return [
     {
@@ -63,28 +65,6 @@ function pluginAssetsManifest(): Plugin[] {
     {
       name: 'vike:build:pluginAssetsManifest:pre',
       apply: 'build',
-      // Compatiblity with Environment API. It replaces `vike:build:pluginAssetsManifest:pre` when compatible
-      // See https://vite.dev/guide/api-environment-plugins.html#per-environment-plugins
-      applyToEnvironment() {
-        return {
-          name: 'vike:build:pluginAssetsManifest:pre:env-api-compat',
-          apply: 'build',
-          enforce: 'pre',
-          writeBundle: {
-            order: 'pre',
-            sequential: true,
-            async handler(options, bundle) {
-              if (this.environment.name === 'ssr') {
-                await writeAssetsManifestFile(getOutDirs(this.environment.config))
-              }
-              if (viteIsSSR(this.environment.config)) {
-                // Replace __VITE_ASSETS_MANIFEST__ in all server-side bundles
-                await set_macro_ASSETS_MANIFEST(options, bundle, assetsJsonFilePath)
-              }
-            }
-          }
-        }
-      },
       // Ensures that we can reuse `assetsJsonFilePath`
       sharedDuringBuild: true,
       // Make sure other writeBundle() hooks are called after this writeBundle() hook.
@@ -95,17 +75,40 @@ function pluginAssetsManifest(): Plugin[] {
         order: 'pre',
         sequential: true,
         async handler(options, bundle) {
-          if (viteIsSSR(config)) {
-            await writeAssetsManifestFile(getOutDirs(config))
-            await set_macro_ASSETS_MANIFEST(options, bundle, assetsJsonFilePath)
-          }
+          handleAssetsManifest(config, this.environment, options, bundle)
         }
       }
     }
   ]
+}
 
-  async function writeAssetsManifestFile(outDirs: OutDirs) {
+assertIsSingleModuleInstance('ewq/TODO.ts')
+let assetsJsonFilePath: string | undefined
+
+async function handleAssetsManifest(
+  config: ResolvedConfig,
+  viteEnv: Environment | undefined,
+  options: Options,
+  bundle: Bundle
+) {
+  const configEnv = viteEnv?.config ?? config
+  const isServerBuild = viteIsSSR(configEnv)
+  const isServerSideBuild = viteEnv ? viteEnv.name === 'ssr' : isServerBuild
+  const outDirs = getOutDirs(viteEnv?.config ?? config)
+  if (isServerBuild) {
+    assert(!assetsJsonFilePath)
     assetsJsonFilePath = path.posix.join(outDirs.outDirRoot, 'assets.json')
+    await writeAssetsManifestFile(outDirs, assetsJsonFilePath, config)
+  }
+  if (isServerSideBuild) {
+    assert(assetsJsonFilePath)
+    // Replace __VITE_ASSETS_MANIFEST__ in all server-side bundles
+    await set_macro_ASSETS_MANIFEST(options, bundle, assetsJsonFilePath)
+  }
+}
+
+  async function writeAssetsManifestFile(outDirs: OutDirs, assetsJsonFilePath: string, config: ResolvedConfig) {
+    const isServerAssetsFixEnabled = fixServerAssets_isEnabled() && (await isV1Design(config))
     const clientManifestFilePath = path.posix.join(outDirs.outDirClient, manifestTempFile)
     const serverManifestFilePath = path.posix.join(outDirs.outDirServer, manifestTempFile)
     if (!isServerAssetsFixEnabled) {
@@ -117,4 +120,3 @@ function pluginAssetsManifest(): Plugin[] {
     await fs.rm(clientManifestFilePath)
     await fs.rm(serverManifestFilePath)
   }
-}
