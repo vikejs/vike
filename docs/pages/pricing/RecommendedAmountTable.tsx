d56639e80e80eb30e4628e8a711d24833f43ec17
Romuald Brillout <git@brillout.com>
2025-03-05 13:23:03 +0100

    chatgpt

--- docs/pages/pricing/RecommendedAmountTable.tsx
+++ docs/pages/pricing/RecommendedAmountTable.tsx
@@ -2,41 +2,42 @@ import React from 'react';
 import './RecommendedAmountTable.css';
 
 const pricingData = [
-  { 
-    type: '≤2 regular committers', 
-    small: 'Free', 
-    midsize: 'Free', 
-    large: 'Free' 
+  {
+    type: '≤2 regular committers',
+    small: 'Free',
+    midsize: 'Free',
+    large: 'Free'
   },
-  { 
-    type: 'Hobby use<wbr></wbr> case ≥3 regular committers', 
-    small: 'Free', 
-    midsize: 'Free', 
-    large: 'Free' 
+  {
+    type: 'Hobby use<wbr></wbr> case',
+    small: 'Free',
+    midsize: 'Free',
+    large: 'Free'
   },
-  { 
-    type: 'Small use<wbr></wbr> case ≥3 regular committers', 
-    small: '0-50$<wbr></wbr> / month', 
-    midsize: '50-100$<wbr></wbr> / month', 
-    large: '100-200$<wbr></wbr> / month' 
+  {
+    type: 'Small use<wbr></wbr> case',
+    small: '1$ - 50$<wbr></wbr> / month',
+    midsize: '1$ - 100$<wbr></wbr> / month',
+    large: '1$ - 200$<wbr></wbr> / month'
   },
-  { 
-    type: 'Midsize use<wbr></wbr> case ≥3 regular committers', 
-    small: '50-100$<wbr></wbr> / month', 
-    midsize: '100-200$<wbr></wbr> / month', 
-    large: '200-500$<wbr></wbr> / month' 
+  {
+    type: 'Midsize use<wbr></wbr> case',
+    small: '1$ - 100$<wbr></wbr> / month',
+    midsize: '100$ - 200$<wbr></wbr> / month',
+    large: '200$ - 500$<wbr></wbr> / month'
   },
-  { 
-    type: 'Large use<wbr></wbr> case ≥3 regular committers', 
-    small: '100-200$<wbr></wbr> / month', 
-    midsize: '200-500$<wbr></wbr> / month', 
-    large: '500-2000$<wbr></wbr> / month' 
+  {
+    type: 'Large use<wbr></wbr> case',
+    small: '100$ - 200$<wbr></wbr> / month',
+    midsize: '200$ - 500$<wbr></wbr> / month',
+    large: '500$ - 2000$<wbr></wbr> / month'
   },
 ];
 
 const RecommendedAmountTable = () => {
   return (
     <div className="table-container">
+      <h2 className="table-title">Who pays and how much?</h2>
       <table className="pricing-table">
         <thead>
           <tr>
@@ -50,21 +51,42 @@ const RecommendedAmountTable = () => {
           {pricingData.map((row, index) => (
             <tr key={index}>
               <td className="row-header-cell">
-                <div dangerouslySetInnerHTML={{ 
-                  __html: row.type.replace(/≥3/g, '<span class="break-mobile">≥3</span>') 
-                }} />
+                {/* Safely render the row type with possible <wbr> tags */}
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: row.type
+                  }}
+                />
               </td>
               <td className="price-cell">
-                {row.small}
-                {row.small !== 'Free' && <div className="recommendation">Recommendation</div>}
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: row.small
+                  }}
+                />
+                {row.small !== 'Free' && (
+                  <div className="recommendation">Recommendation</div>
+                )}
               </td>
               <td className="price-cell">
-                {row.midsize}
-                {row.midsize !== 'Free' && <div className="recommendation">Recommendation</div>}
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: row.midsize
+                  }}
+                />
+                {row.midsize !== 'Free' && (
+                  <div className="recommendation">Recommendation</div>
+                )}
               </td>
               <td className="price-cell">
-                {row.large}
-                {row.large !== 'Free' && <div className="recommendation">Recommendation</div>}
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: row.large
+                  }}
+                />
+                {row.large !== 'Free' && (
+                  <div className="recommendation">Recommendation</div>
+                )}
               </td>
             </tr>
           ))}
