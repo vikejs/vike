3b811b410c5d0c98135463bdd4bab6b829b395f9
Romuald Brillout <git@brillout.com>
2025-03-17 16:59:45 +0100

    comment

--- vike/utils/objectAssign.ts
+++ vike/utils/objectAssign.ts
@@ -13,8 +13,8 @@ function objectAssign<Obj extends object, ObjAddendum extends object | null>(
   if (objAddendum) {
     if (!objAddendumCanBePageContextObject) {
       // We only need this assert() in the rare case when the user is expected to mutate `pageContext` after the Vike hook was executed (and its promise resolved).
-      // - The only use case I can think of is the user mutating `pageContext` after the onRenderClient() promise resolved (which can happen when client-side rendering finishes after onRenderClient() resolves). In that use case, having Vike await async Vike hooks isn't enough.
-      // - IIRC it was mostly needed for preserving the getters added by getPageContextUrlComputed() but we don't need this anymore
+      // - The only use case I can think of is the user mutating `pageContext` after the onRenderClient() promise resolved (which can happen when client-side rendering finishes after onRenderClient() resolves). In that case, having Vike await async Vike hooks isn't enough.
+      // - IIRC this assert() was mostly needed for preserving the getters added by getPageContextUrlComputed() but we don't need this anymore.
       assert(!('_isPageContextObject' in objAddendum))
     }
     Object.defineProperties(obj, Object.getOwnPropertyDescriptors(objAddendum))
