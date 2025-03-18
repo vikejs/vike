a6f48028ce5ce01bddedb39cc8341fa34da8abf3
Rom <git@brillout.com>
2025-03-18 16:17:08 +0100

    feat: `passToClient` nested props (#2278)

---
+++ vike/node/runtime/html/serializePageContextClientSide.spec.ts
@@ -0,0 +1,50 @@
+import { describe, it, expect } from 'vitest'
+import { getPropKeys } from './serializePageContextClientSide'
+
+describe('getPropKeys', () => {
+  it('splits on dots when no escape is present', () => {
+    expect(getPropKeys('foo.bar.baz')).toEqual(['foo', 'bar', 'baz'])
+  })
+
+  it('preserves escaped dots (default \\ escape)', () => {
+    expect(getPropKeys('foo\\.bar.baz')).toEqual(['foo.bar', 'baz'])
+  })
+
+  it('handles multiple escaped dots', () => {
+    expect(getPropKeys('foo\\.bar\\.baz.qux')).toEqual(['foo.bar.baz', 'qux'])
+  })
+
+  it('does not split on double escape sequences', () => {
+    expect('a\\\\b').toEqual('a\\\\b')
+    expect(getPropKeys('a\\\\b')).toEqual(['a\\\\b'])
+    expect(getPropKeys('a\\\\.b')).toEqual(['a\\.b'])
+    expect(getPropKeys('a\\\\\\.b')).toEqual(['a\\\\.b'])
+    expect(getPropKeys('a\\\\\\\\.b')).toEqual(['a\\\\\\.b'])
+
+    expect(getPropKeys('foo\\\\.bar')).toEqual(['foo\\.bar'])
+    expect('foo\\\\.bar').toEqual('foo\\\\.bar')
+    expect(getPropKeys('foo\\\\.bar')).toEqual(['foo\\.bar'])
+    expect('foo\\\\.bar.baz').toEqual('foo\\\\.bar.baz')
+    expect(getPropKeys('foo\\\\.bar.baz')).toEqual(['foo\\.bar', 'baz'])
+  })
+
+  it('handles input without dots correctly', () => {
+    expect(getPropKeys('foobar')).toEqual(['foobar'])
+  })
+
+  it('handles leading and trailing dots', () => {
+    expect(getPropKeys('.foo.bar.')).toEqual(['', 'foo', 'bar', ''])
+  })
+
+  it('handles consecutive dots', () => {
+    expect(getPropKeys('foo..bar')).toEqual(['foo', '', 'bar'])
+  })
+
+  it('handles only escaped dots', () => {
+    expect(getPropKeys('foo\\.bar\\.baz')).toEqual(['foo.bar.baz'])
+  })
+
+  it('supports escaping dots at start and end', () => {
+    expect(getPropKeys('\\.foo\\.bar\\.')).toEqual(['.foo.bar.'])
+  })
+})
