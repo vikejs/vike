b1a24bdeedeb3044586857792026adacdbf17701
Romuald Brillout <git@brillout.com>
2025-03-25 10:16:30 +0100

    docs

--- vike/shared/page-configs/Config.ts
+++ vike/shared/page-configs/Config.ts
@@ -373,7 +373,11 @@ type ConfigBuiltIn = {
          */
         enable?: boolean | null
         /**
-         * TODO/now
+         * If you pre-render all your pages then Vike removes the `dist/server/` directory after pre-rendering has finished.
+         *
+         * If you set this setting to `true` then Vike won't remove the `dist/server/` directory.
+         *
+         * https://vike.dev/prerender#keepDistServer
          *
          * @default false
          */
