083c149741c5dbda0ef658168a4679c911ef87fd
Romuald Brillout <git@brillout.com>
2025-04-01 11:11:02 +0200

    comment

--- vike/node/plugin/plugins/importUserCode/v1-design/getVikeConfig/resolvePointerImport.ts
+++ vike/node/plugin/plugins/importUserCode/v1-design/getVikeConfig/resolvePointerImport.ts
@@ -122,8 +122,6 @@ function resolveImportPathWithNode(
   const importerFilePathAbsolute = importerFilePath.filePathAbsoluteFilesystem
   assertPosixPath(importerFilePathAbsolute)
   const cwd = path.posix.dirname(importerFilePathAbsolute)
-  // We still can't use import.meta.resolve() as of 23.1.0 (November 2024) because `parent` argument requires an experimental flag.
-  // - https://stackoverflow.com/questions/54977743/do-require-resolve-for-es-modules#comment139581675_62272600
   // filePathAbsoluteFilesystem is expected to be null when pointerImportData.importPath is a Vite path alias
   const filePathAbsoluteFilesystem = requireResolve(pointerImportData.importPath, cwd)
   return filePathAbsoluteFilesystem
