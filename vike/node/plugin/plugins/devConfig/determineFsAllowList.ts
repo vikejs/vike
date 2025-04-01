ff6dcd97220f5f36658ec3af636f51134ad36db5
Romuald Brillout <git@brillout.com>
2025-04-01 11:17:59 +0200

    fix

--- vike/node/plugin/plugins/devConfig/determineFsAllowList.ts
+++ vike/node/plugin/plugins/devConfig/determineFsAllowList.ts
@@ -2,13 +2,14 @@ export { determineFsAllowList }
 
 import { searchForWorkspaceRoot } from 'vite'
 import type { ResolvedConfig } from 'vite'
-import path from 'path'
-import { assert, requireResolveNonUserFile } from '../../utils.js'
-import { dirname } from 'path'
+import path from 'node:path'
+import { assert, assertPosixPath, requireResolveNonUserFile } from '../../utils.js'
 import { fileURLToPath } from 'url'
 // @ts-ignore import.meta.url is shimmed at dist/cjs by dist-cjs-fixup.js.
 const importMetaUrl: string = import.meta.url
-const __dirname_ = dirname(fileURLToPath(importMetaUrl))
+assertPosixPath(importMetaUrl)
+const __dirname_ = path.posix.dirname(fileURLToPath(importMetaUrl))
+assertPosixPath(__dirname_)
 
 async function determineFsAllowList(config: ResolvedConfig) {
   const fsAllow = config.server.fs.allow
@@ -25,7 +26,7 @@ async function determineFsAllowList(config: ResolvedConfig) {
   // Add node_modules/vike/
   {
     // [RELATIVE_PATH_FROM_DIST] Current directory: node_modules/vike/dist/esm/node/plugin/plugins/config/
-    const vikeRoot = path.join(__dirname_, '../../../../../../')
+    const vikeRoot = path.posix.join(__dirname_, '../../../../../../')
     // Assert that `vikeRoot` is indeed pointing to `node_modules/vike/`
     requireResolveNonUserFile(`${vikeRoot}/dist/esm/node/plugin/plugins/devConfig/index.js`, importMetaUrl)
     fsAllow.push(vikeRoot)
