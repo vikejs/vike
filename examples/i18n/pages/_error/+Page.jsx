bd0ee296effa783eda196a1e46d693f656f36d6c
Romuald Brillout <git@brillout.com>
2025-04-01 23:57:02 +0200

    error page

---
+++ examples/i18n/pages/_error/+Page.jsx
@@ -0,0 +1,32 @@
+export { Page }
+
+import React from 'react'
+import { usePageContext } from '../../renderer/usePageContext'
+
+function Page() {
+  const pageContext = usePageContext()
+  let { is404, abortReason } = pageContext
+  if (!abortReason) {
+    abortReason = is404 ? 'Page not found' : 'Something went wrong'
+  }
+  return (
+    <Center>
+      <p style={{ fontSize: '1.3em' }}>{abortReason}</p>
+    </Center>
+  )
+}
+
+function Center({ style, ...props }) {
+  return (
+    <div
+      style={{
+        height: 'calc(100vh - 100px)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        ...style
+      }}
+      {...props}
+    ></div>
+  )
+}
