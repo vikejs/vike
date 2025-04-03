d08277302bd7f421d34f7a224ac312425c3d3874
Romuald Brillout <git@brillout.com>
2025-04-03 17:24:39 +0200

    fix: simplify and fix vike:pluginModuleBanner (fix #2338)

--- vike/node/plugin/plugins/build/pluginModuleBanner.ts
+++ vike/node/plugin/plugins/build/pluginModuleBanner.ts
@@ -2,18 +2,14 @@ export { pluginModuleBanner }
 
 import type { ResolvedConfig, Plugin } from 'vite'
 import MagicString from 'magic-string'
-import { assert } from '../../utils.js'
 import { removeVirtualIdTag } from '../../../shared/virtual-files.js'
-import { isViteServerBuild, isViteServerBuild_safe } from '../../shared/isViteServerBuild.js'
+import { isViteServerBuild_safe } from '../../shared/isViteServerBuild.js'
 
 // Rollup's banner feature doesn't work with Vite: https://github.com/vitejs/vite/issues/8412
 // But, anyways, we want to prepend the banner at the beginning of each module, not at the beginning of each file (I believe that's what Rollup's banner feature does).
 
-const vikeModuleBannerPlaceholder = 'vikeModuleBannerPlaceholder'
-
 function pluginModuleBanner(): Plugin {
   let config: ResolvedConfig
-  let disabled = false
 
   return {
     name: 'vike:pluginModuleBanner',
@@ -21,56 +17,19 @@ function pluginModuleBanner(): Plugin {
     apply: 'build',
     configResolved(config_) {
       config = config_
-      if (config.build.minify && config.build.minify !== 'esbuild') {
-        // Doesn't work upon `config.build.minify === 'terser'`
-        // https://github.com/vikejs/vike/issues/2315
-        disabled = true
-      }
-    },
-    generateBundle: {
-      order: 'post',
-      handler(_options, bundle) {
-        if (disabled) return
-        for (const module of Object.values(bundle)) {
-          if (module.type === 'chunk') {
-            if (isViteServerBuild(config)) {
-              const codeOld = module.code
-              const codeNew = codeOld.replace(
-                /vikeModuleBannerPlaceholder\("([^"]*)"\);/g,
-                '/* $1 [vike:pluginModuleBanner] */'
-              )
-
-              // Ensure we don't break source map
-              {
-                const l1 = 'vikeModuleBannerPlaceholder("");'.length
-                const l2 = '/*  [vike:pluginModuleBanner] */'.length
-                assert(l1 === l2)
-                assert(codeNew.length === codeOld.length)
-              }
-
-              if (codeNew.includes(vikeModuleBannerPlaceholder)) {
-                console.log('codeNew', codeNew)
-                assert(false)
-              }
-              module.code = codeNew
-            } else {
-              assert(!module.code.includes(vikeModuleBannerPlaceholder))
-            }
-          }
-        }
-      }
     },
     transform: {
       order: 'post',
       handler(code, id, options) {
-        if (disabled) return
         if (!isViteServerBuild_safe(config, options)) return
         if (id.startsWith('\0')) id = id
         id = removeVirtualIdTag(id)
         if (id.startsWith(config.root)) id = id.slice(config.root.length + 1)
         const s = new MagicString(code)
-        // No need to insert a new line; Rollup formats the code and will insert a new line.
-        s.prepend(`${vikeModuleBannerPlaceholder}(${JSON.stringify(id)}); `)
+        // Use legal comment so that esbuild doesn't remove it.
+        // - Terser still removes the comment, but I guess users use terser to minify JavaScript so I guess it's a good thing that comment is removed.
+        // - https://esbuild.github.io/api/#legal-comments
+        s.prepend(`/*! ${id} [vike:pluginModuleBanner] */\n`)
         return {
           code: s.toString(),
           map: s.generateMap({ hires: true, source: id })
