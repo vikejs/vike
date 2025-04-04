4b149b7985fb029224801cdf75d181dfb121aa01
brillout <github.com@brillout.com>
2025-04-04 09:56:14 +0000

    Deploying to gh-pages from @ vikejs/vike@a60100fa14e774bbc50c040ef2aaed54084f2068 🚀

---
+++ assets/entries/pages_vuetify.CzhrY62d.js
@@ -0,0 +1,16 @@
+import{j as e,i as a,L as n,o as l}from"../chunks/chunk-D_9sJncj.js";import"../chunks/chunk-DfYVyrH-.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import{C as s}from"../chunks/chunk-IRV4H-0g.js";import{E as i}from"../chunks/chunk-zAsiT6PE.js";/* empty css                      *//* empty css                      */import"../chunks/chunk-DEAtuRVl.js";import"../chunks/chunk-BNGjDYoD.js";const p=[];function r(o){const t={a:"a",blockquote:"blockquote",li:"li",p:"p",ul:"ul",...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{tool:"vuetify",url:"https://vuetifyjs.com",hasExtension:!1}),`
+`,e.jsx(t.p,{children:"Examples of using Vike with Vuetify:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(i,{timestamp:"2024.03",repo:"IT4Change/boilerplate-frontend"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(i,{timestamp:"2022.12",repo:"brillout/vite-plugin-ssr-vuetify"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(i,{timestamp:"2021.11",repo:"jamesladd/aws-vite-ssr-vuetify"}),`
+`]}),`
+`]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:["vite-plugin-ssr was the ",e.jsx(t.a,{href:"https://vite-plugin-ssr.com/vike",children:"previous name of Vike"}),"."]}),`
+`]})]})}function d(o={}){const{wrapper:t}=o.components||{};return t?e.jsx(t,{...o,children:e.jsx(r,{...o})}):r(o)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:d,pageSectionsExport:p},Symbol.toStringTag,{value:"Module"})),P={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:l}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/vuetify/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:u}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:n}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:a}}};export{P as configValuesSerialized};
