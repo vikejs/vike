64aaefaea02a9e5dc25000d1536da3c5041548e2
brillout <github.com@brillout.com>
2025-04-04 06:51:47 +0000

    Deploying to gh-pages from @ vikejs/vike@b254b94defa5f03f27076b26b4ab2a9e6588dfaf 🚀

---
+++ assets/entries/pages_socket-io.Bks02p7N.js
@@ -0,0 +1,14 @@
+import{j as e,i as r,L as a,o as n}from"../chunks/chunk-CgU9kwc-.js";import"../chunks/chunk-8qobm1k9.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import{C as l}from"../chunks/chunk-HFH8esNo.js";import{E as s}from"../chunks/chunk-DrsmcFKQ.js";/* empty css                      *//* empty css                      */import"../chunks/chunk-DP_TVwis.js";import"../chunks/chunk-BzgzbFY_.js";const p=[];function i(o){const t={a:"a",blockquote:"blockquote",li:"li",p:"p",ul:"ul",...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(l,{url:"https://socket.io"}),`
+`,e.jsx(t.p,{children:"Example of using Vike with Socket.IO:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(s,{timestamp:"2023.05",repo:"phiberber/vps-react-socket-io-v1"}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:["vite-plugin-ssr was the ",e.jsx(t.a,{href:"https://vite-plugin-ssr.com/vike",children:"previous name of Vike"}),"."]}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(t.p,{children:"See also:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://github.com/vikejs/vike/discussions/906",children:"#906 - WebSocket / Socket.IO implementation"})}),`
+`]})]})}function d(o={}){const{wrapper:t}=o.components||{};return t?e.jsx(t,{...o,children:e.jsx(i,{...o})}):i(o)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:d,pageSectionsExport:p},Symbol.toStringTag,{value:"Module"})),P={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:n}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/socket-io/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:u}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:a}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:r}}};export{P as configValuesSerialized};
