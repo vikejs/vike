67ae01fbcd4a72ab5478289f18980185684bc31e
brillout <github.com@brillout.com>
2025-04-07 16:49:51 +0000

    Deploying to gh-pages from @ vikejs/vike@9ed151c514988d840a41b9f37141e8cd429aa0c8 🚀

---
+++ assets/entries/pages_netlify-functions.AQBB3FqS.js
@@ -0,0 +1,16 @@
+import{j as e,i as n,L as a,o as l}from"../chunks/chunk-Bk-4JIbo.js";import{L as s}from"../chunks/chunk-BQ8eiXkO.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-BuumvMGq.js";import{E as i}from"../chunks/chunk-B8owcjXs.js";/* empty css                      *//* empty css                      */const p=[];function r(o){const t={a:"a",blockquote:"blockquote",li:"li",p:"p",ul:"ul",...o.components};return e.jsxs(e.Fragment,{children:[e.jsxs(t.p,{children:["You can deploy your Vike app to ",e.jsx(t.a,{href:"https://www.netlify.com/products/functions/",children:"Netlify Functions"}),"."]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:["For deploying a pre-rendered app to Netlify, see ",e.jsx(s,{href:"/netlify"})," instead."]}),`
+`]}),`
+`,e.jsx(t.p,{children:"Example:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(i,{timestamp:"2023.09",repo:"brunsten/vike-react-netlify"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(i,{timestamp:"2022.07",repo:"AaronBeaudoin/vite-plugin-ssr-example-netlify"}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:["vite-plugin-ssr was the ",e.jsx(t.a,{href:"https://vite-plugin-ssr.com/vike",children:"previous name of Vike"}),"."]}),`
+`]}),`
+`]}),`
+`]})]})}function d(o={}){const{wrapper:t}=o.components||{};return t?e.jsx(t,{...o,children:e.jsx(r,{...o})}):r(o)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:d,pageSectionsExport:p},Symbol.toStringTag,{value:"Module"})),b={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:l}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/netlify-functions/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:u}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:a}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:n}}};export{b as configValuesSerialized};
