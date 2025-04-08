82ea4311e8de523e2f870e2e85ca8b926b6b0ded
brillout <github.com@brillout.com>
2025-04-08 11:15:10 +0000

    Deploying to gh-pages from @ vikejs/vike@1ea91e022109fbb684ae0bb925b33f1050c91a46 🚀

---
+++ assets/entries/pages_client.CPLmQr2T.js
@@ -0,0 +1,59 @@
+import{j as e,b as s,i as l,L as o,o as a}from"../chunks/chunk-CKzAuXwH.js";import{L as i}from"../chunks/chunk-DUsA13Od.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-VLiKoQz4.js";/* empty css                      *//* empty css                      */const d=[{pageSectionId:"examples",pageSectionLevel:2,pageSectionTitle:"Examples"},{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function r(t){const n={a:"a",blockquote:"blockquote",code:"code",figure:"figure",li:"li",p:"p",pre:"pre",span:"span",ul:"ul",...t.components};return e.jsxs(e.Fragment,{children:[e.jsxs(n.p,{children:["You can define a ",e.jsx(n.code,{children:"+client.js"})," file to add client-side JavaScript."]}),`
+`,e.jsxs(n.p,{children:["It's usually used for initializing ",e.jsx(i,{href:"/error-tracking",children:"error tracking"}),", user tracking, cookie consent, CSS frameworks, etc."]}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"js","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"js","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"// /pages/+client.js"})}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"// Environment: client"})}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#24292E"},children:"console."}),e.jsx(n.span,{style:{color:"#6F42C1"},children:"log"}),e.jsx(n.span,{style:{color:"#24292E"},children:"("}),e.jsx(n.span,{style:{color:"#032F62"},children:`"I'm the first line of client-side JavaScript."`}),e.jsx(n.span,{style:{color:"#24292E"},children:")"})]})]})})}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"In production, it's the very first JavaScript code to be executed. In development, it's run first after Vite's HMR preamble."}),`
+`]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The code of ",e.jsx(n.code,{children:"+client.js"})," isn't re-executed upon client-side page navigation. For executing code upon the lifecycle of page rendering, see following hooks instead:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(i,{href:"/onHydrationEnd"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(i,{href:"/onAfterRenderClient"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(i,{href:"/onBeforeRenderClient"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(i,{href:"/onPageTransitionStart"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(i,{href:"/onPageTransitionEnd"}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["Currently, ",e.jsx(n.code,{children:"+client.js"})," isn't cumulative but it's work-in-progress, see ",e.jsxs(n.a,{href:"https://github.com/vikejs/vike/issues/1834",children:["1834 - Make ",e.jsx(n.code,{children:"+client.js"})," cumulative"]}),"."]}),`
+`]}),`
+`,e.jsx("h2",{id:"examples",children:"Examples"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(s,{path:"/examples/render-modes/pages/html-js/+client.js"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(i,{href:"/render-modes#html-only"}),`
+`]}),`
+`]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(i,{href:"/onHydrationEnd"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(i,{href:"/onAfterRenderClient"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(i,{href:"/onBeforeRenderClient"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(i,{href:"/onPageTransitionStart"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(i,{href:"/onPageTransitionEnd"}),`
+`]}),`
+`]})]})}function c(t={}){const{wrapper:n}=t.components||{};return n?e.jsx(n,{...t,children:e.jsx(r,{...t})}):r(t)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:c,pageSectionsExport:d},Symbol.toStringTag,{value:"Module"})),E={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:a}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/client/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:p}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:o}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:l}}};export{E as configValuesSerialized};
---
+++ assets/entries/pages_tailwind-css.CCPN8xNw.js
@@ -0,0 +1,22 @@
+import{j as e,i as n,L as l,o as r}from"../chunks/chunk-CKzAuXwH.js";import"../chunks/chunk-VLiKoQz4.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import{U as s}from"../chunks/chunk-CdnTTOix.js";import{C as d}from"../chunks/chunk-C7pJ0did.js";import{E as o}from"../chunks/chunk-DSEb_JYD.js";/* empty css                      *//* empty css                      */import"../chunks/chunk-DUsA13Od.js";import"../chunks/chunk-DhmBXfWr.js";const p=[{pageSectionId:"examples",pageSectionLevel:2,pageSectionTitle:"Examples"},{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function a(i){const t={a:"a",blockquote:"blockquote",code:"code",li:"li",p:"p",ul:"ul",...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(d,{url:"https://tailwindcss.com"}),`
+`,e.jsx(s,{children:"Tailwind CSS"}),`
+`,e.jsx("h2",{id:"examples",children:"Examples"}),`
+`,e.jsx(t.p,{children:"Minimal example using Vike + Tailwind CSS:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(o,{timestamp:"2022.08",repo:"brillout/vite-plugin-ssr_tailwind-css"}),`
+`]}),`
+`]}),`
+`,e.jsxs(t.p,{children:["Example using Vike + Storeon + ",e.jsx(t.code,{children:"@phensley/cldr"})," + TailwindCSS + PostCSS + ",e.jsx(t.code,{children:"schema-dts"}),":"]}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(o,{timestamp:"2021.10",repo:"mate-h/vite-ssr-web"}),`
+`]}),`
+`]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:["vite-plugin-ssr was the ",e.jsx(t.a,{href:"https://vite-plugin-ssr.com/vike",children:"previous name of Vike"}),"."]}),`
+`]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://tailwindcss.com/docs/guides/vite",children:"Tailwind > Vite Installation Guide"})}),`
+`]})]})}function u(i={}){const{wrapper:t}=i.components||{};return t?e.jsx(t,{...i,children:e.jsx(a,{...i})}):a(i)}const c=Object.freeze(Object.defineProperty({__proto__:null,default:u,pageSectionsExport:p},Symbol.toStringTag,{value:"Module"})),U={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:r}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/tailwind-css/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:c}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:l}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:n}}};export{U as configValuesSerialized};
