a6f103347955c514bb0799e021751560691ae0d1
Romuald Brillout <git@brillout.com>
2025-04-09 19:07:14 +0200

    fix: ignore .history/ from crawling

--- vike/node/plugin/plugins/importUserCode/v1-design/getVikeConfig/crawlPlusFiles/ignorePatternsBuiltIn.ts
+++ vike/node/plugin/plugins/importUserCode/v1-design/getVikeConfig/crawlPlusFiles/ignorePatternsBuiltIn.ts
@@ -8,6 +8,8 @@ export const ignorePatternsBuiltIn = [
   // +Page.telefunc.js
   // ```
   '**/*.telefunc.*',
+  // https://github.com/vikejs/vike/issues/1589#issuecomment-2031925598
+  '**/.history/**',
   // https://github.com/vikejs/vike/discussions/2222
   '**/*.generated.*'
 ] as const
