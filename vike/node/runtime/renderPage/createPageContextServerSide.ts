6ed0c64db0feed91e6bf303f0ed07cb3f8a5bc58
Romuald Brillout <git@brillout.com>
2025-04-15 10:13:30 +0200

    minor refactor: rename PageContextServerSideCreated => PageContextCreatedServerSide

--- vike/node/runtime/renderPage/createPageContextServerSide.ts
+++ vike/node/runtime/renderPage/createPageContextServerSide.ts
@@ -1,13 +1,13 @@
 export { createPageContextServerSide }
 export { createPageContextServerSideWithoutGlobalContext }
-export type { PageContextServerSideCreated }
+export type { PageContextCreatedServerSide }
 
 import { assert, assertUsage, assertWarning, normalizeHeaders, objectAssign } from '../utils.js'
 import { getPageContextUrlComputed } from '../../../shared/getPageContextUrlComputed.js'
 import type { GlobalContextInternal } from '../globalContext.js'
 import type { PageContextInit } from '../renderPage.js'
 
-type PageContextServerSideCreated = Awaited<ReturnType<typeof createPageContextServerSide>>
+type PageContextCreatedServerSide = Awaited<ReturnType<typeof createPageContextServerSide>>
 async function createPageContextServerSide(
   pageContextInit: PageContextInit,
   globalContext: GlobalContextInternal,
