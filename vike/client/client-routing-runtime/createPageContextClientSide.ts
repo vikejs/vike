a2f68a03e7aa8f5b8af3aeaed4dc31ec0f176424
Romuald Brillout <git@brillout.com>
2025-04-15 23:13:08 +0200

    refactor: globalContext client-routing-runtime

--- vike/client/client-routing-runtime/createPageContextClientSide.ts
+++ vike/client/client-routing-runtime/createPageContextClientSide.ts
@@ -1,41 +1,26 @@
 export { createPageContextClientSide }
 
 import { createPageContextShared } from '../../shared/createPageContextShared.js'
-import { getPageConfigsRuntime } from '../../shared/getPageConfigsRuntime.js'
 import { getPageContextUrlComputed } from '../../shared/getPageContextUrlComputed.js'
-import { loadPageRoutes } from '../../shared/route/loadPageRoutes.js'
 import { getBaseServer } from './getBaseServer.js'
 import { getGlobalContext } from './globalContextClientSide.js'
 import { assert, augmentType, isBaseServer, objectAssign } from './utils.js'
 
-// @ts-ignore
-import * as virtualFileExports from 'virtual:vike:importUserCode:client:client-routing'
-const { pageFilesAll, allPageIds, pageConfigs, pageConfigGlobal } = getPageConfigsRuntime(virtualFileExports)
-
 async function createPageContextClientSide(urlOriginal: string) {
-  const { pageRoutes, onBeforeRouteHook } = await loadPageRoutes(
-    pageFilesAll,
-    pageConfigs,
-    pageConfigGlobal,
-    allPageIds
-  )
+  const globalContext = await getGlobalContext()
 
   const baseServer = getBaseServer()
   assert(isBaseServer(baseServer))
+
   const pageContextCreated = {
     isClientSide: true,
     isPrerendering: false,
     urlOriginal,
-    globalContext: getGlobalContext(),
+    globalContext,
+    ...globalContext,
     _urlHandler: null,
     _urlRewrite: null,
-    _baseServer: baseServer,
-    _pageFilesAll: pageFilesAll,
-    _pageConfigs: pageConfigs,
-    _pageConfigGlobal: pageConfigGlobal,
-    _allPageIds: allPageIds,
-    _pageRoutes: pageRoutes,
-    _onBeforeRouteHook: onBeforeRouteHook
+    _baseServer: baseServer
   }
   const pageContextUrlComputed = getPageContextUrlComputed(pageContextCreated)
   objectAssign(pageContextCreated, pageContextUrlComputed)
--- vike/client/client-routing-runtime/globalContextClientSide.ts
+++ vike/client/client-routing-runtime/globalContextClientSide.ts
@@ -1,17 +1,44 @@
 export { getGlobalContext }
-export type { GlobalContextPublicClientSide }
+export type { GlobalContextClientSidePublic }
 
+import { getPageConfigsRuntime } from '../../shared/getPageConfigsRuntime.js'
+import { loadPageRoutes } from '../../shared/route/loadPageRoutes.js'
 import { getGlobalObject } from './utils.js'
 
-type GlobalContex = Record<string, unknown>
-// Same type for now
-type GlobalContextPublicClientSide = GlobalContex
+// @ts-ignore
+import * as virtualFileExports from 'virtual:vike:importUserCode:client:client-routing'
+const { pageFilesAll, allPageIds, pageConfigs, pageConfigGlobal } = getPageConfigsRuntime(virtualFileExports)
+
+type GlobalContextClientSidePublic = {
+  // Nothing public for now
+}
+type GlobalContextClientSide = Awaited<ReturnType<typeof createGlobalContext>>
 
 const globalObject = getGlobalObject<{
-  globalContext?: GlobalContextPublicClientSide
+  globalContext?: GlobalContextClientSide
 }>('client-routing-runtime/globalContextClientSide.ts', {})
 
-function getGlobalContext(): GlobalContextPublicClientSide {
-  if (!globalObject.globalContext) globalObject.globalContext = {}
+async function getGlobalContext(): Promise<GlobalContextClientSide> {
+  if (!globalObject.globalContext) {
+    globalObject.globalContext = await createGlobalContext()
+  }
   return globalObject.globalContext
+}
+
+async function createGlobalContext() {
+  const { pageRoutes, onBeforeRouteHook } = await loadPageRoutes(
+    pageFilesAll,
+    pageConfigs,
+    pageConfigGlobal,
+    allPageIds
+  )
+  const globalContext = {
+    _pageRoutes: pageRoutes,
+    _onBeforeRouteHook: onBeforeRouteHook,
+    _pageFilesAll: pageFilesAll,
+    _pageConfigs: pageConfigs,
+    _pageConfigGlobal: pageConfigGlobal,
+    _allPageIds: allPageIds
+  }
+  return globalContext
 }
