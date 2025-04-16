5e3397b8f963f58e2df38c42cda4352540a2b6a2
Romuald Brillout <git@brillout.com>
2025-04-16 12:33:05 +0200

    minor refactor

--- vike/utils/objectAssign.ts
+++ vike/utils/objectAssign.ts
@@ -5,13 +5,12 @@ import { assert } from './assert.js'
 // Same as Object.assign() but:
 //  - With type inference
 //  - Preserves property descriptors, which we need for preserving the getters of getPageContextUrlComputed()
-function objectAssign<Obj extends object, ObjAddendum extends object | null>(
+function objectAssign<Obj extends object, ObjAddendum extends object | null | undefined>(
   obj: Obj,
   objAddendum: ObjAddendum,
   objAddendumCanBePageContextObject?: true
 ): asserts obj is Obj & ObjAddendum {
-  if (objAddendum) {
-    if (!objAddendumCanBePageContextObject) assert(!('_isPageContextObject' in objAddendum))
-    Object.defineProperties(obj, Object.getOwnPropertyDescriptors(objAddendum))
-  }
+  if (!objAddendum) return
+  if (!objAddendumCanBePageContextObject) assert(!('_isPageContextObject' in objAddendum))
+  Object.defineProperties(obj, Object.getOwnPropertyDescriptors(objAddendum))
 }
