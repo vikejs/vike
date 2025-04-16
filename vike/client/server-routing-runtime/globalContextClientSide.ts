3b13307895724c099505645b76c18820024e897d
Romuald Brillout <git@brillout.com>
2025-04-16 13:20:30 +0200

    dedupe

--- vike/client/server-routing-runtime/globalContextClientSide.ts
+++ vike/client/server-routing-runtime/globalContextClientSide.ts
@@ -1,46 +1,13 @@
 export { getGlobalContext }
 export type { GlobalContextClientSidePublic }
 
-import { getPageConfigsRuntime } from '../../shared/getPageConfigsRuntime.js'
-import { getGlobalObject, objectReplace } from './utils.js'
-
+import { createGetGlobalContext } from '../shared/createGetGlobalContext.js'
 // @ts-ignore
 import * as virtualFileExports from 'virtual:vike:importUserCode:client:server-routing'
-const { pageFilesAll, allPageIds, pageConfigs, pageConfigGlobal } = getPageConfigsRuntime(virtualFileExports)
 
 type GlobalContextClientSidePublic = {
   // Nothing public for now
 }
-type GlobalContextClientSide = Awaited<ReturnType<typeof createGlobalContext>>
+type GlobalContextClientSide = Awaited<ReturnType<typeof getGlobalContext>>
 
-const globalObject = getGlobalObject<{
-  globalContext?: GlobalContextClientSide
-}>('server-routing-runtime/globalContextClientSide.ts', {})
-
-async function getGlobalContext(): Promise<GlobalContextClientSide> {
-  if (
-    !globalObject.globalContext ||
-    // Don't break HMR
-    globalObject.globalContext._virtualFileExports !== virtualFileExports
-  ) {
-    const globalContextCreated = await createGlobalContext()
-    if (!globalObject.globalContext) {
-      globalObject.globalContext = globalContextCreated
-    } else {
-      // Ensure all `globalContext` references are preserved & updated
-      objectReplace(globalObject.globalContext, globalContextCreated)
-    }
-  }
-  return globalObject.globalContext
-}
-
-async function createGlobalContext() {
-  const globalContext = {
-    _virtualFileExports: virtualFileExports,
-    _pageFilesAll: pageFilesAll,
-    _pageConfigs: pageConfigs,
-    _pageConfigGlobal: pageConfigGlobal,
-    _allPageIds: allPageIds
-  }
-  return globalContext
-}
+const getGlobalContext = createGetGlobalContext(virtualFileExports, false)
