975c3e85088f5bb0c40e398cb2e01e65cf753279
Romuald Brillout <git@brillout.com>
2025-04-18 21:28:40 +0200

    minor refactor

--- vike/client/shared/getContextSerializedInHtml.ts
+++ vike/client/shared/getContextSerializedInHtml.ts
@@ -3,24 +3,28 @@ export { getPageContextSerializedInHtml }
 import { parse } from '@brillout/json-serializer/parse'
 import { hasProp, assert, assertUsage } from '../server-routing-runtime/utils.js'
 
+// elements should exist because:
+// 1. <script id="vike_pageContext" type="application/json"> appears before the <script> that loads Vike's client runtime (which includes this file)
+// 2. <script id="vike_pageContext" type="application/json"> is neither async nor defer
+// See https://github.com/vikejs/vike/pull/1271
+const elemId_pageContext = 'vike_pageContext'
+
 function getPageContextSerializedInHtml(): { pageId: string; routeParams: Record<string, string> } {
-  // elem should exist because:
-  // 1. <script id="vike_pageContext" type="application/json"> appears before the <script> that loads Vike's client runtime (which includes this file)
-  // 2. <script id="vike_pageContext" type="application/json"> is neither async nor defer
-  // See https://github.com/vikejs/vike/pull/1271
-  const id = 'vike_pageContext'
+  const pageContextSerializedInHtml = findAndParseJson(elemId_pageContext)
+  assert(hasProp(pageContextSerializedInHtml, 'pageId', 'string'))
+  assert(hasProp(pageContextSerializedInHtml, 'routeParams', 'string{}'))
+  return pageContextSerializedInHtml
+}
+
+function findAndParseJson(id: string) {
   const elem = document.getElementById(id)
   assertUsage(
     elem,
-    // It seems like it can be missing because of malformed HTML: https://github.com/vikejs/vike/issues/913
+    // It seems like it can be missing when HTML is malformed: https://github.com/vikejs/vike/issues/913
     `Couldn't find #${id} (which Vike automatically injects in the HTML): make sure it exists (i.e. don't remove it and make sure your HTML isn't malformed)`
   )
-  const pageContextJson = elem.textContent
-  assert(pageContextJson)
-
-  const pageContextSerializedInHtml = parse(pageContextJson)
-  assert(hasProp(pageContextSerializedInHtml, 'pageId', 'string'))
-  assert(hasProp(pageContextSerializedInHtml, 'routeParams', 'string{}'))
-
-  return pageContextSerializedInHtml
+  const jsonStr = elem.textContent
+  assert(jsonStr)
+  const json = parse(jsonStr)
+  return json
 }
