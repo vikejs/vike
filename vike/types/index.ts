a7b1d3724ce5ad866e32caf560701952f727d9f8
Romuald Brillout <git@brillout.com>
2025-04-18 10:35:24 +0200

    minor refactor

--- vike/types/index.ts
+++ vike/types/index.ts
@@ -1,8 +1,10 @@
-export type { PageContext } from '../shared/types.js'
-export type { PageContextServer } from '../shared/types.js'
-export type { PageContextClient } from '../shared/types.js'
-export type { PageContextWithServerRouting } from '../shared/types.js'
-export type { PageContextClientWithServerRouting } from '../shared/types.js'
+export type {
+  PageContext,
+  PageContextServer,
+  PageContextClient,
+  PageContextWithServerRouting,
+  PageContextClientWithServerRouting
+} from '../shared/types.js'
 
 // TODO/v1-release: remove these three exports
 export type { PageContextBuiltInServer } from '../shared/types.js'
