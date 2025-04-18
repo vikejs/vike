fcd53d15dc4e66e63d35a9df267d1db19c6061c0
brillout <github.com@brillout.com>
2025-04-18 19:43:53 +0000

    Deploying to gh-pages from @ vikejs/vike@291fb8866f4690f1b6e051ddb64f8771b4571358 🚀

---
+++ assets/entries/pages_pinia.DG_OcXcE.js
@@ -0,0 +1,28 @@
+import{j as e,i as r,L as l,o as s}from"../chunks/chunk-Ck57FMdp.js";import{L as n}from"../chunks/chunk-D1qdXa5o.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-CaJXs0al.js";import{C as d}from"../chunks/chunk-BAm0c8ve.js";import{E as o}from"../chunks/chunk-Ci3Vu388.js";/* empty css                      *//* empty css                      */import"../chunks/chunk-B8KtPg6J.js";const p=[{pageSectionId:"manual-integration",pageSectionLevel:2,pageSectionTitle:"Manual integration"},{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function a(t){const i={a:"a",blockquote:"blockquote",code:"code",li:"li",p:"p",ul:"ul",...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(d,{tool:"pinia",url:"https://pinia.vuejs.org",hasExtension:"vue"}),`
+`,e.jsx("h2",{id:"manual-integration",children:"Manual integration"}),`
+`,e.jsx(i.p,{children:"To manually integrate Pinia, see:"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(n,{href:"/store#ssr",doNotInferSectionTitle:!0}),`
+`]}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://pinia.vuejs.org/ssr/",children:"Pinia > Server Side Rendering (SSR)"})}),`
+`]}),`
+`,e.jsxs(i.p,{children:["Examples of integrating Pinia without using ",e.jsx(n,{href:"/vike-vue",children:e.jsx(i.code,{children:"vike-vue"})}),":"]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(o,{timestamp:"2024.03",repo:"IT4Change/boilerplate-frontend"}),`
+`]}),`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(o,{timestamp:"2023.10",repo:"brillout/vike-with-pinia"}),`
+`]}),`
+`]}),`
+`,e.jsxs(i.blockquote,{children:[`
+`,e.jsxs(i.p,{children:["You can also have a look at ",e.jsxs(i.a,{href:"https://github.com/vikejs/vike-vue/tree/main/packages/vike-vue-pinia",children:["the source code of ",e.jsx(i.code,{children:"vike-vue-pinia"})]}),"."]}),`
+`]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(n,{href:"/store#ssr",doNotInferSectionTitle:!0}),`
+`]}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://pinia.vuejs.org/ssr/",children:"Pinia > Server Side Rendering (SSR)"})}),`
+`]})]})}function u(t={}){const{wrapper:i}=t.components||{};return i?e.jsx(i,{...t,children:e.jsx(a,{...t})}):a(t)}const c=Object.freeze(Object.defineProperty({__proto__:null,default:u,pageSectionsExport:p},Symbol.toStringTag,{value:"Module"})),E={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:s}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/pinia/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:c}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:l}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:r}}};export{E as configValuesSerialized};
---
+++ assets/entries/pages_store.DoChwGyP.js
@@ -0,0 +1,83 @@
+import{j as e,i as r,L as a,o as i}from"../chunks/chunk-Ck57FMdp.js";import{L as s}from"../chunks/chunk-D1qdXa5o.js";/* empty css                      */import{C as o}from"../chunks/chunk-B8KtPg6J.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-CaJXs0al.js";/* empty css                      */import{M as d}from"../chunks/chunk-CWq2tgZ6.js";/* empty css                      *//* empty css                      */const c=[{pageSectionId:"ssr",pageSectionLevel:2,pageSectionTitle:"SSR"},{pageSectionId:"initial-state",pageSectionLevel:3,pageSectionTitle:"Initial state"},{pageSectionId:"data-fetching",pageSectionLevel:3,pageSectionTitle:"Data fetching"}];function l(t){const n={a:"a",blockquote:"blockquote",code:"code",figure:"figure",li:"li",ol:"ol",p:"p",pre:"pre",span:"span",strong:"strong",ul:"ul",...t.components};return e.jsxs(e.Fragment,{children:[e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.strong,{children:"What is a store?"})}),`
+`,e.jsx(n.p,{children:"A store (aka state management) is a tool that helps you manage complex UI state."}),`
+`,e.jsx(n.p,{children:"Not properly managing UI state is one of the most common cause for buggy user interfaces. A store enables you to get under control even the most complex UI state logic."}),`
+`,e.jsx(n.p,{children:"A store works by representing state changes as atomic changes to an immutable data structure, enabling a fundamentally more robust state management."}),`
+`]}),`
+`,e.jsx(n.p,{children:"In principle, you can use Vike with any store."}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(s,{href:"/redux"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(s,{href:"/pinia"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(s,{href:"/vuex"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(s,{href:"/tools#stores",children:"... more"}),`
+`]}),`
+`]}),`
+`,e.jsx(n.p,{children:"The documentation on this page is about manually integrating stores."}),`
+`,e.jsx(d,{children:e.jsxs(n.p,{children:["Instead of manually integrating stores, we generally recommend using ",e.jsx(s,{href:"/extensions",children:"Vike extensions"})," such as ",e.jsx(n.a,{href:"https://github.com/vikejs/vike-vue/tree/main/packages/vike-vue-pinia#readme",children:e.jsx(n.code,{children:"vike-vue-pinia"})}),"."]})}),`
+`,e.jsx(o,{children:e.jsxs(n.p,{children:["Contribution welcome to ",e.jsx(n.a,{href:"https://github.com/vikejs/vike/issues/1715",children:"create Vike extensions"})," integrating stores."]})}),`
+`,e.jsx("h2",{id:"ssr",children:"SSR"}),`
+`,`
+`,e.jsxs(n.p,{children:["When using ",e.jsx(s,{href:"/ssr",children:"SSR"}),", the initial state of the store is determined on the server side (during SSR) and then passed to the client side."]}),`
+`,e.jsx("h3",{id:"initial-state",children:"Initial state"}),`
+`,e.jsxs(n.p,{children:["If you're using SSR, you need to ensure that the store's initial state is exactly the same between the client- and server-side (otherwise you may get a ",e.jsx(s,{href:"/hydration-mismatch",children:"hydration mismatch"}),"). Here's how you typically achieve this:"]}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Determine the store's initial state on the server-side (during SSR) and ",e.jsxs(s,{href:"/pageContext#custom",children:["make it available as ",e.jsx(n.code,{children:"pageContext.storeInitialState"})]}),".",`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["You typically create the store at ",e.jsx(s,{href:"/onBeforeRenderHtml",children:e.jsx(n.code,{children:"onBeforeRenderHtml()"})}),"  and then retrieve its initial sate at ",e.jsx(s,{href:"/onAfterRenderHtml",children:e.jsx(n.code,{children:"onAfterRenderHtml()"})}),":"]}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"js","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"js","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"// pages/+onBeforeRenderHtml.js"})}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"import"}),e.jsx(n.span,{style:{color:"#24292E"},children:" { createStore } "}),e.jsx(n.span,{style:{color:"#D73A49"},children:"from"}),e.jsx(n.span,{style:{color:"#032F62"},children:" 'awesome-store'"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"export"}),e.jsx(n.span,{style:{color:"#D73A49"},children:" function"}),e.jsx(n.span,{style:{color:"#6F42C1"},children:" onBeforeRenderHtml"}),e.jsx(n.span,{style:{color:"#24292E"},children:"("}),e.jsx(n.span,{style:{color:"#E36209"},children:"pageContext"}),e.jsx(n.span,{style:{color:"#24292E"},children:") {"})]}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#24292E"},children:"  pageContext.store "}),e.jsx(n.span,{style:{color:"#D73A49"},children:"="}),e.jsx(n.span,{style:{color:"#6F42C1"},children:" createStore"}),e.jsx(n.span,{style:{color:"#24292E"},children:"()"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#24292E"},children:"}"})})]})})}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"js","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"js","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"// pages/+onAfterRenderHtml.js"})}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"export"}),e.jsx(n.span,{style:{color:"#D73A49"},children:" function"}),e.jsx(n.span,{style:{color:"#6F42C1"},children:" onAfterRenderHtml"}),e.jsx(n.span,{style:{color:"#24292E"},children:"("}),e.jsx(n.span,{style:{color:"#E36209"},children:"pageContext"}),e.jsx(n.span,{style:{color:"#24292E"},children:") {"})]}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#24292E"},children:"  pageContext.storeInitialState "}),e.jsx(n.span,{style:{color:"#D73A49"},children:"="}),e.jsx(n.span,{style:{color:"#24292E"},children:" pageContext.store."}),e.jsx(n.span,{style:{color:"#6F42C1"},children:"getState"}),e.jsx(n.span,{style:{color:"#24292E"},children:"()"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#24292E"},children:"}"})})]})})}),`
+`,e.jsxs(n.p,{children:["If you use React then you may also need to use ",e.jsx(s,{href:"/Wrapper",children:e.jsx(n.code,{children:"+Wrapper"})}),":"]}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"js","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"js","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"// pages/+Wrapper.jsx"})}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"import"}),e.jsx(n.span,{style:{color:"#24292E"},children:" { Provider } "}),e.jsx(n.span,{style:{color:"#D73A49"},children:"from"}),e.jsx(n.span,{style:{color:"#032F62"},children:" 'awesome-store/react'"})]}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"import"}),e.jsx(n.span,{style:{color:"#24292E"},children:" { usePageContext } "}),e.jsx(n.span,{style:{color:"#D73A49"},children:"from"}),e.jsx(n.span,{style:{color:"#032F62"},children:" 'vike-react/usePageContext'"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"export"}),e.jsx(n.span,{style:{color:"#D73A49"},children:" default"}),e.jsx(n.span,{style:{color:"#D73A49"},children:" function"}),e.jsx(n.span,{style:{color:"#6F42C1"},children:" StoreProvider"}),e.jsx(n.span,{style:{color:"#24292E"},children:"({ "}),e.jsx(n.span,{style:{color:"#E36209"},children:"children"}),e.jsx(n.span,{style:{color:"#24292E"},children:" }) {"})]}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"  const"}),e.jsx(n.span,{style:{color:"#005CC5"},children:" pageContext"}),e.jsx(n.span,{style:{color:"#D73A49"},children:" ="}),e.jsx(n.span,{style:{color:"#6F42C1"},children:" usePageContext"}),e.jsx(n.span,{style:{color:"#24292E"},children:"()"})]}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"  return"}),e.jsx(n.span,{style:{color:"#24292E"},children:" <"}),e.jsx(n.span,{style:{color:"#005CC5"},children:"Provider"}),e.jsx(n.span,{style:{color:"#6F42C1"},children:" store"}),e.jsx(n.span,{style:{color:"#D73A49"},children:"="}),e.jsx(n.span,{style:{color:"#24292E"},children:"{pageContext.store}>{children}</"}),e.jsx(n.span,{style:{color:"#005CC5"},children:"Provider"}),e.jsx(n.span,{style:{color:"#24292E"},children:">"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#24292E"},children:"}"})})]})})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Make ",e.jsx(n.code,{children:"pageContext.storeInitialState"})," available to the client-side by using ",e.jsx(s,{href:"/passToClient",children:e.jsx(n.code,{children:"passToClient"})}),".",`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["At ",e.jsx(n.code,{children:"pages/+config.js"}),":"]}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"js","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"js","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"// pages/+config.js"})}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"export"}),e.jsx(n.span,{style:{color:"#D73A49"},children:" default"}),e.jsx(n.span,{style:{color:"#24292E"},children:" {"})]}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#24292E"},children:"  passToClient: ["}),e.jsx(n.span,{style:{color:"#032F62"},children:"'storeInitialState'"}),e.jsx(n.span,{style:{color:"#24292E"},children:"]"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#24292E"},children:"}"})})]})})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["On the client-side, initialize the store with ",e.jsx(n.code,{children:"pageContext.storeInitialState"}),".",`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["Typically at ",e.jsx(s,{href:"/onBeforeRenderClient",children:e.jsx(n.code,{children:"onBeforeRenderClient()"})}),":"]}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"js","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"js","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"// pages/+onBeforeRenderClient.js"})}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"import"}),e.jsx(n.span,{style:{color:"#24292E"},children:" { createStore } "}),e.jsx(n.span,{style:{color:"#D73A49"},children:"from"}),e.jsx(n.span,{style:{color:"#032F62"},children:" 'awesome-store'"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"export"}),e.jsx(n.span,{style:{color:"#D73A49"},children:" function"}),e.jsx(n.span,{style:{color:"#6F42C1"},children:" onBeforeRenderClient"}),e.jsx(n.span,{style:{color:"#24292E"},children:"("}),e.jsx(n.span,{style:{color:"#E36209"},children:"pageContext"}),e.jsx(n.span,{style:{color:"#24292E"},children:") {"})]}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#24292E"},children:"  pageContext.store "}),e.jsx(n.span,{style:{color:"#D73A49"},children:"="}),e.jsx(n.span,{style:{color:"#6F42C1"},children:" createStore"}),e.jsx(n.span,{style:{color:"#24292E"},children:"(pageContext.storeInitialState)"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#24292E"},children:"}"})})]})})}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx("h3",{id:"data-fetching",children:"Data fetching"}),`
+`,e.jsx(n.p,{children:"Typically, when using a store, your components never directly access fetched data: instead they only interact with the store and access data over the store."}),`
+`,e.jsxs(n.p,{children:["If you're using SSR, you typically populate the store with ",e.jsx(s,{href:"/data-fetching#initial-data",children:"initial data fetched on the server-side"}),". You can achieve this by using ",e.jsx(n.code,{children:"pageContext.storeInitialState"})," as shown ",e.jsx(s,{href:"#initial-state",children:"above"}),"."]})]})}function h(t={}){const{wrapper:n}=t.components||{};return n?e.jsx(n,{...t,children:e.jsx(l,{...t})}):l(t)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:h,pageSectionsExport:c},Symbol.toStringTag,{value:"Module"})),T={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:i}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/store/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:p}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:a}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:r}}};export{T as configValuesSerialized};
