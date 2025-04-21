bd8feeec27861d3e4df75e9b16fd1280ea197138
brillout <github.com@brillout.com>
2025-04-21 18:43:47 +0000

    Deploying to gh-pages from @ vikejs/vike@50161ea93228d6d4f303d1c5de0e1c0be120a876 🚀

---
+++ assets/entries/pages_client-routing.CjxgTJjC.js
@@ -0,0 +1,25 @@
+import{j as e,i as r,L as a,o as s}from"../chunks/chunk-BWttE6Mk.js";import{L as n}from"../chunks/chunk-BsETOhGP.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-ZFVB8OoR.js";import{M as l}from"../chunks/chunk-DGY80rEP.js";/* empty css                      *//* empty css                      */import"../chunks/chunk-vaMug2X6.js";const d=[{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function o(i){const t={em:"em",li:"li",p:"p",ul:"ul",...i.components};return e.jsxs(e.Fragment,{children:[e.jsxs(t.p,{children:[e.jsx(t.em,{children:"Client(-side) Routing"}),` denotes the practice of implementing page navigation on the client-side:
+when the user navigates to a new page,
+instead of completely discarding the current page and requesting the HTML of the new page,
+the new page is rendered by manipulating the DOM of the current page.`]}),`
+`,e.jsx(l,{}),`
+`,e.jsx(t.p,{children:"Client Routing enables:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Faster page navigation."}),`
+`,e.jsx(t.li,{children:"Client-side state preserved across navigation."}),`
+`,e.jsx(t.li,{children:"Nested Layouts."}),`
+`,e.jsx(t.li,{children:"Custom page transition animations."}),`
+`]}),`
+`,e.jsxs(t.p,{children:["We further discuss these use cases at ",e.jsx(n,{href:"/server-routing-vs-client-routing#which-one-to-choose"}),"."]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/clientRouting"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/server-routing"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/server-routing-vs-client-routing"}),`
+`]}),`
+`]})]})}function p(i={}){const{wrapper:t}=i.components||{};return t?e.jsx(t,{...i,children:e.jsx(o,{...i})}):o(i)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:p,pageSectionsExport:d},Symbol.toStringTag,{value:"Module"})),P={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:s}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/client-routing/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:u}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:a}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:r}}};export{P as configValuesSerialized};
