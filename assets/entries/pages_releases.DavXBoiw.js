7e239a9e33c21049dea6dd17fc9b1487d292afce
brillout <github.com@brillout.com>
2025-04-22 17:46:22 +0000

    Deploying to gh-pages from @ vikejs/vike@484b58fb2031ef794c8fb85ea575e38f092db42c 🚀

---
+++ assets/entries/pages_releases.DavXBoiw.js
@@ -0,0 +1,22 @@
+import{j as e,i as a,L as l,o as n}from"../chunks/chunk-CKIqXXhl.js";import{L as o}from"../chunks/chunk-ClQRvjUJ.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-D5A6Xfqc.js";/* empty css                      */const s=[{pageSectionId:"2024",pageSectionLevel:2,pageSectionTitle:"2024"},{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function r(t){const i={a:"a",code:"code",li:"li",ul:"ul",...t.components};return e.jsxs(e.Fragment,{children:[e.jsx("h2",{id:"2024",children:"2024"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(o,{href:"/releases/2024-09"}),`
+`]}),`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(o,{href:"/releases/2024-08"}),`
+`]}),`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(o,{href:"/releases/2024-07"}),`
+`]}),`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(o,{href:"/releases/2024-06"}),`
+`]}),`
+`]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsxs(i.a,{href:"https://github.com/vikejs/vike/blob/main/CHANGELOG.md",children:[e.jsx(i.code,{children:"vike"})," > ",e.jsx(i.code,{children:"CHANGELOG.md"})]})}),`
+`,e.jsx(i.li,{children:e.jsxs(i.a,{href:"https://github.com/vikejs/vike-react/blob/main/packages/vike-react/CHANGELOG.md",children:[e.jsx(i.code,{children:"vike-react"})," > ",e.jsx(i.code,{children:"CHANGELOG.md"})]})}),`
+`,e.jsx(i.li,{children:e.jsxs(i.a,{href:"https://github.com/vikejs/vike-vue/blob/main/packages/vike-vue/CHANGELOG.md",children:[e.jsx(i.code,{children:"vike-vue"})," > ",e.jsx(i.code,{children:"CHANGELOG.md"})]})}),`
+`,e.jsx(i.li,{children:e.jsxs(i.a,{href:"https://github.com/vikejs/vike-solid/blob/main/CHANGELOG.md",children:[e.jsx(i.code,{children:"vike-solid"})," > ",e.jsx(i.code,{children:"CHANGELOG.md"})]})}),`
+`]})]})}function d(t={}){const{wrapper:i}=t.components||{};return i?e.jsx(i,{...t,children:e.jsx(r,{...t})}):r(t)}const c=Object.freeze(Object.defineProperty({__proto__:null,default:d,pageSectionsExport:s},Symbol.toStringTag,{value:"Module"})),y={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:n}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/releases/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:c}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:l}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:a}}};export{y as configValuesSerialized};
