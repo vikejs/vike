07e3f620cc2f592de794e73068ec0ebd9a4db1b8
brillout <github.com@brillout.com>
2025-04-24 21:49:05 +0000

    Deploying to gh-pages from @ vikejs/vike@71a131afe21550cb57308e045b7821493052e6c4 🚀

---
+++ assets/chunks/chunk-D2PpqnMd.js
@@ -0,0 +1,3 @@
+import{j as n}from"./chunk-BEm8bNjM.js";function o(e){const t={a:"a",blockquote:"blockquote",p:"p",...e.components};return n.jsxs(t.blockquote,{children:[`
+`,n.jsxs(t.p,{children:["Instead of providing a list of URLs, if you want a parameterized route to be resolved dynamically on the client-side, then see the workaround at ",n.jsx(t.a,{href:"https://github.com/vikejs/vike/issues/1476",children:"#1476 - Pre-rendered dynamic routes (static host deployment)"}),"."]}),`
+`]})}function r(e={}){const{wrapper:t}=e.components||{};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}export{r as M};
