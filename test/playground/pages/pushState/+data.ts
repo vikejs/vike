4af0e84bdacebb895fbbffee8ede02299f613987
Romuald Brillout <git@brillout.com>
2025-04-24 11:06:59 +0200

    update test

--- test/playground/pages/pushState/+data.ts
+++ test/playground/pages/pushState/+data.ts
@@ -1,4 +1,18 @@
-export default async () => {
+export { data }
+
+import type { PageContextServer } from 'vike/types'
+import { assert } from '../../utils/assert'
+import { getGlobalContext, getGlobalContextAsync } from 'vike'
+
+async function data(pageContext: PageContextServer) {
+  // TEST: pageContext.isClientSide
+  assert(!pageContext.isClientSide)
+  assert(!pageContext.globalContext.isClientSide)
+
+  // TEST: getGlobalContext()
+  assert((await getGlobalContext()) === pageContext.globalContext)
+  assert((await getGlobalContextAsync(import.meta.env.PROD)) === pageContext.globalContext)
+
   const timestamp = new Date().getTime()
   return { timestamp }
 }
