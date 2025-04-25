0746305e647e80f67e57631645ba21ab2d238e5d
Romuald Brillout <git@brillout.com>
2025-04-25 10:17:57 +0200

    minor refactor

--- vike/node/plugin/shared/applyRegExWithMagicString.ts
+++ vike/node/plugin/shared/applyRegExWithMagicString.ts
@@ -2,7 +2,7 @@ export { applyRegExpWithMagicString }
 
 import type MagicString from 'magic-string'
 
-function applyRegExpWithMagicString(magicString: MagicString, regExp: string | RegExp, replacement: string) {
+function applyRegExpWithMagicString(magicString: MagicString, regExp: string | RegExp, replacement: unknown) {
   const envStatementRegEx = typeof regExp === 'string' ? new RegExp(regExp, 'g') : regExp
   let match: RegExpExecArray | null
   while ((match = envStatementRegEx.exec(magicString.original))) {
