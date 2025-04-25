78fce88fe0ca079f07e6a8839683a77b27036a68
Romuald Brillout <git@brillout.com>
2025-04-25 09:54:26 +0200

    minor refactor

--- vike/node/plugin/shared/applyRegExWithMagicString.ts
+++ vike/node/plugin/shared/applyRegExWithMagicString.ts
@@ -2,10 +2,10 @@ export { applyRegExpWithMagicString }
 
 import type MagicString from 'magic-string'
 
-function applyRegExpWithMagicString(magicString: MagicString, envStatementRegExStr: string, envVal: string) {
-  const envStatementRegEx = new RegExp(envStatementRegExStr, 'g')
+function applyRegExpWithMagicString(magicString: MagicString, regExpStr: string, replacement: string) {
+  const envStatementRegEx = new RegExp(regExpStr, 'g')
   let match: RegExpExecArray | null
   while ((match = envStatementRegEx.exec(magicString.original))) {
-    magicString.overwrite(match.index, match.index + match[0].length, JSON.stringify(envVal))
+    magicString.overwrite(match.index, match.index + match[0].length, JSON.stringify(replacement))
   }
 }
