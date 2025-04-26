40afec74063508ec68572b40158f5130b76a53d3
brillout <github.com@brillout.com>
2025-04-26 08:12:41 +0000

    Deploying to gh-pages from @ vikejs/vike@f5beb8daf9731c7b1159e8568b6dd9ff2e408fd3 🚀

---
+++ assets/chunks/chunk-CFV2uRSs.js
@@ -0,0 +1,1 @@
+import{j as i}from"./chunk-O-4rQP1Y.js";const d=["diff-entire-file","diff-entire-file-removed"].join(" "),s=["diff-entire-file","diff-entire-file-added"].join(" ");function f({children:e}){return i.jsxs("div",{className:d,children:[" ",e," "]})}function r({children:e}){return i.jsxs("div",{className:s,children:[" ",e," "]})}export{f as F,r as a};
