d20eed2b59984a6d4f94d92145c72f4cdbc2eb06
brillout <github.com@brillout.com>
2025-04-27 11:20:42 +0000

    Deploying to gh-pages from @ vikejs/vike@9fb18db0466cf985f32a340017060be00dcf5222 🚀

---
+++ assets/entries/pages_filesystem-routing.BAyUqWWt.js
@@ -0,0 +1,98 @@
+import{j as e,i as t,L as l,o}from"../chunks/chunk-BpkosQjM.js";import{L as s}from"../chunks/chunk-DpzyxQQZ.js";/* empty css                      */import{W as a}from"../chunks/chunk-BR8gfvOF.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-CChgn8K1.js";import{U as d}from"../chunks/chunk-BWxoV_yS.js";/* empty css                      *//* empty css                      *//* empty css                      */const c=[{pageSectionId:"ignored-directories",pageSectionLevel:2,pageSectionTitle:"Ignored directories"},{pageSectionId:"case-sensitive",pageSectionLevel:2,pageSectionTitle:"Case sensitive"},{pageSectionId:"crawl",pageSectionLevel:2,pageSectionTitle:"Crawl"},{pageSectionId:"renderer",pageSectionLevel:2,pageSectionTitle:"`renderer/`"},{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function r(i){const n={a:"a",blockquote:"blockquote",code:"code",figure:"figure",li:"li",p:"p",pre:"pre",span:"span",ul:"ul",...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"Detailed information about Vike's Filesystem Routing."}),`
+`,e.jsxs(n.p,{children:["For a quick overview see ",e.jsx(s,{href:"/routing#filesystem-routing",doNotInferSectionTitle:!0})," instead."]}),`
+`,e.jsx("h2",{id:"ignored-directories",children:"Ignored directories"}),`
+`,e.jsx(n.p,{children:"Following directories are ignored by Filesystem Routing:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"index/"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"pages/"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"src/"})}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"(someDir)/"})," (any directory inside parentheses)"]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`FILESYSTEM                                                     URL
+=========================================================      ========
+pages/index/+Page.js                                           /
+(marketing)/src/pages/jobs/+Page.js                            /jobs
+(some)/(path)/src/pages/jobs/+Page.js                          /jobs
+contact/+Page.js                                               /contact
+pages/pages/src/(some-dir)/src/index/pages/about/+Page.js      /about
+`})}),`
+`,e.jsxs(n.p,{children:["Note that they aren't ignored by ",e.jsx(s,{href:"/config#inheritance",children:"config inheritance"}),":"]}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"yaml","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"yaml","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#032F62"},children:"pages/about/+Page.js"})}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"# This +config.js file doesn't apply to pages/about/+Page.js"})}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#032F62"},children:"src/pages/+config.js"})})]})})}),`
+`,e.jsx(n.p,{children:"See also:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(s,{href:"/routing#groups"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(s,{href:"/routing#src",doNotInferSectionTitle:!0}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(s,{href:"/routing#domain-driven-file-structure"}),`
+`]}),`
+`]}),`
+`,e.jsx("h2",{id:"case-sensitive",children:"Case sensitive"}),`
+`,e.jsx(n.p,{children:"Filesystem Routing is case sensitive:"}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"bash","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"bash","data-theme":"github-light",style:{display:"grid"},children:[e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#6F42C1"},children:"FILESYSTEM"}),e.jsx(n.span,{style:{color:"#032F62"},children:"                     URL"})]}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#24292E"},children:"========================       "}),e.jsx(n.span,{style:{color:"#032F62"},children:"======"})]}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#6F42C1"},children:"pages/HELLO/+Page.js"}),e.jsx(n.span,{style:{color:"#032F62"},children:"           /HELLO"})]})]})})}),`
+`,e.jsx("h2",{id:"crawl",children:"Crawl"}),`
+`,e.jsx(n.p,{children:"Vike crawls:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Files inside ",e.jsxs(n.a,{href:"https://vitejs.dev/config/shared-options.html#root",children:["Vite's ",e.jsx(n.code,{children:"root"})," directory"]}),".",`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["Consequently, all your ",e.jsxs(s,{href:"/config#files",children:[e.jsx(n.code,{children:"+"})," files"]})," must live inside ",e.jsx(n.code,{children:"root"}),`.
+To make a directory or file outside of `,e.jsx(n.code,{children:"root"})," crawlable, use a symlink (e.g. ",e.jsx(n.code,{children:"$ ln -s"}),") and set ",e.jsx(n.code,{children:'VIKE_CRAWL="{git:false}"'}),"."]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Skips ",e.jsx(n.a,{href:"https://github.com/vikejs/vike/blob/main/vike/node/plugin/plugins/importUserCode/v1-design/getVikeConfig/crawlPlusFiles/ignorePatternsBuiltIn.ts",children:"built-in ignore patterns"})," such as ",e.jsx(n.code,{children:"node_modules/"})," and ",e.jsx(n.code,{children:"ejected/"}),".",`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["To add custom ignore patterns, see ",e.jsx(n.a,{href:"https://github.com/vikejs/vike/issues/2228",children:"#2228"}),"."]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Skips ",e.jsx(n.code,{children:".gitignore"})," files (if you use Git).",`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["If you want to also crawl these then set ",e.jsx(n.code,{children:'VIKE_CRAWL="{git:false}"'}),"."]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Skips soft-symlinked (",e.jsx(n.code,{children:"$ ln -s"}),") directories (if you use Git).",`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["If you want to also crawl these then set ",e.jsx(n.code,{children:'VIKE_CRAWL="{git:false}"'}),"."]}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.p,{children:["By default, if you use Git, Vike uses ",e.jsx(n.code,{children:"$ git ls-files"})," to crawl your files. If you set ",e.jsx(n.code,{children:'VIKE_CRAWL="{git:false}"'})," then Vike uses ",e.jsx(n.a,{href:"https://github.com/SuperchupuDev/tinyglobby",children:e.jsx(n.code,{children:"tinyglobby"})})," instead of Git."]}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"bash","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"bash","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"# Use tinyglobby instead of Git to crawl files"})}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#6F42C1"},children:"$"}),e.jsx(n.span,{style:{color:"#032F62"},children:' VIKE_CRAWL="{git:false}"'}),e.jsx(n.span,{style:{color:"#032F62"},children:" npm"}),e.jsx(n.span,{style:{color:"#032F62"},children:" run"}),e.jsx(n.span,{style:{color:"#032F62"},children:" dev"})]})]})})}),`
+`,e.jsxs(n.p,{children:["You can tell Vike to skip certain files and/or directories from being crawled, see ",e.jsx(n.a,{href:"https://github.com/vikejs/vike/issues/2228",children:"#2228"}),"."]}),`
+`,e.jsx("h2",{id:"renderer",children:e.jsx("code",{children:"renderer/"})}),`
+`,e.jsxs(n.p,{children:["If you don't use a ",e.jsx(d,{})," then we recommend placing your UI framework integration inside a ",e.jsx(n.code,{children:"renderer/"})," directory."]}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"yaml","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"yaml","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"# Usual Vike file structure"})}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#032F62"},children:"/pages/"})}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#032F62"},children:"/components/"})}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#032F62"},children:"/server/"})}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"# Code that specifies how pages are rendered"})}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#032F62"},children:"/renderer/+onRenderHtml.js"})}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#032F62"},children:"/renderer/+onRenderClient.js"})}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#032F62"},children:"/renderer/Layout.{jsx,vue}"}),e.jsx(n.span,{style:{color:"#6A737D"},children:"  # React/Vue/... component that wraps the `Page` component"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#032F62"},children:"/renderer/Layout.css"})}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#032F62"},children:"/renderer/Header.{jsx,vue}"}),e.jsx(n.span,{style:{color:"#6A737D"},children:" # Website header used for every page"})]}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#032F62"},children:"/renderer/Footer.{jsx,vue}"}),e.jsx(n.span,{style:{color:"#6A737D"},children:" # Website footer used for every page"})]}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#032F62"},children:"/renderer/logo.svg"}),e.jsx(n.span,{style:{color:"#6A737D"},children:" # Website logo (favicon and used by <Header>)"})]})]})})}),`
+`,e.jsxs(n.p,{children:["The hooks ",e.jsx(n.code,{children:"/renderer/+onRender{Html,Client}.js"})," apply as default to all pages ",e.jsx(n.code,{children:"/pages/**/+Page.js"}),"."]}),`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"renderer/"})," directory doesn't add any functionality: defining the hooks ",e.jsx(n.code,{children:"+onRender{Html,Client}.js"})," at ",e.jsx(n.code,{children:"/renderer/"})," is equivalent to defining them at ",e.jsx(n.code,{children:"/pages/"})," or ",e.jsx(n.code,{children:"/"}),". It's just an optional convenience for moving rendering logic outside of ",e.jsx(n.code,{children:"pages/"}),": in order to avoid cluttering the ",e.jsx(n.code,{children:"pages/"})," directory and to organize and put all rendering code in one place."]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"renderer/"})," directory is different from the ",e.jsx(s,{href:"#ignored-directories",children:"list of ignored directories"})," because it's ignored not only by Filesystem Routing but also by ",e.jsx(s,{href:"/config#inheritance",children:"config inheritance"}),". It's the only directory that is ignored by config inheritance."]}),`
+`]}),`
+`,e.jsx(a,{children:e.jsxs(n.p,{children:["We recommend defining a ",e.jsx(n.code,{children:"renderer/"})," directory only if you are implementing a custom UI framework (React/Vue/Solid/...) integration."]})}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(s,{href:"/routing"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(s,{href:"/filesystemRoutingRoot"}),`
+`]}),`
+`]})]})}function h(i={}){const{wrapper:n}=i.components||{};return n?e.jsx(n,{...i,children:e.jsx(r,{...i})}):r(i)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:h,pageSectionsExport:c},Symbol.toStringTag,{value:"Module"})),C={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:o}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/filesystem-routing/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:p}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:l}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:t}}};export{C as configValuesSerialized};
---
+++ assets/entries/pages_stream.Zx1CUDoE.js
@@ -0,0 +1,67 @@
+import{j as e,i as r,L as t,o as l}from"../chunks/chunk-BpkosQjM.js";import{L as n}from"../chunks/chunk-DpzyxQQZ.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-CChgn8K1.js";import{U as o}from"../chunks/chunk-BWxoV_yS.js";import{I as d}from"../chunks/chunk-BGlV2f97.js";/* empty css                      *//* empty css                      */const c=[{pageSectionId:"inheritance",pageSectionLevel:2,pageSectionTitle:"Inheritance"},{pageSectionId:"without-vike-react-vue-solid",pageSectionLevel:2,pageSectionTitle:"Without `vike-{react,vue,solid}`"},{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function i(a){const s={a:"a",blockquote:"blockquote",br:"br",code:"code",figure:"figure",li:"li",p:"p",pre:"pre",span:"span",strong:"strong",ul:"ul",...a.components};return e.jsxs(e.Fragment,{children:[e.jsxs(s.p,{children:["Default value: ",e.jsx(s.code,{children:"false"}),". (Or ",e.jsx(s.code,{children:"true"})," if using a ",e.jsx(n,{href:"/extensions",children:"Vike extension"})," that requires streaming.)",e.jsx(s.br,{}),`
+`,"Requires: ",e.jsx(n,{href:"/ssr",children:e.jsx(s.code,{children:"ssr: true"})}),"."]}),`
+`,e.jsxs(d,{children:["the ",e.jsx(s.code,{children:"stream"})," setting"]}),`
+`,e.jsxs(s.p,{children:["The ",e.jsx(s.code,{children:"stream"})," setting allows you to:"]}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"Enable or disable HTML Streaming."}),`
+`,e.jsxs(s.li,{children:["Specify the stream type (either a ",e.jsx(s.a,{href:"https://nodejs.org/api/stream.html",children:"Node.js Stream"})," or a ",e.jsx(s.a,{href:"https://developer.mozilla.org/en-US/docs/Web/API/Streams_API",children:"Web Stream"}),")."]}),`
+`]}),`
+`,e.jsx("span",{id:"what-is-streaming"}),`
+`,e.jsxs(s.blockquote,{children:[`
+`,e.jsxs(s.p,{children:[e.jsx(s.strong,{children:"What is HTML Streaming?"})," If you're unfamiliar with HTML streaming then check out ",e.jsx(s.a,{href:"https://github.com/reactwg/react-18/discussions/37",children:"this explanation of SSR, HTML streaming, and Progressive Rendering"}),". Although it explains it in the context of React, we also recommend reading it if you use a UI framework other than React."]}),`
+`]}),`
+`,e.jsx(s.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(s.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"js","data-theme":"github-light",children:e.jsxs(s.code,{"data-language":"js","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"// /pages/+config.js"})}),`
+`,e.jsx(s.span,{"data-line":"",children:" "}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#D73A49"},children:"export"}),e.jsx(s.span,{style:{color:"#D73A49"},children:" default"}),e.jsx(s.span,{style:{color:"#24292E"},children:" {"})]}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"  // Enable HTML Streaming. Let Vike extensions decide whether to use a Node.js or Web stream."})}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#24292E"},children:"  stream: "}),e.jsx(s.span,{style:{color:"#005CC5"},children:"true"}),e.jsx(s.span,{style:{color:"#24292E"},children:","})]}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"  // Enable HTML Streaming. Force the stream to be a Web Stream."})}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#24292E"},children:"  stream: "}),e.jsx(s.span,{style:{color:"#032F62"},children:"'web'"}),e.jsx(s.span,{style:{color:"#24292E"},children:","})]}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"  // Enable HTML Streaming. Force the stream to be a Node.js Stream."})}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#24292E"},children:"  stream: "}),e.jsx(s.span,{style:{color:"#032F62"},children:"'node'"})]}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#24292E"},children:"}"})})]})})}),`
+`,e.jsx("h2",{id:"inheritance",children:"Inheritance"}),`
+`,e.jsx(s.p,{children:"Enable for all your pages:"}),`
+`,e.jsx(s.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(s.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"js","data-theme":"github-light",children:e.jsxs(s.code,{"data-language":"js","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"// /pages/+config.js"})}),`
+`,e.jsx(s.span,{"data-line":"",children:" "}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"// This config applies to all pages (/pages/**)."})}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#D73A49"},children:"export"}),e.jsx(s.span,{style:{color:"#D73A49"},children:" default"}),e.jsx(s.span,{style:{color:"#24292E"},children:" {"})]}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#24292E"},children:"  stream: "}),e.jsx(s.span,{style:{color:"#005CC5"},children:"false"})]}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#24292E"},children:"}"})})]})})}),`
+`,e.jsx(s.p,{children:"Enable only for some pages:"}),`
+`,e.jsx(s.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(s.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"js","data-theme":"github-light",children:e.jsxs(s.code,{"data-language":"js","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"// /pages/admin/+config.js"})}),`
+`,e.jsx(s.span,{"data-line":"",children:" "}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"/* This config applies only to admin pages (/pages/admin/**) such as:"})}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"   FILESYSTEM                            URL"})}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"   /pages/admin/+Page.js                 /admin"})}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"   /pages/admin/user/@id/+Page.js        /admin/user/@id"})}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"   /pages/admin/product/@id/+Page.js     /admin/product/@id"})}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"*/"})}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#D73A49"},children:"export"}),e.jsx(s.span,{style:{color:"#D73A49"},children:" default"}),e.jsx(s.span,{style:{color:"#24292E"},children:" {"})]}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#24292E"},children:"  stream: "}),e.jsx(s.span,{style:{color:"#005CC5"},children:"false"})]}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#24292E"},children:"}"})})]})})}),`
+`,e.jsxs(s.p,{children:["For an improved file and config organization, you can consider using a ",e.jsx(n,{href:"/routing#domain-driven-file-structure",children:"domain-driven file structure"}),"."]}),`
+`,e.jsxs("h2",{id:"without-vike-react-vue-solid",children:["Without ",e.jsx("code",{children:"vike-{react,vue,solid}"})]}),`
+`,e.jsxs(s.p,{children:["In case you don't use a ",e.jsx(o,{}),", you can:"]}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:["Integrate HTML streaming yourself, see ",e.jsx(n,{href:"/streaming"}),"."]}),`
+`,e.jsxs(s.li,{children:["Implement the ",e.jsx(s.code,{children:"stream"})," setting yourself by using ",e.jsx(n,{href:"/meta",children:"meta"}),". Examples:",`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:e.jsxs(s.a,{href:"https://github.com/vikejs/vike-react/blob/main/packages/vike-react",children:[e.jsx(s.code,{children:"vike-react"})," source code"]})}),`
+`,e.jsx(s.li,{children:e.jsxs(s.a,{href:"https://github.com/vikejs/vike-vue/blob/main/packages/vike-vue",children:[e.jsx(s.code,{children:"vike-vue"})," source code"]})}),`
+`,e.jsx(s.li,{children:e.jsxs(s.a,{href:"https://github.com/vikejs/vike-solid/blob/main/vike-solid",children:[e.jsx(s.code,{children:"vike-solid"})," source code"]})}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:[`
+`,e.jsx(n,{href:"/streaming"}),`
+`]}),`
+`,e.jsxs(s.li,{children:[`
+`,e.jsx(n,{href:"/ssr"}),`
+`]}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://github.com/reactwg/react-18/discussions/37",children:"Explanation of SSR, HTML streaming, and Progressive Rendering"})}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"https://nodejs.org/api/stream.html",children:"Node.js Streams"})," (Node.js documentation)"]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"https://developer.mozilla.org/en-US/docs/Web/API/Streams_API",children:"Web Streams"})," (MDN documentation)"]}),`
+`]})]})}function h(a={}){const{wrapper:s}=a.components||{};return s?e.jsx(s,{...a,children:e.jsx(i,{...a})}):i(a)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:h,pageSectionsExport:c},Symbol.toStringTag,{value:"Module"})),k={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:l}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/stream/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:p}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:t}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:r}}};export{k as configValuesSerialized};
