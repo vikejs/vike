0626e803f16622c26d3909ec8df727b1240ed025
brillout <github.com@brillout.com>
2025-04-30 08:06:32 +0000

    Deploying to gh-pages from @ vikejs/vike@85671209e4b6bce930cac5494257adceb0626756 🚀

---
+++ assets/entries/pages_build-your-own-framework.BilqhbsT.js
@@ -0,0 +1,71 @@
+import{j as e,i as a,L as o,o as t}from"../chunks/chunk-BFP3BUXr.js";import{L as r}from"../chunks/chunk-aW8T63tz.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-DRegmVmw.js";/* empty css                      */const i=[{pageSectionId:"getting-started",pageSectionLevel:2,pageSectionTitle:"Getting started"},{pageSectionId:"first-step",pageSectionLevel:3,pageSectionTitle:"First step"},{pageSectionId:"progressively-create-your-framework",pageSectionLevel:3,pageSectionTitle:"Progressively create your framework"},{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function l(n){const s={blockquote:"blockquote",code:"code",figure:"figure",li:"li",p:"p",pre:"pre",span:"span",ul:"ul",...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(s.p,{children:"Vike enables you to create your own framework tailored to your company's requirements."}),`
+`,e.jsxs(s.blockquote,{children:[`
+`,e.jsxs(s.p,{children:["It's a powerful way to scale your teams, and to ensure a cohesive stack throughout your company. See ",e.jsx(r,{href:"/why#build-your-own-framework"}),"."]}),`
+`]}),`
+`,e.jsx(s.p,{children:"Your framework can include everything (frontend, backend, data fetching, authentication, deployment, error tracking, analytics, ...) so that your Product Developers can focus on what they do best: building user interfaces."}),`
+`,e.jsx("h2",{id:"getting-started",children:"Getting started"}),`
+`,e.jsx("h3",{id:"first-step",children:"First step"}),`
+`,e.jsxs(s.p,{children:["We recommend getting started by creating a regular Vike app. Once you are satisfied with your stack then progressively move code from the user-land (code inside the app's Git repository) to your framework (code inside ",e.jsx(s.code,{children:"node_modules/my-framework/"}),")."]}),`
+`,e.jsx("h3",{id:"progressively-create-your-framework",children:"Progressively create your framework"}),`
+`,e.jsx(s.p,{children:"Once you are satisfied wit the stack of your regular Vike app, progressively create your framework."}),`
+`,e.jsx(s.p,{children:"You can achieve that by progressively applying one or more of the following steps:"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:["Create your own Vike extension that contains all your configuration.",`
+`,e.jsx(s.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(s.pre,{className:"has-diff",style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"js","data-theme":"github-light",children:e.jsxs(s.code,{"data-language":"js","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"// pages/+config.js"})}),`
+`,e.jsx(s.span,{"data-line":"",children:" "}),`
+`,e.jsxs(s.span,{className:"diff remove","data-line":"",children:[e.jsx(s.span,{style:{color:"#D73A49"},children:"import"}),e.jsx(s.span,{style:{color:"#24292E"},children:" react "}),e.jsx(s.span,{style:{color:"#D73A49"},children:"from"}),e.jsx(s.span,{style:{color:"#032F62"},children:" 'vike-react/config'"})]}),`
+`,e.jsxs(s.span,{className:"diff remove","data-line":"",children:[e.jsx(s.span,{style:{color:"#D73A49"},children:"import"}),e.jsx(s.span,{style:{color:"#24292E"},children:" apollo "}),e.jsx(s.span,{style:{color:"#D73A49"},children:"from"}),e.jsx(s.span,{style:{color:"#032F62"},children:" 'vike-react-apollo/config'"})]}),`
+`,e.jsxs(s.span,{className:"diff add","data-line":"",children:[e.jsx(s.span,{style:{color:"#D73A49"},children:"import"}),e.jsx(s.span,{style:{color:"#24292E"},children:" myFramework "}),e.jsx(s.span,{style:{color:"#D73A49"},children:"from"}),e.jsx(s.span,{style:{color:"#032F62"},children:" 'my-framework/config'"})]}),`
+`,e.jsx(s.span,{"data-line":"",children:" "}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#D73A49"},children:"export"}),e.jsx(s.span,{style:{color:"#D73A49"},children:" default"}),e.jsx(s.span,{style:{color:"#24292E"},children:" {"})]}),`
+`,e.jsxs(s.span,{className:"diff remove","data-line":"",children:[e.jsx(s.span,{style:{color:"#24292E"},children:"  ssr: "}),e.jsx(s.span,{style:{color:"#005CC5"},children:"false"}),e.jsx(s.span,{style:{color:"#24292E"},children:", "})]}),`
+`,e.jsx(s.span,{className:"diff remove","data-line":"",children:e.jsx(s.span,{style:{color:"#24292E"},children:"  extends: [react, apollo] "})}),`
+`,e.jsx(s.span,{className:"diff add","data-line":"",children:e.jsx(s.span,{style:{color:"#24292E"},children:"  extends: [myFramework] "})}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#24292E"},children:"}"})})]})})}),`
+`]}),`
+`,e.jsxs(s.li,{children:[e.jsx(r,{href:"/meta",children:"Create custom settings"}),", so that your Product Developers can easily configure your framework.",`
+`,e.jsx(s.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(s.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"js","data-theme":"github-light",children:e.jsxs(s.code,{"data-language":"js","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"// pages/+config.js"})}),`
+`,e.jsx(s.span,{"data-line":"",children:" "}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#D73A49"},children:"import"}),e.jsx(s.span,{style:{color:"#24292E"},children:" myFramework "}),e.jsx(s.span,{style:{color:"#D73A49"},children:"from"}),e.jsx(s.span,{style:{color:"#032F62"},children:" 'my-framework/config'"})]}),`
+`,e.jsx(s.span,{"data-line":"",children:" "}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#D73A49"},children:"export"}),e.jsx(s.span,{style:{color:"#D73A49"},children:" default"}),e.jsx(s.span,{style:{color:"#24292E"},children:" {"})]}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#24292E"},children:"  myCustomSetting: "}),e.jsx(s.span,{style:{color:"#005CC5"},children:"true"}),e.jsx(s.span,{style:{color:"#24292E"},children:","})]}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#24292E"},children:"  extends: [myFramework]"})}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#24292E"},children:"}"})})]})})}),`
+`]}),`
+`,e.jsxs(s.li,{children:["Create your own CLI.",`
+`,e.jsx(s.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(s.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"json","data-theme":"github-light",children:e.jsxs(s.code,{"data-language":"json","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"// package.json"})}),`
+`,e.jsx(s.span,{"data-line":"",children:" "}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#24292E"},children:"{"})}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#005CC5"},children:'  "scripts"'}),e.jsx(s.span,{style:{color:"#24292E"},children:": {"})]}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#005CC5"},children:'    "dev"'}),e.jsx(s.span,{style:{color:"#24292E"},children:": "}),e.jsx(s.span,{style:{color:"#032F62"},children:'"my-framework"'}),e.jsx(s.span,{style:{color:"#24292E"},children:","})]}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#005CC5"},children:'    "build"'}),e.jsx(s.span,{style:{color:"#24292E"},children:": "}),e.jsx(s.span,{style:{color:"#032F62"},children:'"my-framework build"'}),e.jsx(s.span,{style:{color:"#24292E"},children:","})]}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#005CC5"},children:'    "preview"'}),e.jsx(s.span,{style:{color:"#24292E"},children:": "}),e.jsx(s.span,{style:{color:"#032F62"},children:'"my-framework preview"'})]}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#24292E"},children:"  }"})}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#24292E"},children:"}"})})]})})}),`
+`,e.jsxs(s.blockquote,{children:[`
+`,e.jsxs(s.p,{children:["You can build your own CLI by using ",e.jsx(r,{href:"/api",children:"Vike's JavaScript API"}),"."]}),`
+`]}),`
+`]}),`
+`,e.jsx(s.li,{children:"Move your server code to be built into your framework."}),`
+`,e.jsxs(s.li,{children:["Move deployment logic to be built into your framework.",`
+`,e.jsx(s.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(s.pre,{className:"has-diff",style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"json","data-theme":"github-light",children:e.jsxs(s.code,{"data-language":"json","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#6A737D"},children:"// package.json"})}),`
+`,e.jsx(s.span,{"data-line":"",children:" "}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#24292E"},children:"{"})}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#005CC5"},children:'  "scripts"'}),e.jsx(s.span,{style:{color:"#24292E"},children:": {"})]}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#005CC5"},children:'    "dev"'}),e.jsx(s.span,{style:{color:"#24292E"},children:": "}),e.jsx(s.span,{style:{color:"#032F62"},children:'"my-framework"'}),e.jsx(s.span,{style:{color:"#24292E"},children:","})]}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#005CC5"},children:'    "build"'}),e.jsx(s.span,{style:{color:"#24292E"},children:": "}),e.jsx(s.span,{style:{color:"#032F62"},children:'"my-framework build"'}),e.jsx(s.span,{style:{color:"#24292E"},children:","})]}),`
+`,e.jsxs(s.span,{"data-line":"",children:[e.jsx(s.span,{style:{color:"#005CC5"},children:'    "preview"'}),e.jsx(s.span,{style:{color:"#24292E"},children:": "}),e.jsx(s.span,{style:{color:"#032F62"},children:'"my-framework preview"'}),e.jsx(s.span,{style:{color:"#24292E"},children:","})]}),`
+`,e.jsxs(s.span,{className:"diff add","data-line":"",children:[e.jsx(s.span,{style:{color:"#005CC5"},children:'    "deploy"'}),e.jsx(s.span,{style:{color:"#24292E"},children:": "}),e.jsx(s.span,{style:{color:"#032F62"},children:'"my-framework deploy"'})]}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#24292E"},children:"  }"})}),`
+`,e.jsx(s.span,{"data-line":"",children:e.jsx(s.span,{style:{color:"#24292E"},children:"}"})})]})})}),`
+`]}),`
+`,e.jsx(s.li,{children:"Add utilities to your framework, such as error tracking or analytics."}),`
+`]}),`
+`,e.jsx(s.p,{children:"Feel free to reach out to the Vike team if you have any questions, and sponsorship is welcome for a tight-knit collaboration."}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:[`
+`,e.jsx(r,{href:"/api"}),`
+`]}),`
+`]})]})}function d(n={}){const{wrapper:s}=n.components||{};return s?e.jsx(s,{...n,children:e.jsx(l,{...n})}):l(n)}const c=Object.freeze(Object.defineProperty({__proto__:null,default:d,pageSectionsExport:i},Symbol.toStringTag,{value:"Module"})),v={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:t}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/build-your-own-framework/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:c}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:o}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:a}}};export{v as configValuesSerialized};
---
+++ assets/entries/pages_cli.D5dIOMf1.js
@@ -0,0 +1,74 @@
+import{j as e,i as r,L as t,o as a}from"../chunks/chunk-BFP3BUXr.js";import{L as s}from"../chunks/chunk-aW8T63tz.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-DRegmVmw.js";/* empty css                      */const o=[{pageSectionId:"vike-settings",pageSectionLevel:2,pageSectionTitle:"Vike settings"},{pageSectionId:"cli-options",pageSectionLevel:4,pageSectionTitle:"CLI options"},{pageSectionId:"vike-config-environment-variable",pageSectionLevel:4,pageSectionTitle:"`VIKE_CONFIG` environment variable"},{pageSectionId:"vite-settings",pageSectionLevel:2,pageSectionTitle:"Vite settings"},{pageSectionId:"cli-options",pageSectionLevel:4,pageSectionTitle:"CLI options"},{pageSectionId:"vite-config-environment-variable",pageSectionLevel:4,pageSectionTitle:"`VITE_CONFIG` environment variable"},{pageSectionId:"json5-syntax",pageSectionLevel:2,pageSectionTitle:"JSON5 syntax"},{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function l(i){const n={a:"a",blockquote:"blockquote",code:"code",figure:"figure",li:"li",p:"p",pre:"pre",span:"span",ul:"ul",...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"Usage:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"$ vike dev"})," Start development server."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"$ vike build"})," Build for production."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"$ vike preview"})," Start preview server using production build (only works for ",e.jsx(s,{href:"/glossary#ssg",children:"SSG apps"}),")."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"$ vike prerender"})," ",e.jsx(s,{href:"/pre-rendering",children:"Pre-render"})," pages (only needed when ",e.jsx(s,{href:"/prerender#disableautorun",children:e.jsx(n.code,{children:"prerender.disableAutoRun"})})," is ",e.jsx(n.code,{children:"true"}),")."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"$ vike -v"})," Print Vike's installed version."]}),`
+`]}),`
+`,e.jsx("h2",{id:"vike-settings",children:"Vike settings"}),`
+`,e.jsx("h4",{id:"cli-options",children:"CLI options"}),`
+`,e.jsxs(n.p,{children:["You can pass ",e.jsx(s,{href:"/settings",children:"any Vike setting"})," as CLI option, for example:"]}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"bash","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"bash","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"# Make development server available over LAN and public addresses"})}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#6F42C1"},children:"$"}),e.jsx(n.span,{style:{color:"#032F62"},children:" vike"}),e.jsx(n.span,{style:{color:"#032F62"},children:" dev"}),e.jsx(n.span,{style:{color:"#005CC5"},children:" --host"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"# Change port"})}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#6F42C1"},children:"$"}),e.jsx(n.span,{style:{color:"#032F62"},children:" vike"}),e.jsx(n.span,{style:{color:"#032F62"},children:" preview"}),e.jsx(n.span,{style:{color:"#005CC5"},children:" --port"}),e.jsx(n.span,{style:{color:"#005CC5"},children:" 80"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"# Change mode"})}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#6F42C1"},children:"$"}),e.jsx(n.span,{style:{color:"#032F62"},children:" vike"}),e.jsx(n.span,{style:{color:"#032F62"},children:" build"}),e.jsx(n.span,{style:{color:"#005CC5"},children:" --mode"}),e.jsx(n.span,{style:{color:"#032F62"},children:" staging"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"# Change pre-render settings"})}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#6F42C1"},children:"$"}),e.jsx(n.span,{style:{color:"#032F62"},children:" vike"}),e.jsx(n.span,{style:{color:"#032F62"},children:" build"}),e.jsx(n.span,{style:{color:"#005CC5"},children:" --prerender"}),e.jsx(n.span,{style:{color:"#032F62"},children:' "{parallel:4,noExtraDir:true}"'})]})]})})}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["These are all Vike settings: ",e.jsx(s,{href:"/host",noBreadcrumb:!0})," / ",e.jsx(s,{href:"/port",noBreadcrumb:!0})," / ",e.jsx(s,{href:"/mode",noBreadcrumb:!0})," / ",e.jsx(s,{href:"/prerender",noBreadcrumb:!0}),". (Vike aliases some Vite settings for convenience.)"]}),`
+`]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The CLI option value syntax is JavaScript-like, see ",e.jsx(s,{href:"#json5-syntax"}),"."]}),`
+`]}),`
+`,e.jsxs("h4",{id:"vike-config-environment-variable",children:[e.jsx("code",{children:"VIKE_CONFIG"})," environment variable"]}),`
+`,e.jsxs(n.p,{children:["You can also use the ",e.jsx(n.code,{children:"VIKE_CONFIG"})," environment variable, for example:"]}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"bash","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"bash","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"# Make development server available over LAN and public addresses"})}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#6F42C1"},children:"$"}),e.jsx(n.span,{style:{color:"#032F62"},children:' VIKE_CONFIG="{host:true}"'}),e.jsx(n.span,{style:{color:"#032F62"},children:" vike"}),e.jsx(n.span,{style:{color:"#032F62"},children:" dev"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"# Also when running Vike's CLI over a package.json script"})}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#6F42C1"},children:"$"}),e.jsx(n.span,{style:{color:"#032F62"},children:' VIKE_CONFIG="{prerender:{parallel:4,noExtraDir:true}}"'}),e.jsx(n.span,{style:{color:"#032F62"},children:" npm"}),e.jsx(n.span,{style:{color:"#032F62"},children:" run"}),e.jsx(n.span,{style:{color:"#032F62"},children:" build"})]})]})})}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"VIKE_CONFIG"})," value syntax is JavaScript-like, see ",e.jsx(s,{href:"#json5-syntax"}),"."]}),`
+`]}),`
+`,e.jsx("h2",{id:"vite-settings",children:"Vite settings"}),`
+`,e.jsx("h4",{id:"cli-options",children:"CLI options"}),`
+`,e.jsxs(n.p,{children:["The following ",e.jsx(n.a,{href:"https://vite.dev/guide/cli#options",children:"Vite CLI options"})," are supported:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"--host"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"--mode"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"--port"})}),`
+`]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["These are actually all Vike settings, which alias Vite settings. See: ",e.jsx(s,{href:"/host",noBreadcrumb:!0})," / ",e.jsx(s,{href:"/port",noBreadcrumb:!0})," / ",e.jsx(s,{href:"/mode",noBreadcrumb:!0}),"."]}),`
+`]}),`
+`,e.jsxs(n.p,{children:["For other Vite settings, use ",e.jsx(n.code,{children:"vite.config.js"})," or ",e.jsx(n.code,{children:"VITE_CONFIG"}),"."]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"Reach out if you believe some other Vite setting should also be added the list of CLI options."}),`
+`]}),`
+`,e.jsxs("h4",{id:"vite-config-environment-variable",children:[e.jsx("code",{children:"VITE_CONFIG"})," environment variable"]}),`
+`,e.jsxs(n.p,{children:["You can use the ",e.jsx(n.code,{children:"VITE_CONFIG"})," environment variable to pass ",e.jsx(n.a,{href:"https://vite.dev/config/",children:"any Vite setting"}),", for example:"]}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"bash","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"bash","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"# Set Vite's server.host setting to true"})}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#6F42C1"},children:"$"}),e.jsx(n.span,{style:{color:"#032F62"},children:' VITE_CONFIG="{server:{host:true}}"'}),e.jsx(n.span,{style:{color:"#032F62"},children:" vike"}),e.jsx(n.span,{style:{color:"#032F62"},children:" dev"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"# Also when running Vike's CLI over a package.json script"})}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#6F42C1"},children:"$"}),e.jsx(n.span,{style:{color:"#032F62"},children:` VITE_CONFIG="{build:{outDir:'build'}}"`}),e.jsx(n.span,{style:{color:"#032F62"},children:" npm"}),e.jsx(n.span,{style:{color:"#032F62"},children:" run"}),e.jsx(n.span,{style:{color:"#032F62"},children:" build"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"# Also when running Vike's CLI over a package.json script"})}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#6F42C1"},children:"$"}),e.jsx(n.span,{style:{color:"#032F62"},children:` VITE_CONFIG="{mode:'staging'}"`}),e.jsx(n.span,{style:{color:"#032F62"},children:" npm"}),e.jsx(n.span,{style:{color:"#032F62"},children:" run"}),e.jsx(n.span,{style:{color:"#032F62"},children:" preview"})]})]})})}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"VITE_CONFIG"})," value syntax is JavaScript-like, see ",e.jsx(s,{href:"#json5-syntax"}),"."]}),`
+`]}),`
+`,e.jsx("h2",{id:"json5-syntax",children:"JSON5 syntax"}),`
+`,e.jsxs(n.p,{children:["You can set the value of CLI options, ",e.jsx(n.code,{children:"VITE_CONFIG"}),", and ",e.jsx(n.code,{children:"VIKE_CONFIG"})," using JavaScript-like syntax, ",e.jsx(n.a,{href:"https://json.org/example.html",children:"JSON syntax"}),", or ",e.jsx(n.a,{href:"https://json5.org/#example",children:"JSON5 syntax"}),". (Vike uses ",e.jsx(n.a,{href:"https://json5.org",children:"JSON5"})," to parse the values.)"]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(s,{href:"/api"}),`
+`]}),`
+`]})]})}function d(i={}){const{wrapper:n}=i.components||{};return n?e.jsx(n,{...i,children:e.jsx(l,{...i})}):l(i)}const c=Object.freeze(Object.defineProperty({__proto__:null,default:d,pageSectionsExport:o},Symbol.toStringTag,{value:"Module"})),S={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:a}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/cli/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:c}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:t}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:r}}};export{S as configValuesSerialized};
