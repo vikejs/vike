00f37d20bcfa09fcc41a58add3c1613a24513625
Romuald Brillout <git@brillout.com>
2025-05-02 14:29:24 +0200

    fmt

--- docs/pages/banner/Banner.tsx
+++ docs/pages/banner/Banner.tsx
@@ -37,47 +37,45 @@ function BannerCommon({ logoOnly, logoText = 'Vike' }: { logoOnly?: boolean; log
           scale: '1.3'
         }}
       >
-        <>
-          <div
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            marginBottom: -14,
+            marginLeft: -25
+          }}
+        >
+          <img
+            src={vikeLogo}
             style={{
-              display: 'flex',
-              alignItems: 'center',
-              marginBottom: -14,
-              marginLeft: -25
+              height: 60,
+              objectFit: 'contain',
+              marginRight: 11,
+              position: 'relative',
+              top: -4
+            }}
+          />
+          <span
+            className="logo-font"
+            style={{
+              fontSize: '2.7em',
+              // letterSpacing: 1,
+              color: '#707070',
+              lineHeight: '1em',
+              //*
+              fontWeight: 410
+              /*/
+              fontWeight: 510,
+              background: '-webkit-linear-gradient(300deg, hsl(50.37deg 100% 37.8%) 30%, hsl(347deg 100% 27.56%))',
+              backgroundClip: 'text',
+              WebkitBackgroundClip: 'text',
+              WebkitTextFillColor: 'transparent'
+              //*/
             }}
           >
-            <img
-              src={vikeLogo}
-              style={{
-                height: 60,
-                objectFit: 'contain',
-                marginRight: 11,
-                position: 'relative',
-                top: -4
-              }}
-            />
-            <span
-              className="logo-font"
-              style={{
-                fontSize: '2.7em',
-                // letterSpacing: 1,
-                color: '#707070',
-                lineHeight: '1em',
-                //*
-                fontWeight: 410
-                /*/
-                fontWeight: 510,
-                background: '-webkit-linear-gradient(300deg, hsl(50.37deg 100% 37.8%) 30%, hsl(347deg 100% 27.56%))',
-                backgroundClip: 'text',
-                WebkitBackgroundClip: 'text',
-                WebkitTextFillColor: 'transparent'
-                //*/
-              }}
-            >
-              {logoText}
-            </span>
-          </div>
-        </>
+            {logoText}
+          </span>
+        </div>
         {!logoOnly && <HeroTagline taglineSecondaryStyle={{ marginTop: 19 }} />}
       </div>
     </div>
