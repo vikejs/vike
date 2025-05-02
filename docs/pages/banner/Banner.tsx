e1713ac06cb5b552f2796cea425dc9a4352011e3
Romuald Brillout <git@brillout.com>
2025-05-02 14:50:49 +0200

    marketing: improve cover

--- docs/pages/banner/Banner.tsx
+++ docs/pages/banner/Banner.tsx
@@ -7,14 +7,35 @@ import vikeLogo from '../../assets/logo/vike.svg'
 import './Banner.css'
 
 function BannerVikings() {
-  return <BannerCommon logoOnly logoText="Vikings" />
+  return (
+    <BannerCommon
+      logoOnly
+      logoText="Viking"
+      logoScale={1.1}
+      logoTextStyle={{
+        fontWeight: 496,
+        marginLeft: 11,
+        fontSize: '3.13em',
+        letterSpacing: 0.5,
+        background: '-webkit-linear-gradient(300deg, #973636 20%, #cb4545)',
+        backgroundClip: 'text',
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent'
+      }}
+    />
+  )
 }
 
 function Banner() {
   return <BannerCommon />
 }
 
-function BannerCommon({ logoOnly, logoText = 'Vike' }: { logoOnly?: boolean; logoText?: string }) {
+function BannerCommon({
+  logoOnly,
+  logoText = 'Vike',
+  logoScale = 1.3,
+  logoTextStyle
+}: { logoOnly?: boolean; logoText?: string; logoScale?: number; logoTextStyle?: React.CSSProperties }) {
   return (
     <div
       style={{
@@ -34,7 +55,7 @@ function BannerCommon({ logoOnly, logoText = 'Vike' }: { logoOnly?: boolean; log
           position: 'relative',
           top: -4,
           // Needs to have the right size upon 600x315 (that's the viewport we use to generate banner.png as explained in the HOW-TO above)
-          scale: '1.3'
+          scale: logoScale
         }}
       >
         <div
@@ -58,19 +79,12 @@ function BannerCommon({ logoOnly, logoText = 'Vike' }: { logoOnly?: boolean; log
           <span
             className="logo-font"
             style={{
-              fontSize: '2.7em',
-              // letterSpacing: 1,
+              fontSize: '2.85em',
+              fontWeight: 440,
+              fontStyle: 'italic',
               color: '#707070',
-              lineHeight: '1em',
-              //*
-              fontWeight: 410
-              /*/
-              fontWeight: 510,
-              background: '-webkit-linear-gradient(300deg, hsl(50.37deg 100% 37.8%) 30%, hsl(347deg 100% 27.56%))',
-              backgroundClip: 'text',
-              WebkitBackgroundClip: 'text',
-              WebkitTextFillColor: 'transparent'
-              //*/
+              lineHeight: '1.2em',
+              ...logoTextStyle
             }}
           >
             {logoText}
