209cc801e802729d310b014cd2d9eda4ba02268e
Romuald Brillout <git@brillout.com>
2025-05-08 22:43:48 +0200

    Bati => vike.dev/new

--- docs/components/UseScaffolder.tsx
+++ docs/components/UseScaffolder.tsx
@@ -1,11 +1,12 @@
 export { UseScaffolder }
 
+import { Link } from '@brillout/docpress'
 import React from 'react'
 
 function UseScaffolder({ children }: { children: string | React.ReactElement }) {
   return (
     <p>
-      You can use <a href="https://batijs.dev/">Bati</a> to scaffold a Vike app that uses {children}.
+      You can use <Link href="/new">vike.dev/new</Link> to scaffold a Vike app that uses {children}.
     </p>
   )
 }
