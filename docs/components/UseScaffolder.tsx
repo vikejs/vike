2cfb42b5dff40952f28202840b79a737c1da6a1e
Romuald Brillout <git@brillout.com>
2025-05-08 22:42:11 +0200

    minor refactor

--- docs/components/UseScaffolder.tsx
+++ docs/components/UseScaffolder.tsx
@@ -5,11 +5,7 @@ import React from 'react'
 function UseScaffolder({ children }: { children: string | React.ReactElement }) {
   return (
     <p>
-      You can use <Bati /> to scaffold a Vike app that uses {children}.
+      You can use <a href="https://batijs.dev/">Bati</a> to scaffold a Vike app that uses {children}.
     </p>
   )
-}
-
-function Bati() {
-  return <a href="https://batijs.dev/">Bati</a>
 }
