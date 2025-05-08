667e14d646479fdbb809c1815af9b0d9b241231c
Rom <git@brillout.com>
2025-05-08 22:59:22 +0200

    docs: link and mention vike.dev/new instead of Bati (#2445)

---
+++ docs/components/UseScaffolder.tsx
@@ -0,0 +1,12 @@
+export { UseScaffolder }
+
+import { Link } from '@brillout/docpress'
+import React from 'react'
+
+function UseScaffolder({ children }: { children: string | React.ReactElement }) {
+  return (
+    <p>
+      You can use <Link href="/new">vike.dev/new</Link> to scaffold a Vike app that uses {children}.
+    </p>
+  )
+}
--- docs/components/UseVikeExtensionUiFramework.tsx
+++ docs/components/UseVikeExtensionUiFramework.tsx
@@ -1,20 +1,20 @@
 export { UseVikeExtensionUiFramework }
 
 import React from 'react'
-import { UiFrameworkExtension } from '../components'
+import { UiFrameworkExtension, UseScaffolder } from '../components'
+
+UseScaffolder
 
 function UseVikeExtensionUiFramework({ featureName }: { featureName: string }) {
   return (
     <blockquote>
       <p>
         Instead of manually integrating {featureName} yourself, you can use a <UiFrameworkExtension /> which already
-        integrates {featureName}. And you can use <Bati /> to scaffold an app that uses{' '}
-        <UiFrameworkExtension name noLink />.
+        integrates {featureName}.
       </p>
+      <UseScaffolder>
+        <UiFrameworkExtension name noLink />
+      </UseScaffolder>
     </blockquote>
   )
-}
-
-function Bati() {
-  return <a href="https://batijs.dev/">Bati</a>
 }
