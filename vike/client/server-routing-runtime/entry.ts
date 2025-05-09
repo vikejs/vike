9ecbad30fd10ff77dfe823ce22c112e5522cb669
Romuald Brillout <git@brillout.com>
2025-05-09 17:21:21 +0200

    minor refactor

--- vike/client/server-routing-runtime/entry.ts
+++ vike/client/server-routing-runtime/entry.ts
@@ -3,10 +3,9 @@ assertServerRouting()
 
 import { createPageContextClientSide } from './createPageContextClientSide.js'
 import { executeOnRenderClientHook } from '../shared/executeOnRenderClientHook.js'
-import { getHookFromPageContext } from '../../shared/hooks/getHook.js'
 import { assertSingleInstance_onClientEntryServerRouting } from './utils.js'
 import { removeFoucBuster } from '../shared/removeFoucBuster.js'
-import { executeHook } from '../../shared/hooks/executeHook.js'
+import { executeHookNew } from '../../shared/hooks/executeHook.js'
 import { preparePageContextForUserConsumptionClient } from './preparePageContextForUserConsumptionClient.js'
 // @ts-ignore Since dist/cjs/client/ is never used, we can ignore this error.
 const isProd: boolean = import.meta.env.PROD
@@ -19,6 +18,5 @@ hydrate()
 async function hydrate() {
   const pageContext = await createPageContextClientSide()
   await executeOnRenderClientHook(pageContext, preparePageContextForUserConsumptionClient)
-  const hook = getHookFromPageContext(pageContext, 'onHydrationEnd')
-  if (hook) await executeHook(() => hook.hookFn(pageContext), hook, pageContext)
+  await executeHookNew('onHydrationEnd', pageContext, preparePageContextForUserConsumptionClient)
 }
