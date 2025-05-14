dedb432788a8145b51471bb4ad63acc6f380b774
brillout <github.com@brillout.com>
2025-05-14 17:03:48 +0000

    Deploying to gh-pages from @ vikejs/vike@9e8989804c082763117bd3152b764edd9776f563 🚀

---
+++ assets/entries/pages_onBeforeRenderClient.BfibjDL3.js
@@ -0,0 +1,47 @@
+import{j as e,i as r,L as l,o as s}from"../chunks/chunk-qUIiwDNp.js";import{L as t}from"../chunks/chunk-ZmUpfLkO.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-Dt3G1Mv-.js";import{U as a}from"../chunks/chunk-Dr2FMG3C.js";import{I as d}from"../chunks/chunk-B4bG3RTT.js";/* empty css                      *//* empty css                      *//* empty css                      */const c=[{pageSectionId:"without-vike-react-vue-solid",pageSectionLevel:2,pageSectionTitle:"Without `vike-{react,vue,solid}`"},{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function o(i){const n={blockquote:"blockquote",code:"code",figure:"figure",li:"li",p:"p",pre:"pre",span:"span",ul:"ul",...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"Environment: client."}),`
+`,e.jsx(d,{list:["vike-react","vike-vue"],children:e.jsx(n.code,{children:"onBeforeRenderClient()"})}),`
+`,e.jsx(n.p,{children:"Hook called at the beginning of rendering the page on the client-side. It's usually used for integrating tools, such as state management tools."}),`
+`,e.jsxs(n.p,{children:["It's called upon rendering the first page (e.g. ",e.jsx(t,{href:"/hydration",children:"hydration"})," when using ",e.jsx(t,{href:"/ssr",children:"SSR"}),") and upon page navigation."]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["If you want to perform an action only upon hydration then use ",e.jsx(t,{href:"/pageContext#isHydration",children:e.jsx(n.code,{children:"pageContext.isHydration"})}),":"]}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"js","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"js","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"// +onBeforeRenderClient.js"})}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"export"}),e.jsx(n.span,{style:{color:"#D73A49"},children:" function"}),e.jsx(n.span,{style:{color:"#6F42C1"},children:" onBeforeRenderClient"}),e.jsx(n.span,{style:{color:"#24292E"},children:"("}),e.jsx(n.span,{style:{color:"#E36209"},children:"pageContext"}),e.jsx(n.span,{style:{color:"#24292E"},children:") {"})]}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"  if"}),e.jsx(n.span,{style:{color:"#24292E"},children:" (pageContext.isHydration) {"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"    // ..."})}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#24292E"},children:"  }"})}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#24292E"},children:"}"})})]})})}),`
+`]}),`
+`,e.jsxs(n.p,{children:["It's called by ",e.jsx(n.code,{children:"vike-{react,vue}"})," at the beginning of its ",e.jsx(t,{href:"/onRenderClient",children:e.jsx(n.code,{children:"onRenderClient()"})})," implementation."]}),`
+`,e.jsxs("h2",{id:"without-vike-react-vue-solid",children:["Without ",e.jsx("code",{children:"vike-{react,vue,solid}"})]}),`
+`,e.jsxs(n.p,{children:["If you don't use any ",e.jsx(a,{})," then use ",e.jsxs(t,{href:"/onRenderClient",children:["the ",e.jsx(n.code,{children:"onRenderClient()"})," hook"]})," instead."]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(t,{href:"/onAfterRenderClient"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(t,{href:"/onRenderClient"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(t,{href:"/onHydrationEnd"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(t,{href:"/onPageTransitionStart"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(t,{href:"/onPageTransitionEnd"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(t,{href:"/onBeforeRenderHtml"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(t,{href:"/onAfterRenderHtml"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(t,{href:"/hooks"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(t,{href:"/client"}),`
+`]}),`
+`]})]})}function h(i={}){const{wrapper:n}=i.components||{};return n?e.jsx(n,{...i,children:e.jsx(o,{...i})}):o(i)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:h,pageSectionsExport:c},Symbol.toStringTag,{value:"Module"})),k={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:s}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/onBeforeRenderClient/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:p}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:l}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:r}}};export{k as configValuesSerialized};
