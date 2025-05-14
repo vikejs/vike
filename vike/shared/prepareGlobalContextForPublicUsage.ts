6fb76b6553a6e62cf0b71db0de1ca8e55d02f4c3
Romuald Brillout <git@brillout.com>
2025-05-14 11:29:08 +0200

    small refactor

--- vike/shared/prepareGlobalContextForPublicUsage.ts
+++ vike/shared/prepareGlobalContextForPublicUsage.ts
@@ -1,13 +1,14 @@
 export { prepareGlobalContextForPublicUsage }
 export type { GlobalContextPrepareMinimum }
 
-import { getProxyForPublicUsage } from './utils.js'
+import { assert, getProxyForPublicUsage } from './utils.js'
 
 type GlobalContextPrepareMinimum = { _isOriginalObject: true; isGlobalContext: true }
 
 function prepareGlobalContextForPublicUsage<GlobalContext extends GlobalContextPrepareMinimum>(
   globalContext: GlobalContext
 ) {
+  assert(globalContext._isOriginalObject) // ensure we preserve the original object reference
   const globalContextPublic = getProxyForPublicUsage(globalContext, 'globalContext')
   return globalContextPublic
 }
