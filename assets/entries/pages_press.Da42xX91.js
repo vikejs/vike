901013f966ac789dc08ed57531373dbc9e598125
brillout <github.com@brillout.com>
2025-05-18 21:25:30 +0000

    Deploying to gh-pages from @ vikejs/vike@5dc426514f5b024cab7aa99644535b2dcc0964dd 🚀

---
+++ assets/entries/pages_press.Da42xX91.js
@@ -0,0 +1,26 @@
+import{j as e,i as r,L as a,o as s}from"../chunks/chunk-CW98EJ9W.js";import{L as o}from"../chunks/chunk-zM1tqlFQ.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-CEmQ5Muk.js";import{v as l}from"../chunks/chunk-Bpgn1i5F.js";/* empty css                      *//* empty css                      */const d="/assets/static/vike-dark.Pkud2yI_.svg",c=[{pageSectionId:"logo",pageSectionLevel:2,pageSectionTitle:"Logo"},{pageSectionId:"text",pageSectionLevel:2,pageSectionTitle:"Text"},{pageSectionId:"banner",pageSectionLevel:2,pageSectionTitle:"Banner"}];function n(i){const t={a:"a",blockquote:"blockquote",code:"code",li:"li",p:"p",strong:"strong",ul:"ul",...i.components};return e.jsxs(e.Fragment,{children:[e.jsx("h2",{id:"logo",children:"Logo"}),`
+`,e.jsxs("div",{style:{position:"relative",marginBottom:30},children:[e.jsx(t.p,{children:e.jsx(t.strong,{children:"vike.svg"})}),e.jsx("img",{src:l,height:48}),e.jsxs("div",{style:{position:"absolute",right:0,top:-16,width:"60%",height:"calc(100% + 37px)",paddingLeft:"calc(50% - 200px)",color:"white",background:"black",float:"right"},children:[e.jsx(t.p,{children:e.jsx(t.strong,{children:"vike-dark.svg"})}),e.jsx("img",{src:d,height:48})]})]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:["See ",e.jsxs(t.a,{href:"https://github.com/vikejs/vike-assets",children:["GitHub > ",e.jsx(t.code,{children:"vikejs/vike-assets"})]})," for alternative logos. (Animated, favicon size, variants for different contexts, ...)"]}),`
+`]}),`
+`,e.jsx("h2",{id:"text",children:"Text"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(o,{href:"/"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(o,{href:"/why"}),`
+`]}),`
+`]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsx(t.p,{children:"Reach out if you want more material."}),`
+`]}),`
+`,e.jsx("h2",{id:"banner",children:"Banner"}),`
+`,e.jsx("img",{src:"/banner.jpg",height:"250"}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsx(t.p,{children:"Generated from:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:["Source: ",e.jsx("a",{href:"/banner",rel:"external",children:e.jsx(t.code,{children:"/banner"})})," and ",e.jsx("a",{href:"/banner/cover",rel:"external",children:e.jsx(t.code,{children:"/banner/cover"})})]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.a,{href:"https://gist.github.com/brillout/e7d0a5585471c4ea40d43d2caaadafad",children:"Instructions"})," for generating a 1200x630 ",e.jsx(t.code,{children:"banner.jpg"})]}),`
+`]}),`
+`]})]})}function p(i={}){const{wrapper:t}=i.components||{};return t?e.jsx(t,{...i,children:e.jsx(n,{...i})}):n(i)}const h=Object.freeze(Object.defineProperty({__proto__:null,default:p,pageSectionsExport:c},Symbol.toStringTag,{value:"Module"})),w={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:s}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/press/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:h}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:a}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:r}}};export{w as configValuesSerialized};
