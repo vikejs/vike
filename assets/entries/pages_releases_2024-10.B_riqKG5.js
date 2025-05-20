8818ed78d52878bb754c53f251fa2fd741e20659
brillout <github.com@brillout.com>
2025-05-20 14:56:05 +0000

    Deploying to gh-pages from @ vikejs/vike@8b518619ac3aae640162008c792d8d01cd22df96 🚀

---
+++ assets/entries/pages_releases_2024-10.B_riqKG5.js
@@ -0,0 +1,28 @@
+import{j as e,i as o,L as a,o as d}from"../chunks/chunk-DQz7C77c.js";import{L as n}from"../chunks/chunk-CUWUWe6R.js";/* empty css                      */import{a as l,C as s}from"../chunks/chunk-CV19K5hX.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-Ct7vVvts.js";/* empty css                      *//* empty css                      */const c=[{pageSectionId:"new-bati-integrations",pageSectionLevel:2,pageSectionTitle:"New Bati integrations"},{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function r(t){const i={a:"a",br:"br",code:"code",li:"li",p:"p",strong:"strong",ul:"ul",...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(l,{children:e.jsx(i.p,{children:"This page is work-in-progress."})}),`
+`,e.jsx(s,{children:e.jsxs(i.p,{children:["We are ",e.jsx(i.a,{href:"https://github.com/vikejs/vike/issues/1350",children:"looking for sponsors"}),"."]})}),`
+`,e.jsxs(i.p,{children:[e.jsx(i.strong,{children:"Fixes:"})," ",e.jsx(i.code,{children:"n"})," (TODO)"]}),`
+`,e.jsx(i.p,{children:e.jsx(i.strong,{children:"Features"})}),`
+`,e.jsx(n,{href:"#new-bati-integrations"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(i.p,{children:e.jsx(i.strong,{children:"Project"})}),`
+`,e.jsx(i.p,{children:e.jsx(i.strong,{children:"See also"})}),`
+`,e.jsxs(i.p,{children:[e.jsxs(i.a,{href:"https://github.com/vikejs/vike/blob/main/CHANGELOG.md",children:[e.jsx(i.code,{children:"vike"})," > ",e.jsx(i.code,{children:"CHANGELOG.md"})]}),e.jsx(i.br,{}),`
+`,e.jsxs(i.a,{href:"https://github.com/vikejs/vike-react/blob/main/packages/vike-react/CHANGELOG.md",children:[e.jsx(i.code,{children:"vike-react"})," > ",e.jsx(i.code,{children:"CHANGELOG.md"})]}),e.jsx(i.br,{}),`
+`,e.jsxs(i.a,{href:"https://github.com/vikejs/vike-vue/blob/main/packages/vike-vue/CHANGELOG.md",children:[e.jsx(i.code,{children:"vike-vue"})," > ",e.jsx(i.code,{children:"CHANGELOG.md"})]}),e.jsx(i.br,{}),`
+`,e.jsxs(i.a,{href:"https://github.com/vikejs/vike-solid/blob/main/CHANGELOG.md",children:[e.jsx(i.code,{children:"vike-solid"})," > ",e.jsx(i.code,{children:"CHANGELOG.md"})]})]}),`
+`,e.jsx("h2",{id:"new-bati-integrations",children:"New Bati integrations"}),`
+`,e.jsxs(i.p,{children:["New ",e.jsx(i.a,{href:"https://batijs.dev",children:"Bati"})," integrations:"]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:"TODO"}),`
+`]}),`
+`,e.jsx(s,{children:e.jsxs(i.p,{children:["Author: ",e.jsx(i.a,{href:"https://github.com/phonzammi",children:e.jsx(i.code,{children:"phonzammi"})}),", ",e.jsx(i.a,{href:"https://github.com/magne4000",children:e.jsx(i.code,{children:"magne4000"})})]})}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsxs(i.a,{href:"https://github.com/vikejs/vike/blob/main/CHANGELOG.md",children:[e.jsx(i.code,{children:"vike"})," > ",e.jsx(i.code,{children:"CHANGELOG.md"})]})}),`
+`,e.jsx(i.li,{children:e.jsxs(i.a,{href:"https://github.com/vikejs/vike-react/blob/main/packages/vike-react/CHANGELOG.md",children:[e.jsx(i.code,{children:"vike-react"})," > ",e.jsx(i.code,{children:"CHANGELOG.md"})]})}),`
+`,e.jsx(i.li,{children:e.jsxs(i.a,{href:"https://github.com/vikejs/vike-vue/blob/main/packages/vike-vue/CHANGELOG.md",children:[e.jsx(i.code,{children:"vike-vue"})," > ",e.jsx(i.code,{children:"CHANGELOG.md"})]})}),`
+`,e.jsx(i.li,{children:e.jsxs(i.a,{href:"https://github.com/vikejs/vike-solid/blob/main/CHANGELOG.md",children:[e.jsx(i.code,{children:"vike-solid"})," > ",e.jsx(i.code,{children:"CHANGELOG.md"})]})}),`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(n,{href:"/releases"}),`
+`]}),`
+`]})]})}function h(t={}){const{wrapper:i}=t.components||{};return i?e.jsx(i,{...t,children:e.jsx(r,{...t})}):r(t)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:h,pageSectionsExport:c},Symbol.toStringTag,{value:"Module"})),A={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:d}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/releases/2024-10/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:p}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:a}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:o}}};export{A as configValuesSerialized};
