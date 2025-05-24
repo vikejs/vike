0297213f3e0a6cf40e3e27df415fa252297d751f
brillout <github.com@brillout.com>
2025-05-24 08:36:58 +0000

    Deploying to gh-pages from @ vikejs/vike@5337d51ee004db058f239034f01dd7e6e9b2e8ee 🚀

--- assets/chunks/chunk-jFMeDGda.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./chunk-CGJO_5KW.js";import{L as n}from"./chunk-DlLEOVaK.js";/* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              */import"./chunk-1fQNkwW1.js";function s(o){const e={blockquote:"blockquote",code:"code",p:"p",...o.components};return t.jsxs(t.Fragment,{children:[t.jsxs(e.p,{children:["This setting is global: its value always applies to all your pages. In other words, it doesn't support ",t.jsx(n,{href:"/config#inheritance",children:"config inheritance"}),"."]}),`
-`,t.jsxs(e.blockquote,{children:[`
-`,t.jsxs(e.p,{children:["You cannot set it at ",t.jsx(e.code,{children:"pages/some-page/+config.js"}),": you have to set it at ",t.jsx(e.code,{children:"pages/+config.js"})," (or some other global location) instead."]}),`
-`]}),`
-`,t.jsxs(e.blockquote,{children:[`
-`,t.jsxs(e.p,{children:["If you want to set different values for different pages, then consider creating a custom setting as shown at ",t.jsx(n,{href:"/head-tags#custom-settings"}),"."]}),`
-`]})]})}function g(o={}){const{wrapper:e}=o.components||{};return e?t.jsx(e,{...o,children:t.jsx(s,{...o})}):s(o)}export{g as M};
