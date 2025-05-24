c9fb920967cd24d8e504ad1278208fec2d4ca797
brillout <github.com@brillout.com>
2025-05-24 10:08:44 +0000

    Deploying to gh-pages from @ vikejs/vike@e63189b77534bf4353b0e538dd3f4eadb115b3d4 🚀

---
+++ assets/entries/pages_use-cases.DczIsQo6.js
@@ -0,0 +1,27 @@
+import{j as e,i as o,L as t,o as s}from"../chunks/chunk-DKIhOipQ.js";import{U as a}from"../chunks/chunk-DDz5_V2F.js";import{L as l}from"../chunks/chunk-DZAwQnFa.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-Dg0VW9L9.js";/* empty css                      */const d=[];function r(n){const i={a:"a",blockquote:"blockquote",code:"code",li:"li",p:"p",ul:"ul",...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(i.p,{children:"Vike is used by all kinds of organizations, from small startups to large companies, across a wide range of use cases."}),`
+`,e.jsx(i.p,{children:"Examples of popular websites using Vike:"}),`
+`,e.jsx(a,{}),`
+`,e.jsxs(i.blockquote,{children:[`
+`,e.jsxs(i.p,{children:["You can check whether a website is using Vike by searching for ",e.jsx(i.code,{children:"vike_pageContext"}),` in its HTML.
+Note that Spline and Contra don't use Vike for their landing page but for their main app:`]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:["Check ",e.jsx(i.a,{href:"https://app.spline.design",children:"app.spline.design"})," instead of ",e.jsx(i.a,{href:"https://spline.design",children:"spline.design"})]}),`
+`,e.jsxs(i.li,{children:["Check ",e.jsx(i.a,{href:"https://contra.com/brillout",children:"contra.com/brillout"})," instead of ",e.jsx(i.a,{href:"https://contra.com",children:"contra.com"})]}),`
+`]}),`
+`]}),`
+`,e.jsx(i.p,{children:"Use case examples:"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[e.jsx(i.a,{href:"https://www.burda.com",children:"Burda"})," (one of Germany's largest media companies), migrating their diverse stacks spanning across many teams into one unified ",e.jsx(l,{href:"/why#build-your-own-framework",children:"company internal framework"})," based on Vike and Solid."]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.a,{href:"https://www.alignable.com",children:"Alignable"})," (social network), using a bespoke integration of ","Ruby on Rails <=> Vike <=> React","."]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.a,{href:"https://contra.com",children:"Contra"})," (freelance market), large codebase (>150 routes) with a bespoke integration of ","React <=> Vike <=> GraphQL Relay",", and with a custom rendering strategy mixing SPA and SSR."]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.a,{href:"https://www.ecosia.org",children:"Ecosia"})," (search engine), migrating all their frontend from Nuxt to Vike for much better performance, and for progressively migrating from Vue 2 to Vue 3."]}),`
+`]}),`
+`,e.jsxs(i.blockquote,{children:[`
+`,e.jsx(i.p,{children:"The mentioned companies are/were all sponsoring Vike:"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/orgs/burdaforward/sponsoring",children:"BurdaForward (Burda subsidiary) is sponsoring Vike's creator"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/AlignableUser?tab=sponsoring",children:"AlignableUser is sponsoring Vike's creator"})}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.a,{href:"https://github.com/Ecosia#:~:text=People-,Sponsoring,-Top%20languages",children:"Ecosia is sponsoring Vike"})," (see ",e.jsx(i.code,{children:"Sponsoring"}),")"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.a,{href:"https://github.com/orgs/contra/sponsoring",children:"Contra was sponsoring Vike's creator"})," (click on ",e.jsx(i.code,{children:"Past Sponsorships"}),")"]}),`
+`]}),`
+`]})]})}function c(n={}){const{wrapper:i}=n.components||{};return i?e.jsx(i,{...n,children:e.jsx(r,{...n})}):r(n)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:c,pageSectionsExport:d},Symbol.toStringTag,{value:"Module"})),T={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:s}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/use-cases/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:p}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:t}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:o}}};export{T as configValuesSerialized};
