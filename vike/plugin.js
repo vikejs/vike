00b5207f017206ad8b7b5eb5f239f19c50606732
Romuald Brillout <git@brillout.com>
2025-06-03 14:02:00 +0200

    fix dist refs

--- vike/plugin.js
+++ vike/plugin.js
@@ -1,3 +1,3 @@
 // Some tools still need this as of January 2025
-export * from './dist/esm/node/plugin/index.js'
-export { default } from './dist/esm/node/plugin/index.js'
+export * from './dist/esm/node/vite/index.js'
+export { default } from './dist/esm/node/vite/index.js'
