e3fb18d82b4402d1b52328e59a08d7465eb01dd0
Romuald Brillout <git@brillout.com>
2025-06-03 15:49:11 +0200

    minor refactor

--- vike/node/vite/index.ts
+++ vike/node/vite/index.ts
@@ -9,7 +9,7 @@ export type { VikeVitePluginOptions }
 
 import { type Plugin } from 'vite'
 import { assertUsage } from './utils.js'
-import { build } from './plugins/build.js'
+import { build } from './plugins/pluginBuild.js'
 import { pluginPreview } from './plugins/pluginPreview.js'
 import { pluginDev } from './plugins/pluginDev.js'
 import { pluginVirtualFiles } from './plugins/pluginVirtualFiles.js'
