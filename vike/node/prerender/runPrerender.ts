451eb83b472751da6a0d6f8e6055b6c1e51985d5
Romuald Brillout <git@brillout.com>
2025-06-09 10:34:17 +0200

    minor refactor

--- vike/node/prerender/runPrerender.ts
+++ vike/node/prerender/runPrerender.ts
@@ -143,9 +143,10 @@ type PrerenderOptions = APIOptions & {
 }
 
 async function runPrerenderFromAPI(options: PrerenderOptions = {}): Promise<{ viteConfig: ResolvedConfig }> {
-  return await runPrerender(options, 'prerender()')
   // - We purposely propagate the error to the user land, so that the error interrupts the user land. It's also, I guess, a nice-to-have that the user has control over the error.
   // - We don't use logErrorHint() because we don't have control over what happens with the error. For example, if the user land purposely swallows the error then the hint shouldn't be logged. Also, it's best if the hint is shown to the user *after* the error, but we cannot do/guarentee that.
+  const { viteConfig } = await runPrerender(options, 'prerender()')
+  return { viteConfig }
 }
 async function runPrerenderFromCLIPrerenderCommand(): Promise<void> {
   try {
