cbb3855ad223381300380aa322dca147e8cde414
Romuald Brillout <git@brillout.com>
2025-06-09 22:01:18 +0200

    fix: fix file env error message

--- vike/node/vite/plugins/pluginFileEnv.ts
+++ vike/node/vite/plugins/pluginFileEnv.ts
@@ -11,6 +11,7 @@ import {
   assertUsage,
   assertWarning,
   capitalizeFirstLetter,
+  isFilePathAbsolute,
   joinEnglish,
   rollupSourceMapRemove
 } from '../utils.js'
@@ -131,8 +132,15 @@ function pluginFileEnv(): Plugin {
     )}-only file ${modulePathPretty} (https://vike.dev/file-env) imported on the ${envActual}-side`
 
     if (importers.length > 0) {
-      const importPaths = importers.map((importer) => getModuleFilePathAbsolute(importer, config))
-      errMsg += ` by ${joinEnglish(importPaths, 'and')}`
+      const importPaths = importers
+        .filter((importer) =>
+          // Can be Vike's virtual module: https://github.com/vikejs/vike/issues/2483
+          isFilePathAbsolute(importer)
+        )
+        .map((importer) => getModuleFilePathAbsolute(importer, config))
+      if (importPaths.length > 0) {
+        errMsg += ` by ${joinEnglish(importPaths, 'and')}`
+      }
     }
 
     if (onlyWarn) {
