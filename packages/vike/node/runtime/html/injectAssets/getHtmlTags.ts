1a85760af847006b3e60461a9ceee26e2f685651
Romuald Brillout <git@brillout.com>
2025-06-19 22:53:27 +0200

    Revert "fix"
    This reverts commit 07da239a2c039585f2e256c8ca3b6c6df59222f7.

--- packages/vike/node/runtime/html/injectAssets/getHtmlTags.ts
+++ packages/vike/node/runtime/html/injectAssets/getHtmlTags.ts
@@ -224,7 +224,7 @@ async function getHtmlTags(
 
 function mergeScriptEntries(pageAssets: PageAsset[], viteDevScript: string): null | string {
   const scriptEntries = pageAssets.filter((pageAsset) => pageAsset.isEntry && pageAsset.assetType === 'script')
-  let scriptEntry = `${viteDevScript}${scriptEntries.map((asset) => inferAssetTag(asset)).join('')}`.trim()
+  let scriptEntry = `${viteDevScript}${scriptEntries.map((asset) => inferAssetTag(asset)).join('')}`
   // scriptEntry = mergeScriptTags(scriptEntry)
   return scriptEntry
 }
