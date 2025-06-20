3a205c3f0e379445061b592604f37edfd0afde75
Romuald Brillout <git@brillout.com>
2025-06-20 10:07:45 +0200

    update +docpress

--- packages/vike.dev/+docpress.tsx
+++ packages/vike.dev/+docpress.tsx
@@ -3,44 +3,42 @@ export { config as default }
 import type { Config } from '@brillout/docpress'
 import { headings } from './headings'
 import { headingsDetached, categories } from './headingsDetached'
-import logoUrl from './assets/logo/vike.svg'
-import faviconUrl from './assets/logo/vike-favicon.svg'
+import logo from './assets/logo/vike.svg'
+import favicon from './assets/logo/vike-favicon.svg'
 import { PROJECT_VERSION } from './PROJECT_VERSION'
-import { discordInvite, githubDiscussions } from './links'
+import { discordInvite } from './links'
 import { TopNavigation } from './TopNavigation'
 import React from 'react'
 
 const config: Config = {
-  projectInfo: {
-    projectName: 'Vike',
-    projectVersion: PROJECT_VERSION,
-    githubRepository: 'https://github.com/vikejs/vike',
-    githubIssues: 'https://github.com/vikejs/vike/issues/new',
-    githubDiscussions: githubDiscussions,
-    discordInvite: discordInvite,
-    twitterProfile: 'https://x.com/vike_js',
-    blueskyHandle: 'vike.dev',
-  },
-  docsDir: 'packages/vike.dev',
-  logoUrl,
-  faviconUrl,
+  name: 'Vike',
+  version: PROJECT_VERSION,
+  url: 'https://vike.dev',
+  tagline: 'The Framework *You* Control - Next.js & Nuxt alternative for unprecedented flexibility and stability.',
+  logo,
+  favicon,
+  banner: 'https://vike.dev/banner.jpg',
+
+  github: 'https://github.com/vikejs/vike',
+  discord: discordInvite,
+  twitter: '@vike_js',
+  bluesky: 'vike.dev',
+
   headings,
   headingsDetached,
   categories,
-  tagline: 'The Framework *You* Control - Next.js & Nuxt alternative for unprecedented flexibility and stability.',
-  twitterHandle: '@vike_js',
-  websiteUrl: 'https://vike.dev',
-  sponsorGithubAccount: 'vikejs',
+
   algolia: {
     appId: 'YMV9Y4B58S',
     apiKey: '9ac178c1a29ba00e8afb98365015f677',
     indexName: 'vike',
   },
-  bannerUrl: 'https://vike.dev/banner.jpg',
-  // i18n: true,
+
   pressKit: true,
-  navMaxWidth: 1140,
+  docsDir: 'packages/vike.dev',
+
   topNavigation: <TopNavigation />,
+  navMaxWidth: 1140,
   navLogoSize: 41,
   navLogoStyle: {
     position: 'relative',
