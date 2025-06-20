712145b3396708eaf5af2a5c1d0490a3bf6b7f59
Romuald Brillout <git@brillout.com>
2025-06-20 12:39:11 +0200

    minor refactor

--- packages/vike/node/vite/plugins/pluginVirtualFiles.ts
+++ packages/vike/node/vite/plugins/pluginVirtualFiles.ts
@@ -72,24 +72,26 @@ function handleFileAddRemove(server: ViteDevServer, config: ResolvedConfig) {
     const { moduleGraph } = server
     const isVikeConfig = await isVikeConfigDependency(file, moduleGraph)
     const reload = () => reloadConfig(file, config, isRemove ? 'removed' : 'created', server)
-    if (isVikeConfig) {
-      if (isVikeConfig.isNotProcessedByVite) {
-        reload()
-      } else {
-        // Let Vite handle it
-        assert(existsInViteModuleGraph(file, moduleGraph))
-      }
-    } else {
-      // Trick: when importing a file that doesn't exist => we don't know whether `file` is that missing file => we take a leap of faith when the conditions below are met.
-      // - Not sure how reliable that trick is.
-      // - Reloading Vike's config is cheap and file creation/removal is rare => the trick is worth it.
-      // - Reproduction:
-      //   ```bash
-      //   rm someDep.js && sleep 2 && git checkout someDep.js
-      //   ```
-      if (isScriptFile(file) && getVikeConfigError() && !existsInViteModuleGraph(file, moduleGraph)) {
-        reload()
-      }
+
+    if (isVikeConfig?.isNotProcessedByVite) {
+      reload()
+      return
+    }
+    if (isVikeConfig && !isVikeConfig.isNotProcessedByVite) {
+      // Let Vite handle it
+      assert(existsInViteModuleGraph(file, moduleGraph))
+      return
+    }
+    // Trick: when importing a file that doesn't exist => we don't know whether `file` is that missing file => we take a leap of faith when the conditions below are met.
+    // - Not sure how reliable that trick is.
+    // - Reloading Vike's config is cheap and file creation/removal is rare => the trick is worth it.
+    // - Reproduction:
+    //   ```bash
+    //   rm someDep.js && sleep 2 && git checkout someDep.js
+    //   ```
+    if (isScriptFile(file) && getVikeConfigError() && !existsInViteModuleGraph(file, moduleGraph)) {
+      reload()
+      return
     }
   }
 }
