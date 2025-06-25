17636a1c3c348a4c1000d62b606f9cbd22a1fb65
Romuald Brillout <git@brillout.com>
2025-06-25 14:32:59 +0200

    small refactor

--- packages/vike.dev/+redirects.ts
+++ packages/vike.dev/+redirects.ts
@@ -7,4 +7,11 @@ import type { Config } from 'vike/types'
 const redirects = {
   '/common-problems': '/common-issues',
   '/data-tools': '/data-fetching#page-data-with-tools',
+  '/https': '/server#https',
+  '/css-tools': '/css-in-js',
+  '/errors': '/error-tracking',
+  '/SPA-vs-SSR-vs-HTML': '/render-modes',
+  '/Head-setting': '/head-tags',
+  '/head': '/head-tags',
+  '/ClientOnly': '/clientOnly',
 } satisfies Config['redirects']
--- packages/vike.dev/pages/ClientOnly-legacy/+route.ts
+++
@@ -1,7 +0,0 @@
-// We define:
-//   pages/ClientOnly-legacy/+Page.mdx
-// Instead of:
-//   pages/ClientOnly/+Page.mdx
-// In order to avoid the error/conflict with pages/clientOnly/+Page.mdx (lower-case **c**) on MacOs and Windows.
-// https://github.com/vikejs/vike/issues/1731
-export default '/ClientOnly'
