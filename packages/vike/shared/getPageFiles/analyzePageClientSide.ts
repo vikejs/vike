2adaf3a66dc120894c52a9938f15b967bab905b1
Romuald Brillout <git@brillout.com>
2025-06-25 17:33:02 +0200

    minor refactor: rename TODO:v1-release => TO-DO\/next-major-release

--- packages/vike/shared/getPageFiles/analyzePageClientSide.ts
+++ packages/vike/shared/getPageFiles/analyzePageClientSide.ts
@@ -43,7 +43,7 @@ function analyzePageClientSide(pageFilesAll: PageFile[], pageId: string): Analys
   return { isHtmlOnly, isClientRouting, clientEntries, clientDependencies, pageFilesClientSide, pageFilesServerSide }
 }
 
-// TODO:v1-release: remove
+// TO-DO/next-major-release: remove
 async function analyzePageClientSideInit(
   pageFilesAll: PageFile[],
   pageId: string,
