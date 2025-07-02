27b9d93f46a65d174ef0d5f0f40269ffa8c5b388
Romuald Brillout <git@brillout.com>
2025-07-02 10:30:50 +0200

    small refactor

--- packages/vike.dev/+redirects.ts
+++ packages/vike.dev/+redirects.ts
@@ -2,10 +2,12 @@ export { redirects }
 
 import type { Config } from 'vike/types'
 import type { HeadingsURL } from './headings'
+import { checkType } from './utils/checkType'
 
+// Use TypeScript to check whether redirect targets point to an existing page
 type RemoveHash<T extends string> = T extends `${infer Path}#${string}` ? Path : T
 type RedirectsURL = RemoveHash<(typeof redirects)[keyof typeof redirects]>
-const _typeCheck: HeadingsURL = 0 as any as RedirectsURL
+checkType<HeadingsURL>(0 as any as RedirectsURL)
 
 const redirects = {
   '/common-problems': '/common-issues',
