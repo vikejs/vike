f791d68c511755e2b9977f17e01cb7e32ddc20ab
Romuald Brillout <git@brillout.com>
2025-07-02 11:08:41 +0200

    docs: improve custom integration recommendation

--- packages/vike.dev/components/CustomIntegrationWarning.tsx
+++ packages/vike.dev/components/CustomIntegrationWarning.tsx
@@ -1,14 +1,48 @@
 export { CustomIntegrationWarning }
 
-import { Link } from '@brillout/docpress'
+import { Link, Advanced } from '@brillout/docpress'
 import React from 'react'
-import { UiFrameworkExtension, type UiFrameworkExtensionList, Advanced } from '../components'
+import { UiFrameworkExtension, type UiFrameworkExtensionList } from '../components'
 
-function CustomIntegrationWarning({ list }: { list?: UiFrameworkExtensionList }) {
+function CustomIntegrationWarning({ uiFramework }: { uiFramework?: 'react' | 'vue' | 'solid' }) {
+  const list: UiFrameworkExtensionList | undefined = uiFramework && [`vike-${uiFramework}`]
+  const uiFrameworkName = !uiFramework ? 'React/Vue/Solid/...' : uiFramework[0].toUpperCase() + uiFramework.slice(1)
+  const uiFrameworkExtension = !list ? (
+    <UiFrameworkExtension succinct />
+  ) : (
+    <UiFrameworkExtension name noLink list={list} />
+  )
   return (
     <Advanced>
-      Custom integrations can be complex and we generally recommend using{' '}
-      <UiFrameworkExtension name noLink list={list} /> instead, see <Link href="/extension-vs-custom" />.
+      Custom integrations can be complex and we usually recommend using {uiFrameworkExtension} instead.
+      <br />
+      <br />
+      <span style={{ fontFamily: 'emoji' }}>👉</span> We generally recommend a custom integration only in following
+      scenarios:
+      <ul>
+        <li>
+          You are building an app with a simple architecture.
+          <blockquote>
+            <p>
+              For example, <code>https://vike.dev</code> has a simple architecture and uses a custom integration.
+            </p>
+            <p>
+              You can read the source code of {uiFrameworkExtension} (it's small!) and check whether you'll need most of
+              the code or not — if you do then it most likely makes sense to use {uiFrameworkExtension}.
+            </p>
+          </blockquote>
+        </li>
+        <li>
+          You have already tried {uiFrameworkExtension} but ran into a fundamental blocker with it.
+          <blockquote>
+            <p>We recommend starting a discussion with a {uiFrameworkExtension} maintainer.</p>
+          </blockquote>
+        </li>
+        <li>You are curious and want to deepen your {uiFrameworkName} knowledge.</li>
+      </ul>
+      <p>
+        See also: <Link href="/extension-vs-custom" />.
+      </p>
     </Advanced>
   )
 }
