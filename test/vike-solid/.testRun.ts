ecd6ef90baad3e9c4fb1817e9c91254361d1ba10
Romuald Brillout <git@brillout.com>
2025-07-07 12:03:11 +0200

    fix ensureWasClientSideRouted

--- test/vike-solid/.testRun.ts
+++ test/vike-solid/.testRun.ts
@@ -73,21 +73,21 @@ function testNavigationBetweenWithSSRAndWithoutSSR() {
     body = await page.textContent('body')
     expect(body).toContain(t1)
     expect(body).not.toContain(t2)
-    ensureWasClientSideRouted('/pages/without-ssr')
+    await ensureWasClientSideRouted('/pages/without-ssr')
 
     await page.click('a:has-text("Welcome")')
     await testCounter()
     body = await page.textContent('body')
     expect(body).toContain(t2)
     expect(body).not.toContain(t1)
-    ensureWasClientSideRouted('/pages/without-ssr')
+    await ensureWasClientSideRouted('/pages/without-ssr')
 
     await page.click('a:has-text("Without SSR")')
     await testCounter()
     body = await page.textContent('body')
     expect(body).toContain(t1)
     expect(body).not.toContain(t2)
-    ensureWasClientSideRouted('/pages/without-ssr')
+    await ensureWasClientSideRouted('/pages/without-ssr')
   })
 }
 
@@ -153,10 +153,10 @@ function testUseConfig() {
   test('useConfig() hydration', async () => {
     await page.goto(getServerUrl() + '/')
     await testCounter()
-    ensureWasClientSideRouted('/pages/index')
+    await ensureWasClientSideRouted('/pages/index')
     await page.click('a:has-text("useConfig()")')
     await testCounter()
-    ensureWasClientSideRouted('/pages/index')
+    await ensureWasClientSideRouted('/pages/index')
     await page.goto(getServerUrl() + '/images')
     await testCounter()
   })
@@ -197,7 +197,7 @@ function findFirstPageId(html: string) {
   expect(html).toContain('<script id="vike_pageContext" type="application/json">')
   expect(html).toContain('"pageId"')
   expect(html.split('"pageId"').length).toBe(2)
-  const match = partRegex`"pageId":"${/([^"]+)/}"`.exec(html)
+  const match = partRegex`"pageId":"!${/([^"]+)/}"`.exec(html)
   expect(match).toBeTruthy()
   const pageId = match![1]
   expect(pageId).toBeTruthy()
