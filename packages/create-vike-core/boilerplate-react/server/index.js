e8013153b8bb1be90f925aeecefb3e020a192a8a
Romuald Brillout <git@brillout.com>
2025-07-08 11:40:03 +0200

    fix .ts => .js

--- packages/create-vike-core/boilerplate-react/server/index.js
+++ packages/create-vike-core/boilerplate-react/server/index.js
@@ -1,11 +1,11 @@
-import express from 'express'
-import { apply } from 'vike-server/express'
-import { serve } from 'vike-server/express/serve'
+import { Hono } from 'hono'
+import { apply } from 'vike-server/hono'
+import { serve } from 'vike-server/hono/serve'
 
 export default startServer()
 
-async function startServer() {
-  const app = express()
+function startServer() {
+  const app = new Hono()
   apply(app)
   const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
   return serve(app, { port })
