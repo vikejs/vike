211de5bcc8758b8301ff0bf8ee1b0623cf5ef7a6
Romuald Brillout <git@brillout.com>
2025-07-22 19:19:45 +0200

    use Vite builder API

--- examples/cloudflare-workers-new/pages/+config.js
+++ examples/cloudflare-workers-new/pages/+config.js
@@ -4,6 +4,7 @@ import vikeReact from 'vike-react/config'
 import { Layout } from './Layout'
 
 const config = {
+  vite6BuilderApp: true,
   // https://vike.dev/Layout
   Layout: Layout,
   // https://vike.dev/extends
