1c35f48f337439efeb52d7cd7867532431892770
Romuald Brillout <git@brillout.com>
2025-07-25 09:16:07 +0200

    fix: define builder.buildApp() in pre hook

--- packages/vike/node/vite/plugins/pluginBuild/pluginBuildApp.ts
+++ packages/vike/node/vite/plugins/pluginBuild/pluginBuildApp.ts
@@ -10,6 +10,35 @@ import { getVikeConfigInternal } from '../../shared/resolveVikeConfigInternal.js
 function pluginBuildApp(): Plugin[] {
   return [
     {
+      name: 'vike:build:pluginBuildApp:pre',
+      apply: 'build',
+      enforce: 'pre',
+      config: {
+        order: 'pre',
+        async handler(_config) {
+          const vikeConfig = await getVikeConfigInternal()
+          if (!vikeConfig.config.vite6BuilderApp) return
+
+          return {
+            builder: {
+              // Can be overridden by another plugin e.g vike-vercel https://github.com/vikejs/vike/pull/2184#issuecomment-2659425195
+              async buildApp(builder) {
+                assert(builder.environments.client)
+                assert(builder.environments.ssr)
+                await builder.build(builder.environments.client)
+                await builder.build(builder.environments.ssr)
+
+                if (isPrerenderForceExit()) {
+                  runPrerender_forceExit()
+                  assert(false)
+                }
+              },
+            },
+          }
+        },
+      },
+    },
+    {
       name: 'vike:build:pluginBuildApp',
       apply: 'build',
       async config(config) {
@@ -17,20 +46,6 @@ function pluginBuildApp(): Plugin[] {
         if (!vikeConfig.config.vite6BuilderApp) return
 
         return {
-          builder: {
-            // Can be overridden by another plugin e.g vike-vercel https://github.com/vikejs/vike/pull/2184#issuecomment-2659425195
-            async buildApp(builder) {
-              assert(builder.environments.client)
-              assert(builder.environments.ssr)
-              await builder.build(builder.environments.client)
-              await builder.build(builder.environments.ssr)
-
-              if (isPrerenderForceExit()) {
-                runPrerender_forceExit()
-                assert(false)
-              }
-            },
-          },
           environments: {
             ssr: {
               consumer: 'server',
