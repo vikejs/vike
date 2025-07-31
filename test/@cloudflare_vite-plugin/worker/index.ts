4b8ec3c03dc8546f6ed27e90d142c7bc587a51fd
Romuald Brillout <git@brillout.com>
2025-07-31 22:24:28 +0200

    simplify

--- test/@cloudflare_vite-plugin/worker/index.ts
+++ test/@cloudflare_vite-plugin/worker/index.ts
@@ -3,22 +3,6 @@ import { handleSsr } from './ssr'
 export default {
   async fetch(request) {
     const url = new URL(request.url)
-
-    if (url.pathname.startsWith('/api/')) {
-      return Response.json({
-        name: 'Cloudflare',
-      })
-    }
-
-    if (!isAssetUrl(url)) {
-      return await handleSsr(url)
-    }
-
-    return new Response(null, { status: 404 })
+    return await handleSsr(url)
   },
 } satisfies ExportedHandler<Env>
-
-function isAssetUrl(url: URL) {
-  const { pathname } = new URL(url)
-  return pathname.startsWith('/assets/')
-}
