fe3ee0cd34c33904e87ff61f5aeecdaa008e04db
Romuald Brillout <git@brillout.com>
2025-08-01 09:53:29 +0200

    debug example

--- examples/cloudflare-workers-react/pages/+onCreateGlobalContext.server.ts
+++ examples/cloudflare-workers-react/pages/+onCreateGlobalContext.server.ts
@@ -1,24 +1,21 @@
 export { onCreateGlobalContext }
 
 import type { GlobalContextServer } from 'vike/types'
+// @ts-ignore
+// import { env } from 'cloudflare:workers'
+// console.log('env 1', env)
 
 // TODO/now make it work or replace with +onCreatePageContext.server.js
 // TODO/now add test (copy from it)
 
 async function onCreateGlobalContext(globalContext: GlobalContextServer) {
-  let env: Record<string, string>
-  if (import.meta.env.DEV) {
-    const { getPlatformProxy } = await import('wrangler')
-    const proxy = await getPlatformProxy()
-    env = proxy.env as any
-  } else {
-    // @ts-ignore
-    const exportsAll = await import('cloudflare:workers')
-    // TODO/now remove
-    console.log("import('cloudflare:workers') ", exportsAll)
-    env = exportsAll.env
-  }
+  console.log('+onCreateGlobalContext()')
+  // @ts-ignore
+  const { env } = await import('cloudflare:workers')
+  console.log('env 2', env)
+  console.log("env['SOME_ENV_VAR']", env['SOME_ENV_VAR'])
   globalContext.someEnvVar = env['SOME_ENV_VAR']
+  console.log('globalContext.someEnvVar', globalContext.someEnvVar)
 }
 
 declare global {
