489368451bd8524ef16965ba203cbc3bf6437f03
Romuald Brillout <git@brillout.com>
2025-08-02 08:42:58 +0200

    wip

---
+++ examples/react-full/pages/+config.ts
@@ -0,0 +1,7 @@
+import type { Config } from 'vike/types'
+import vikeCloudflare from 'vike-cloudflare/config'
+
+export default {
+  prerender: true,
+  extends: [vikeCloudflare]
+} satisfies Config
