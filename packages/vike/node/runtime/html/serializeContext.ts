0b182aa1c87ea003a166e76eccc257a81a57aa8c
Romuald Brillout <git@brillout.com>
2025-08-04 14:13:22 +0200

    comment

--- packages/vike/node/runtime/html/serializeContext.ts
+++ packages/vike/node/runtime/html/serializeContext.ts
@@ -146,9 +146,8 @@ function serializeValue(value: unknown, varName?: `pageContext${string}` | `glob
     forbidReactElements: true,
     valueName: varName,
     // Prevent Google from crawling URLs in JSON:
-    //  - https://github.com/vikejs/vike/discussions/2541#discussioncomment-13660198
-    //  - https://github.com/vikejs/vike/discussions/2277
-    //  - https://github.com/vikejs/vike/pull/2542
+    // - https://github.com/vikejs/vike/pull/2603
+    // - https://github.com/brillout/json-serializer/blob/38edbb9945de4938da1e65d6285ce1dd123a45ef/test/main.spec.ts#L44-L95
     replacer(_key, value) {
       if (typeof value === 'string' && value.startsWith('/')) {
         // No need to use a reviver: https://github.com/brillout/json-serializer/blob/70fc8ed3741306391b51655b05df24e6963d1fdb/test/main.spec.ts#L74-L80
