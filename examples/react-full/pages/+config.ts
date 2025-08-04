6942ed974f0ba1e9ca213b48bc95646989b7fd38
Joël Charles <joel.charles91@gmail.com>
2025-08-04 15:10:56 +0200

    chore: set keepDistServer: true

--- examples/react-full/pages/+config.ts
+++ examples/react-full/pages/+config.ts
@@ -2,6 +2,9 @@ import type { Config } from 'vike/types'
 import vikeCloudflare from 'vike-cloudflare/config'
 
 export default {
-  prerender: true,
+  prerender: {
+    enable: true,
+    keepDistServer: true
+  },
   extends: [vikeCloudflare]
 } satisfies Config
