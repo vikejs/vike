62f3b4be04bc0317555d43f9c369ed937f5cc863
Romuald Brillout <git@brillout.com>
2025-08-05 08:35:21 +0200

    use ts

--- .github/workflows/ci/utils.mjs
+++ .github/workflows/ci/utils.mjs
@@ -1,16 +1,13 @@
 export { cmd }
 export { isObject }
-
 import { execSync } from 'node:child_process'
 
-/** @type { (command: string, options?: { cwd?: string }) => string } */
-function cmd(command, { cwd } = { cwd: undefined }) {
+function cmd(command: string, { cwd }: { cwd?: string } = { cwd: undefined }): string {
   let stdout = execSync(command, { encoding: 'utf8', cwd })
   stdout = stdout.split(/\s/).filter(Boolean).join(' ')
   return stdout
 }
 
-/** @type { (value: unknown) => value is Record<string, unknown> } */
-function isObject(value) {
+function isObject(value: unknown): value is Record<string, unknown> {
   return typeof value === 'object' && value !== null
 }
