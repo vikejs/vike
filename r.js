ce7acee2d0879b2144686f072e8b73085b33ef5a
Romuald Brillout <git@brillout.com>
2025-08-05 10:24:20 +0200

    Revert "migration script"
    This reverts commit 44948af8d95148eaf9f1d540d80dfdbba882930e.

--- r.js
+++
@@ -1,8 +0,0 @@
-import { getFiles, replaceFile } from '@brillout/replace'
-
-const files = await getFiles()
-files
-  .filter((f) => f.endsWith('test-e2e.json'))
-  .forEach((file) => {
-    replaceFile(file, (fileContent) => fileContent.replace('"name"', '"ci": { "job"') + '}')
-  })
