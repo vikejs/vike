89ae7505e945a271ec4e9fc68834cd73da784229
Romuald Brillout <git@brillout.com>
2025-08-12 14:52:54 +0200

    minor refactor

--- packages/vike/node/runtime/renderPage/handlePageContextRequestUrl.ts
+++ packages/vike/node/runtime/renderPage/handlePageContextRequestUrl.ts
@@ -1,6 +1,7 @@
 export { handlePageContextRequestUrl }
 
 import { pageContextJsonFileExtension, doNotCreateExtraDirectory } from '../../../shared/getPageContextRequestUrl.js'
+import { modifyUrl } from '../../../shared/modifyUrl.js'
 import { baseServer, parseUrl, assert, slice } from '../utils.js'
 
 type UrlParsed = ReturnType<typeof parseUrl>
@@ -30,13 +31,12 @@ function isMatch(urlParsed: UrlParsed) {
 
 function processUrl(urlParsed: UrlParsed, url: string) {
   // We cannot use `urlParsed.pathname` because it would break the `urlParsed.pathnameOriginal` value of subsequent `parseUrl()` calls.
-  const { origin, pathnameOriginal, searchOriginal, hashOriginal } = urlParsed
+  const { pathnameOriginal } = urlParsed
   assert(doNotCreateExtraDirectory === false)
   const urlSuffix = `/index${pageContextJsonFileExtension}`
   assert(pathnameOriginal.endsWith(urlSuffix), { url })
   let pathnameModified = slice(pathnameOriginal, 0, -1 * urlSuffix.length)
   if (pathnameModified === '') pathnameModified = '/'
-  assert(url === `${origin || ''}${pathnameOriginal}${searchOriginal || ''}${hashOriginal || ''}`, { url })
-  const urlWithoutPageContextRequestSuffix = `${origin || ''}${pathnameModified}${searchOriginal || ''}${hashOriginal || ''}`
+  const urlWithoutPageContextRequestSuffix = modifyUrl(url, { pathname: pathnameModified })
   return { urlWithoutPageContextRequestSuffix }
 }
