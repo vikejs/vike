58b980f22b6b5b7ca4727dbbe599597b449c70f8
Romuald Brillout <git@brillout.com>
2025-08-12 11:35:21 +0200

    minor refactor

--- packages/vike/node/runtime/renderPage/createPageContextServerSide.ts
+++ packages/vike/node/runtime/renderPage/createPageContextServerSide.ts
@@ -12,13 +12,7 @@ type PageContextCreated = Awaited<ReturnType<typeof createPageContextServerSide>
 function createPageContextServerSide(
   pageContextInit: PageContextInit,
   globalContext: GlobalContextServerInternal,
-  {
-    isPrerendering,
-    ssr: { urlHandler, isClientSideNavigation } = {
-      urlHandler: null,
-      isClientSideNavigation: false,
-    },
-  }:
+  args:
     | {
         isPrerendering: false
         ssr: {
@@ -32,6 +26,13 @@ function createPageContextServerSide(
       },
 ) {
   assert(pageContextInit.urlOriginal)
+  const {
+    isPrerendering,
+    ssr: { urlHandler, isClientSideNavigation } = {
+      urlHandler: null,
+      isClientSideNavigation: false,
+    },
+  } = args
 
   const pageContextCreated = createPageContext(pageContextInit, isPrerendering)
 
