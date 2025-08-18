8ae35f6aaa5fd261fa6f2295bdc49401b08c4b9c
Romuald Brillout <git@brillout.com>
2025-08-18 11:30:27 +0200

    align

--- test/vike-solid/.testRun.ts
+++ test/vike-solid/.testRun.ts
@@ -1,5 +1,6 @@
 export { testRun }
 
+// @ts-ignore
 import assert from 'node:assert'
 import { autoRetry, expect, fetchHtml, getServerUrl, page, partRegex, run, test } from '@brillout/test-e2e'
 const dataHk = partRegex`data-hk=${/[0-9-]+/}`
