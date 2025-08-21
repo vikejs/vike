1f14034cb33372ecd0eab10ea9016e4515b7ec52
Romuald Brillout <git@brillout.com>
2025-08-21 11:15:49 +0200

    minor refactor

--- packages/vike/utils/getGlobalObject.ts
+++ packages/vike/utils/getGlobalObject.ts
@@ -4,24 +4,28 @@ export { assertIsSingleModuleInstance }
 import { assert } from './assert.js'
 
 // We use the file name and file directory as key: there should be only one getGlobalObject() usage per file.
-type Key = `${string}.ts`
+type ModuleId = `${string}.ts`
 
 /** Share information across module instances. */
-function getGlobalObject<T extends Record<string, unknown> = never>(key: Key, defaultValue: T): T {
-  const globalObjects = getGlobalObjects()
-  const globalObject = (globalObjects[key] = globalObjects[key] || defaultValue)
-  return globalObject
+function getGlobalObject<T extends Record<string, unknown> = never>(moduleId: ModuleId, defaultValue: T): T {
+  const globals = getGlobals()
+  const globalObject = (globals[moduleId] ??= defaultValue)
+  return globalObject as T
 }
 
 /** Assert that the module is instantiated only once. */
-function assertIsSingleModuleInstance(key: Key): void {
-  const globalObjects = getGlobalObjects()
-  assert(!(key in globalObjects))
+function assertIsSingleModuleInstance(moduleId: ModuleId): void {
+  const globalObjects = getGlobals()
+  assert(!(moduleId in globalObjects))
 }
 
-function getGlobalObjects() {
-  const projectKey = '_vike'
-  // @ts-ignore
-  const globalObjects: any = (globalThis[projectKey] = globalThis[projectKey] || {})
-  return globalObjects
+function getGlobals() {
+  globalThis._vike ??= {}
+  globalThis._vike.globals ??= {}
+  return globalThis._vike.globals!
+}
+declare global {
+  var _vike: {
+    globals?: Record<string, Record<string, unknown>>
+  }
 }
