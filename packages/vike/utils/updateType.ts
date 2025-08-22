8e6608044c268a0391e69df880e116e78d452f8d
Romuald Brillout <git@brillout.com>
2025-08-22 14:44:37 +0200

    comment

--- packages/vike/utils/updateType.ts
+++ packages/vike/utils/updateType.ts
@@ -2,7 +2,7 @@ export { updateType }
 
 import { assert } from './assert.js'
 
-/** Help TypeScript augment the type of objects. */
+/** Help TypeScript update the type of dynamically modified objects. */
 function updateType<Thing, Clone>(thing: Thing, clone: Clone): asserts thing is Thing & Clone {
   // @ts-ignore
   assert(thing === clone)
