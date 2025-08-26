5b2fff3723381961ae6076f5b28bd0453e59b3c6
Romuald Brillout <git@brillout.com>
2025-08-26 16:39:52 +0200

    update to-do

--- packages/vike/node/runtime/globalContext.ts
+++ packages/vike/node/runtime/globalContext.ts
@@ -345,6 +345,7 @@ async function loadProdBuildEntry(outDir?: string) {
 }
 
 // This is the production entry, see:
+// TODO/now update link
 // https://github.com/vikejs/vike/blob/798e5465dc3e3e6723b38b601a50350c0a006fb8/packages/vike/node/vite/plugins/pluginBuild/pluginBuildEntry.ts#L47
 async function setGlobalContext_prodBuildEntry(prodBuildEntry: unknown) {
   debug('setGlobalContext_prodBuildEntry()')
