a3fb52e3934a1e2063516be1b337fb92778e17fa
Rom <git@brillout.com>
2025-08-28 13:31:18 +0200

    fix: Cache-Control header for 5xx HTTP responses (#2669)

--- packages/vike/node/runtime/renderPage/headersResponse.ts
+++ packages/vike/node/runtime/renderPage/headersResponse.ts
@@ -12,15 +12,16 @@ function resolveHeadersResponseFinal(
   },
   statusCode: number,
 ) {
-  const headers: [string, string][] = []
   const headersResponse = pageContext.headersResponse || new Headers()
-  headersResponse.forEach((value, key) => {
-    headers.push([key, value])
-  })
-  // TODO/now fix
+
   // 5xx error pages are temporary and shouldn't be cached.
   // This overrides any previously set Cache-Control value.
   if (statusCode >= 500) headersResponse.set('Cache-Control', cacheControlDisable)
+
+  const headers: [string, string][] = []
+  headersResponse.forEach((value, key) => {
+    headers.push([key, value])
+  })
   return headers
 }
 
