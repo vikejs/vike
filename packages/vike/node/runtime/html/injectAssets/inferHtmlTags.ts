9886405c487b25c23413cf8bea2072972700c416
Romuald Brillout <git@brillout.com>
2025-08-28 12:36:27 +0200

    update to-do

--- packages/vike/node/runtime/html/injectAssets/inferHtmlTags.ts
+++ packages/vike/node/runtime/html/injectAssets/inferHtmlTags.ts
@@ -7,7 +7,6 @@ import { assert } from '../../utils.js'
 import type { PageAsset } from '../../renderPage/getPageAssets.js'
 import { inferNonceAttr, type PageContextCspNonce } from '../../csp.js'
 
-// TODO/now rename scriptCommonAttrs scriptCommonAttrs
 // We can't use `defer` here. With `defer`, the entry script won't start before `</body>` has been parsed, preventing progressive hydration during SSR streaming, see https://github.com/vikejs/vike/pull/1271
 const scriptCommonAttrs = 'type="module" async'
 
