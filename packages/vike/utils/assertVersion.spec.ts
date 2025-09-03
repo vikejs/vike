91097f8b045de457c30c3d956dcb45be0d019ca5
Romuald Brillout <git@brillout.com>
2025-09-03 13:12:22 +0200

    improve tests

--- packages/vike/utils/assertVersion.spec.ts
+++ packages/vike/utils/assertVersion.spec.ts
@@ -28,9 +28,9 @@ describe('isVersionOrAbove()', () => {
     expect(isVersionOrAbove('0.9.0', ['1.0.0', '2.0.0'])).toBe(false) // lower than all
     expect(isVersionOrAbove('5.0.0', ['1.0.0', '2.0.0'])).toBe(true) // higher than any
 
-    expect(isVersionOrAbove('1.2.2', ['1.2.3', '2.3.4'])).toBe(false)
-    expect(isVersionOrAbove('1.2.3', ['1.2.3', '2.3.4'])).toBe(true)
-    expect(isVersionOrAbove('1.2.4', ['1.2.3', '2.3.4'])).toBe(true)
+    expect(isVersionOrAbove('1.2.2', ['0.1.2', '1.2.3', '2.3.4'])).toBe(false)
+    expect(isVersionOrAbove('1.2.3', ['0.1.2', '1.2.3', '2.3.4'])).toBe(true)
+    expect(isVersionOrAbove('1.2.4', ['0.1.2', '1.2.3', '2.3.4'])).toBe(true)
   })
 
   it('pre-release versions', () => {
