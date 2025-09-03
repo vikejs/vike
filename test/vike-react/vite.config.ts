83573f1e8c69e7cb26d2504ecdd606b6b8a59fc5
Romuald Brillout <git@brillout.com>
2025-09-03 18:39:09 +0200

    minor refactor: rename import { UserConfig } from 'vite' => import type { UserConfig } from 'vite'

--- test/vike-react/vite.config.ts
+++ test/vike-react/vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react'
 import vike from 'vike/plugin'
-import { UserConfig } from 'vite'
+import type { UserConfig } from 'vite'
 
 export default {
   plugins: [react(), vike()],
