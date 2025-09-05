681507d580f874a9fbd124c4a406c4549dd9159c
Romuald Brillout <git@brillout.com>
2025-09-05 18:11:05 +0200

    wip

--- docs/headings.ts
+++ docs/headings.ts
@@ -135,7 +135,7 @@ const headings = [
     level: 2,
     title: 'Pre-rendering (SSG)',
     url: '/pre-rendering',
-    sectionTitles: ['SPA'],
+    sectionTitles: ['SPA', 'SSG vs SSR'],
   },
   {
     level: 2,
