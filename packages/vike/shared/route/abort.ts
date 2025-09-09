94a65c1224af3dfdee7afb8875d31f215be8106a
Romuald Brillout <git@brillout.com>
2025-09-09 18:25:26 +0200

    minor refactor

--- packages/vike/shared/route/abort.ts
+++ packages/vike/shared/route/abort.ts
@@ -32,7 +32,7 @@ import pc from '@brillout/picocolors'
 // For improved IntelliSense, we define the list of status code directly on redirect()'s argument type
 type RedirectStatusCode = number & Parameters<typeof redirect>[1]
 // For improved IntelliSense, we duplicate this list
-type AbortStatusCode = Parameters<InferTwoOverloads<typeof render>[0]>[0]
+type AbortStatusCode = number & Parameters<InferTwoOverloads<typeof render>[0]>[0]
 
 type UrlRedirect = {
   url: string
