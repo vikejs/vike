eba9b0a4097cad3a16d3b96c0c587063a16de4b8
Romuald Brillout <git@brillout.com>
2025-09-09 18:57:27 +0200

    Revert "debug"
    This reverts commit 2b0f2b993e2cb9e791032b1fc550318c25bcb33e.

--- packages/vike/node/vite/plugins/pluginCommon.ts
+++ packages/vike/node/vite/plugins/pluginCommon.ts
@@ -54,7 +54,6 @@ function pluginCommon(vikeVitePluginOptions: unknown): Plugin[] {
             configVikePromise: Promise.resolve({
               prerender: vikeConfig.prerenderContext.isPrerenderingEnabled,
             }),
-            ssr: { external: ['vike'] },
           }
         },
       },
