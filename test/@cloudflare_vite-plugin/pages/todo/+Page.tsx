7f5a135f92cf1eee101cf10d61bc7fbdd8e3db65
Romuald Brillout <git@brillout.com>
2025-09-12 00:40:13 +0200

    align CF example

---
+++ test/@cloudflare_vite-plugin/pages/todo/+Page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TodoList } from './TodoList.js'
+
+export default function Page() {
+  return (
+    <>
+      <h1>To-do List</h1>
+      <TodoList />
+    </>
+  )
+}
--- test/@cloudflare_vite-plugin/testRun.ts
+++ test/@cloudflare_vite-plugin/testRun.ts
@@ -1,7 +1,7 @@
 export { testRun }
 export { testCloudflareBindings }
 
-import { expect, getServerUrl, page, test } from '@brillout/test-e2e'
+import { autoRetry, expect, getServerUrl, page, test } from '@brillout/test-e2e'
 import { testCounter, testRunClassic } from '../../test/utils'
 
 type CMD = 'npm run dev' | 'npm run preview'
@@ -9,6 +9,7 @@ type CMD = 'npm run dev' | 'npm run preview'
 function testRun(cmd: CMD) {
   testCloudflareBindings()
   testRunClassic(cmd)
+  testTodolist()
 }
 
 function testCloudflareBindings() {
@@ -20,4 +21,44 @@ function testCloudflareBindings() {
     expect(bodyText).toContain('some-value')
     expect(bodyText).toContain(`pageContext.globalContext.someEnvVar === ${JSON.stringify('some-value')}`)
   })
+}
+
+function testTodolist() {
+  test('To-Do list', async () => {
+    await page.goto(getServerUrl() + '/todo')
+    await page.locator('button', { hasText: 'Reset' }).click()
+    await autoRetry(
+      async () => {
+        expect(await getNumberOfItems()).toBe(3)
+      },
+      { timeout: 5000 },
+    )
+    {
+      const reactText = await page.textContent('#root')
+      expect(reactText).toContain('Buy milk')
+      expect(reactText).not.toContain('Buy bananas')
+    }
+    const todoItemNew = `Buy bananas ${Math.random()}`
+    await page.fill('input[type="text"]', todoItemNew)
+    await page.click('button[type="submit"]')
+    await autoRetry(
+      async () => {
+        expect(await getNumberOfItems()).toBe(4)
+      },
+      { timeout: 5000 },
+    )
+    expect(await page.textContent('#root')).toContain(todoItemNew)
+    await fullPageReload()
+    expect(await getNumberOfItems()).toBe(4)
+    expect(await page.textContent('#root')).toContain(todoItemNew)
+  })
+}
+async function getNumberOfItems() {
+  return await page.evaluate(() => document.querySelectorAll('li').length)
+}
+
+async function fullPageReload() {
+  await page.goto(getServerUrl() + '/')
+  await testCounter()
+  await page.goto(getServerUrl() + '/todo')
 }
