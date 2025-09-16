bdbcda614135bcf6e54ebd9f94ff16c07193e911
Romuald Brillout <git@brillout.com>
2025-09-17 00:53:23 +0200

    comment

--- packages/vike/client/runtime-client-routing/renderPageClientSide.ts
+++ packages/vike/client/runtime-client-routing/renderPageClientSide.ts
@@ -838,6 +838,7 @@ function logError(err: unknown) {
   if (
     isObject(err) &&
     // Set by vike-react
+    // https://github.com/vikejs/vike-react/blob/195a208c6b77e7f34496e1f637278a36c60fbe07/packages/vike-react/src/integration/onRenderClient.tsx#L109
     err.isAlreadyLogged
   ) {
     return
