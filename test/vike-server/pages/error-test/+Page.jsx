9ef33ff49b6dedeff1ff1e4dd6fce7c1a217e520
Romuald Brillout <git@brillout.com>
2025-09-17 22:02:14 +0200

    remove test

--- test/vike-server/pages/error-test/+Page.jsx
+++
@@ -1,8 +0,0 @@
-export { Page }
-
-function Page() {
-  // This will throw an error during rendering to test the onError hook
-  throw new Error('Test error for onError hook')
-  
-  return <div>This should not be rendered</div>
-}
--- test/vike-server/pages/error-test/+data.js
+++
@@ -1,6 +0,0 @@
-export { data }
-
-async function data() {
-  // This will throw an error during data fetching to test the onError hook
-  throw new Error('Test data error for onError hook')
-}
