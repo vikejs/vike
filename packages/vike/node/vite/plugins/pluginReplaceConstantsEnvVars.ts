3d03e9038de37971c82cbfaed0f918f7472144e9
Rom <git@brillout.com>
2025-09-17 20:00:26 +0200

    fix: support constants like `import.meta.env.DEV` in Vitest (#2734)

--- packages/vike/node/vite/plugins/pluginReplaceConstantsEnvVars.ts
+++ packages/vike/node/vite/plugins/pluginReplaceConstantsEnvVars.ts
@@ -80,6 +80,8 @@ function pluginReplaceConstantsEnvVars(): Plugin[] {
           const replacements = Object.entries(envVarsAll)
             // Skip env vars that start with [`config.envPrefix`](https://vite.dev/config/shared-options.html#envprefix) => they are already handled by Vite
             .filter(([envName]) => !envPrefix.some((prefix) => envName.startsWith(prefix)))
+            // Skip import.meta.env.DEV and company => they are already handled by Vite
+            .filter(([envName]) => !['DEV', 'PROD', 'SSR', 'MODE', 'BASE_URL'].includes(envName))
             .map(([envName, envVal]) => {
               const envStatement = `import.meta.env.${envName}` as const
               const envStatementRegExpStr = escapeRegex(envStatement) + '\\b'
