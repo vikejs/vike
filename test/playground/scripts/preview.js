a9320d80372326a67fd02a0b2252c707432ca4ad
Romuald Brillout <git@brillout.com>
2025-09-17 21:26:33 +0200

    fix test

--- test/playground/scripts/preview.js
+++ test/playground/scripts/preview.js
@@ -1,6 +1,7 @@
 import { build, preview } from 'vike/api'
 import assert from 'node:assert'
 await build()
+if (process.env.BUILD_TWICE) await build()
 assert(globalThis.prerenderContextWasTested)
 console.log('Starting preview server...')
 const { viteConfig } = await preview()
