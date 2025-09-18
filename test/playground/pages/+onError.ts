594f0666900bdfbfdb8ffa2464ab1d1cf34955c7
Romuald Brillout <git@brillout.com>
2025-09-18 15:00:20 +0200

    docs

--- test/playground/pages/+onError.ts
+++ test/playground/pages/+onError.ts
@@ -1,6 +1,6 @@
 export { onError }
 
-import { Config } from 'vike/types'
+import type { Config } from 'vike/types'
 
 const onError: Config['onError'] = (error) => {
   console.error('+onError', error)
