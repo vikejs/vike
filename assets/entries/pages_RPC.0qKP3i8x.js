1b31232d8299407f24afb64e1415b2e99155ea56
brillout <github.com@brillout.com>
2025-09-18 14:24:24 +0000

    Deploying to gh-pages from @ vikejs/vike@80b3567f6f76d85aa6e9fbff140fcf882eaec9a2 🚀

---
+++ assets/entries/pages_RPC.0qKP3i8x.js
@@ -0,0 +1,37 @@
+import{o as i,a as r}from"../chunks/chunk-YrzSF-5M.js";import{j as e}from"../chunks/chunk-DylOdVSZ.js";import{L as l}from"../chunks/chunk-DnqwWAsR.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */const s=[{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function o(n){const t={a:"a",blockquote:"blockquote",code:"code",li:"li",p:"p",strong:"strong",ul:"ul",...n.components};return e.jsxs(e.Fragment,{children:[e.jsxs(t.p,{children:["We generally recommend using Vike with an RPC tool. See ",e.jsx(l,{href:"/data-fetching"}),"."]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsx(t.p,{children:e.jsx(t.strong,{children:"What is RPC?"})}),`
+`,e.jsxs(t.p,{children:[e.jsxs(t.a,{href:"https://en.wikipedia.org/wiki/Remote_procedure_call",children:[e.jsx(t.strong,{children:"R"}),"emote ",e.jsx(t.strong,{children:"P"}),"rocedure ",e.jsx(t.strong,{children:"C"}),"all"]})," (RPC, aka Server Actions) enables the frontend to seamlessly call backend functions, see in-depth explanation ",e.jsx(t.a,{href:"https://telefunc.com/RPC",children:"here"}),"."]}),`
+`,e.jsx(t.p,{children:"It's an increasingly popular data-fetching technique used instead of GraphQL and REST. (You can also use multiple data-fetching techniques, although the use case for it is seldom.)"}),`
+`]}),`
+`,e.jsx(t.p,{children:"Common RPC tools:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(t.p,{children:e.jsx(t.a,{href:"https://telefunc.com",children:"Telefunc"})}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsx(t.p,{children:"Telefunc was created by the Vike team."}),`
+`]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:["You'll soon be able to use Telefunc also to fetch initial data (instead of using the ",e.jsx(t.code,{children:"data()"})," hook), see ",e.jsx(t.a,{href:"https://github.com/brillout/telefunc/issues/102",children:"Telefunc > #102"}),"."]}),`
+`]}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(t.p,{children:e.jsx(t.a,{href:"https://trpc.io/",children:"tRPC"})}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsx(t.p,{children:"Similar to Telefunc:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Con: Telefunc is simpler."}),`
+`,e.jsx(t.li,{children:"Pro: supports real-time data using websockets."}),`
+`]}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(l,{href:"/data-fetching"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(l,{href:"/api-routes"}),`
+`]}),`
+`]})]})}function a(n={}){const{wrapper:t}=n.components||{};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}const d=Object.freeze(Object.defineProperty({__proto__:null,default:a,pageSectionsExport:s},Symbol.toStringTag,{value:"Module"})),T={hasServerOnlyHook:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!1}},isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:r}},onCreatePageContext:{type:"cumulative",definedAtData:[{filePathToShowToUser:"@brillout/docpress/renderer/onCreatePageContext",fileExportPathToShowToUser:[]}],valueSerialized:[{type:"pointer-import",value:i}]},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/RPC/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:d}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}}};export{T as configValuesSerialized};
