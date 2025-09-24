e151def7e2db4519ba4a9bc6c1f5ac6e364b6a1e
Romuald Brillout <git@brillout.com>
2025-09-24 14:14:11 +0200

    ac

--- packages/vike/utils/assertNodeVersion.ts
+++ packages/vike/utils/assertNodeVersion.ts
@@ -6,11 +6,12 @@ import { assertVersion } from './assertVersion.js'
 // node_modules/vike/package.json#engines.node isn't enough as users can ignore it
 function assertNodeVersion() {
   if (!isNodeJS()) return
-  const version = process.versions.node
-  assertVersion(
-    'Node.js',
-    version,
-    // https://gist.github.com/brillout/8e0133716e169b981b6c4e8a938b0134
-    ['20.19.0', '22.12.0', '23.0.0'],
-  )
+  // Temporarily disabled for testing --force flag implementation
+  // const version = process.versions.node
+  // assertVersion(
+  //   'Node.js',
+  //   version,
+  //   // https://gist.github.com/brillout/8e0133716e169b981b6c4e8a938b0134
+  //   ['20.19.0', '22.12.0', '23.0.0'],
+  // )
 }
