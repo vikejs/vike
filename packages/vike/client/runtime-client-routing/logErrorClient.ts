c78ba6c187239b9e800a12882b7c9006ce90655c
Romuald Brillout <git@brillout.com>
2025-09-27 16:13:27 +0200

    minor refactor

---
+++ packages/vike/client/runtime-client-routing/logErrorClient.ts
@@ -0,0 +1,15 @@
+export { logErrorClient }
+
+import { isObject } from './utils.js'
+
+function logErrorClient(err: unknown) {
+  if (
+    isObject(err) &&
+    // Set by vike-react
+    // https://github.com/vikejs/vike-react/blob/195a208c6b77e7f34496e1f637278a36c60fbe07/packages/vike-react/src/integration/onRenderClient.tsx#L109
+    err.isAlreadyLogged
+  ) {
+    return
+  }
+  console.error(err)
+}
