6d5ed71068a95e5a2a7c28647de460b833e4e185
Romuald Brillout <git@brillout.com>
2025-09-30 22:21:15 +0200

    fix: support getEnhancedError()

--- packages/vike/node/runtime/logErrorServer.ts
+++ packages/vike/node/runtime/logErrorServer.ts
@@ -1,11 +1,17 @@
 export { logErrorServer }
 
 import pc from '@brillout/picocolors'
-import { isObject } from './utils.js'
+import { isCallable, isObject } from './utils.js'
 import { execHookOnError } from './renderPage/execHookOnError.js'
 
 function logErrorServer(err: unknown) {
   execHookOnError(err)
+
+  // Set by react-streaming
+  // - It doesn't seem to be needed? (The error Vike receives is already enhanced.) Can we remove this?
+  if (isObject(err) && isCallable(err.getEnhancedError)) {
+    err = err.getEnhancedError(err)
+  }
 
   // We ensure we print a string; Cloudflare Workers doesn't seem to properly stringify `Error` objects.
   // - TO-DO/eventually: is that still true? Let's eventually remove it and see if it crashes Cloudflare.
