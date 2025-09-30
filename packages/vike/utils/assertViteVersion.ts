9de18506ea504c64a2928d501efbc2a800d701a2
Romuald Brillout <git@brillout.com>
2025-09-30 21:43:37 +0200

    comment

--- packages/vike/utils/assertViteVersion.ts
+++ packages/vike/utils/assertViteVersion.ts
@@ -2,12 +2,12 @@ export { assertViteVersion }
 
 import { assertVersion } from './assertVersion.js'
 
-// node_modules/vike/package.json#engines.node isn't enough as users can ignore it
+// node_modules/vike/package.json#peerDependencies isn't enough, as users often ignore it
 function assertViteVersion(viteVersion: string) {
-  // package.json#peerDependencies isn't enough as users often ignore it
-  // This assertion isn't reliable: the user may still use a Vite version older than 6.0.0 — see https://github.com/vitejs/vite/pull/19355
-  // TO-DO/eventually: let's also use this.meta.viteVersion
-  // - https://github.com/vitejs/vite/pull/20088
-  // - https://github.com/vitejs/vite/blob/main/packages/vite/CHANGELOG.md#700-2025-06-24
+  // - This assertion isn't reliable: the user may still use a Vite version older than 6.0.0 — see https://github.com/vitejs/vite/pull/19355
+  //   - TO-DO/eventually: let's also use this.meta.viteVersion
+  //     - this.meta.viteVersion was released in vite@7.0.0 => let's use it only after Vike requires a Vite version above 7.0.0
+  //     - https://github.com/vitejs/vite/blob/main/packages/vite/CHANGELOG.md#700-2025-06-24
+  //     - https://github.com/vitejs/vite/pull/20088
   assertVersion('Vite', viteVersion, ['6.3.0'])
 }
