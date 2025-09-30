f8f0bcb2d24e2041f8669a5dda0e9aef003f5c8e
Romuald Brillout <git@brillout.com>
2025-09-30 21:50:53 +0200

    fix: remove prettifyThisError() support

--- packages/vike/node/runtime/logErrorServer.ts
+++ packages/vike/node/runtime/logErrorServer.ts
@@ -1,21 +1,11 @@
 export { logErrorServer }
 
 import pc from '@brillout/picocolors'
-import { isCallable, isObject } from './utils.js'
+import { isObject } from './utils.js'
 import { execHookOnError } from './renderPage/execHookOnError.js'
 
 function logErrorServer(err: unknown) {
   execHookOnError(err)
-
-  // TODO https://gist.github.com/brillout/066293a687ab7cf695e62ad867bc6a9c
-  if (
-    isObject(err) &&
-    // Set by react-streaming
-    // https://github.com/brillout/react-streaming/blob/0f93e09059a5936a1fb581bc1ce0bce473e0d5e0/src/server/renderToStream/common.ts#L36
-    isCallable(err.prettifyThisError)
-  ) {
-    err = err.prettifyThisError(err)
-  }
 
   // We ensure we print a string; Cloudflare Workers doesn't seem to properly stringify `Error` objects.
   // - TO-DO/eventually: is that still true? Let's eventually remove it and see if it crashes Cloudflare.
