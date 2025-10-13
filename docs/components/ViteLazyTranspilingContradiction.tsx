967cf8cd810a25e5303b0ee9e0033963d0291c33
Romuald Brillout <git@brillout.com>
2025-10-13 13:36:23 +0200

    docs: polish /markdown

--- docs/components/ViteLazyTranspilingContradiction.tsx
+++ docs/components/ViteLazyTranspilingContradiction.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 function ViteLazyTranspilingContradiction() {
   return (
     <>
-      contradicts <Link href="/lazy-transpiling">Vite's lazy-transpiling approach</Link> and it would, therefore,
+      contradicts <Link href="/lazy-transpiling">Vite's lazy-transpiling approach</Link> and it would
       significantly slow down development speed
     </>
   )
