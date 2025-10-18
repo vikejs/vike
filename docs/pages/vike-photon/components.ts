e24b4fefd54c0a837bed6789c21897eacf0b6a30
Romuald Brillout <git@brillout.com>
2025-10-18 16:16:56 +0200

    minor refactor

--- docs/pages/vike-photon/components.ts
+++ docs/pages/vike-photon/components.ts
@@ -1,4 +1,4 @@
-export * from './BetaWarning'
+export * from './VikePhotonBetaWarning'
 export { default as PhotonAutoLoad } from './PhotonAutoLoad.mdx'
 export { default as PhotonBuiltInServer } from './PhotonBuiltInServer.mdx'
 export * from './VikePhotonDescription'
