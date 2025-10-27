a1e5b0851df7d8c0c32394e3c9b678565c03712c
Romuald Brillout <git@brillout.com>
2025-10-27 23:29:02 +0100

    wip

--- docs/pages/blog/BlogHeader.tsx
+++ docs/pages/blog/BlogHeader.tsx
@@ -3,7 +3,7 @@ export { BlogHeader }
 import { getMaintainer, getMaintainerAvatar, type Maintainer, type MaintainerUsername } from '../team/maintainersList'
 import React from 'react'
 
-function BlogHeader({ authors, date }: { authors: MaintainerUsername[]; date: Date }) {
+function BlogHeader({ authors, date }: { authors: MaintainerUsername[]; date?: Date }) {
   const maintainers = authors.map(getMaintainer)
   return (
     <div
@@ -29,20 +29,22 @@ function BlogHeader({ authors, date }: { authors: MaintainerUsername[]; date: Da
           <Author key={maintainer.username} maintainer={maintainer} />
         ))}
       </div>
-      <div
-        style={{
-          color: '#999',
-          fontWeight: 400,
-          fontSize: 15,
-          fontStyle: 'italic',
-        }}
-      >
-        {date.toLocaleDateString('en-US', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        })}
-      </div>
+      {date && (
+        <div
+          style={{
+            color: '#999',
+            fontWeight: 400,
+            fontSize: 15,
+            fontStyle: 'italic',
+          }}
+        >
+          {date.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          })}
+        </div>
+      )}
     </div>
   )
 }
