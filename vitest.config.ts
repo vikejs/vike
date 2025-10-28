961aba2ee834df929593d7ed1283636aef295084
Romuald Brillout <git@brillout.com>
2025-10-28 19:26:31 +0100

    update vitest.config.ts

---
+++ vitest.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vitest/config'
+
+const env = {
+  NO_COLOR: '1',
+}
+
+export default defineConfig({
+  test: {
+    projects: [
+      {
+        test: {
+          include: ['packages/**/*.spec.ts'],
+          name: 'unit',
+          env,
+        },
+      },
+      {
+        test: {
+          // **/*.test.ts => @brillout/test-e2e
+          include: ['test/**/*.spec.ts'],
+          name: 'e2e',
+          env,
+        },
+      },
+    ],
+  },
+})
