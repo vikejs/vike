55042e9e2e64e405823a0efabf9abc3f67308e1f
Romuald Brillout <git@brillout.com>
2025-11-03 22:52:22 +0100

    add test

--- test/playground/+vite.ts
+++ test/playground/+vite.ts
@@ -1,11 +1,12 @@
 import mdx from '@mdx-js/rollup'
+import assert from 'node:assert'
 import { getVikeConfig } from 'vike/plugin'
 import type { Config } from 'vike/types'
 import type { UserConfig } from 'vite'
 
 export default (() => {
-  // @ts-ignore
-  const vike = getVikeConfig()
+  const vikeConfig = getVikeConfig()
+  assert(vikeConfig.pages['/pages/markdown']!.route === '/markdown')
   return {
     // TEST: use +vite to add Vite plugin
     plugins: [mdx()],
