7de03bce931147ed296dd24b724f8aa2dcf07cae
Romuald Brillout <git@brillout.com>
2025-11-03 22:31:42 +0100

    reproduce

--- test/playground/+vite.ts
+++ test/playground/+vite.ts
@@ -1,7 +1,25 @@
 import mdx from '@mdx-js/rollup'
+import { getVikeConfig } from 'vike/plugin'
 import type { UserConfig } from 'vite'
 
 export default {
   // TEST: use +vite to add Vite plugin
-  plugins: [mdx()],
+  plugins: [
+    mdx(),
+    (async () => {
+      console.log('+vite plugin', new Error().stack)
+      await sleep(1)
+      // @ts-ignore
+      const vike = getVikeConfig()
+      console.log('vike', vike)
+      console.log(2)
+      return {
+        name: 'bla',
+      }
+    })(),
+  ],
 } satisfies UserConfig
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
