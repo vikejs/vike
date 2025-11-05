a387ee3fac5e71a04611469016058cc1ada80c11
Romuald Brillout <git@brillout.com>
2025-11-05 19:23:58 +0100

    reproduce

--- examples/telefunc/pages/index/+onBeforeRender.ts
+++ examples/telefunc/pages/index/+onBeforeRender.ts
@@ -6,6 +6,8 @@ import { todoItems } from '../../database/todoItems'
 
 const onBeforeRender = async (pageContext: PageContextServer) => {
   const todoItemsInitial = todoItems
+  // @ts-ignore
+  pageContext.routeParams.url = new URL('https://vike.dev/bla')
   return {
     pageContext: {
       pageProps: {
