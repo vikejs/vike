8fb60e4e2fc0380ab90bd08b925ee46ce749c6f8
Romuald Brillout <git@brillout.com>
2025-11-09 11:15:22 +0100

    comment

--- test/playground/pages/config-meta/effect/with-effect-set-value/+config.ts
+++ test/playground/pages/config-meta/effect/with-effect-set-value/+config.ts
@@ -1,7 +1,5 @@
 import { Config } from 'vike/types'
 
-// dependentSetting is influenced both by the ConfigEffect
-// of settingWithEffect and by a locally set value
 export default {
   settingWithEffect: 'setEnvAndValue',
 } satisfies Config
