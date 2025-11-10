713ffb3664e49ee5f623a43d625b44789dcd8b59
Romuald Brillout <git@brillout.com>
2025-11-10 22:10:41 +0100

    minor refactor

--- packages/vike/node/runtime/globalContext.ts
+++ packages/vike/node/runtime/globalContext.ts
@@ -353,9 +353,9 @@ async function loadProdBuildEntry(outDir?: string) {
 // https://github.com/vikejs/vike/blob/8c350e8105a626469e87594d983090919e82099b/packages/vike/node/vite/plugins/pluginBuild/pluginProdBuildEntry.ts#L47
 async function setGlobalContext_prodBuildEntry(prodBuildEntry: unknown) {
   debug('setGlobalContext_prodBuildEntry()')
-  setNodeEnvProduction()
   assert(!isNonRunnableDev())
   assertProdBuildEntry(prodBuildEntry)
+  setNodeEnvProduction()
   globalObject.prodBuildEntry = prodBuildEntry
   globalObject.prodBuildEntryPrevious = prodBuildEntry
   assert(globalObject.prodBuildEntry) // ensure no infinite loop
