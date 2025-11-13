ef97627982e62c24b88b429aff7fda727502cd93
Romuald Brillout <git@brillout.com>
2025-11-13 13:29:55 +0100

    comment

--- packages/vike/types/defineConfig.ts
+++ packages/vike/types/defineConfig.ts
@@ -2,6 +2,8 @@ export { defineConfig }
 
 import type { Config } from './Config.js'
 
+// For JavaScript users. AFAICT there isn't another practical reason to use defineConfig() instead of `Config`.
+// https://github.com/vikejs/vike/issues/1156
 function defineConfig<T extends Config>(config: T): T {
   return config
 }
