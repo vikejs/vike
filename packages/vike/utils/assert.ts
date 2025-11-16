b3b0e985b429a41f096eb9700ebe682d4fc5bb0f
Romuald Brillout <git@brillout.com>
2025-11-16 22:21:42 +0100

    fix: polish bug error message

--- packages/vike/utils/assert.ts
+++ packages/vike/utils/assert.ts
@@ -52,7 +52,7 @@ function assert(condition: unknown, debugInfo?: unknown): asserts condition {
       return null
     }
     const debugInfoSerialized = typeof debugInfo === 'string' ? debugInfo : JSON.stringify(debugInfo)
-    return pc.dim(`Debug info for Vike maintainers (you can ignore this): ${debugInfoSerialized}`)
+    return pc.dim(`Debug for maintainers (you can ignore this): ${debugInfoSerialized}`)
   })()
 
   const link = pc.underline('https://github.com/vikejs/vike/issues/new?template=bug.yml')
