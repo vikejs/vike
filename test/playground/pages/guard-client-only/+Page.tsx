d13d1ac14bd9a3bba1b2742ed737571ed9ba71d1
Romuald Brillout <git@brillout.com>
2025-11-21 15:26:23 +0100

    fix typos

--- test/playground/pages/guard-client-only/+Page.tsx
+++ test/playground/pages/guard-client-only/+Page.tsx
@@ -4,7 +4,7 @@ import { assert } from '../../utils/assert'
 import React from 'react'
 
 function Page() {
-  // Never rendered on the client-side becaue of +guard.client.ts
+  // Never rendered on the client-side because of +guard.client.ts
   assert(import.meta.env.SSR)
   return <>I am only rendered on the server-side.</>
 }
