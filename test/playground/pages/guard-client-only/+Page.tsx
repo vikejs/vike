19f459b25eb16ace936847501ff4871519a16df6
Romuald Brillout <git@brillout.com>
2025-11-21 14:22:20 +0100

    test

---
+++ test/playground/pages/guard-client-only/+Page.tsx
@@ -0,0 +1,10 @@
+export { Page }
+
+import { assert } from '../../utils/assert'
+import React from 'react'
+
+function Page() {
+  // Never rendered on the client-side becaue of +guard.client.ts
+  assert(import.meta.env.SSR)
+  return <>I am only rendered on the server-side.</>
+}
