ee748d7bfe3a8f971fe5de607eade36f9d1698b9
Romuald Brillout <git@brillout.com>
2025-11-21 13:32:06 +0100

    update to-do

--- test/playground/pages/guard-client/+guard.client.ts
+++ test/playground/pages/guard-client/+guard.client.ts
@@ -1,5 +1,9 @@
 export { guard }
 
+// TODO:
+// - Move to pages/guard-client-only/
+// - Simply always `throw render()` here and test whehter it works both client-side nav and first-page visist. Use a proper @brillout/e2e test for this.
+
 import type { PageContextClient } from 'vike/types'
 
 async function guard(pageContext: PageContextClient) {
