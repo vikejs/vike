43b26e3ff3fcdbcab091b896cc65c58bbbdbe2ee
Romuald Brillout <git@brillout.com>
2025-11-25 18:48:00 +0100

    Revert "assert"
    This reverts commit bfed62ad3cdd302129de1dc56e63eb7011e4508e.

--- test/playground/pages/star-wars/index/+onData.ts
+++ test/playground/pages/star-wars/index/+onData.ts
@@ -8,11 +8,7 @@ function onData(pageContext: PageContext & { data: Data }) {
   assert(pageContext.data.title === '6 Star Wars Movies')
   if (pageContext.isClientSide) {
     assert(pageContext.isHydration === false)
-    if (pageContext.pageContextsAborted.length === 0) {
-      assert(pageContext.isClientSideNavigation === true)
-    } else {
-      assert(pageContext.isClientSideNavigation === false)
-    }
+    assert(pageContext.isClientSideNavigation === true || pageContext.pageContextsAborted.length > 0)
   } else {
     assert(pageContext.isHydration === undefined)
     assert(pageContext.isClientSideNavigation === false)
