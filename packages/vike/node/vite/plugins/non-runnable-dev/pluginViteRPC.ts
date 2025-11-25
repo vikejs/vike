caafc391f3d6748f521febbf0cee1cc156f24c39
Romuald Brillout <git@brillout.com>
2025-11-25 20:40:25 +0100

    minor refactor: rename node\/runtime => server\/runtime

--- packages/vike/node/vite/plugins/non-runnable-dev/pluginViteRPC.ts
+++ packages/vike/node/vite/plugins/non-runnable-dev/pluginViteRPC.ts
@@ -8,7 +8,7 @@ import { getViteConfigRuntime } from '../../shared/getViteConfigRuntime.js'
 
 /* We cannot use [`filter.id`](https://rolldown.rs/plugins/hook-filters) because Vite's optimizeDeps bundles the package `vike` into node_modules/.vite/deps_ssr/chunk-WBC5FHD7.js
 const distFileIsNonRunnableDev = requireResolveDistFile('dist/esm/utils/isNonRunnableDev.js')
-const distFileGlobalContext = requireResolveDistFile('dist/esm/node/runtime/globalContext.js')
+const distFileGlobalContext = requireResolveDistFile('dist/esm/server/runtime/globalContext.js')
 const filterRolldown = {
   id: {
     include: [distFileIsNonRunnableDev, distFileGlobalContext].map(
