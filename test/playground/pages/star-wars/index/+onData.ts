bfed62ad3cdd302129de1dc56e63eb7011e4508e
Romuald Brillout <git@brillout.com>
2025-11-25 18:47:33 +0100

    assert

--- test/playground/pages/star-wars/index/+onData.ts
+++ test/playground/pages/star-wars/index/+onData.ts
@@ -8,7 +8,11 @@ function onData(pageContext: PageContext & { data: Data }) {
   assert(pageContext.data.title === '6 Star Wars Movies')
   if (pageContext.isClientSide) {
     assert(pageContext.isHydration === false)
-    assert(pageContext.isClientSideNavigation === true || pageContext.pageContextsAborted.length > 0)
+    if (pageContext.pageContextsAborted.length === 0) {
+      assert(pageContext.isClientSideNavigation === true)
+    } else {
+      assert(pageContext.isClientSideNavigation === false)
+    }
   } else {
     assert(pageContext.isHydration === undefined)
     assert(pageContext.isClientSideNavigation === false)
