65b0d0146baf92475f37973a290134d97ac8ef62
Rom <git@brillout.com>
2025-11-27 17:40:06 +0100

    minor misc (#2862)

--- packages/vike/utils/pick.ts
+++ packages/vike/utils/pick.ts
@@ -1,7 +1,4 @@
-export function pick<Obj extends Record<string, unknown>, Keys extends keyof Obj>(
-  obj: Obj,
-  keys: Keys[],
-): Pick<Obj, Keys> {
+export function pick<Obj extends object, Keys extends keyof Obj>(obj: Obj, keys: Keys[]): Pick<Obj, Keys> {
   const result = {} as Pick<Obj, Keys>
   for (const key of keys) {
     if (key in obj) {
