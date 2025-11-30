d66d8c3a5a17c84ab2c9574a73b115a565a89796
Romuald Brillout <git@brillout.com>
2025-11-30 12:42:38 +0100

    remove getEnhancedError

--- packages/vike/server/runtime/logErrorServer.ts
+++ packages/vike/server/runtime/logErrorServer.ts
@@ -1,7 +1,7 @@
 export { logErrorServer }
 
 import pc from '@brillout/picocolors'
-import { isCallable, isObject } from '../utils.js'
+import { isObject } from '../utils.js'
 import { execHookOnError } from './renderPageServer/execHookOnError.js'
 import { assertPageContext_logRuntime, type PageContext_logRuntime } from './loggerRuntime.js'
 
@@ -10,14 +10,6 @@ function logErrorServer(err: unknown, pageContext: PageContext_logRuntime) {
   assertPageContext_logRuntime(pageContext)
 
   execHookOnError(err)
-
-  // Set by react-streaming
-  // - https://github.com/brillout/react-streaming/blob/0fb5510d0a5a614f577668a519bccd62de40aed8/src/server/renderToStream/common.ts#L59-L62
-  // - https://gist.github.com/brillout/066293a687ab7cf695e62ad867bc6a9c
-  // - It doesn't seem to be needed? (The error Vike receives is already enhanced.) Should we remove this?
-  if (isObject(err) && isCallable(err.getEnhancedError)) {
-    err = err.getEnhancedError(err)
-  }
 
   // We ensure we print a string; Cloudflare Workers doesn't seem to properly stringify `Error` objects.
   // - TO-DO/eventually: is that still true? Let's eventually remove it and see if it crashes Cloudflare.
