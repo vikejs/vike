b8db64c7cd19c86f85084e0d95bac5d5aa1e9232
Romuald Brillout <git@brillout.com>
2025-12-01 22:33:12 +0100

    minor refactor

--- packages/vike/node/vite/shared/loggerDev.ts
+++ packages/vike/node/vite/shared/loggerDev.ts
@@ -54,6 +54,7 @@ addOnBeforeAssertErr((err) => {
 
 type LogType = 'info' | 'warn' | 'error' | 'error-resolve'
 type LogCategory = 'config' | `request(${number})`
+type ProjectTag = '[vike]' | '[vite]'
 
 function logRuntimeInfoDev(msg: string, pageContext: PageContext_logRuntime, logType: LogType) {
   assertPageContext_logRuntime(pageContext)
@@ -163,7 +164,7 @@ function logDev(
   msg: string,
   logType: LogType,
   category: LogCategory | null,
-  projectTag: '[vike]' | '[vite]',
+  projectTag: ProjectTag,
   doNotPrependTags?: boolean,
 ) {
   if (!doNotPrependTags) msg = prependTags(msg, projectTag, category, logType)
@@ -231,7 +232,7 @@ function applyViteSourceMapToStackTrace(thing: unknown) {
   viteDevServer.ssrFixStacktrace(thing as Error)
 }
 
-function prependTags(msg: string, projectTag: '[vite]' | '[vike]', category: LogCategory | null, logType: LogType) {
+function prependTags(msg: string, projectTag: ProjectTag, category: LogCategory | null, logType: LogType) {
   const color = (s: string) => {
     if (logType === 'error' && !hasRed(msg)) return pc.bold(pc.red(s))
     if (logType === 'error-resolve' && !hasGreen(msg)) return pc.bold(pc.green(s))
