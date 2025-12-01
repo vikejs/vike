030ec193f8fd56374be552c678762c025c6340b2
Romuald Brillout <git@brillout.com>
2025-12-01 15:07:03 +0100

    minor refactor

--- packages/vike/utils/requireResolve.ts
+++ packages/vike/utils/requireResolve.ts
@@ -197,7 +197,7 @@ function addFileExtensionsToRequireResolve(require_: NodeJS.Require) {
 
 function getPackageNodeModulesDirectory() {
   // [RELATIVE_PATH_FROM_DIST] Current file: node_modules/${packageName}/dist/utils/requireResolve.js
-  assert(importMetaUrl.includes('/dist/utils/') || importMetaUrl.includes('/dist/cjs/utils/'))
+  assert(importMetaUrl.includes('/dist/utils/'))
   const packageNodeModulesDirectory = path.posix.join(removeFilePrefix(path.dirname(importMetaUrl)), '../../')
   // Return `node_modules/${packageName}/`
   return packageNodeModulesDirectory
