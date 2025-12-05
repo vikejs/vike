3e50b1e1cb40872c678fa6ba99ebbdd0d0a05a69
Romuald Brillout <git@brillout.com>
2025-12-05 13:26:56 +0100

    wip

--- packages/vike/utils/getBetterError.ts
+++ packages/vike/utils/getBetterError.ts
@@ -28,26 +28,26 @@ function getBetterError(
   // Modifications
   const { message: modsMessage, ...mods } = modifications
   Object.assign(errBetter, mods)
-    if (typeof modsMessage === 'string') {
-      const messagePrev = errBetter.message
-      errBetter.message = modsMessage
-      const oldMessageIndex = errBetter.stack.indexOf(messagePrev)
-      if (oldMessageIndex >= 0) {
-        // Completely replace the beginning of err.stack — removing prefix such as "SyntaxError: "
-        // - Following isn't always true: `err.stack.startsWith(err.message)` — because err.stack can start with "SyntaxError: " where err.message doesn't
-        const afterOldMessage = errBetter.stack.slice(oldMessageIndex + messagePrev.length)
-        errBetter.stack = modsMessage + afterOldMessage
-      } else {
-        warnMalformed(err)
-      }
-    } else if (modsMessage?.prepend) {
-        errBetter.message = modsMessage.prepend + errBetter.message
-        errBetter.stack = modsMessage.prepend + errBetter.stack
-    } else if (modsMessage?.append) {
-        const messagePrev = errBetter.message
-        errBetter.message = errBetter.message + modsMessage.append
-        errBetter.stack = errBetter.stack.replace(messagePrev, errBetter.message)
-      }
+  if (typeof modsMessage === 'string') {
+    const messagePrev = errBetter.message
+    errBetter.message = modsMessage
+    const oldMessageIndex = errBetter.stack.indexOf(messagePrev)
+    if (oldMessageIndex >= 0) {
+      // Completely replace the beginning of err.stack — removing prefix such as "SyntaxError: "
+      // - Following isn't always true: `err.stack.startsWith(err.message)` — because err.stack can start with "SyntaxError: " where err.message doesn't
+      const afterOldMessage = errBetter.stack.slice(oldMessageIndex + messagePrev.length)
+      errBetter.stack = modsMessage + afterOldMessage
+    } else {
+      warnMalformed(err)
+    }
+  } else if (modsMessage?.prepend) {
+    errBetter.message = modsMessage.prepend + errBetter.message
+    errBetter.stack = modsMessage.prepend + errBetter.stack
+  } else if (modsMessage?.append) {
+    const messagePrev = errBetter.message
+    errBetter.message = errBetter.message + modsMessage.append
+    errBetter.stack = errBetter.stack.replace(messagePrev, errBetter.message)
+  }
 
   // Enable users to retrieve the original error
   Object.assign(errBetter, { getOriginalError: () => (err as any)?.getOriginalError?.() ?? err })
