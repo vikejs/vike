15b8a052917adec250fe2f642a8feb56f1a01323
Romuald Brillout <git@brillout.com>
2025-12-06 16:35:12 +0100

    minor refactor

--- packages/vike/node/vite/plugins/pluginCommon.ts
+++ packages/vike/node/vite/plugins/pluginCommon.ts
@@ -1,5 +1,5 @@
 export { pluginCommon }
-export let viteVersionUser = null as string | null
+export let viteVersionUser = null as string | null | undefined
 
 import { type InlineConfig, type Plugin, type ResolvedConfig, type UserConfig } from 'vite'
 import {
@@ -49,7 +49,7 @@ function pluginCommon(vikeVitePluginOptions: unknown): Plugin[] {
       config: {
         order: 'pre',
         async handler(configFromUser, env) {
-          viteVersionUser = this?.meta?.viteVersion
+          viteVersionUser = this?.meta?.viteVersion as string | undefined // is `undefined` on old Vite versions
           const isDev = isDevCheck(env)
           const isBuild = env.command === 'build'
           const isPreview = env.isPreview!!
