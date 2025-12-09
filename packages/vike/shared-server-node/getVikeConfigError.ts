dae98ecb64618aa8c25732887d706a2d44b859a8
Rom <git@brillout.com>
2025-12-09 23:12:17 +0100

    fix: fix auto-reloading of resolved runtime error in + file (#2920)

--- packages/vike/shared-server-node/getVikeConfigError.ts
+++ packages/vike/shared-server-node/getVikeConfigError.ts
@@ -1,16 +1,19 @@
 export { getVikeConfigError }
 export { getVikeConfigErrorBuild }
 export { setVikeConfigError }
+export { debugFileChange }
 
-import { getGlobalObject } from './utils.js'
+import { getGlobalObject, createDebug } from './utils.js'
 
 const globalObject = getGlobalObject('server/shared/getVikeConfigError.ts', {
   errorRuntime: false as VikeConfigHasError,
   errorBuild: false as VikeConfigHasError,
 })
+const debugFileChange = createDebug('vike:file-change')
 
 type VikeConfigHasError = false | { err: unknown }
 function setVikeConfigError(val: { errorRuntime: VikeConfigHasError } | { errorBuild: VikeConfigHasError }) {
+  debugFileChange('setVikeConfigError()', val)
   if ('errorRuntime' in val) globalObject.errorRuntime = val.errorRuntime
   if ('errorBuild' in val) globalObject.errorBuild = val.errorBuild
 }
