b194380191c7685424dfd7931f6d728743479456
Romuald Brillout <git@brillout.com>
2025-12-11 15:58:10 +0100

    wip

---
+++ packages/vike/shared-server-client/assertVirtualFileExports.ts
@@ -0,0 +1,22 @@
+export { assertVirtualFileExports }
+
+import { assert } from './utils.js'
+
+function assertVirtualFileExports<ModuleExports>(
+  moduleExports: ModuleExports,
+  test: (moduleExports: ModuleExports) => boolean,
+  moduleId?: string,
+) {
+  if (!moduleExports || !test(moduleExports)) {
+    /* https://github.com/vikejs/vike/issues/2903#issuecomment-3642285811
+    throw getProjectError('@cloudflare/vite-plugin error https://github.com/vikejs/vike/issues/2903#issuecomment-3642285811')
+    /*/
+    assert(false, { moduleExports, moduleExportsKeys: getKeys(moduleExports), moduleId })
+    //*/
+  }
+}
+
+function getKeys(obj: any) {
+  if (obj === undefined) return null
+  return [...Object.getOwnPropertyNames(obj), ...Object.getOwnPropertySymbols(obj), ...Object.keys(obj)]
+}
