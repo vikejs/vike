c8ae05cd9d4be01ab9bc7d1291f9a931311e5c6f
Romuald Brillout <git@brillout.com>
2025-12-11 14:33:30 +0100

    fmt

--- docs/pages/vike-photon/PhotonDescription.tsx
+++ docs/pages/vike-photon/PhotonDescription.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 export const PhotonDescription = () => (
   <>
-    automatically integrates Vike with any JavaScript server (Express.js, Hono, Fastify, ...) and deployment provider (Cloudflare,
-    Vercel, Netlify, self-hosting, ...).
+    automatically integrates Vike with any JavaScript server (Express.js, Hono, Fastify, ...) and deployment provider
+    (Cloudflare, Vercel, Netlify, self-hosting, ...).
   </>
 )
