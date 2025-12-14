02ce758a8830e2af07c9c17048b2d23242037e42
Romuald Brillout <git@brillout.com>
2025-12-14 20:29:39 +0100

    minor refactor

--- packages/vike/utils/assert.ts
+++ packages/vike/utils/assert.ts
@@ -26,7 +26,7 @@ assertSingleInstance_onAssertModuleLoad()
 
 const tagProject = `[vike]` as const
 const tagProjectWithVersion = `[vike@${PROJECT_VERSION}]` as const
-const tagBug = 'Bug'
+const tagAssertBug = 'Bug'
 type Tag = 'Bug' | 'Wrong Usage' | 'Error' | 'Warning'
 
 const numberOfStackTraceLinesToRemove = 2
@@ -50,7 +50,7 @@ function assert(condition: unknown, debugInfo?: unknown): asserts condition {
     .filter(Boolean)
     .join(' ')
   errMsg = addWhitespace(errMsg)
-  errMsg = addTagAssert(errMsg, tagBug)
+  errMsg = addTagAssert(errMsg, tagAssertBug)
   errMsg = addTagProject(errMsg, true)
   const internalError = createErrorWithCleanStackTrace(errMsg, numberOfStackTraceLinesToRemove)
 
@@ -162,7 +162,7 @@ function addTagProject(msg: string, showProjectVersion = false): string {
 }
 
 function isVikeBug(err: unknown): boolean {
-  return String(err).includes(`[${tagBug}]`)
+  return String(err).includes(`[${tagAssertBug}]`)
 }
 
 // Called upon `DEBUG=vike:error`
