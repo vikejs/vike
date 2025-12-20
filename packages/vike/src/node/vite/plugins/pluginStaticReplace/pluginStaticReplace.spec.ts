0129c1c02e3434d789c74e59c0ce1dd4fcdce09e
Romuald Brillout <git@brillout.com>
2025-12-20 16:23:13 +0100

    update to-do

--- packages/vike/src/node/vite/plugins/pluginStaticReplace/pluginStaticReplace.spec.ts
+++ packages/vike/src/node/vite/plugins/pluginStaticReplace/pluginStaticReplace.spec.ts
@@ -42,7 +42,6 @@ const optionsReact: TransformStaticReplaceOptions = {
         match: {
           function: 'import:vike-react/useHydrated:useHydrated',
         },
-        // TODO/ai: change API so that `with` should be set to JSON.stringify(false) instead (it's more flexible)
         replace: { with: false },
       },
     },
