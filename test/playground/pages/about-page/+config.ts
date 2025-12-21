e3968bb14fb1226cd4e7d5e66d19fedc82fda44c
Romuald Brillout <git@brillout.com>
2025-12-21 23:16:32 +0100

    Revert "test"
    This reverts commit 0ca897888c29538276b55e27083607b2fa340577.

--- test/playground/pages/about-page/+config.ts
+++ test/playground/pages/about-page/+config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'vike/types'
 export default {
   // TEST: define `Page` over import string at `+config.js > export default { Page }` instead of +Page.js file.
   // @ts-expect-error I ain't sure whether import strings should be part of the official type, but I think so.
-  Page: 'import:./Page.jsx',
+  Page: 'import:./Page.jsx:default',
   // TEST: route defined over `+config.js > export default { route }`
   route: '/about',
   prerenderSetOverEffect: false,
