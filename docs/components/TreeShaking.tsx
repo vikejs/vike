df2ee694119971fdccda82e95df2ed34596a1744
Rom <git@brillout.com>
2025-12-22 15:59:58 +0100

    docs: `<ClientOnly>` & `useHydrated()` (#2992)

--- docs/components/TreeShaking.tsx
+++ docs/components/TreeShaking.tsx
@@ -4,16 +4,21 @@ import { NoteWithCustomIcon } from '@brillout/docpress'
 import iconScissors from '../assets/icons/scissors.svg'
 import React from 'react'
 
-function TreeShaking({ children }: { children: React.ReactNode }) {
+function TreeShaking({ children, description }: { children?: React.ReactNode; description?: string }) {
   return (
     <NoteWithCustomIcon icon={<IconTreeShaking />}>
       <p>
         <b>Features automatic code removal</b>.
       </p>
       <p>
-        Code meant only for the server (or client) is automatically removed and never loaded on the client (or server).
-        (It's a widespread technique <a href="https://vite.dev/guide/ssr.html#conditional-logic">commonly</a> called{' '}
-        <a href="https://rollupjs.org/faqs/#what-is-tree-shaking">tree-shaking</a>.)
+        {description ?? (
+          <>
+            Code meant only for the server (or client) is automatically removed and never loaded on the client (or
+            server). (It's a widespread technique{' '}
+            <a href="https://vite.dev/guide/ssr.html#conditional-logic">commonly</a> called{' '}
+            <a href="https://rollupjs.org/faqs/#what-is-tree-shaking">tree-shaking</a>.)
+          </>
+        )}
       </p>
       {children}
     </NoteWithCustomIcon>
