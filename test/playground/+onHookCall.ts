0c3831e98ed35e92232cf8e9082a5d798ad4edc8
Romuald Brillout <git@brillout.com>
2025-12-23 21:43:30 +0100

    fix CI

--- test/playground/+onHookCall.ts
+++ test/playground/+onHookCall.ts
@@ -8,6 +8,11 @@ export const onHookCall: Config['onHookCall'] = async (hook, context) => {
   } catch (err) {
     // Swallowing isn't possible — that's a good thing?
     console.log(`Error caught by +onHookCall for ${hook.name}:`, err)
+    // Why do we need this line to avoid the CI from breaking? If we remove it we get:
+    // ```
+    // [/test/playground/test-preview.test.ts][npm run preview][Browser Error] Error: [vike][Error] The guard() hook defined by /pages/guard-client-only/+guard.client.ts timed out: it didn't finish after 30 seconds (https://vike.dev/hooksTimeout)
+    // ```
+    throw err
   } finally {
     console.log('After hook:', hook.name, hook.filePath)
   }
