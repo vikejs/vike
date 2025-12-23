242e171ede1a27d6c4509169e32e6a3533c17eb2
Romuald Brillout <git@brillout.com>
2025-12-23 13:40:21 +0100

    fix typos

--- test/playground/+onHookCall.ts
+++ test/playground/+onHookCall.ts
@@ -1,12 +1,12 @@
 import type { Config } from 'vike/types'
 
 export const onHookCall: Config['onHookCall'] = async (hook, context) => {
-  console.log('Befor hook:', hook.name, hook.filePath)
+  console.log('Before hook:', hook.name, hook.filePath)
   try {
     await hook.call()
   } catch (err) {
     // Swallowing isn't possible — that's a good thing?
-    console.log('Error catched by +onHookCall:', err)
+    console.log('Error caught by +onHookCall:', err)
   } finally {
     console.log('After hook:', hook.name, hook.filePath)
   }
