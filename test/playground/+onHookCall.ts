47fd806bee98b31c83f503a94a43cd5a1f4a6cd0
Romuald Brillout <git@brillout.com>
2025-12-23 12:07:59 +0100

    polish

--- test/playground/+onHookCall.ts
+++ test/playground/+onHookCall.ts
@@ -1,14 +1,7 @@
 import type { Config } from 'vike/types'
 
 export const onHookCall: Config['onHookCall'] = async (hook, context) => {
-  console.log(
-    'Befor hook:',
-    hook.name,
-    hook.filePath,
-    `sync:${hook.sync}`,
-    `!!pageContext:${!!context.pageContext}`,
-    `!!globalContext:${!!context.globalContext}`,
-  )
+  console.log('Befor hook:', hook.name, hook.filePath)
   await hook.call()
   console.log('After hook:', hook.name, hook.filePath)
 }
