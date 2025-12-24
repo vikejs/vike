8ec1c3e82e2ce652f129af1bb723638dd0636078
Romuald Brillout <git@brillout.com>
2025-12-24 10:22:32 +0100

    remove newly introduced test flakiness

--- test/playground/+onBeforeRoute.ts
+++ test/playground/+onBeforeRoute.ts
@@ -3,5 +3,7 @@ export { onBeforeRoute }
 import type { PageContext } from 'vike/types'
 
 function onBeforeRoute(pageContext: PageContext) {
+  /*
   console.log('+onBeforeRoute', pageContext.urlPathname)
+  //*/
 }
