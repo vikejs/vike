d97b95abb117d28808cbbc7ddd331764cf25396e
Romuald Brillout <git@brillout.com>
2025-12-25 10:59:55 +0100

    minor refactor

--- packages/vike/src/client/runtime-server-routing/entry.ts
+++ packages/vike/src/client/runtime-server-routing/entry.ts
@@ -1,22 +1,16 @@
 import { assertServerRouting } from '../../utils/assertRoutingType.js'
 assertServerRouting()
 
-import {
-  createPageContextClientSide,
-  type PageContextCreatedClient_ServerRouting,
-} from './createPageContextClientSide.js'
+import { createPageContextClientSide } from './createPageContextClientSide.js'
 import { execHookOnRenderClient } from '../shared/execHookOnRenderClient.js'
-import { assertSingleInstance_onClientEntryServerRouting, assertUsage, objectAssign, updateType } from './utils.js'
+import { assertSingleInstance_onClientEntryServerRouting, assertUsage, objectAssign } from './utils.js'
 import { removeFoucBuster } from '../shared/removeFoucBuster.js'
 import { execHook } from '../../shared-server-client/hooks/execHook.js'
 import { preparePageContextForPublicUsageClient } from './preparePageContextForPublicUsageClient.js'
 import { setVirtualFileExportsGlobalEntry } from '../shared/getGlobalContextClientInternalShared.js'
 // @ts-expect-error
 import * as virtualFileExportsGlobalEntry from 'virtual:vike:global-entry:client:server-routing'
-import {
-  loadPageConfigsLazyClientSide,
-  type PageContext_loadPageConfigsLazyClientSide,
-} from '../shared/loadPageConfigsLazyClientSide.js'
+import { loadPageConfigsLazyClientSide } from '../shared/loadPageConfigsLazyClientSide.js'
 import { getPageContextSerializedInHtml } from '../shared/getJsonSerializedInHtml.js'
 import { getCurrentUrl } from '../shared/getCurrentUrl.js'
 
@@ -30,19 +24,9 @@ async function hydrate() {
   const pageContext = await createPageContextClientSide()
 
   objectAssign(pageContext, getPageContextSerializedInHtml())
-
   assertPristineUrl()
 
-  // Sets pageContext.config to local configs (overrides the pageContext.config set above)
-  updateType(pageContext, await loadPageConfigsLazyClientSideAndExecHook(pageContext))
-
-  await execHookOnRenderClient(pageContext, preparePageContextForPublicUsageClient)
-  await execHook('onHydrationEnd', pageContext, preparePageContextForPublicUsageClient)
-}
-
-async function loadPageConfigsLazyClientSideAndExecHook<
-  PageContext extends PageContext_loadPageConfigsLazyClientSide & PageContextCreatedClient_ServerRouting,
->(pageContext: PageContext) {
+  // Sets pageContext.config using local configs — overrides the pageContext.config set using global configs at createPageContextClientSide()
   const pageContextAddendum = await loadPageConfigsLazyClientSide(
     pageContext.pageId,
     pageContext._pageFilesAll,
@@ -53,7 +37,9 @@ async function loadPageConfigsLazyClientSideAndExecHook<
 
   await execHook('onCreatePageContext', pageContext, preparePageContextForPublicUsageClient)
 
-  return pageContext
+  await execHookOnRenderClient(pageContext, preparePageContextForPublicUsageClient)
+
+  await execHook('onHydrationEnd', pageContext, preparePageContextForPublicUsageClient)
 }
 
 function assertPristineUrl() {
