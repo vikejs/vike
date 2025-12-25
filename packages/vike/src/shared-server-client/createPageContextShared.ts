6d08f54cb68ee8d0b905a86b9bd7d075d500db62
Romuald Brillout <git@brillout.com>
2025-12-25 17:29:40 +0100

    fmt

--- packages/vike/src/shared-server-client/createPageContextShared.ts
+++ packages/vike/src/shared-server-client/createPageContextShared.ts
@@ -5,12 +5,24 @@ export type { PageContextCreatedBase }
 
 import { changeEnumerable, objectAssign } from './utils.js'
 import type { GlobalConfigPublic } from './page-configs/resolveVikeConfigPublic.js'
-import type { PageContextCreatedClient, PageContextCreatedClientBase } from '../client/runtime-client-routing/createPageContextClientSide.js'
-import type { PageContextCreatedServer, PageContextCreatedServerBase } from '../server/runtime/renderPageServer/createPageContextServerSide.js'
-import type { PageContextCreatedClient_ServerRouting, PageContextCreatedClientBase_ServerRouting } from '../client/runtime-server-routing/createPageContextClientSide.js'
+import type {
+  PageContextCreatedClient,
+  PageContextCreatedClientBase,
+} from '../client/runtime-client-routing/createPageContextClientSide.js'
+import type {
+  PageContextCreatedServer,
+  PageContextCreatedServerBase,
+} from '../server/runtime/renderPageServer/createPageContextServerSide.js'
+import type {
+  PageContextCreatedClient_ServerRouting,
+  PageContextCreatedClientBase_ServerRouting,
+} from '../client/runtime-server-routing/createPageContextClientSide.js'
 
 type PageContextCreated = PageContextCreatedServer | PageContextCreatedClient | PageContextCreatedClient_ServerRouting
-type PageContextCreatedBase = PageContextCreatedServerBase | PageContextCreatedClientBase | PageContextCreatedClientBase_ServerRouting
+type PageContextCreatedBase =
+  | PageContextCreatedServerBase
+  | PageContextCreatedClientBase
+  | PageContextCreatedClientBase_ServerRouting
 
 function createPageContextShared<T extends Record<string, unknown>>(
   pageContextCreated: T,
