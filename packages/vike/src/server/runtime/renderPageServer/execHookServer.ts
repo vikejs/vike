ec1d65f6e41d699e5bd18d5798edeacbce19f4c4
Romuald Brillout <git@brillout.com>
2026-01-04 23:52:48 +0100

    minor refactor

--- packages/vike/src/server/runtime/renderPageServer/execHookServer.ts
+++ packages/vike/src/server/runtime/renderPageServer/execHookServer.ts
@@ -2,13 +2,13 @@ export { execHookServer }
 export type { PageContextExecHookServer }
 
 import { execHookDirect, PageContextExecHook } from '../../../shared-server-client/hooks/execHook.js'
-import { getPageContextPublicServer, type PageContextPublicServer } from './getPageContextPublicServer.js'
+import { getPageContextPublicServer } from './getPageContextPublicServer.js'
 import type { PageContextConfig } from '../../../shared-server-client/getPageFiles.js'
 import type { HookName } from '../../../types/Config.js'
 import { getHooksFromPageContextNew } from '../../../shared-server-client/hooks/getHook.js'
 import { getFileSuffixes } from '../../../shared-server-node/getFileSuffixes.js'
 
-type PageContextExecHookServer = PageContextConfig & PageContextExecHook & PageContextPublicServer
+type PageContextExecHookServer = PageContextConfig & PageContextExecHook & { isClientSideNavigation: boolean }
 async function execHookServer(hookName: HookName, pageContext: PageContextExecHookServer) {
   const allHooks = getHooksFromPageContextNew(hookName, pageContext)
   const hooks = !pageContext.isClientSideNavigation
