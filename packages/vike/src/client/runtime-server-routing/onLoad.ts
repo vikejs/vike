596cd45715f3779a305cd8e688dac46879821939
Romuald Brillout <git@brillout.com>
2026-01-05 14:26:11 +0100

    wip

--- packages/vike/src/client/runtime-server-routing/onLoad.ts
+++ packages/vike/src/client/runtime-server-routing/onLoad.ts
@@ -1,7 +1,9 @@
 export { onLoad }
 
 import { assertIsBrowser } from '../../utils/assertIsBrowser.js'
+import { assertServerRouting } from '../runtime-client-routing/utils.js'
 
 function onLoad() {
   assertIsBrowser()
+  assertServerRouting()
 }
