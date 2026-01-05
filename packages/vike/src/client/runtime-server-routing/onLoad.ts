4ceac6764a88aabb6991ebdad00c74c35fd6e4cb
Romuald Brillout <git@brillout.com>
2026-01-05 14:34:39 +0100

    [AI] Remove unused onLoad files

--- packages/vike/src/client/runtime-server-routing/onLoad.ts
+++
@@ -1,9 +0,0 @@
-export { onLoad }
-
-import { assertIsBrowser } from '../../utils/assertIsBrowser.js'
-import { assertServerRouting } from '../../utils/assertRoutingType.js'
-
-function onLoad() {
-  assertIsBrowser()
-  assertServerRouting()
-}
