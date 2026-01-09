caaf78b66bb257e668652b8c66a621e6a608287f
brillout <github.com@brillout.com>
2026-01-09 14:36:13 +0000

    Deploying to gh-pages from @ vikejs/vike@cd3eeb708f49dde477739dcb58008e45c8e97bc2 🚀

---
+++ assets/entries/pages_extensions.DCp30W6v.js
@@ -0,0 +1,65 @@
+import{o as l,a as o}from"../chunks/chunk-s_Ka62my.js";import{j as e}from"../chunks/chunk-C8nyMdhy.js";import{L as t}from"../chunks/chunk-BEJI2KvG.js";/* empty css                      */import{C as r}from"../chunks/chunk-Bu7-ihKy.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import{u as a}from"../chunks/chunk-Dmi2Rm13.js";/* empty css                      *//* empty css                      *//* empty css                      */const c=[{pageSectionId:"server",pageSectionLevel:2,pageSectionTitle:"Server"},{pageSectionId:"react",pageSectionLevel:2,pageSectionTitle:"React"},{pageSectionId:"vue",pageSectionLevel:2,pageSectionTitle:"Vue"},{pageSectionId:"solid",pageSectionLevel:2,pageSectionTitle:"Solid"},{pageSectionId:"svelte",pageSectionLevel:2,pageSectionTitle:"Svelte"},{pageSectionId:"angular",pageSectionLevel:2,pageSectionTitle:"Angular"}];function s(n){const i={a:"a",blockquote:"blockquote",code:"code",li:"li",p:"p",strong:"strong",ul:"ul",...a(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(i.p,{children:"Vike extensions integrate tools on your behalf."}),`
+`,e.jsxs(i.blockquote,{children:[`
+`,e.jsxs(i.p,{children:["While extensions are optional, we recommend using extensions unless you have a clear reason not to. See ",e.jsx(t,{href:"/extension-vs-custom"}),"."]}),`
+`]}),`
+`,e.jsx("h2",{id:"server",children:"Server"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(t,{href:"/vike-photon",children:e.jsx(i.code,{children:"vike-photon"})}),`
+`]}),`
+`]}),`
+`,e.jsx("h2",{id:"react",children:"React"}),`
+`,e.jsx(i.p,{children:e.jsx(i.strong,{children:"Core"})}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(t,{href:"/vike-react",children:e.jsx(i.code,{children:"vike-react"})}),`
+`]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.a,{href:"https://github.com/nitedani/vike-react-rsc#readme",children:e.jsx(i.code,{children:"vike-react-rsc"})})," - 🚧 Same as ",e.jsx(i.code,{children:"vike-react"})," but with ",e.jsx(t,{href:"/react#react-server-components",children:"Server Components"})]}),`
+`]}),`
+`,e.jsx(i.p,{children:e.jsx(i.strong,{children:"Data fetching"})}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-query#readme",children:e.jsx(i.code,{children:"vike-react-query"})})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-apollo#readme",children:e.jsx(i.code,{children:"vike-react-apollo"})})}),`
+`]}),`
+`,e.jsx(i.p,{children:e.jsx(i.strong,{children:"State management"})}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-redux#readme",children:e.jsx(i.code,{children:"vike-react-redux"})})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-zustand#readme",children:e.jsx(i.code,{children:"vike-react-zustand"})})}),`
+`]}),`
+`,e.jsx(i.p,{children:e.jsx(i.strong,{children:"Misc"})}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-chakra#readme",children:e.jsx(i.code,{children:"vike-react-chakra"})})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-antd#readme",children:e.jsx(i.code,{children:"vike-react-antd"})})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-styled-components#readme",children:e.jsx(i.code,{children:"vike-react-styled-components"})})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-styled-jsx#readme",children:e.jsx(i.code,{children:"vike-react-styled-jsx"})})}),`
+`]}),`
+`,e.jsxs(r,{children:[e.jsx(i.a,{href:"https://github.com/vikejs/vike/issues/1715",children:"Contributions welcome"})," to create extensions."]}),`
+`,e.jsx("h2",{id:"vue",children:"Vue"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(t,{href:"/vike-vue",children:e.jsx(i.code,{children:"vike-vue"})}),`
+`]}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-vue/tree/main/packages/vike-vue-query#readme",children:e.jsx(i.code,{children:"vike-vue-query"})})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-vue/tree/main/packages/vike-vue-pinia#readme",children:e.jsx(i.code,{children:"vike-vue-pinia"})})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/Mahdi-heydari/vike-vue-vuetify#readme",children:e.jsx(i.code,{children:"vike-vue-vuetify"})})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/Jearce/vike-vue-naive-ui#readme",children:e.jsx(i.code,{children:"vike-vue-naive-ui"})})}),`
+`]}),`
+`,e.jsxs(r,{children:[e.jsx(i.a,{href:"https://github.com/vikejs/vike/issues/1715",children:"Contributions welcome"})," to create extensions."]}),`
+`,e.jsx("h2",{id:"solid",children:"Solid"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(t,{href:"/vike-solid",children:e.jsx(i.code,{children:"vike-solid"})}),`
+`]}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-solid/tree/main/packages/vike-solid-query#readme",children:e.jsx(i.code,{children:"vike-solid-query"})})}),`
+`]}),`
+`,e.jsxs(r,{children:[e.jsx(i.a,{href:"https://github.com/vikejs/vike/issues/1715",children:"Contributions welcome"})," to create extensions."]}),`
+`,e.jsx("h2",{id:"svelte",children:"Svelte"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:["🚧 ",e.jsx(i.a,{href:"https://github.com/brandonxiang/vike-svelte#readme",children:e.jsx(i.code,{children:"vike-svelte"})})]}),`
+`]}),`
+`,e.jsx(r,{children:"Rough proof-of-concept: contributions welcome to take over and finish it."}),`
+`,e.jsx("h2",{id:"angular",children:"Angular"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:["🚧 ",e.jsx(i.a,{href:"https://github.com/vikejs/vike-angular#readme",children:e.jsx(i.code,{children:"vike-angular"})})]}),`
+`]}),`
+`,e.jsx(r,{children:"Rough proof-of-concept: contributions welcome to take over and finish it."})]})}function d(n={}){const{wrapper:i}={...a(),...n.components};return i?e.jsx(i,{...n,children:e.jsx(s,{...n})}):s(n)}const h=Object.freeze(Object.defineProperty({__proto__:null,default:d,pageSectionsExport:c},Symbol.toStringTag,{value:"Module"})),P={hasServerOnlyHook:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!1}},isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},guardEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:o}},onCreatePageContext:{type:"cumulative",definedAtData:[{filePathToShowToUser:"@brillout/docpress/renderer/onCreatePageContext",fileExportPathToShowToUser:[]}],valueSerialized:[{type:"pointer-import",value:l}]},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/extensions/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:h}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}}};export{P as configValuesSerialized};
