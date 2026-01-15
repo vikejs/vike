1d0a05096b4a067074528403c27df085f3aeca74
Romuald Brillout <git@brillout.com>
2026-01-16 00:44:06 +0100

    fix: improve browser check (fix #3036)

--- packages/vike/src/utils/isBrowser.ts
+++ packages/vike/src/utils/isBrowser.ts
@@ -1,5 +1,7 @@
+/** Test whether the environment is a *real* browser (not a browser simulation such as `jsdom`). */
 export function isBrowser() {
-  // Using `typeof window !== 'undefined'` alone is not enough because some users use https://www.npmjs.com/package/ssr-window
-  return typeof window !== 'undefined' && typeof window.scrollY === 'number'
-  // Alternatively, test whether environment is a *real* browser: https://github.com/brillout/picocolors/blob/d59a33a0fd52a8a33e4158884069192a89ce0113/picocolors.js#L87-L89
+  // - Using `typeof window !== 'undefined'` alone isn't narrow enough because some users use https://www.npmjs.com/package/ssr-window
+  // - Using `typeof window !== "undefined" && typeof window.scrollY === "number"` still isn't narrow enough because of jsdom
+  // - https://github.com/jsdom/jsdom/issues/1537#issuecomment-1689368267
+  return Object.getOwnPropertyDescriptor(globalThis, 'window')?.get?.toString().includes('[native code]') ?? false
 }
