adae7c4542498c1f1a8f379be48e4c738bd6647c
Romuald Brillout <git@brillout.com>
2026-01-23 09:21:26 +0100

    less logs

--- test/playground/+onHookCall.shared.ts
+++ test/playground/+onHookCall.shared.ts
@@ -1,22 +1,29 @@
 import type { Config, PageContext } from 'vike/types'
 import { assert } from './utils/assert'
 
+//*
+const LOG = false
+/*/
+const LOG = true
+//*/
+
 type Hook = Parameters<Extract<Config['onHookCall'], Function>>[0]
 export async function onHookCall(hook: Hook, pageContext: PageContext) {
   assert(pageContext === null || pageContext.isClientSide === true || pageContext.isClientSide === false)
-  console.log(
-    // spellcheck-ignore
-    'Befor hook:',
-    hook.name,
-    hook.filePath,
-  )
+  LOG &&
+    console.log(
+      // spellcheck-ignore
+      'Befor hook:',
+      hook.name,
+      hook.filePath,
+    )
   try {
     await hook.call()
   } catch (err) {
-    console.log(`Error caught by +onHookCall for ${hook.name}:`, err)
+    LOG && console.log(`Error caught by +onHookCall for ${hook.name}:`, err)
     // By design, swallowing isn't possible
     if (Math.random() > 0.5) throw err
   } finally {
-    console.log('After hook:', hook.name, hook.filePath)
+    LOG && console.log('After hook:', hook.name, hook.filePath)
   }
 }
