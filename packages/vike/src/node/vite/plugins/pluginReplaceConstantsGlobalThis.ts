4d0a444e1b11599484ce51c698c9ca9633656948
Romuald Brillout <git@brillout.com>
2026-02-04 14:41:10 +0100

    wip

--- packages/vike/src/node/vite/plugins/pluginReplaceConstantsGlobalThis.ts
+++ packages/vike/src/node/vite/plugins/pluginReplaceConstantsGlobalThis.ts
@@ -22,7 +22,7 @@ declare global {
   /** Like `import.meta.env.SSR` but works for `node_modules/` packages with `ssr.external` */
   var __VIKE__IS_CLIENT: boolean
   var __VIKE__IS_DEBUG: boolean
-  /** Whether vike is NOT externalized (i.e., `import.meta.env` is defined). `false` when vike is `ssr.external` */
+  /** Whether the code is processed by Vite, i.e. `false` when the code is `ssr.external` */
   var __VIKE__IS_NOT_EXTERNAL: boolean
 }
 
@@ -31,7 +31,6 @@ const VIRTUAL_FILE_ID_constantsGlobalThis = 'virtual:vike:server:constantsGlobal
 const isDebugVal = isDebug()
 globalThis.__VIKE__IS_CLIENT = false
 globalThis.__VIKE__IS_DEBUG = isDebugVal
-// When vike is ssr.external, the virtual file isn't imported, so we default to false
 globalThis.__VIKE__IS_NOT_EXTERNAL = false
 
 // === Rolldown filter
@@ -59,6 +58,7 @@ function pluginReplaceConstantsGlobalThis(): Plugin[] {
             define: {
               'globalThis.__VIKE__IS_DEV': JSON.stringify(isDev),
               'globalThis.__VIKE__IS_DEBUG': JSON.stringify(isDebugVal),
+              'globalThis.__VIKE__IS_NOT_EXTERNAL': JSON.stringify(true),
             },
           }
         },
@@ -101,7 +101,7 @@ function pluginReplaceConstantsGlobalThis(): Plugin[] {
             `globalThis.__VIKE__IS_DEV = ${JSON.stringify(isDev)};`,
             `globalThis.__VIKE__IS_CLIENT = false;`,
             `globalThis.__VIKE__IS_DEBUG = ${JSON.stringify(isDebugVal)};`,
-            `globalThis.__VIKE__IS_NOT_EXTERNAL = true;`,
+            `globalThis.__VIKE__IS_NOT_EXTERNAL = false;`,
           ].join('\n')
           return code
         },
