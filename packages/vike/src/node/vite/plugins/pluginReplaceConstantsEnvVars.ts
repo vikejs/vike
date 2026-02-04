cf326c7660a6a91b7af6e0614eb5c83eaf2e028b
Romuald Brillout <git@brillout.com>
2026-02-04 15:39:28 +0100

    comment

--- packages/vike/src/node/vite/plugins/pluginReplaceConstantsEnvVars.ts
+++ packages/vike/src/node/vite/plugins/pluginReplaceConstantsEnvVars.ts
@@ -121,7 +121,7 @@ function pluginReplaceConstantsEnvVars(): Plugin[] {
 
           // Replace bare `import.meta.env` expression with `null` for the user-land.
           // - Otherwise Vite replaces it with an object missing PUBLIC_ENV__ variables which is confusing for users.
-          // - We purposely don't support replacing `import.meta.env` with an object to incentivize to write code-splitting friendly code.
+          // - We purposely don't support replacing `import.meta.env` with an object to incentivize the user write code-splitting friendly code.
           // - `define: { 'import.meta.env': JSON.stringify(null) }` doesn't work because it also replaces `import.meta.env` inside `import.meta.env.SONE_ENV`
           const bareImportMetaEnvRegex = /\bimport\.meta\.env(?!\.)/g
           const isUserLand = !id.includes('node_modules') && id.startsWith(config.root) // skip node_modules/ as well as linked dependencies
