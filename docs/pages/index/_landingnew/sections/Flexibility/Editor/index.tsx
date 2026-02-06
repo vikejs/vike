27411d7cebe65582c44e2dcdbc98975441a8e3b5
Richard Unterberg <richard.unterberg@lipsia.digital>
2026-02-06 18:46:07 +0100

    add: shiki

---
+++ docs/pages/index/_landingnew/sections/Flexibility/Editor/index.tsx
@@ -0,0 +1,16 @@
+// react tree view component:
+// +
+// shiki
+
+import React from 'react'
+import FlexFileTree from './Tree'
+
+const ReadOnlyEditor = () => {
+  return (
+    <>
+      <FlexFileTree />
+    </>
+  )
+}
+
+export default ReadOnlyEditor
