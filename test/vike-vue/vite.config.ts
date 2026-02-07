181d1b27547fb295a25938ab866b266faeaf4bc8
Romuald Brillout <git@brillout.com>
2026-02-07 11:58:51 +0100

    prepare Vite 8

--- test/vike-vue/vite.config.ts
+++ test/vike-vue/vite.config.ts
@@ -15,6 +15,12 @@ const config: UserConfig = {
       dependencies: ['vue-toast-notification'],
     }),
   ],
+  legacy: {
+    // @ts-ignore Vite 8 setting (we're currently using Vite 7)
+    // https://discord.com/channels/804011606160703521/831456449632534538/1469360902670385325
+    // https://main.vite.dev/guide/migration#consistent-commonjs-interop
+    inconsistentCjsInterop: true,
+  },
 }
 
 export default config
