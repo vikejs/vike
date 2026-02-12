7c9b4a539ca85dd6b373fea3ff03c687cb3ed589
Romuald Brillout <git@brillout.com>
2026-02-12 11:04:21 +0100

    fmt

--- test/vavite/server/handlerEntry.ts
+++ test/vavite/server/handlerEntry.ts
@@ -1,4 +1,4 @@
-import { IncomingMessage, ServerResponse } from 'http'
+import { IncomingMessage, ServerResponse } from 'node:http'
 import { renderPage } from 'vike/server'
 
 export default async function handler(request: IncomingMessage, res: ServerResponse) {
