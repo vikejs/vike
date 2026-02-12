c45131d14cab3dd8a496962e1ce860348ecb589b
Romuald Brillout <git@brillout.com>
2026-02-12 11:21:00 +0100

    rm redundant test

--- test/vavite/.test-build.test.ts
+++
@@ -1,9 +0,0 @@
-import { runCommandThatTerminates, expect } from '@brillout/test-e2e'
-
-let err: any
-try {
-  await runCommandThatTerminates('pnpm run build')
-} catch (err_) {
-  err = err_
-}
-expect(err).toBe(undefined)
