c4cc4cf1966f1572789efd259f65bea6eab5c266
Romuald Brillout <git@brillout.com>
2026-02-12 12:25:56 +0100

    comment

--- test/vavite/vite.config.ts
+++ test/vavite/vite.config.ts
@@ -3,6 +3,8 @@ import vike from 'vike/plugin'
 import { defineConfig } from 'vite'
 import { vavite } from 'vavite'
 
+// Copied from https://github.com/cyco130/vavite/blob/8b7d7cc9563c6b3ca3ff5a6f27fabaaeb1495811/examples/vike/vite.config.ts
+
 export default defineConfig({
   buildSteps: [
     { name: 'client' },
