1c3b1039cae7a0197ecba02d3f95140aea90963c
Romuald Brillout <git@brillout.com>
2026-02-12 11:50:07 +0100

    fix test

--- test/vavite/.test-dev.test.ts
+++ test/vavite/.test-dev.test.ts
@@ -1,7 +1,3 @@
 import { testRunClassic } from '../utils'
 
-testRunClassic('npm run dev', {
-  tolerateError({ logText }) {
-    return logText.includes("Vite's CLI is deprecated") || logText.includes('Run the built server entry')
-  },
-})
+testRunClassic('npm run dev')
--- test/vavite/.test-preview.test.ts
+++ test/vavite/.test-preview.test.ts
@@ -1,7 +1,9 @@
 import { testRunClassic } from '../utils'
 
 testRunClassic('npm run preview', {
+  serverIsReadyMessage: 'Server listening',
   tolerateError({ logText }) {
+    return false
     return logText.includes("Vite's CLI is deprecated") || logText.includes('Run the built server entry')
   },
 })
