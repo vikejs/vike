59e469066404960c44759ed66a4fbb222f524850
Romuald Brillout <git@brillout.com>
2026-02-18 11:06:28 +0100

    minor refactor

--- docs/pages/pricing/PricingTable.tsx
+++ docs/pages/pricing/PricingTable.tsx
@@ -5,9 +5,6 @@ import './PricingTable.css'
 
 const fontSizePrice = 36
 const noteColor = '#64748b'
-/*
-<div>{'=>'} Use Vike just like any other open source project</div>
-*/
 
 const styleTierDescription = { color: '#64748b', fontSize: '0.9em' } satisfies React.CSSProperties
 
@@ -155,16 +152,10 @@ function Check({ children }: { children: React.ReactNode }) {
   )
 }
 
-function GreenCheckmark({ size = 18 }) {
+function GreenCheckmark() {
+  const size = 18
   return (
-    <svg
-      width={size}
-      height={size}
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      style={{ flexShrink: 0 }}
-    >
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M20 6L9 17l-5-5" stroke="#10b981" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   )
