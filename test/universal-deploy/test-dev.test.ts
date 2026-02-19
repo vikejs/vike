70214ad6caf86bdda3295d4606b516384da64ad6
Joël Charles <joel.charles91@gmail.com>
2026-02-19 12:22:50 +0100

    test: fix UD test

--- test/universal-deploy/test-dev.test.ts
+++ test/universal-deploy/test-dev.test.ts
@@ -1,2 +1,8 @@
 import { testRunClassic } from '../../test/utils'
-testRunClassic('npm run dev', { skipAboutPage: true })
+testRunClassic('npm run dev', {
+  skipAboutPage: true,
+  tolerateExitCode: [143],
+  tolerateError({ logText, logSource }) {
+    return logText.includes('terminated with non-0 error code 143') && logSource === 'run() failure'
+  },
+})
--- test/universal-deploy/test-preview.test.ts
+++ test/universal-deploy/test-preview.test.ts
@@ -1,2 +1,2 @@
 import { testRunClassic } from '../../test/utils'
-testRunClassic('npm run preview', { skipAboutPage: true })
+testRunClassic('npm run preview', { skipAboutPage: true, serverIsReadyMessage: 'Listening on:' })
