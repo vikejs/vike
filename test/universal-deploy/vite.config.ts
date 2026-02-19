21c78c8c44fdc7e06d76ea731838b6e66255ab25
Joël Charles <joel.charles91@gmail.com>
2026-02-19 17:15:17 +0100

    chore: comment

--- test/universal-deploy/vite.config.ts
+++ test/universal-deploy/vite.config.ts
@@ -5,5 +5,13 @@ import vike from 'vike/plugin'
 import { node } from '@universal-deploy/node/vite'
 
 export default defineConfig({
-  plugins: [vike(), react(), telefunc(), node()],
+  plugins: [
+    vike(),
+    react(),
+    telefunc(),
+    // TODO currently vike-photon has some heuristics to determine which target to use.
+    //  Extract this logic to a new `@universal-deploy/auto` or directly into Vike
+    //  https://github.com/vikejs/vike-photon/blob/438bffdb9a82650a49ee5345a82d0cc779afc3c8/packages/vike-photon/src/plugin/plugins/targets.ts#L25
+    node(),
+  ],
 })
