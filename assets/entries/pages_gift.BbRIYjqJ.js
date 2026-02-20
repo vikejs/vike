e218b0f98299505d4f03601eb03e8d75fd9ce17e
brillout <github.com@brillout.com>
2026-02-20 13:52:38 +0000

    Deploying to gh-pages from @ vikejs/vike@3faa3d308366e583e479385fafd42a171d81dcc8 🚀

---
+++ assets/entries/pages_gift.BbRIYjqJ.js
@@ -0,0 +1,67 @@
+import{o as s,a as d}from"../chunks/chunk-C-YWxeMP.js";import{j as e}from"../chunks/chunk-Dp_tCe4P.js";import{L as c}from"../chunks/chunk-gdkYz9uR.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import{u as a}from"../chunks/chunk-CC450nSq.js";import{T as p,a as h,b as o,c as t}from"../chunks/chunk-CMGhYvDa.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */const r=({why:i,addendum:n})=>e.jsxs("p",{children:[e.jsx("code",{children:"TODO_1"}),": Your website URL (if nothing's public: the domain name where your app's client runs).",e.jsx("br",{}),e.jsx("code",{children:"TODO_2"}),": Explain why",i,".",n]}),u=[{pageSectionId:"who-s-eligible",pageSectionLevel:2,pageSectionTitle:"Who's eligible?"},{pageSectionId:"how-to-apply",pageSectionLevel:2,pageSectionTitle:"How to apply?"},{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function l(i){const n={blockquote:"blockquote",code:"code",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...a(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"The gift program ensures everyone can use Vike."}),`
+`,e.jsx(n.p,{children:"There are two programs:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Free forever"}),`
+`,e.jsx(n.li,{children:"Extended free trial"}),`
+`]}),`
+`,e.jsx("h2",{id:"who-s-eligible",children:"Who's eligible?"}),`
+`,e.jsx(n.p,{children:"Your team is eligible if it has limited resources and purchasing a license key would be too expensive."}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["A good rule of thumb is the number of employees. For example, we consider an organization eligible if it has ",e.jsx(n.strong,{children:"fewer than 10 paid employees"}),". The threshold is ",e.jsx(n.strong,{children:"higher in countries with lower revenue"}),"."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"If you expect more resources in the future, apply for an extended free trial."}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"Eligible examples:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Startup"}),`
+`,e.jsx(n.li,{children:"Open Source (without much revenue)"}),`
+`,e.jsx(n.li,{children:"Amateur sports club"}),`
+`]}),`
+`]}),`
+`,e.jsx(n.p,{children:"You can also apply for an extended free trial for other reasons (e.g. pending clearance from your company's finance department)."}),`
+`,e.jsx(n.p,{children:"We tend to be generous, so don't hesitate to apply — it's easy and quick."}),`
+`,e.jsx("h2",{id:"how-to-apply",children:"How to apply?"}),`
+`,e.jsx(n.p,{children:"Contact us to apply."}),`
+`,e.jsxs(p,{children:[e.jsxs(h,{children:[e.jsx(o,{children:"Free forever"}),e.jsx(o,{children:"Extended free trial"})]}),e.jsxs(t,{children:[e.jsx("span",{children:"Email (pre-filled subject & body): "}),e.jsx("a",{href:"mailto:vike-gift@brillout.com?subject="+encodeURIComponent("Free forever")+"&body="+encodeURIComponent(`== Website
+
+TODO_1
+
+== Reason
+
+TODO_2`),children:"vike-gift@brillout.com"}),e.jsx(n.p,{children:"Subject:"}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`Free forever
+`})}),e.jsx(n.p,{children:"Body:"}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`== Website
+
+TODO_1
+
+== Reason
+
+TODO_2
+`})}),e.jsx(r,{why:" your organization has limited resources"})]}),e.jsxs(t,{children:[e.jsx("span",{children:"Email (pre-filled subject & body): "}),e.jsx("a",{href:"mailto:vike-gift@brillout.com?subject="+encodeURIComponent("Extended free trial")+"&body="+encodeURIComponent(`== Website
+
+TODO_1
+
+== Reason
+
+TODO_2
+
+== Extend period
+
+Two years`),children:"vike-gift@brillout.com"}),e.jsx(n.p,{children:"Subject:"}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`Extended free trial
+`})}),e.jsx(n.p,{children:"Body:"}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`== Website
+
+TODO_1
+
+== Reason
+
+TODO_2
+
+== Extend period
+
+Two years
+`})}),e.jsx(r,{addendum:e.jsxs(e.Fragment,{children:[e.jsx("br",{}),e.jsx("code",{children:"Extend period"}),": can be modified. For example, you can set it to the duration after which you expect to easily afford a license key. You can apply again after the extended period ends."]})})]})]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(c,{href:"/pricing"}),`
+`]}),`
+`]})]})}function x(i={}){const{wrapper:n}={...a(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(l,{...i})}):l(i)}const j=Object.freeze(Object.defineProperty({__proto__:null,TODO_APPLY:r,default:x,pageSectionsExport:u},Symbol.toStringTag,{value:"Module"})),k={hasServerOnlyHook:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!1}},isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},guardEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:d}},onCreatePageContext:{type:"cumulative",definedAtData:[{filePathToShowToUser:"@brillout/docpress/renderer/onCreatePageContext",fileExportPathToShowToUser:[]}],valueSerialized:[{type:"pointer-import",value:s}]},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/gift/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:j}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}}};export{k as configValuesSerialized};
