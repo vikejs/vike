8eaaa87397d33ddfc8affa20dc0ed9e11cd3b469
Romuald Brillout <git@brillout.com>
2026-02-24 15:09:18 +0100

    minor refactor

--- packages/vike/src/utils/preventConstantFolding.ts
+++ packages/vike/src/utils/preventConstantFolding.ts
@@ -1,6 +1,6 @@
 /** Prevent compilers from constant folding `'a' + 'b'` into `'ab'`*/
 export function preventConstantFolding() {
-  const undefined_ = (globalThis as any).__vike_this_property_is_never_defined
-  if (undefined_) return 'this_value_is_never_used'
+  const undefined_ = (globalThis as any).__vike__this_property_is_never_defined
+  if (undefined_) return '__vike__this_string_is_never_used'
   return ''
 }
