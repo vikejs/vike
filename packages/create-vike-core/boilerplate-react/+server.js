4527204b6ba13510dfefdd9bd4fb2a2cc9c239f0
Joël Charles <joel.charles91@gmail.com>
2026-02-25 15:11:27 +0100

    wip

--- packages/create-vike-core/boilerplate-react/+server.js
+++ packages/create-vike-core/boilerplate-react/+server.js
@@ -5,6 +5,7 @@ import { apply } from '@universal-middleware/hono'
 const app = new Hono()
 apply(app, [vike])
 
+// FIXME does not seem to be loaded
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
 
 // https://vike.dev/server
