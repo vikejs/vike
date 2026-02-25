165333218aacf6caac69b90730926bc65080baea
Joël Charles <joel.charles91@gmail.com>
2026-02-25 15:45:19 +0100

    chore: migrate core boilerplates to UD

---
+++ packages/create-vike-core/boilerplate-vue/+server.js
@@ -0,0 +1,14 @@
+import { Hono } from 'hono'
+import vike from 'vike/universal-middleware'
+import { apply } from '@universal-middleware/hono'
+
+const app = new Hono()
+apply(app, [vike])
+
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
+
+// https://vike.dev/server
+export default {
+  fetch: app.fetch,
+  port,
+}
--- packages/create-vike-core/boilerplate-vue/renderer/+config.js
+++ packages/create-vike-core/boilerplate-vue/renderer/+config.js
@@ -1,13 +1,7 @@
 export { config as default }
 
-import vikeServer from 'vike-server/config'
-
 // https://vike.dev/config
 const config = {
-  // https://vike.dev/vike-server
-  extends: [vikeServer],
-  server: 'server/index.js',
-
   // https://vike.dev/clientRouting
   clientRouting: true,
   hydrationCanBeAborted: false,
