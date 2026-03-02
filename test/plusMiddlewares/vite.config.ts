2dcd771a141dc6a4e6875a1d7ff256d029914862
Joël Charles <joel.charles91@gmail.com>
2026-03-02 16:18:23 +0100

    chore

--- test/plusMiddlewares/vite.config.ts
+++ test/plusMiddlewares/vite.config.ts
@@ -1,9 +1,7 @@
 import react from '@vitejs/plugin-react'
 import vike from 'vike/plugin'
 import type { UserConfig } from 'vite'
-import { node } from '@universal-deploy/node/vite'
 
 export default {
-  // TODO remove node adapter
-  plugins: [react(), vike(), node()],
+  plugins: [react(), vike()],
 } satisfies UserConfig
