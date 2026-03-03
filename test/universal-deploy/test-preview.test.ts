5c8267d8536f6e29906043fee4e892deefa5e20d
Romuald Brillout <git@brillout.com>
2026-03-03 15:22:51 +0100

    update to-do

--- test/universal-deploy/test-preview.test.ts
+++ test/universal-deploy/test-preview.test.ts
@@ -1,6 +1,7 @@
 import { skip } from '@brillout/test-e2e'
 import { testRun } from './.testRun'
 
+// TODO/soon: re-enable test
 // Temporarily skipped on Windows, see https://github.com/vikejs/vike/pull/3106#issuecomment-3991210441
 if (process.platform === 'win32') skip('SKIPPED: temporarily skipped on Windows')
 else testRun('npm run preview', { skipAboutPage: true, serverIsReadyMessage: 'Listening on:' })
