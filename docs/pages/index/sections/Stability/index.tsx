c102229a02e3a93327405f3ef1951469bb149d91
Romuald Brillout <git@brillout.com>
2026-03-25 16:08:41 +0100

    text

--- docs/pages/index/sections/Stability/index.tsx
+++ docs/pages/index/sections/Stability/index.tsx
@@ -29,16 +29,15 @@ const StabilitySection = () => {
               </>
             }
             sub=<>
-              Vike is <b className="font-extrabold">decoupled</b> from unstable fast-moving JavaScript tools.
+              Vike is <b className="font-extrabold">decoupled</b> from unstable fast-evolving JavaScript tools.
             </>
             color="blue"
           />
           <LayoutComponent $size="sm" className="mb-8 md:w-3/4 lg:w-2/3 mx-auto">
             <p>
-              Vike's internal components are <b className="font-semibold">stable primitives</b> remaining relevant for
-              the foreseeable future.{' '}
-              <b className="font-normal">Unstable and/or fast-evolving tools aren't built into Vike by design</b> —
-              instead, Vike extensions provide first-class deep integrations.
+              Vike's internal components are <b className="font-semibold">stable primitives</b>, while{' '}
+              <b className="font-normal">unstable and/or fast-evolving tools aren't built into Vike</b> —
+              instead, Vike extensions provide zero-config deep integrations.
             </p>
           </LayoutComponent>
           <StableGraphic />
