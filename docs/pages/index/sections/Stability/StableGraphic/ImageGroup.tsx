ef49ccd4781d920b870903ffdf65d80938ef02b2
Romuald Brillout <git@brillout.com>
2026-03-25 12:48:50 +0100

    revert hooks visual

--- docs/pages/index/sections/Stability/StableGraphic/ImageGroup.tsx
+++ docs/pages/index/sections/Stability/StableGraphic/ImageGroup.tsx
@@ -1,29 +1,27 @@
 import React from 'react'
 
-const barIndexesDesktop = [0, 1, 2, 3, 4, 5, 6]
-const barIndexesMobile = [0, 1, 2, 3]
+import linesTop from '../../../assets/stability/lines-top.png'
+import linesTopMobile from '../../../assets/stability/lines-top@0.5.png'
 
-const ImageGroup = () => {
-  return (
-    <div className="pointer-events-none absolute inset-x-0 top-8 z-0 select-none md:top-10">
-      <div className="hidden justify-center gap-10 md:flex">
-        {barIndexesDesktop.map((barIndex) => (
-          <span
-            key={barIndex}
-            className="block h-16 w-4 rounded-md bg-linear-to-b from-base-300/0 via-base-300/38 to-base-300/0"
-          />
-        ))}
-      </div>
-      <div className="flex justify-center gap-6 md:hidden">
-        {barIndexesMobile.map((barIndex) => (
-          <span
-            key={barIndex}
-            className="block h-14 w-3 rounded-md bg-linear-to-b from-base-300/0 via-base-300/34 to-base-300/0"
-          />
-        ))}
-      </div>
-    </div>
-  )
-}
+const ImageGroup = () => (
+  <div className="absolute inset-0 pointer-events-none select-none">
+    <img
+      suppressHydrationWarning
+      loading="lazy"
+      src={linesTop}
+      data-speed="0.96"
+      alt=""
+      className="ml-[2.5%] w-[95%] object-fill absolute -top-30 h-30 left-0 md:block hidden"
+    />
+    <img
+      suppressHydrationWarning
+      loading="lazy"
+      src={linesTopMobile}
+      data-speed="0.96"
+      alt=""
+      className="ml-[5%] w-[90%] object-fill absolute -top-20 h-20 left-0 md:hidden block"
+    />
+  </div>
+)
 
 export default ImageGroup
