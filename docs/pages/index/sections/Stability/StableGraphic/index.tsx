e176c9e5e6cb8160449e59dba59f0210b2c2324f
Romuald Brillout <git@brillout.com>
2026-03-25 11:07:47 +0100

    add TODO/ai

--- docs/pages/index/sections/Stability/StableGraphic/index.tsx
+++ docs/pages/index/sections/Stability/StableGraphic/index.tsx
@@ -4,6 +4,8 @@ import VikeComponents from './VikeComponents'
 import EcoComponents from './EcoComponents'
 import StableGraphicLegend from './Legend'
 
+// TODO/ai refactor this graphic: move JS ecosystem legend to the top, keep Vike interneal components to the bottom and separate top from botton with a line with a label "CLEAN DECOUPLING"
+
 const Outer = cm.div`relative mx-auto`
 
 const StableGraphic = () => {
