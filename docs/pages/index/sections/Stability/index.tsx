9a1f11ec80bdf488e162471291f6878445b15ab7
Romuald Brillout <git@brillout.com>
2026-03-25 16:03:19 +0100

    polish text

--- docs/pages/index/sections/Stability/index.tsx
+++ docs/pages/index/sections/Stability/index.tsx
@@ -35,10 +35,10 @@ const StabilitySection = () => {
           />
           <LayoutComponent $size="sm" className="mb-8 md:w-3/4 lg:w-2/3 mx-auto">
             <p>
-              Vike's internal components are <b className="font-semibold">stable primitives</b> that remain relevant for
+              Vike's internal components are <b className="font-semibold">stable primitives</b> remaining relevant for
               the foreseeable future.{' '}
               <b className="font-normal">Unstable and/or fast-evolving tools aren't built into Vike by design</b> —
-              instead, Vike extensions provide first-class deep integrations with cutting-edge tools.
+              instead, Vike extensions provide first-class deep integrations.
             </p>
           </LayoutComponent>
           <StableGraphic />
