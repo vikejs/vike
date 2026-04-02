180efd10513e842e832ff7f6f81d00ce2d10950d
Romuald Brillout <git@brillout.com>
2026-04-02 15:08:50 +0200

    minor refactor

--- packages/vike/src/types/Server.ts
+++ packages/vike/src/types/Server.ts
@@ -1,9 +1,11 @@
+export type { Server }
+
 import type { Fetchable, ServerOptions } from '@universal-deploy/store'
 
 /** Server options.
  *
  * https://vike.dev/server
  */
-export interface Server extends Fetchable {
+interface Server extends Fetchable {
   prod?: Omit<ServerOptions, 'fetch'>
 }
