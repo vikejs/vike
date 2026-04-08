3d53bdc3e3ffcce58ef29ae300e60cbb2e74c161
Joel Charles <joel.charles91@gmail.com>
2026-04-08 15:03:09 +0200

    test: @cloudflare/vite-plugin test with UD

---
+++ test/@cloudflare_vite-plugin_ud/pages/index/+Page.tsx
@@ -0,0 +1,24 @@
+export default Page
+
+import React from 'react'
+import { Counter } from './Counter'
+import { usePageContext } from 'vike-react/usePageContext'
+
+function Page() {
+  const pageContext = usePageContext()
+  return (
+    <>
+      <h1>Welcome</h1>
+      This page is:
+      <ul>
+        <li>Rendered to HTML.</li>
+        <li>
+          Interactive. <Counter />
+        </li>
+      </ul>
+      <p>
+        <code>pageContext.globalContext.someEnvVar === {JSON.stringify(pageContext.globalContext.someEnvVar)}</code>
+      </p>
+    </>
+  )
+}
---
+++ test/@cloudflare_vite-plugin_ud/testRun.ts
@@ -0,0 +1,64 @@
+export { testRun }
+export { testCloudflareBindings }
+
+import { autoRetry, expect, getServerUrl, page, sleep, test } from '@brillout/test-e2e'
+import { testCounter, testRunClassic } from '../../test/utils'
+
+function testRun(cmd: 'pnpm run dev' | 'pnpm run preview') {
+  const isDev = cmd === 'pnpm run dev'
+  testCloudflareBindings()
+  testRunClassic(cmd)
+  testTodolist(isDev)
+}
+
+function testCloudflareBindings() {
+  test('Cloudflare Bindings', async () => {
+    await page.goto(getServerUrl() + '/')
+    await testCounter()
+    const bodyText = await page.textContent('body')
+    expect(bodyText).toContain('someEnvVar')
+    expect(bodyText).toContain('some-value')
+    expect(bodyText).toContain(`pageContext.globalContext.someEnvVar === ${JSON.stringify('some-value')}`)
+  })
+}
+
+function testTodolist(isDev: boolean) {
+  test('To-Do list', async () => {
+    await page.goto(getServerUrl() + '/todo')
+    if (isDev) await sleep(1000) // Seems to be required, otherwise the test is flaky. I don't know why.
+    await page.locator('button', { hasText: 'Reset' }).click()
+    await autoRetry(
+      async () => {
+        expect(await getNumberOfItems()).toBe(3)
+      },
+      { timeout: 5000 },
+    )
+    {
+      const reactText = await page.textContent('#root')
+      expect(reactText).toContain('Buy milk')
+      expect(reactText).not.toContain('Buy bananas')
+    }
+    const todoItemNew = `Buy bananas ${Math.random()}`
+    await page.fill('input[type="text"]', todoItemNew)
+    await page.click('button[type="submit"]')
+    await autoRetry(
+      async () => {
+        expect(await getNumberOfItems()).toBe(4)
+      },
+      { timeout: 5000 },
+    )
+    expect(await page.textContent('#root')).toContain(todoItemNew)
+    await fullPageReload()
+    expect(await getNumberOfItems()).toBe(4)
+    expect(await page.textContent('#root')).toContain(todoItemNew)
+  })
+}
+async function getNumberOfItems() {
+  return await page.evaluate(() => document.querySelectorAll('li').length)
+}
+
+async function fullPageReload() {
+  await page.goto(getServerUrl() + '/')
+  await testCounter()
+  await page.goto(getServerUrl() + '/todo')
+}
