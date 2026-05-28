53da9e433f5d47989735be1faf27eee06b252f41
Rom <git@brillout.com>
2026-05-28 16:06:26 +0200

    fix: make unkown config a warning instead of an error (#3303)

--- packages/vike/src/node/vite/shared/resolveVikeConfigInternal.ts
+++ packages/vike/src/node/vite/shared/resolveVikeConfigInternal.ts
@@ -424,7 +424,7 @@ async function resolveConfigDefinitions(
     configNamesKnownGlobal,
   }
 
-  assertKnownConfigs(configDefinitionsResolved)
+  warnUnknownConfigs(configDefinitionsResolved)
 
   return configDefinitionsResolved
 }
@@ -686,37 +686,36 @@ function setCliAndApiOptions(
   // Vike API — passed options [lowest precedence]
   const vikeApiOperation = getVikeApiOperation()
   if (vikeApiOperation?.options.vikeConfig) {
-    addSources(
-      vikeApiOperation.options.vikeConfig as Record<string, unknown>,
-      { definedBy: 'api', operation: vikeApiOperation.operation },
-      false,
-    )
+    addSources(vikeApiOperation.options.vikeConfig as Record<string, unknown>, {
+      definedBy: 'api',
+      operation: vikeApiOperation.operation,
+    })
   }
 
   const { configFromCliOptions, configFromEnvVar } = getVikeConfigFromCliOrEnv()
   // Vike CLI options
   if (configFromCliOptions) {
-    addSources(configFromCliOptions, { definedBy: 'cli' }, true)
+    addSources(configFromCliOptions, { definedBy: 'cli' })
   }
   // VIKE_CONFIG [highest precedence]
   if (configFromEnvVar) {
-    addSources(configFromEnvVar, { definedBy: 'env' }, false)
+    addSources(configFromEnvVar, { definedBy: 'env' })
   }
 
   return
 
-  function addSources(configValues: Record<string, unknown>, definedBy: DefinedBy, exitOnError: boolean) {
+  function addSources(configValues: Record<string, unknown>, definedBy: DefinedBy) {
     Object.entries(configValues).forEach(([configName, value]) => {
       const sourceName = `The ${getDefinedByString(definedBy, configName)}` as const
-      assertKnownConfig(
+      const isUnknown = isUnknownConfig(
         configName,
         configDefinitionsResolved.configNamesKnownGlobal,
         configDefinitionsResolved,
         '/' as LocationId,
         false,
         sourceName,
-        exitOnError,
       )
+      if (isUnknown) return
       const sources = (pageConfigGlobal.configValueSources[configName] ??= [])
       sources.unshift(getSourceNonConfigFile(configName, value, definedBy, pageConfigGlobal.configDefinitions))
     })
@@ -1321,8 +1320,7 @@ function getComputed(pageConfig: PageConfigBuildTimeBeforeComputed) {
   return configValuesComputed
 }
 
-// Show error message upon unknown config
-function assertKnownConfigs(configDefinitionsResolved: ConfigDefinitionsResolved) {
+function warnUnknownConfigs(configDefinitionsResolved: ConfigDefinitionsResolved) {
   objectEntries(configDefinitionsResolved.configDefinitionsLocal).forEach(
     ([_locationId, { configNamesKnownLocal, plusFiles }]) => {
       plusFiles.forEach((plusFile) => {
@@ -1330,44 +1328,38 @@ function assertKnownConfigs(configDefinitionsResolved: ConfigDefinitionsResolved
         configNames.forEach((configName) => {
           const { locationId } = plusFile
           const sourceName = plusFile.filePath.filePathToShowToUser
-          assertKnownConfig(
-            configName,
-            configNamesKnownLocal,
-            configDefinitionsResolved,
-            locationId,
-            true,
-            sourceName,
-            false,
-          )
+          isUnknownConfig(configName, configNamesKnownLocal, configDefinitionsResolved, locationId, true, sourceName)
         })
       })
     },
   )
 }
-function assertKnownConfig(
+function isUnknownConfig(
   configName: string,
   configNamesKnownRelevant: string[],
   configDefinitionsResolved: ConfigDefinitionsResolved,
   locationId: LocationId,
   isPlusFile: boolean,
   sourceName: string,
-  exitOnError: boolean,
-): void {
+): boolean {
   const { configNamesKnownAll } = configDefinitionsResolved
 
   if (configNamesKnownRelevant.includes(configName)) {
     assert(configNamesKnownAll.includes(configName))
-    return
+    return false
   }
 
   const configNameColored = pc.cyan(configName)
 
+  const warn = (msg: string) => {
+    assertWarning(false, msg, { onlyOnce: true })
+    return true as const
+  }
+
   // Inheritance issue: config is known but isn't defined at `locationId`
   if (configNamesKnownAll.includes(configName)) {
-    assertUsage(
-      false,
+    return warn(
       `${sourceName} sets the value of the config ${configNameColored} which is a custom config that is defined with ${pc.underline('https://vike.dev/meta')} at a path that doesn't apply to ${locationId} — see ${pc.underline('https://vike.dev/config#inheritance')}` as const,
-      { exitOnError },
     )
   }
 
@@ -1397,7 +1389,7 @@ function assertKnownConfig(
       )
       const errMsgEnhanced =
         `${errMsg}. If you want to use the configuration ${configNameColored} documented at ${pc.underline(`https://vike.dev/${configName}`)} then make sure to install ${requiredVikeExtension}. (Alternatively, you can define ${configNameColored} yourself by using ${pc.cyan('meta')}, see ${pc.underline('https://vike.dev/meta')} for more information.)` as const
-      assertUsage(false, errMsgEnhanced, { exitOnError })
+      return warn(errMsgEnhanced)
     }
   }
 
@@ -1416,10 +1408,10 @@ function assertKnownConfig(
         'P',
       )} because it defines a UI component: a ubiquitous JavaScript convention is that the name of UI components start with a capital letter.)` as const
     }
-    assertUsage(false, errMsgEnhanced, { exitOnError })
+    return warn(errMsgEnhanced)
   }
 
-  assertUsage(false, errMsg, { exitOnError })
+  return warn(errMsg)
 }
 
 function determineRouteFilesystem(locationId: LocationId, configValueSources: ConfigValueSources): PageConfigRoute {
