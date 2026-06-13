1405ebc19f12fe553f01d827c56af6f55aa76a00
Romuald Brillout <git@brillout.com>
2026-06-13 21:49:38 +0200

    comment

--- test/universal-deploy/global.d.ts
+++ test/universal-deploy/global.d.ts
@@ -1,7 +1,6 @@
 declare global {
   namespace Vike {
     interface Server {
-      // The server used by +server.ts (via @vikejs/express). It gives pageContext.runtime — and its pageContext.req/pageContext.res aliases — their types.
       server: 'express'
     }
   }
