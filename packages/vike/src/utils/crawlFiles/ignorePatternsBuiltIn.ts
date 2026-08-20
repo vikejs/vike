834f873234387dffa57edbcb89303601b390bf20
Rom <git@brillout.com>
2026-08-20 12:28:13 +0200

    refactor: move ignorePatternsBuiltIn to a standalone file (#3479)

---
+++ packages/vike/src/utils/crawlFiles/ignorePatternsBuiltIn.ts
@@ -0,0 +1,18 @@
+export const ignorePatternsBuiltIn = [
+  '**/node_modules/**',
+  // Ejected Vike extensions, see https://github.com/snake-py/eject
+  '**/ejected/**',
+  // Allow:
+  // ```bash
+  // +Page.js
+  // +Page.telefunc.js
+  // ```
+  '**/*.telefunc.*',
+  // https://github.com/vikejs/vike/issues/1589#issuecomment-2031925598
+  '**/.history/**',
+  // https://github.com/vikejs/vike/discussions/2222
+  '**/*.generated.*',
+  // https://github.com/vikejs/vike/issues/2347
+  '**/*.spec.*',
+  '**/*.test.*',
+] as const
