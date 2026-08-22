35e48ea36e839cd3780399ed9826e6b9a3f5e713
brillout <github.com@brillout.com>
2026-08-22 13:12:21 +0000

    Deploying to gh-pages from @ vikejs/vike@d6f5dc81610f60512e86353a4d58162ab72a60d3 🚀

---
+++ assets/entries/pages_grommet.D7yTNT8T.js
@@ -0,0 +1,26 @@
+import{o as l,a as s}from"../chunks/chunk-BTTKAGv3.js";import{j as e}from"../chunks/chunk-BLMxdf0g.js";import{L as n}from"../chunks/chunk-B9EpAeGO.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import{a as i}from"../chunks/chunk-DjRTiJNY.js";import{C as a}from"../chunks/chunk-G2cQWiI-.js";import{E as d}from"../chunks/chunk-Dc-Nrj_1.js";/* empty css                      */import"../chunks/chunk-D3NBTglP.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-CuPF6Fdu.js";const p=[{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function r(o){const t={a:"a",li:"li",p:"p",ul:"ul",...i(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{url:"https://grommet.io"}),`
+`,e.jsxs(t.p,{children:["When using Grommet, which is based on ",e.jsx(t.a,{href:"https://styled-components.com",children:"styled-components"}),", you need to collect the page's styles upon SSR:"]}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx("a",{href:"https://styled-components.com/docs/advanced#server-side-rendering",children:"styled-components docs > Server Side Rendering"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/css-in-js",doNotInferSectionTitle:!0}),`
+`]}),`
+`]}),`
+`,e.jsxs(t.p,{children:["See also: ",e.jsx(n,{href:"/styled-components"}),"."]}),`
+`,e.jsx(t.p,{children:"Example:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(d,{timestamp:"2024.04",repo:"AurelienLourot/vike-react-styled-components-grommet"}),`
+`]}),`
+`]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/css-in-js",doNotInferSectionTitle:!0}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/styled-components"}),`
+`]}),`
+`]})]})}function m(o={}){const{wrapper:t}={...i(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(r,{...o})}):r(o)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:m,pageSectionsExport:p},Symbol.toStringTag,{value:"Module"})),_={hasServerOnlyHook:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!1}},isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},guardEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:s}},onCreatePageContext:{type:"cumulative",definedAtData:[{filePathToShowToUser:"@brillout/docpress/renderer/onCreatePageContext",fileExportPathToShowToUser:[]}],valueSerialized:[{type:"pointer-import",value:l}]},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/grommet/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:u}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}}};export{_ as configValuesSerialized};
