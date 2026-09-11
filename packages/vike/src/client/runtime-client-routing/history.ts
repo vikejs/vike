558026393f379d82a5ae37cdaae417484c744fc7
Romuald Brillout <git@brillout.com>
2026-09-11 11:13:37 +0200

    small refactor

--- packages/vike/src/client/runtime-client-routing/history.ts
+++ packages/vike/src/client/runtime-client-routing/history.ts
@@ -95,6 +95,7 @@ function historyApiPushState(state: StateEnhanced, url: string) {
   // Calling the monkey patched history.pushState() (not the original) so that other tools (e.g. user tracking) can listen to Vike's pushState() calls.
   // - https://github.com/vikejs/vike/issues/1582
   window.history.pushState(state, '', url)
+  assertIsEnhanced(window.history.state as unknown)
 }
 function historyApiReplaceState(state: StateEnhanced, url?: string) {
   const url_ = url ?? null // Passing `undefined` chokes older Edge versions.
