5c2865871701f63ccdac1730f8ba737a551a0b2e
brillout <github.com@brillout.com>
2026-09-25 20:07:13 +0000

    Deploying to gh-pages from @ vikejs/vike@11362b176dd2b8de7596feabc2bbaeda71e34e37 🚀

---
+++ assets/entries/pages_settings.BMjha2_1.js
@@ -0,0 +1,84 @@
+import{o as d,a as l}from"../chunks/chunk-Bp1SB7TL.js";import{j as e}from"../chunks/chunk-CSHFdzS5.js";import{L as n}from"../chunks/chunk-BWx23KEi.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import{a as t}from"../chunks/chunk-CZFEZMGk.js";/* empty css                      *//* empty css                      *//* empty css                      */const o=[{pageSectionId:"basics",pageSectionLevel:2,pageSectionTitle:"Basics"},{pageSectionId:"html-shell",pageSectionLevel:2,pageSectionTitle:"HTML shell"},{pageSectionId:"vite",pageSectionLevel:2,pageSectionTitle:"Vite"},{pageSectionId:"advanced",pageSectionLevel:2,pageSectionTitle:"Advanced"},{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function i(r){const s={a:"a",blockquote:"blockquote",code:"code",li:"li",p:"p",strong:"strong",ul:"ul",...t(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsxs(s.p,{children:["List of built-in settings. (You can ",e.jsx(n,{href:"/meta",children:"create your own settings"}),".)"]}),`
+`,e.jsxs(s.p,{children:["See also: ",e.jsx(n,{href:"/hooks"}),"."]}),`
+`,e.jsx("h2",{id:"basics",children:"Basics"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/Page",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"Page"})})}),": The page's root component."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/Layout",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"Layout"})})}),": The page's layout component."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/Wrapper",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"Wrapper"})})}),": Component(s) that wrap the page's root component."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/routing",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"route"})})}),": The page's route."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/prerender",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"prerender"})})}),": Whether to ",e.jsx(n,{href:"/pre-rendering",children:"pre-render"})," the page, and pre-rendering settings."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/ssr",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"ssr"})})}),": Enable/disable Server-Side Rendering (SSR)."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/stream",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"stream"})})}),": Enable/disable HTML Streaming and specify stream type."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/server",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"server"})})}),": Server integration settings."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/client",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"client"})})}),": Add client code."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/redirects",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"redirects"})})}),": Permanent redirections (HTTP status code ",e.jsx(s.code,{children:"301"}),")."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/precompress",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"precompress"})})}),": Create ",e.jsx(s.code,{children:".br"}),"/",e.jsx(s.code,{children:".gz"})," files of static assets at build time."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/keepScrollPosition",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"keepScrollPosition"})})}),": Whether the page scrolls to the top upon navigation."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/prefetchStaticAssets",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"prefetchStaticAssets"})})}),": Link prefetching settings."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/base-url",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"baseAssets"})})}),": Base URL of server."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/base-url",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"baseServer"})})}),": Base URL of static assets."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/headersResponse",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"headersResponse"})})}),": Add HTTP headers to the HTTP response."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/csp",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"csp"})})}),": Content Security Policy (CSP)."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/extends",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"extends"})})}),": Install ",e.jsx(n,{href:"/extensions"}),"."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/react-setting",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"react"})})}),": Options passed to React functions such as ",e.jsx(s.code,{children:"createRoot()"})," or ",e.jsx(s.code,{children:"hydrateRoot()"}),"."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/vue-setting",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"vue"})})}),": Vue integration options, such as wrapping pages with Vue's ",e.jsx(s.code,{children:"<KeepAlive>"}),"."]}),`
+`]}),`
+`,e.jsx("h2",{id:"html-shell",children:"HTML shell"}),`
+`,e.jsxs(s.p,{children:["Settings that set ",e.jsx(s.code,{children:"<head>"})," tags (aka document metadata):"]}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/title",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"title"})})}),": Set the page's title."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/description",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"description"})})}),": Set the page's description."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/image",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"image"})})}),": Set the page's preview image upon URL sharing."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/viewport",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"viewport"})})}),": Set the page's viewport size on mobile devices."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/Head",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"Head"})})}),": Add arbitrary ",e.jsx(s.code,{children:"<head>"})," tags."]}),`
+`]}),`
+`,e.jsxs(s.p,{children:["See also: ",e.jsx(n,{href:"/head-tags"}),"."]}),`
+`,e.jsxs(s.blockquote,{children:[`
+`,e.jsxs(s.p,{children:["You can also create your own document metadata settings, see ",e.jsx(n,{href:"/head-tags#custom-settings"}),"."]}),`
+`]}),`
+`,e.jsx(s.p,{children:"Other settings that modify the page HTML shell:"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/lang",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"lang"})})}),": Set the page's language (",e.jsx(s.code,{children:"<html lang>"}),")."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/htmlAttributes",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"htmlAttributes"})})}),": Add ",e.jsx(s.code,{children:"<html>"})," attributes (e.g. ",e.jsx(s.code,{children:'<html class="dark">'}),")."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/bodyAttributes",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"bodyAttributes"})})}),": Add ",e.jsx(s.code,{children:"<body>"})," attributes (e.g. ",e.jsx(s.code,{children:'<body class="dark">'}),")."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/rootAttributes",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"rootAttributes"})})}),": Add root element attributes (e.g. ",e.jsx(s.code,{children:'<div id="root" role="none">'}),")."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/headHtmlBegin",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"headHtmlBegin"})})}),": Insert HTML at the beginning of ",e.jsx(s.code,{children:"<head>"}),"."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/headHtmlEnd",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"headHtmlEnd"})})}),": Insert HTML at the end of ",e.jsx(s.code,{children:"<head>"}),"."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/bodyHtmlBegin",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"bodyHtmlBegin"})})}),": Insert HTML at the beginning of ",e.jsx(s.code,{children:"<body>"}),"."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/bodyHtmlEnd",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"bodyHtmlEnd"})})}),": Insert HTML at the end of ",e.jsx(s.code,{children:"<body>"}),"."]}),`
+`]}),`
+`,e.jsx("h2",{id:"vite",children:"Vite"}),`
+`,e.jsxs(s.p,{children:["Settings that alias ",e.jsx(s.a,{href:"https://vite.dev",children:"Vite"})," settings (see also ",e.jsx(n,{href:"/cli"}),"):"]}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/host",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"host"})})}),": Make development/preview server available over LAN and public addresses."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/port",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"port"})})}),": Change port of development/preview server."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/root",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"root"})})}),": Set the ",e.jsx(s.a,{href:"https://vite.dev/config/shared-options.html#root",children:"project root directory"}),"."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/mode",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"mode"})})}),": Set the ",e.jsx(s.a,{href:"https://vite.dev/guide/env-and-mode.html#modes",children:"mode"})," to run in."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/force",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"force"})})}),": Disable ",e.jsx(s.a,{href:"https://vitejs.dev",children:"Vite"}),"'s cache."]}),`
+`]}),`
+`,e.jsx("h2",{id:"advanced",children:"Advanced"}),`
+`,e.jsxs(s.blockquote,{children:[`
+`,e.jsx(s.p,{children:"Most users don't need to know about these settings."}),`
+`]}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/meta",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"meta"})})}),": Create new hooks or settings, or modify existing ones."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/passToClient",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"passToClient"})})}),": Determines what ",e.jsx(s.code,{children:"pageContext"})," values are sent to the client-side."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/clientRouting",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"clientRouting"})})}),": Enable ",e.jsx(n,{href:"/client-routing",children:"Client Routing"}),"."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/hydrationCanBeAborted",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"hydrationCanBeAborted"})})}),": Whether your ",e.jsx(n,{href:"/ui-frameworks",children:"UI framework"})," allows the ",e.jsx(n,{href:"/hydration",children:"hydration"})," to be aborted."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/url-normalization",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"trailingSlash"})})}),": Whether URLs should end with a trailing slash."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/url-normalization",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"disableUrlNormalization"})})}),": Disable automatic URL normalization."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/reactStrictMode",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"reactStrictMode"})})}),": Whether to use React's ",e.jsx(s.code,{children:"<StrictMode>"}),"."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/clientHooks",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"clientHooks"})})}),": Whether hooks are loaded on the client-side."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/filesystemRoutingRoot",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"filesystemRoutingRoot"})})}),": URL root for ",e.jsx(n,{href:"/filesystem-routing",children:"Filesystem Routing"}),"."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/injectScriptsAt",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"injectScriptsAt"})})}),": Where scripts are injected in the HTML."]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.a,{href:"/ai",children:e.jsx(s.strong,{children:e.jsx(s.code,{children:"ai"})})}),": Whether Vike checks your ",e.jsx(n,{href:"/ai#skill",children:e.jsx(s.code,{children:"vike/SKILL.md"})})," is installed and up-to-date."]}),`
+`]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:[`
+`,e.jsx(n,{href:"/hooks"}),`
+`]}),`
+`,e.jsxs(s.li,{children:[`
+`,e.jsx(n,{href:"/config"}),`
+`]}),`
+`]})]})}function c(r={}){const{wrapper:s}={...t(),...r.components};return s?e.jsx(s,{...r,children:e.jsx(i,{...r})}):i(r)}const h=Object.freeze(Object.defineProperty({__proto__:null,default:c,pageSectionsExport:o},Symbol.toStringTag,{value:"Module"})),R={hasServerOnlyHook:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!1}},isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},guardEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:l}},onCreatePageContext:{type:"cumulative",definedAtData:[{filePathToShowToUser:"@brillout/docpress/renderer/onCreatePageContext",fileExportPathToShowToUser:[]}],valueSerialized:[{type:"pointer-import",value:d}]},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/settings/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:h}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}}};export{R as configValuesSerialized};
